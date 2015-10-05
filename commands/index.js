aa382c0608c405a73fd5d5fc684e0d98050a2192
Michael Bleigh <bleigh@google.com>
2015-10-05 16:26:00 -0700

    Doing some work on some things.

--- commands/index.js
+++ commands/index.js
@@ -7,9 +7,6 @@ module.exports = function(client) {
     return cmd.runner();
   };
 
-  // client.collab = loadCommand('collab');
-  // client.collab.invite = loadCommand('collab-invite');
-  // client.collab.remove = loadCommand('collab-remove');
   client.data = {
     get: loadCommand('data-get'),
     push: loadCommand('data-push'),
@@ -17,12 +14,12 @@ module.exports = function(client) {
     remove: loadCommand('data-remove'),
     update: loadCommand('data-update')
   };
-  // client.deploy = loadCommand('deploy');
-  // client.deploy.hosting = loadCommand('deploy-hosting');
-  // client.deploy.rules = loadCommand('deploy-rules');
-  // client.disable = {
-  //   hosting: loadCommand('disable-hosting')
-  // };
+  client.deploy = loadCommand('deploy');
+  client.deploy.hosting = loadCommand('deploy-hosting');
+  client.deploy.rules = loadCommand('deploy-rules');
+  client.disable = {
+    hosting: loadCommand('disable-hosting')
+  };
   client.help = loadCommand('help');
   client.init = loadCommand('init');
   client.list = loadCommand('list');
