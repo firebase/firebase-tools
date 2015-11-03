7dc7ce0b0f1de9217fd344d5fa5e4bd6fb6bab21
Tamal Saha <tamal@google.com>
2015-11-03 12:38:58 -0800

    Add `firebase deploy:functions`

---
+++ commands/deploy-functions.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var Command = require('../lib/command');
+var requireAccess = require('../lib/requireAccess');
+var requireConfig = require('../lib/requireConfig');
+var acquireRefs = require('../lib/acquireRefs');
+var deploy = require('../lib/deploy');
+
+module.exports = new Command('deploy:functions')
+  .description('deploy functions of the current app')
+  // .option('-P, --project <project_id>', 'override the project ID specified in firebase.json')
+  .before(requireAccess)
+  .before(requireConfig)
+  .before(acquireRefs)
+  .action(function(options) {
+    return deploy(['functions'], options);
+  });
