7b6753c6ceb81936b149be8866991576e059c235
Chris Raynor <raynor@google.com>
2015-11-10 16:53:48 -0800

    Fixing up config requires and removing a few unused files

--- commands/deploy-rules.js
+++ commands/deploy-rules.js
@@ -5,7 +5,6 @@ var requireAccess = require('../lib/requireAccess');
 var requireConfig = require('../lib/requireConfig');
 var acquireRefs = require('../lib/acquireRefs');
 var deploy = require('../lib/deploy');
-var Config = require('../lib/config');
 var RSVP = require('rsvp');
 var utils = require('../lib/utils');
 
@@ -17,7 +16,7 @@ module.exports = new Command('deploy:rules')
   .before(requireAccess)
   .before(acquireRefs)
   .action(function(options) {
-    var config = Config.load(options);
+    var config = options.config;
     if (!config.has('rules')) {
       utils.logSuccess('Nothing to deploy (no "rules" specified in firebase.json)');
       return RSVP.resolve();
