a6da8c66f58305ca2e747a8a484121ece45b7ecc
Tamal Saha <TamalSaha@users.noreply.github.com>
2015-11-12 13:06:57 -0800

    Update functions-log.js

--- commands/functions-log.js
+++ commands/functions-log.js
@@ -16,7 +16,6 @@ module.exports = new Command('functions:log [function_name]')
   .description('read logs from GCF Kubernetes cluster')
   .option('-P, --project <project_id>', 'override the project ID specified in firebase.json')
   .option('-f, --follow', 'tail logs from GCF Kubernetes cluster')
-  // .option('-F, --function <function_name>', 'name of function for which to display logs')
   .before(requireConfig)
   .before(requireAccess)
   .action(function(function_name, options) {
