7e9bd7afabab3588332a64ed04d488996f4be58d
Tamal Saha <tamal@google.com>
2015-11-16 13:18:25 -0800

    Address bleigh's comments.

--- commands/functions-log.js
+++ commands/functions-log.js
@@ -1,32 +1,33 @@
 'use strict';
 
-require('shelljs/global');
 var _ = require('lodash');
 var acquireRefs = require('../lib/acquireRefs');
 var chalk = require('chalk');
 var Command = require('../lib/command');
+var FirebaseError = require('../lib/error');
 var getProjectId = require('../lib/getProjectId');
 var logger = require('../lib/logger');
 var requireAccess = require('../lib/requireAccess');
 var requireConfig = require('../lib/requireConfig');
 var RSVP = require('rsvp');
+var sh = require('shelljs');
 var utils = require('../lib/utils');
 
-module.exports = new Command('functions:log [function_name]')
+module.exports = new Command('functions:log [name]')
   .description('read logs from GCF Kubernetes cluster')
   .option('-P, --project <project_id>', 'override the project ID specified in firebase.json')
   .option('-f, --follow', 'tail logs from GCF Kubernetes cluster')
   .before(requireConfig)
   .before(requireAccess)
-  .action(function(function_name, options) {
-    if (!which('gcloud')) {
-      logger.info('Sorry, this command requires gcloud');
-      exit(1);
+  .action(function(name, options) {
+    if (!sh.which('gcloud')) {
+      return RSVP.reject(new FirebaseError(
+        'Must install gcloud (https://cloud.google.com/sdk/gcloud/) to use functions:log', {exit: 1}));
     }
 
-    if (!which('kubectl')) {
-      logger.info('Sorry, this command requires kubectl');
-      exit(1);
+    if (!sh.which('kubectl')) {
+      return RSVP.reject(new FirebaseError(
+        'Must have kubectl for functions:log. Please check that you have latest version of gcloud', {exit: 1}));
     }
 
     var projectId = getProjectId(options);
@@ -37,15 +38,15 @@ module.exports = new Command('functions:log [function_name]')
 
     var cmd = 'gcloud config set project ' + projectId;
     utils.logBullet(cmd);
-    exec(cmd, {silent:true});
+    sh.exec(cmd, {silent:true});
 
     cmd = 'gcloud config set container/cluster gcf-cluster-us-central1-f';
     utils.logBullet(cmd);
-    exec(cmd, {silent:true});
+    sh.exec(cmd, {silent:true});
 
     cmd = 'gcloud container clusters get-credentials gcf-cluster-us-central1-f';
     utils.logBullet(cmd);
-    var out = exec(cmd, {silent:true}).output;
+    var out = sh.exec(cmd, {silent:true}).output;
     if (out.toLowerCase().indexOf('error: ') > -1) {
       echo(out);
       exit(1);
@@ -53,7 +54,7 @@ module.exports = new Command('functions:log [function_name]')
 
     cmd = 'kubectl get pods -o json';
     utils.logBullet(cmd);
-    var out = exec(cmd, {silent:true}).output;
+    var out = sh.exec(cmd, {silent:true}).output;
     var pods = _.chain(JSON.parse(out).items)
       .pluck('metadata.name')
       .indexBy(function(name) {
@@ -62,35 +63,35 @@ module.exports = new Command('functions:log [function_name]')
       .value();
 
     cmd = 'kubectl logs';
-    if (_.has(options, 'follow')) {
+    if (options.follow) {
       cmd += ' -f';
     }
     if (_.isEmpty(pods)) {
-      logger.info('Sorry, no running pod found.');
-      exit(1);
-    } else if (function_name) {
-      if (pods[function_name]) {
-        cmd += ' ' + pods[function_name] + ' worker';
+      return RSVP.reject(new FirebaseError(
+        'Sorry, no running pod found.', {exit: 1}));
+    } else if (name) {
+      if (pods[name]) {
+        cmd += ' ' + pods[name] + ' worker';
       } else {
-        logger.warn('Sorry, no running pod found for given function name ' + chalk.bold(function_name));
-        exit(1);
+        return RSVP.reject(new FirebaseError(
+          'Sorry, no running pod found for given function name ' + chalk.bold(name), {exit: 1}));
       }
     } else {
       if (_.size(pods) == 1) {
         cmd += ' ' + _.values(pods)[0] + ' worker';
       } else {
-        logger.info('Please select one of the available functions: ' + chalk.bold(_.keys(pods).join(', ')));
-        exit(1);
+        return RSVP.reject(new FirebaseError(
+          'Please select one of the available functions: ' + chalk.bold(_.keys(pods).join(', ')), {exit: 1}));
       }
     }
     utils.logSuccess(cmd);
     logger.info();
-    if (_.has(options, 'follow')) {
-      exec(cmd, {silent:false});
+    if (options.follow) {
+      sh.exec(cmd, {silent:false});
     } else {
-      var out = exec(cmd, {silent:true}).output;
-      echo(out);
+      var out = sh.exec(cmd, {silent:true}).output;
+      sh.echo(out);
     }
 
-    return RSVP.resolve('done');
+    return RSVP.resolve(true);
   });
