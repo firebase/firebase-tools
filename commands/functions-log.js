ee15dde7c21b4c40c66951cddab90f97a293e531
Tamal Saha <tamal@google.com>
2016-01-19 16:45:35 -0800

    Address mbleigh's comments.

--- commands/functions-log.js
+++ commands/functions-log.js
@@ -14,13 +14,14 @@ var RSVP = require('rsvp');
 var sh = require('shelljs');
 var utils = require('../lib/utils');
 
-module.exports = new Command('functions:log [name]')
+module.exports = new Command('functions:log')
   .description('read logs from GCF Kubernetes cluster')
   .option('-P, --project <project_id>', 'override the project ID specified in firebase.json')
-  .option('-n, --pageSize <page_size>', 'specify page size')
+  .option('-f, --function <function_name>', 'specify function name whose logs will be fetched')
+  .option('-n, --lines <num_lines>', 'specify number of log lines to fetch')
   .before(requireConfig)
   .before(requireAccess)
-  .action(function(name, options) {
+  .action(function(options) {
     var projectId = getProjectId(options);
     var authClient;
     return api.getAccessToken().then(function(result) {
@@ -28,10 +29,10 @@ module.exports = new Command('functions:log [name]')
     }).then(function(client) {
       authClient = client;
       var filter = 'resource.type="cloud_function" labels."cloudfunctions.googleapis.com/region"="us-central1"'
-      if (name) {
-        filter += ' labels."cloudfunctions.googleapis.com/function_name"="'+ name +'"'
+      if (options.function) {
+        filter += ' labels."cloudfunctions.googleapis.com/function_name"="'+ options.function +'"'
       }
-      return gcp.cloudlogging.entries(authClient, projectId, filter, options.pageSize || 35);
+      return gcp.cloudlogging.entries(authClient, projectId, filter, options.lines || 35);
     }).then(function(entries) {
       if ( _.isEmpty(entries)) {
         logger.info('No log entries found.')
@@ -39,14 +40,13 @@ module.exports = new Command('functions:log [name]')
         _.forEach(entries, function(entry, id) {
           var __ = ' '
           logger.info(
-            entry.severity.substring(0,1), __,
-            entry.labels.execution_id || '              -', __,
             entry.timestamp, __,
-            entry.resource.labels.function_name, __,
+            entry.severity.substring(0,1), __,
+            entry.resource.labels.function_name + ':', __,
             entry.textPayload);
         });
       }
-      return RSVP.resolve();
+      return RSVP.resolve(entries);
     }).catch(function(err) {
       return RSVP.reject(new FirebaseError(
         'Failed to list log entries ' + err.message , {exit: 1}));
