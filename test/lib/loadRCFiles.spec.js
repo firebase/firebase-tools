69fe49b373090dd32c4a5dda330d1f1fb5c99c6b
Michael Bleigh <bleigh@google.com>
2016-03-10 18:22:04 -0800

    Fixing tests.

--- test/lib/loadRCFiles.spec.js
+++ test/lib/loadRCFiles.spec.js
@@ -4,19 +4,23 @@ var chai = require('chai');
 var expect = chai.expect;
 
 var path = require('path');
-var loadRCFiles = require('../../lib/loadRCFiles');
+var loadRCFile = require('../../lib/loadRCFile');
 
 var fixturesDir = path.resolve(__dirname, '../fixtures');
 
-describe('loadRCFiles', function() {
-  it('should merge all detected files', function() {
-    var result = loadRCFiles(path.resolve(fixturesDir, 'fbrc/conflict'));
-    expect(result.project.default).to.eq('conflict');
-    return expect(result.project.other).to.eq('top');
+describe('loadRCFile', function() {
+  it('should load from nearest project directory', function() {
+    var result = loadRCFile(path.resolve(fixturesDir, 'fbrc/conflict'));
+    expect(result.project.default).to.eq('top');
   });
 
-  it('should not crash when encountering malformed files', function() {
-    var result = loadRCFiles(path.resolve(fixturesDir, 'fbrc/invalid'));
-    return expect(result.project.other).to.eq('top');
+  it('should be an empty object when not in project dir', function() {
+    var result = loadRCFile(__dirname);
+    return expect(result).to.deep.eq({});
+  });
+
+  it('should not throw up on invalid json', function() {
+    var result = loadRCFile(path.resolve(fixturesDir, 'fbrc/invalid'));
+    return expect(result).to.deep.eq({});
   });
 });
