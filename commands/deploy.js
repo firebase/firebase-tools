70826e4911077db72e8f96e1ebafa0748bc6ccf7
Michael Bleigh <bleigh@google.com>
2016-03-22 13:23:12 -0700

    Consolidate all deploy:* commands with deploy --only <targets>

--- commands/deploy.js
+++ commands/deploy.js
@@ -5,15 +5,23 @@ var requireAccess = require('../lib/requireAccess');
 var requireConfig = require('../lib/requireConfig');
 var acquireRefs = require('../lib/acquireRefs');
 var deploy = require('../lib/deploy');
+var _ = require('lodash');
+
+// in order of least time-consuming to most time-consuming
+var VALID_TARGETS = ['database', 'storage', 'functions', 'hosting'];
 
 module.exports = new Command('deploy')
   .description('deploy code and assets to your Firebase project')
   .option('-p, --public <path>', 'override the Hosting public directory specified in firebase.json')
   .option('-m, --message <message>', 'an optional message describing this deploy')
+  .option('--only <targets>', 'only deploy to specified, comma-separated targets (e.g. "hosting,storage")')
   .before(requireConfig)
   .before(requireAccess)
   .before(acquireRefs)
   .action(function(options) {
-    // deploy in order of least time-consuming to most time-consuming
-    return deploy(['database', 'storage', 'functions', 'hosting'], options);
+    var targets = VALID_TARGETS;
+    if (options.only) {
+      targets = _.intersection(targets, options.only.split(','));
+    }
+    return deploy(targets, options);
   });
