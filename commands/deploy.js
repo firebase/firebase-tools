382f90fd849e7223d775685d6b85d872f8e0fd34
Michael Bleigh <mbleigh@mbleigh.com>
2016-07-28 18:20:44 -0700

    Adds --except to deploy (#90)

--- commands/deploy.js
+++ commands/deploy.js
@@ -8,6 +8,7 @@ var deploy = require('../lib/deploy');
 var previews = require('../lib/previews');
 var requireConfig = require('../lib/requireConfig');
 var scopes = require('../lib/scopes');
+var utils = require('../lib/utils');
 
 // in order of least time-consuming to most time-consuming
 var VALID_TARGETS = ['database', 'storage', 'functions', 'hosting'];
@@ -22,12 +23,20 @@ module.exports = new Command('deploy')
   .option('-p, --public <path>', 'override the Hosting public directory specified in firebase.json')
   .option('-m, --message <message>', 'an optional message describing this deploy')
   .option('--only <targets>', 'only deploy to specified, comma-separated targets (e.g. "hosting,storage")')
+  .option('--except <targets>', 'deploy to all targets except specified (e.g. "database")')
   .before(requireConfig)
   .before(acquireRefs, deployScopes)
   .action(function(options) {
     var targets = VALID_TARGETS;
+    if (options.only && options.except) {
+      return utils.reject('Cannot specify both --only and --except', {exit: 1});
+    }
+
     if (options.only) {
       targets = _.intersection(targets, options.only.split(','));
+    } else if (options.except) {
+      targets = _.difference(targets, options.except.split(','));
     }
+
     return deploy(targets, options);
   });
