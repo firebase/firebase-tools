26d4e2cabbad96664b8746a9abb95fae60d0d5b3
Lauren Long <laurenzlong@users.noreply.github.com>
2017-03-07 17:32:05 -0800

    Update functions deploy integration test to 0.5 SDK syntax (#190)

--- scripts/assets/functions_to_test.js
+++ scripts/assets/functions_to_test.js
@@ -1,31 +1,31 @@
 var functions = require('firebase-functions');
+var admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-exports.dbAction = functions.database().path('/input/{uuid}').onWrite(function(event) {
+exports.dbAction = functions.database.ref('/input/{uuid}').onWrite(function(event) {
   console.log('Received event:', event);
   return event.data.ref.root.child('output/' + event.params.uuid).set(event.data.val());
 });
 
 exports.nested = {
-  dbAction: functions.database().path('/inputNested/{uuid}').onWrite(function(event) {
+  dbAction: functions.database.ref('/inputNested/{uuid}').onWrite(function(event) {
   console.log('Received event:', event);
     return event.data.ref.root.child('output/' + event.params.uuid).set(event.data.val());
   })
 };
 
-exports.httpsAction = functions.https().onRequest(function(req, res) {
+exports.httpsAction = functions.https.onRequest(function(req, res) {
   res.send(req.body);
 });
 
-exports.pubsubAction = functions.pubsub('topic1').onPublish(function(event) {
+exports.pubsubAction = functions.pubsub.topic('topic1').onPublish(function(event) {
   console.log('Received event:', event);
   var uuid = event.data.json;
-  var app = functions.app;
-  return app.database().ref('output/' + uuid).set(uuid);
+  return admin.database().ref('output/' + uuid).set(uuid);
 });
 
-exports.gcsAction = functions.storage().onChange(function(event) {
+exports.gcsAction = functions.storage.object().onChange(function(event) {
   console.log('Received event:', event);
   var uuid = event.data.name;
-  var app = functions.app;
-  return app.database().ref('output/' + uuid).set(uuid);
+  return admin.database().ref('output/' + uuid).set(uuid);
 });
