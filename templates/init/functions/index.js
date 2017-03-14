af59f4e1e874a41af62055ecf5524a0e48332380
James Daniels <james@jamesdaniels.net>
2017-03-14 15:26:48 -0700

    Fixing the link in functions init

--- templates/init/functions/index.js
+++ templates/init/functions/index.js
@@ -1,7 +1,7 @@
 var functions = require('firebase-functions');
 
 // // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
