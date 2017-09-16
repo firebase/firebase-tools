b52dca92dcdde01e0482b8159d2592c53b9dadd6
Michael Bleigh <mbleigh@mbleigh.com>
2017-09-15 18:03:10 -0700

    WIP

--- commands/target-apply.js
+++ commands/target-apply.js
@@ -1,9 +1,33 @@
 'use strict';
 
+var _ = require('lodash');
+var chalk = require('chalk');
+
 var Command = require('../lib/command');
+var logger = require('../lib/logger');
+var requireConfig = require('../lib/requireConfig');
+var utils = require('../lib/utils');
 
 module.exports = new Command('target:apply <type> <name> <resources...>')
   .description('apply a deploy target to a resource')
-  .action(function(type, name, resources) {
-    console.log(type, name, resources);
+  .before(requireConfig)
+  .action(function(type, name, resources, options) {
+    var changes = [];
+    console.log(options.project);
+
+    _.forEach(resources, function(resource) {
+      var prev = options.findTarget(type, resource);
+      if (prev) {
+        changes.push({resource: resource, target: prev});
+      }
+    });
+
+    options.rc.applyTarget(options.project, type, name, resources);
+
+    utils.logSuccess('Applied target ' + chalk.bold(name) + ' to ' + chalk.bold(resources.join(', ')));
+    _.forEach(changes, function(change) {
+      utils.logWarning('Previous target ' + chalk.bold(change.target) + ' removed from ' + change.resource);
+    });
+    logger.info();
+    logger.info('Updated: ' + name + ' (' + options.rc.targets+ ')')
   });
