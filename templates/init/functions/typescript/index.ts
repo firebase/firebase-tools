8cff7b33def2ba0e272317b57f15d46fc8687961
Thusitha Manathunga <thusitha@serandibsoft.com>
2018-01-22 09:22:01 +0530

    Updated invalid URL for TypeScript start guide

--- templates/init/functions/typescript/index.ts
+++ templates/init/functions/typescript/index.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 
 // // Start writing Firebase Functions
-// // https://firebase.google.com/functions/write-firebase-functions
+// // https://firebase.google.com/docs/functions/typescript
 //
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
