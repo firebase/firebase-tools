30c4e6e8382ce3ecf082a991f4e2fe298fc0f12a
tianhui <ytianhui2004@gmail.com>
2018-05-14 18:37:02 +0900

    Update index.ts
    If there are no functions in the js file,  "run functions locally" will not work using the following command:
    firebase serve --only functions
    Error: An unexpected error has occurred.

--- templates/init/functions/typescript/index.ts
+++ templates/init/functions/typescript/index.ts
@@ -3,6 +3,6 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
