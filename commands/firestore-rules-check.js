fb65c59071cff3a6f8196d6c86746ac458169e35
eleith <online+github@eleith.com>
2018-06-08 16:43:31 -0700

    remove dead code

--- commands/firestore-rules-check.js
+++ commands/firestore-rules-check.js
@@ -3,25 +3,7 @@
 var Command = require("../lib/command");
 var requireAccess = require("../lib/requireAccess");
 var scopes = require("../lib/scopes");
-// var logger = require("../lib/logger");
-// var _ = require("lodash");
 var PrepareRules = require("../lib/deploy/firestore/prepare.js");
-
-/*
-var _prettyPrint = function(indexes) {
-  indexes.forEach(function(index) {
-    logger.info(firestoreIndexes.toPrettyString(index));
-  });
-};
-
-var _makeJsonSpec = function(indexes) {
-  return {
-    indexes: indexes.map(function(index) {
-      return _.pick(index, ["collectionId", "fields"]);
-    }),
-  };
-};
-*/
 
 module.exports = new Command("firestore:rules:check")
   .description("Checks if your firestore rules can compile.")
