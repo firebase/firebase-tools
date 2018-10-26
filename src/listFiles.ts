ae484372f7b843aa706763e9ba7dc69e560d10b9
Bryan Kendall <bkend@google.com>
2018-10-26 15:18:00 -0700

    use default parameter in listfiles

--- src/listFiles.ts
+++ src/listFiles.ts
@@ -1,11 +1,11 @@
 import * as glob from "glob";
 
-export function listFiles(cwd: string, ignore: string[]) {
+export function listFiles(cwd: string, ignore: string[] = []) {
   return glob.sync("**/*", {
     cwd,
     dot: true,
     follow: true,
-    ignore: ["**/firebase-debug.log", ".firebase/*"].concat(ignore || []),
+    ignore: ["**/firebase-debug.log", ".firebase/*"].concat(ignore),
     nodir: true,
     nosort: true,
   });
