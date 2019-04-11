a3bac4c6dc7ce62cd7b3c342883768ea1d797bbe
Ryan Brewster <RyanPBrewster@gmail.com>
2019-04-11 11:53:29 -0700

    Convert RulesDeploy.js into TypeScript (#1189)
    * Convert RulesDeploy.js into TypeScript
    
    * PR feedback
    
    * alternative syntax

--- src/RulesDeploy.ts
+++ src/RulesDeploy.ts
@@ -1,30 +1,33 @@
-"use strict";
+import _ = require("lodash");
+import clc = require("cli-color");
+import fs = require("fs");
 
-var _ = require("lodash");
-var clc = require("cli-color");
-var fs = require("fs");
+import gcp = require("./gcp");
+import logger = require("./logger");
+import FirebaseError = require("./error");
+import utils = require("./utils");
 
-var gcp = require("./gcp");
-var logger = require("./logger");
-var FirebaseError = require("./error");
-var utils = require("./utils");
+export class RulesDeploy {
+  type: any;
+  options: any;
+  project: any;
+  rulesFiles: any;
+  rulesetNames: any;
 
-function RulesDeploy(options, type) {
-  this.type = type;
-  this.options = options;
-  this.project = options.project;
-  this.rulesFiles = {};
-  this.rulesetNames = {};
-}
-
-RulesDeploy.prototype = {
+  constructor(options: any, type: any) {
+    this.type = type;
+    this.options = options;
+    this.project = options.project;
+    this.rulesFiles = {};
+    this.rulesetNames = {};
+  }
   /**
    * Adds a new project-relative file to be included in compilation and
    * deployment for this RulesDeploy.
    */
-  addFile: function(path) {
-    var fullPath = this.options.config.path(path);
-    var src;
+  addFile(path: any): void {
+    const fullPath = this.options.config.path(path);
+    let src;
     try {
       src = fs.readFileSync(fullPath, "utf8");
     } catch (e) {
@@ -33,70 +36,66 @@ RulesDeploy.prototype = {
     }
 
     this.rulesFiles[path] = [{ name: path, content: src }];
-  },
+  }
 
   /**
    * Compile all rulesets tied to this deploy, rejecting on first
    * compilation error.
    */
-  compile: function() {
-    var self = this;
-    var promises = [];
-    _.forEach(this.rulesFiles, function(files, filename) {
-      promises.push(self._compileRuleset(filename, files));
+  compile(): Promise<any> {
+    const promises: any[] = [];
+    _.forEach(this.rulesFiles, (files: any, filename: any) => {
+      promises.push(this._compileRuleset(filename, files));
     });
     return Promise.all(promises);
-  },
+  }
 
   /**
    * Create rulesets for each file added to this deploy, and record
    * the name for use in the release process later.
    */
-  createRulesets: function() {
-    var self = this;
-    var promises = [];
-    _.forEach(this.rulesFiles, function(files, filename) {
+  createRulesets(): Promise<any> {
+    const promises: any[] = [];
+    _.forEach(this.rulesFiles, (files: any, filename: any) => {
       utils.logBullet(
-        clc.bold.cyan(self.type + ":") + " uploading rules " + clc.bold(filename) + "..."
+        clc.bold.cyan(this.type + ":") + " uploading rules " + clc.bold(filename) + "..."
       );
       promises.push(
-        gcp.rules.createRuleset(self.options.project, files).then(function(rulesetName) {
-          self.rulesetNames[filename] = rulesetName;
+        gcp.rules.createRuleset(this.options.project, files).then((rulesetName: any) => {
+          this.rulesetNames[filename] = rulesetName;
         })
       );
     });
     return Promise.all(promises);
-  },
+  }
 
-  release: function(filename, resourceName) {
-    var self = this;
+  release(filename: any, resourceName: any): Promise<any> {
     return gcp.rules
       .updateOrCreateRelease(this.options.project, this.rulesetNames[filename], resourceName)
-      .then(function() {
+      .then(() => {
         utils.logSuccess(
-          clc.bold.green(self.type + ": ") +
+          clc.bold.green(this.type + ": ") +
             "released rules " +
             clc.bold(filename) +
             " to " +
             clc.bold(resourceName)
         );
       });
-  },
+  }
 
-  _compileRuleset: function(filename, files) {
+  private _compileRuleset(filename: any, files: any): Promise<any> {
     utils.logBullet(
       clc.bold.cyan(this.type + ":") +
         " checking " +
         clc.bold(filename) +
         " for compilation errors..."
     );
-    var self = this;
-    return gcp.rules.testRuleset(self.options.project, files).then(function(response) {
+    return gcp.rules.testRuleset(this.options.project, files).then((response: any) => {
       if (response.body && response.body.issues && response.body.issues.length > 0) {
-        var warnings = [];
-        var errors = [];
-        response.body.issues.forEach(function(issue) {
-          var issueMessage =
+        const warnings: any[] = [];
+        const errors: any[] = [];
+        response.body.issues.forEach((issue: any) => {
+          const issueMessage =
             "[" +
             issue.severity.substring(0, 1) +
             "] " +
@@ -114,28 +113,26 @@ RulesDeploy.prototype = {
         });
 
         if (warnings.length > 0) {
-          warnings.forEach(function(warning) {
+          warnings.forEach((warning: any) => {
             utils.logWarning(warning);
           });
         }
 
         if (errors.length > 0) {
-          var add = errors.length === 1 ? "" : "s";
-          var message =
+          const add = errors.length === 1 ? "" : "s";
+          const message =
             "Compilation error" + add + " in " + clc.bold(filename) + ":\n" + errors.join("\n");
           return utils.reject(message, { exit: 1 });
         }
       }
 
       utils.logSuccess(
-        clc.bold.green(self.type + ":") +
+        clc.bold.green(this.type + ":") +
           " rules file " +
           clc.bold(filename) +
           " compiled successfully"
       );
       return Promise.resolve();
     });
-  },
-};
-
-module.exports = RulesDeploy;
+  }
+}
