b1d22bd8c7c1a4192a227a96082cb3279097d62b
Diana Tkachenko <dianatkachenko@google.com>
2019-04-18 22:03:32 -0700

    Refactor functions prepare step to allow for engines check

--- src/deploy/functions/prepare.js
+++ src/deploy/functions/prepare.js
@@ -13,38 +13,32 @@ var logger = require("../../logger");
 var resolveProjectPath = require("../../resolveProjectPath");
 var utils = require("../../utils");
 
-var VALID_FUNCTION_NAME_REGEX = /^[a-z][a-zA-Z0-9_-]{1,62}$/i;
-
-module.exports = function(context, options, payload) {
-  if (!options.config.has("functions")) {
-    return Promise.resolve();
-  }
-
-  var sourceDirName = options.config.get("functions.source");
-
-  if (!fsutils.dirExistsSync(resolveProjectPath(options.cwd, sourceDirName))) {
+// Check that functions directory exists
+var _checkFunctionsDirectoryExists = function(cwd, sourceDirName) {
+  if (!fsutils.dirExistsSync(resolveProjectPath(cwd, sourceDirName))) {
     var msg =
-      "could not deploy functions because the " +
-      clc.bold('"' + sourceDirName + '"') +
-      " directory was not found. Please create it or specify a different source directory in firebase.json";
-
+      `could not deploy functions because the ${clc.bold('"' + sourceDirName + '"')} ` +
+      `directory was not found. Please create it or specify a different source directory in firebase.json`;
     return utils.reject(msg, { exit: 1 });
   }
+};
 
-  // Function name validation
-  var invalidNames = _.reject(_.keys(payload.functions), function(name) {
-    return _.startsWith(name, ".") || VALID_FUNCTION_NAME_REGEX.test(name);
+// Validate function names only contain lower case letters, numbers, and dashes
+var _validateFunctionName = function(functionNames) {
+  var validFunctionNameRegex = /^[a-z][a-zA-Z0-9_-]{1,62}$/i;
+  var invalidNames = _.reject(_.keys(functionNames), function(name) {
+    return _.startsWith(name, ".") || validFunctionNameRegex.test(name);
   });
   if (!_.isEmpty(invalidNames)) {
-    return utils.reject(
-      invalidNames.join(", ") +
-        " function name(s) must be a valid subdomain (lowercase letters, numbers and dashes)",
-      { exit: 1 }
-    );
+    var msg = `${invalidNames.join(
+      ", "
+    )} function name(s) must be a valid subdomain (lowercase letters, numbers and dashes)`;
+    return utils.reject(msg, { exit: 1 });
   }
+};
 
-  // Check main file specified in package.json is present
-  var sourceDir = options.config.path(sourceDirName);
+// Validate contents of package.json - main file present and engines specified
+var _validatePackageJsonFile = function(sourceDirName, sourceDir, projectDir) {
   var packageJsonFile = path.join(sourceDir, "package.json");
   if (fsutils.fileExistsSync(packageJsonFile)) {
     try {
@@ -52,26 +46,44 @@ module.exports = function(context, options, payload) {
       logger.debug("> [functions] package.json contents:", JSON.stringify(data, null, 2));
       var indexJsFile = path.join(sourceDir, data.main || "index.js");
       if (!fsutils.fileExistsSync(indexJsFile)) {
-        return utils.reject(
-          path.relative(options.config.projectDir, indexJsFile) +
-            " does not exist, can't deploy Firebase Functions",
-          { exit: 1 }
-        );
+        var msg = `${path.relative(
+          projectDir,
+          indexJsFile
+        )} does not exist, can't deploy Firebase Functions`;
+        return utils.reject(msg, { exit: 1 });
       }
     } catch (e) {
-      return utils.reject(
-        "There was an error reading " + sourceDirName + path.sep + "package.json:\n\n" + e.message,
-        { exit: 1 }
-      );
+      var msg = `There was an error reading ${sourceDirName}${path.sep}package.json:\n\n ${
+        e.message
+      }`;
+      return utils.reject(msg, { exit: 1 });
     }
   } else if (!fsutils.fileExistsSync(path.join(sourceDir, "function.js"))) {
-    return utils.reject(
-      "No npm package found in functions source directory. Please run 'npm init' inside " +
-        sourceDirName,
-      { exit: 1 }
-    );
+    var msg = `No npm package found in functions source directory. Please run 'npm init' inside ${sourceDirName}`;
+    return utils.reject(msg, { exit: 1 });
   }
+};
+
+module.exports = function(context, options, payload) {
+  if (!options.config.has("functions")) {
+    return Promise.resolve();
+  }
+
+  var sourceDirName = options.config.get("functions.source");
+  var sourceDir = options.config.path(sourceDirName);
+  var projectDir = options.config.projectDir;
+  var functionNames = payload.functions;
   var projectId = getProjectId(options);
+
+  var invalidOptions =
+    _checkFunctionsDirectoryExists(options.cwd, sourceDirName) ||
+    _validateFunctionName(functionNames) ||
+    _validatePackageJsonFile(sourceDirName, sourceDir, projectDir);
+
+  if (invalidOptions) {
+    return invalidOptions;
+  }
+
   return Promise.all([
     ensureApiEnabled.ensure(options.project, "cloudfunctions.googleapis.com", "functions"),
     ensureApiEnabled.check(projectId, "runtimeconfig.googleapis.com", "runtimeconfig", true),
