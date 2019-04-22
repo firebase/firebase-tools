85485d996c506bd5587c329958d0ce0405e899bd
Michael Bleigh <mbleigh@mbleigh.com>
2019-04-22 13:32:16 -0700

    Do null check before matching location, default to "us-central1". Fixes #1208 (#1211)
    * Do null check before matching location, default to "us-central1"
    
    * Remove stray log.

--- src/deploy/functions/release.js
+++ src/deploy/functions/release.js
@@ -117,7 +117,6 @@ module.exports = function(context, options, payload) {
   var projectId = context.projectId;
   var sourceUrl = context.uploadUrl;
   var appEngineLocation = getAppEngineLocation(context.firebaseConfig);
-  utils.logWarning("appEngineLocation " + appEngineLocation);
   // Used in CLI releases v3.4.0 to v3.17.6
   var legacySourceUrlTwo =
     "gs://" + "staging." + context.firebaseConfig.storageBucket + "/firebase-functions-source";
