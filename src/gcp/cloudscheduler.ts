992a030e1db755095d4c0adaabf7c4cb96cce29c
Joe Hanley <joehanley@google.com>
2019-05-01 14:47:57 -0700

    changes to correct jsdoc style

--- src/gcp/cloudscheduler.ts
+++ src/gcp/cloudscheduler.ts
@@ -23,11 +23,11 @@ export interface Job {
   };
 }
 
-/*
-* Creates a cloudScheduler job.
-* If another job with that name already exists, this will return a 409.
-* @param job: The job to create.
-*/
+/**
+ * Creates a cloudScheduler job.
+ * If another job with that name already exists, this will return a 409.
+ * @param job The job to create.
+ */
 export function createJob(job: Job): Promise<any> {
   // the replace below removes the portion of the schedule name after the last /
   // ie: projects/my-proj/locations/us-central1/jobs/firebase-schedule-func-us-east1 would become
@@ -40,11 +40,11 @@ export function createJob(job: Job): Promise<any> {
   });
 }
 
-/*
-* Deletes a cloudScheduler job with the given name.
-* Returns a 404 if no job with that name exists.
-* @param name: The name of the job to delete.
-*/
+/**
+ * Deletes a cloudScheduler job with the given name.
+ * Returns a 404 if no job with that name exists.
+ * @param name The name of the job to delete.
+ */
 export function deleteJob(name: string): Promise<any> {
   return api.request("DELETE", `/${VERSION}/${name}`, {
     auth: true,
@@ -52,11 +52,11 @@ export function deleteJob(name: string): Promise<any> {
   });
 }
 
-/*
-* Gets a cloudScheduler job with the given name.
-* If no job with that name exists, this will return a 404.
-* @param name: The name of the job to get.
-*/
+/**
+ * Gets a cloudScheduler job with the given name.
+ * If no job with that name exists, this will return a 404.
+ * @param name The name of the job to get.
+ */
 export function getJob(name: string): Promise<any> {
   return api.request("GET", `/${VERSION}/${name}`, {
     auth: true,
@@ -64,11 +64,11 @@ export function getJob(name: string): Promise<any> {
   });
 }
 
-/*
-* Updates a cloudScheduler job.
-* Returns a 404 if no job with that name exists.
-* @param job: A job to update. Note that name cannot be updated.
-*/
+/**
+ * Updates a cloudScheduler job.
+ * Returns a 404 if no job with that name exists.
+ * @param job A job to update. Note that name cannot be updated.
+ */
 export function updateJob(job: Job): Promise<any> {
   return api.request("PATCH", `/${VERSION}/${job.name}`, {
     auth: true,
@@ -77,14 +77,14 @@ export function updateJob(job: Job): Promise<any> {
   });
 }
 
-/*
-* Checks for a existing job with the given name.
-* If none is found, it creates a new job.
-* If one is found, and it is identical to the job parameter, it does nothing.
-* Otherwise, if one is found and it is different from the job param, it updates the job.
-* @param job: A job to check for and create, replace, or leave as appropriate.
-* @throws { FirebaseError } if an error respnse other than 404 is recieved on the GET call.
-*/
+/** 
+ * Checks for a existing job with the given name.
+ * If none is found, it creates a new job.
+ * If one is found, and it is identical to the job parameter, it does nothing.
+ * Otherwise, if one is found and it is different from the job param, it updates the job.
+ * @param job A job to check for and create, replace, or leave as appropriate.
+ * @throws { FirebaseError } if an error respnse other than 404 is recieved on the GET call.
+ */
 export async function createOrReplaceJob(job: Job): Promise<any> {
   const jobName = `${job.name.split("/")[5]}`;
   try {
@@ -114,11 +114,11 @@ export async function createOrReplaceJob(job: Job): Promise<any> {
   }
 }
 
-/*
-* Check if two jobs are functionally equivalent.
-* @param existingJob: a job to compare.
-* @param newJob: a job to compare.
-*/
+/**
+ * Check if two jobs are functionally equivalent.
+ * @param existingJob a job to compare.
+ * @param newJob a job to compare.
+ */
 function isIdentical(existingJob: Job, newJob: Job): boolean {
   return (
     existingJob &&
