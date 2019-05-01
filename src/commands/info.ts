1328918564c2e90acd65dd0e24d700c7e2ccb727
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-05-02 00:24:25 +0530

    fixup :construction:

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -6,21 +6,20 @@ import * as Command from "../command";
 
 export default new Command("info")
   .description("Prints debugging information about the environment")
-  .action(() => {
+  .action(async () => {
     logger.info(clc.bold.white("\nEnvironment Info"));
-    envinfo
-      .run(
-        {
-          System: ["OS", "CPU"],
-          Binaries: ["Node", "Yarn", "npm"],
-          Browsers: ["Chrome", "Edge", "Firefox", "Safari"],
-          npmGlobalPackages: ["firebase"],
-        },
-        {
-          showNotFound: true,
-          duplicates: true,
-          fullTree: true,
-        }
-      )
-      .then(logger.info(clc.bold.white()));
+    const info = await envinfo.run(
+      {
+        System: ["OS", "CPU"],
+        Binaries: ["Node", "Yarn", "npm"],
+        Browsers: ["Chrome", "Edge", "Firefox", "Safari"],
+        npmGlobalPackages: ["firebase"],
+      },
+      {
+        showNotFound: true,
+        duplicates: true,
+        fullTree: true,
+      }
+    );
+    logger.info(clc.bold.white(info));
   });
