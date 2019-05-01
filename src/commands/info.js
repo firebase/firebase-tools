b2025d217ffaff32ceb53881ac09d520f28730a6
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-05-01 10:00:49 +0530

    Update eslint config to use es6+ features
    Linting fixes

--- src/commands/info.js
+++ src/commands/info.js
@@ -6,22 +6,21 @@ const envinfo = require("envinfo");
 var Command = require("../command");
 
 module.exports = new Command("info")
-.description("Prints debugging information about the environment")
-.action(async function() {
-  console.log(chalk.bold('\nEnvironment Info:'));
-    var info = await envinfo
-      .run(
-        {
-          System: ['OS', 'CPU'],
-          Binaries: ['Node', 'Yarn', 'npm'],
-          Browsers: ['Chrome', 'Edge', 'Firefox', 'Safari'],
-          npmGlobalPackages: ['firebase'],
-        },
-        {
-          showNotFound: true,
-          duplicates: true,
-          fullTree: true,
-        },
-      );
+  .description("Prints debugging information about the environment")
+  .action(async function() {
+    console.log(chalk.bold("\nEnvironment Info:"));
+    var info = await envinfo.run(
+      {
+        System: ["OS", "CPU"],
+        Binaries: ["Node", "Yarn", "npm"],
+        Browsers: ["Chrome", "Edge", "Firefox", "Safari"],
+        npmGlobalPackages: ["firebase"],
+      },
+      {
+        showNotFound: true,
+        duplicates: true,
+        fullTree: true,
+      }
+    );
     console.log(info);
-});
+  });
