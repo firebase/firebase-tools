d4bec6334dd8987c935e63269675abf0762e5834
Diana Tkachenko <31747099+thechenky@users.noreply.github.com>
2019-05-01 01:29:39 -0700

    Refactor deploy/functions/prepare logic and add tests (#1218)
    * Refactor functions prepare step to allow for engines check
    
    * Split out validation logic for functions prepare step
    
    * Minor fix in wording in the test for consistency
    
    * Removing strict from ts files, fixing imports, adding jsdocs
    
    * adding sandboxing to sinon tests, adding beforeEach() and afterEach()
    
    * Small fix in jsdoc
    
    * Add a comment about legacy behavior when package.json not found
    
    * switch to non-deprecated sinon sandbox, fix throw() in tests, add some comments about buggy behavior
    
    * fix srcmain.js => src/main.js in a test
    
    * fix regex for validating function names to allow capital letters, and update jsdoc to match
    
    * fix wording in error message and jsdoc on what valid function name should look like
    
    * add jsdoc on resolveProjectPath() function
    
    * removing legacy behavior, Firebase Functions => Cloud Functions in error message

--- src/resolveProjectPath.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var path = require("path");
-var detectProjectRoot = require("./detectProjectRoot");
-
-module.exports = function(cwd, filePath) {
-  return path.resolve(detectProjectRoot(cwd), filePath);
-};
