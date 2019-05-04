f375d0dbb06b90d587e63bddaf3092c2942e084c
Jan Wyszynski <wyszynski@google.com>
2019-05-03 18:11:04 -0700

    Remove size changes to isolate `listPath`.

---
+++ src/database/listRemote.ts
@@ -0,0 +1,78 @@
+import * as request from "request";
+import { Response } from "request";
+import * as responseToError from "../responseToError";
+import * as utils from "../utils";
+import * as FirebaseError from "../error";
+import * as logger from "../logger";
+import * as api from "../api";
+
+export interface ListRemote {
+  /**
+   * Call the shallow get API with limitToFirst=numSubPath.
+   * @param path the path to list
+   * @param numSubPath the number of subPaths to fetch.
+   * @return the list of sub pathes found.
+   */
+  listPath(path: string, numSubPath: number, offset?: string, timeout?: number): Promise<string[]>;
+}
+
+export class RTDBListRemote implements ListRemote {
+  private instance: string;
+
+  constructor(instance: string) {
+    this.instance = instance;
+  }
+
+  listPath(path: string, numSubPath: number, offset?: string, timeout?: number): Promise<string[]> {
+    const offsetSuffix = offset ? "&startAfter=" + offset : "";
+    const timeoutSuffix = timeout ? "&timeout=" + timeout + "ms" : "";
+    const url =
+      utils.addSubdomain(api.realtimeOrigin, this.instance) +
+      path +
+      `.json?shallow=true&limitToFirst=${numSubPath}` +
+      offsetSuffix +
+      timeoutSuffix;
+
+    const t0 = Date.now();
+    return api
+      .addRequestHeaders({
+        url,
+      })
+      .then((reqOptionsWithToken) => {
+        return new Promise<string[]>((resolve, reject) => {
+          request.get(reqOptionsWithToken, (err: Error, res: Response, body: any) => {
+            if (err) {
+              return reject(
+                new FirebaseError("Unexpected error while listing subtrees", {
+                  exit: 2,
+                  original: err,
+                })
+              );
+            } else if (res.statusCode >= 400) {
+              return reject(responseToError(res, body));
+            }
+            let data = {};
+            try {
+              data = JSON.parse(body);
+            } catch (e) {
+              return reject(
+                new FirebaseError("Malformed JSON response in shallow get ", {
+                  exit: 2,
+                  original: e,
+                })
+              );
+            }
+            if (data) {
+              return resolve(Object.keys(data));
+            }
+            resolve([]);
+          });
+        });
+      })
+      .then((paths: string[]) => {
+        const dt = Date.now() - t0;
+        logger.debug(`[database] Sucessfully fetched ${paths.length} path at ${path} ${dt}`);
+        return paths;
+      });
+  }
+}
