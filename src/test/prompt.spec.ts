29d84726a8cdfdf9b414aaa6d45da7d76445a2e3
Bryan Kendall <bkend@google.com>
2019-05-24 17:59:37 -0700

    add tests for prompt

---
+++ src/test/prompt.spec.ts
@@ -0,0 +1,100 @@
+import { expect } from "chai";
+import * as sinon from "sinon";
+import * as inquirer from "inquirer";
+
+import * as FirebaseError from "../error";
+import * as prompt from "../prompt";
+
+describe("prompt", () => {
+  let inquirerStub: sinon.SinonStub;
+
+  beforeEach(() => {
+    // Stub inquirer to return a set of fake answers.
+    inquirerStub = sinon.stub(inquirer, "prompt").resolves({
+      some: "fool",
+      with: "values",
+    });
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe("prompt", () => {
+    it("should error if questions are asked in nonInteractive environment", async () => {
+      const o = { nonInteractive: true };
+      const qs: prompt.Question[] = [{ name: "foo" }];
+
+      expect(prompt.prompt(o, qs)).to.be.rejectedWith(FirebaseError, /required.+non\-interactive/);
+    });
+
+    it("should utilize inquirer to prompt for the questions", async () => {
+      const qs: prompt.Question[] = [
+        {
+          name: "foo",
+          message: "this is a test",
+        },
+      ];
+
+      await prompt.prompt({}, qs);
+
+      expect(inquirerStub).calledOnceWithExactly(qs);
+    });
+  });
+
+  describe("promptOnce", () => {
+    it("should provide a name if one is not provided", async () => {
+      await prompt.promptOnce({ message: "foo" });
+
+      expect(inquirerStub).calledOnceWith([{ name: "question", message: "foo" }]);
+    });
+
+    it("should return the value for the given name", async () => {
+      const r = await prompt.promptOnce({ name: "some" });
+
+      expect(r).to.equal("fool");
+      expect(inquirerStub).calledOnce;
+    });
+  });
+
+  describe("convertLabeledListChoices", () => {
+    it("should return a list of the same size", () => {
+      const cs = [
+        {
+          checked: false,
+          name: "foo",
+        },
+        {
+          checked: true,
+          name: "bar",
+        },
+      ];
+
+      expect(prompt.convertLabeledListChoices([])).to.have.length(0);
+      expect(prompt.convertLabeledListChoices(cs)).to.have.length(2);
+    });
+  });
+
+  describe("listLabelToValue", () => {
+    const CHOICES = [
+      {
+        name: "foo",
+        label: "this is foo",
+        checked: false,
+      },
+      {
+        name: "bar",
+        label: "this is bar",
+        checked: false,
+      },
+    ];
+
+    it("should return the value for a name given a label", () => {
+      expect(prompt.listLabelToValue("this is bar", CHOICES)).to.equal("bar");
+    });
+
+    it("should return empty-string for an unknown label", () => {
+      expect(prompt.listLabelToValue("this is baz", CHOICES)).to.equal("");
+    });
+  });
+});
