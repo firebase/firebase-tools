6e9066fe9ee76b92edb4e7143de55d3976a168f8
Sam Stern <samstern@google.com>
2019-05-28 20:37:36 +0200

    Allow runtime function to handle CORS (#1335)

--- src/emulator/functionsEmulator.ts
+++ src/emulator/functionsEmulator.ts
@@ -69,19 +69,6 @@ export class FunctionsEmulator implements EmulatorInstance {
     const hub = express();
 
     hub.use((req, res, next) => {
-      // Allow CORS to facilitate easier testing.
-      // Sources:
-      //  * https://enable-cors.org/server_expressjCannot understand what targets to deploys.html
-      //  * https://stackoverflow.com/a/37228330/324977
-      res.header("Access-Control-Allow-Origin", "*");
-
-      // Callable functions send "Authorization" and "Content-Type".
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Authorization, Accept"
-      );
-      res.header("Access-Control-Allow-Methods", "GET,OPTIONS,POST");
-
       let data = "";
       req.on("data", (chunk: any) => {
         data += chunk;
