ced439076a92eec51eff788400ee055c33d29d64
Diana Tkachenko <dianatkachenko@google.com>
2019-06-10 14:40:50 -0700

    small format and type fixes

--- src/init/features/firestore/index.ts
+++ src/init/features/firestore/index.ts
@@ -2,7 +2,7 @@ import requireAccess = require("../../../requireAccess");
 import * as rules from "./rules";
 import * as indexes from "./indexes";
 
-export async function doSetup(setup: any, config: any): Promise<any> {
+export async function doSetup(setup: any, config: any): Promise<void> {
   setup.config.firestore = {};
 
   await requireAccess.requireAccess({ project: setup.projectId });
