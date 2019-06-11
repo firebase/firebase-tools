9cffe5aba3cf88bea631ff6fccbc5a50468eef41
Diana Tkachenko <31747099+thechenky@users.noreply.github.com>
2019-06-10 22:41:46 -0700

    Add tests to project, storage, and firestore init flows (#1381)
    * convert firebaseApi and init/features/project to typescript
    
    * change to use async/await
    
    * add jsdoc on the interfaces
    
    * fix default export
    
    * format
    
    * remove default function, ts-ify storage
    
    * add cast to Question to keep validate function
    
    * delete project.d.ts and move interfaces in with source code
    
    * fix interfaces, remote lint disables, refactor
    
    * Error => FirebaseError
    
    * add tests to project init and storage init
    
    * add firestore tests - refactor and ts-ifying proved necessary to do this
    
    * use index.ts to remove extra /firestore in imports
    
    * review comment fixes, remove validate function
    
    * small format and type fixes
    
    * sinon-chai changes
    
    * fix bug in stoage initialization
    
    * remove only
    
    * fix storage unit test
    
    * format changes

---
+++ src/test/init/features/project.spec.ts
@@ -0,0 +1,128 @@
+import { expect } from "chai";
+import * as _ from "lodash";
+import * as sinon from "sinon";
+
+import { doSetup, getProjectInfo } from "../../../init/features/project";
+import * as firebaseApi from "../../../firebaseApi";
+import * as prompt from "../../../prompt";
+
+const TEST_FIREBASE_PROJECT = {
+  projectId: "my-project-123",
+  projectNumber: 123456789,
+  displayName: "my-project",
+  name: "projects/my-project",
+  resources: {
+    hostingSite: "my-project",
+    realtimeDatabaseInstance: "my-project",
+    storageBucket: "my-project.appspot.com",
+    locationId: "us-central",
+  },
+};
+
+const ANOTHER_FIREBASE_PROJECT = {
+  projectId: "another-project",
+  projectNumber: 987654321,
+  displayName: "another-project",
+  name: "projects/another-project",
+  resources: {},
+};
+
+const TEST_PROJECT_INFO = {
+  id: "my-project-123",
+  label: "my-project-123 (my-project)",
+  instance: "my-project",
+};
+
+describe("project", () => {
+  const sandbox: sinon.SinonSandbox = sinon.createSandbox();
+  let listProjectsStub: sinon.SinonStub;
+  let getProjectStub: sinon.SinonStub;
+  let promptStub: sinon.SinonStub;
+
+  beforeEach(() => {
+    listProjectsStub = sandbox.stub(firebaseApi, "listProjects");
+    getProjectStub = sandbox.stub(firebaseApi, "getProject");
+    promptStub = sandbox.stub(prompt, "promptOnce");
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  describe("getProjectInfo", () => {
+    it("should get correct project info when no project supplied", async () => {
+      const options = {};
+      listProjectsStub.returns([TEST_FIREBASE_PROJECT, ANOTHER_FIREBASE_PROJECT]);
+      promptStub.returns("my-project-123");
+
+      const project = await getProjectInfo(options);
+
+      expect(project).to.deep.equal(TEST_PROJECT_INFO);
+    });
+
+    it("should get the correct project info when --project is supplied", async () => {
+      const options = { project: "my-project" };
+      getProjectStub.returns(TEST_FIREBASE_PROJECT);
+
+      const project = await getProjectInfo(options);
+
+      expect(project).to.deep.equal(TEST_PROJECT_INFO);
+    });
+
+    it("should return correct project info when choosing new project", async () => {
+      const options = {};
+      listProjectsStub.returns([TEST_FIREBASE_PROJECT, ANOTHER_FIREBASE_PROJECT]);
+      promptStub.returns("[create a new project]");
+
+      const project = await getProjectInfo(options);
+
+      expect(project).to.deep.equal({ id: "[create a new project]" });
+    });
+
+    it("should return correct project info when choosing not to set up project", async () => {
+      const options = {};
+      listProjectsStub.returns([TEST_FIREBASE_PROJECT, ANOTHER_FIREBASE_PROJECT]);
+      promptStub.returns("[don't setup a default project]");
+
+      const project = await getProjectInfo(options);
+
+      expect(project).to.deep.equal({ id: "[don't setup a default project]" });
+    });
+  });
+
+  describe("doSetup", () => {
+    it("should set up the correct properties in the project", async () => {
+      const options = { project: "my-project" };
+      const setup = { config: {}, rcfile: {} };
+      getProjectStub.returns(TEST_FIREBASE_PROJECT);
+
+      await doSetup(setup, {}, options);
+
+      expect(_.get(setup, "projectId")).to.deep.equal("my-project-123");
+      expect(_.get(setup, "instance")).to.deep.equal("my-project");
+      expect(_.get(setup.rcfile, "projects.default")).to.deep.equal("my-project-123");
+    });
+
+    it("should set up the correct properties when choosing new project", async () => {
+      const options = {};
+      const setup = { config: {}, rcfile: {} };
+      listProjectsStub.returns([TEST_FIREBASE_PROJECT, ANOTHER_FIREBASE_PROJECT]);
+      promptStub.returns("[create a new project]");
+
+      await doSetup(setup, {}, options);
+
+      expect(_.get(setup, "createProject")).to.deep.equal(true);
+    });
+
+    it("should set up the correct properties when not choosing a project", async () => {
+      const options = {};
+      const setup = { config: {}, rcfile: {} };
+      listProjectsStub.returns([TEST_FIREBASE_PROJECT, ANOTHER_FIREBASE_PROJECT]);
+      promptStub.returns("[don't setup a default project]");
+
+      await doSetup(setup, {}, options);
+
+      expect(setup).to.deep.equal({ config: {}, rcfile: {}, project: {} });
+    });
+  });
+});
