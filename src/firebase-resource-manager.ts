938019d94f39e645e81d7cef82e159f5eb3d206c
Tri Cao <caot@google.com>
2019-06-25 15:03:01 -0400

    Implement API calls for apps:create command

--- src/firebase-resource-manager.ts
+++ src/firebase-resource-manager.ts
@@ -3,7 +3,7 @@ import * as clc from "cli-color";
 import * as api from "./api";
 import * as FirebaseError from "./error";
 import * as logger from "./logger";
-import { OperationPollerOptions, pollOperation } from "./operation-poller";
+import { pollOperation } from "./operation-poller";
 import { OraWrapper } from "./oraWrapper";
 
 const ONE_SECOND_MILLIS = 1000;
@@ -16,6 +16,38 @@ export enum ParentResourceType {
 export interface ParentResource {
   id: string;
   type: ParentResourceType;
+}
+
+export enum AppPlatform {
+  IOS = "iOS",
+  ANDROID = "Android",
+  WEB = "Web",
+}
+
+export interface AppMetadata {
+  appId: string;
+  displayName: string;
+  appPlatform: AppPlatform;
+}
+
+export interface IosAppMetadata extends AppMetadata {
+  bundleId: string;
+  appPlatform: AppPlatform.IOS;
+}
+
+export interface AndroidAppMetadata extends AppMetadata {
+  packageName: string;
+  shaCertificates?: ShaCertificate[];
+  appPlatform: AppPlatform.ANDROID;
+}
+
+export interface WebAppMetadata extends AppMetadata {
+  appPlatform: AppPlatform.WEB;
+}
+
+export interface ShaCertificate {
+  certType: "SHA_1" | "SHA_256";
+  shaHash: string;
 }
 
 export async function createFirebaseProject(
@@ -115,5 +147,172 @@ async function addFirebaseToCloudProject(projectId: string): Promise<any> {
       "Failed to add Firebase to Google Cloud Platform project. See firebase-debug.log for more info.",
       { exit: 2, original: err }
     );
+  }
+}
+
+/**
+ * Send an API request to create a new Firebase iOS app and poll the LRO to get the new app
+ * information.
+ * @return {Promise} this function returns a promise that resolves to the new iOS app information
+ */
+export async function createIosApp(
+  projectId: string,
+  displayName: string,
+  bundleId: string
+): Promise<IosAppMetadata> {
+  const spinner = new OraWrapper(`Creating your ${clc.red("iOS")} app`);
+  spinner.start();
+
+  try {
+    const response = await api.request("POST", `/v1beta1/projects/${projectId}/iosApps`, {
+      auth: true,
+      origin: api.firebaseApiOrigin,
+      timeout: 15 * ONE_SECOND_MILLIS,
+      data: { displayName, bundleId },
+    });
+    const result = await pollOperation<any>({
+      pollerName: "Create iOS app Poller",
+      apiOrigin: api.firebaseApiOrigin,
+      apiVersion: "v1beta1",
+      operationResourceName: response.body.name /* LRO resource name */,
+    });
+    spinner.succeed();
+    const appMetadata: IosAppMetadata = {
+      appId: result.appId,
+      displayName: result.displayName,
+      bundleId: result.bundleId,
+      appPlatform: AppPlatform.IOS,
+    };
+    logPostAppCreationInformation(appMetadata);
+    return appMetadata;
+  } catch (err) {
+    spinner.fail();
+    logger.debug(err.message);
+    throw new FirebaseError(
+      `Failed to create iOS app for project ${projectId}. See firebase-debug.log for more info.`,
+      { exit: 2, original: err }
+    );
+  }
+}
+
+/**
+ * Send an API request to create a new Firebase Android app and poll the LRO to get the new app
+ * information. Optionally add a SHA certificate to the app if specified.
+ * @return {Promise} this function returns a promise that resolves to the new Android app
+ * information
+ */
+export async function createAndroidApp(
+  projectId: string,
+  displayName: string,
+  packageName: string,
+  shaCertificate?: ShaCertificate
+): Promise<AndroidAppMetadata> {
+  let spinner = new OraWrapper(`Creating your ${clc.red("Android")} app`);
+  let isProjectCreated = false;
+  spinner.start();
+
+  try {
+    const response = await api.request("POST", `/v1beta1/projects/${projectId}/androidApps`, {
+      auth: true,
+      origin: api.firebaseApiOrigin,
+      timeout: 15 * ONE_SECOND_MILLIS,
+      data: { displayName, packageName },
+    });
+    const result = await pollOperation<any>({
+      pollerName: "Create Android app Poller",
+      apiOrigin: api.firebaseApiOrigin,
+      apiVersion: "v1beta1",
+      operationResourceName: response.body.name /* LRO resource name */,
+    });
+    spinner.succeed();
+    isProjectCreated = true;
+    const appMetadata: AndroidAppMetadata = {
+      appId: result.appId,
+      displayName: result.displayName,
+      packageName: result.packageName,
+      appPlatform: AppPlatform.ANDROID,
+    };
+    logPostAppCreationInformation(appMetadata);
+    if (shaCertificate) {
+      spinner = new OraWrapper(`Adding sha certificate to your app`);
+      spinner.start();
+      await api.request("POST", `/v1beta1/projects/-/androidApps/${appMetadata.appId}/sha`, {
+        auth: true,
+        origin: api.firebaseApiOrigin,
+        timeout: 15 * ONE_SECOND_MILLIS,
+        data: shaCertificate,
+      });
+      spinner.succeed();
+      appMetadata.shaCertificates = [shaCertificate];
+    }
+    return appMetadata;
+  } catch (err) {
+    spinner.fail();
+    logger.debug(err.message);
+    const message = !isProjectCreated
+      ? `Failed to create Android app for project ${projectId}`
+      : "Failed to add sha certificate for your Android app";
+    throw new FirebaseError(`${message}. See firebase-debug.log for more info.`, {
+      exit: 2,
+      original: err,
+    });
+  }
+}
+
+/**
+ * Send an API request to create a new Firebase Web app and poll the LRO to get the new app
+ * information.
+ * @return {Promise} this function returns a promise that resolves to the resource name of the
+ *      create Web app LRO, or rejects if an error is thrown
+ */
+export async function createWebApp(
+  projectId: string,
+  displayName: string
+): Promise<WebAppMetadata> {
+  const spinner = new OraWrapper(`Creating your ${clc.red("Web")} app`);
+  spinner.start();
+
+  try {
+    const response = await api.request("POST", `/v1beta1/projects/${projectId}/webApps`, {
+      auth: true,
+      origin: api.firebaseApiOrigin,
+      timeout: 15 * ONE_SECOND_MILLIS,
+      data: { displayName },
+    });
+    const result = await pollOperation<any>({
+      pollerName: "Create Web app Poller",
+      apiOrigin: api.firebaseApiOrigin,
+      apiVersion: "v1beta1",
+      operationResourceName: response.body.name /* LRO resource name */,
+    });
+    spinner.succeed();
+    const appMetadata: WebAppMetadata = {
+      appId: result.appId,
+      displayName: result.displayName,
+      appPlatform: AppPlatform.WEB,
+    };
+    logPostAppCreationInformation(appMetadata);
+    return appMetadata;
+  } catch (err) {
+    spinner.fail();
+    logger.debug(err.message);
+    throw new FirebaseError(
+      `Failed to create Web app for project ${projectId}. See firebase-debug.log for more info.`,
+      { exit: 2, original: err }
+    );
+  }
+}
+
+function logPostAppCreationInformation(appMetadata: AppMetadata): void {
+  logger.log(`🎉🎉🎉 Your Firebase ${appMetadata.appPlatform} App is ready! 🎉🎉🎉`);
+  logger.log("");
+  logger.log("App information:");
+  logger.log(`  - App ID: ${appMetadata.appId}`);
+  logger.log(`  - Display name: ${appMetadata.displayName}`);
+  if (appMetadata.appPlatform === AppPlatform.IOS) {
+    logger.log(`  - Bundle ID: ${(appMetadata as IosAppMetadata).bundleId}`);
+  } else if (appMetadata.appPlatform === AppPlatform.ANDROID) {
+    const androidAppMetadata = appMetadata as AndroidAppMetadata;
+    logger.log(`  - Package name: ${androidAppMetadata.packageName}`);
   }
 }
--- src/test/firebase-resource-manager.spec.ts
+++ src/test/firebase-resource-manager.spec.ts
@@ -3,9 +3,17 @@ import * as sinon from "sinon";
 
 import * as api from "../api";
 import {
+  AndroidAppMetadata,
+  AppPlatform,
+  createAndroidApp,
   createFirebaseProject,
+  createIosApp,
+  createWebApp,
+  IosAppMetadata,
   ParentResource,
   ParentResourceType,
+  ShaCertificate,
+  WebAppMetadata,
 } from "../firebase-resource-manager";
 import * as pollUtils from "../operation-poller";
 import { mockAuth } from "./helpers";
@@ -17,6 +25,16 @@ const PROJECT_NAME = "The Best Project";
 const PARENT_RESOURCE: ParentResource = { id: "1111111111111", type: ParentResourceType.FOLDER };
 const OPERATION_RESOURCE_NAME_1 = "operations/cp.11111111111111111";
 const OPERATION_RESOURCE_NAME_2 = "operations/cp.22222222222222222";
+const APP_ID = "appId";
+const IOS_APP_BUNDLE_ID = "bundleId";
+const IOS_APP_DISPLAY_NAME = "iOS app";
+const ANDROID_APP_PACKAGE_NAME = "com.google.packageName";
+const ANDROID_APP_DISPLAY_NAME = "Android app";
+const WEB_APP_DISPLAY_NAME = "Web app";
+const SHA_CERTIFICATE: ShaCertificate = {
+  certType: "SHA_1",
+  shaHash: "C18FA898916277D97FC5270B1BBF0068F247E1C3",
+};
 
 // TODO(caot): Removed when "Deferred Analytics" and "Deferred Location" are launched
 const TIME_ZONE = "America/Los_Angeles";
@@ -24,28 +42,28 @@ const REGION_CODE = "US";
 const LOCATION_ID = "us-central";
 
 describe("FirebaseResourceManager", () => {
+  let sandbox: sinon.SinonSandbox;
+  let mockOraWrapper: sinon.SinonMock;
+  let apiRequestStub: sinon.SinonStub;
+  let pollOperationStub: sinon.SinonStub;
+  let expectedCalledStubs: sinon.SinonStub[];
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    mockAuth(sandbox);
+    mockOraWrapper = sandbox.mock(OraWrapper.prototype);
+    apiRequestStub = sandbox.stub(api, "request");
+    pollOperationStub = sandbox.stub(pollUtils, "pollOperation");
+    expectedCalledStubs = [];
+  });
+
+  afterEach(() => {
+    mockOraWrapper.verify();
+    expectedCalledStubs.forEach((stub) => expect(stub.calledOnce).to.be.true);
+    sandbox.restore();
+  });
+
   describe("createFirebaseProject", () => {
-    let sandbox: sinon.SinonSandbox;
-    let mockOraWrapper: sinon.SinonMock;
-    let apiRequestStub: sinon.SinonStub;
-    let pollOperationStub: sinon.SinonStub;
-    let expectedCalledStubs: sinon.SinonStub[];
-
-    beforeEach(() => {
-      sandbox = sinon.createSandbox();
-      mockAuth(sandbox);
-      mockOraWrapper = sandbox.mock(OraWrapper.prototype);
-      apiRequestStub = sandbox.stub(api, "request");
-      pollOperationStub = sandbox.stub(pollUtils, "pollOperation");
-      expectedCalledStubs = [];
-    });
-
-    afterEach(() => {
-      mockOraWrapper.verify();
-      expectedCalledStubs.forEach((stub) => expect(stub.calledOnce).to.be.true);
-      sandbox.restore();
-    });
-
     it("should resolve with project data if it succeeds", async () => {
       expectedCalledStubs.push(
         _createCloudProjectApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_1 } }),
@@ -196,6 +214,345 @@ describe("FirebaseResourceManager", () => {
     function _pollAddFirebaseOperationStub(operationResourceName: string): sinon.SinonStub {
       return pollOperationStub.withArgs({
         pollerName: "Add Firebase Poller",
+        apiOrigin: api.firebaseApiOrigin,
+        apiVersion: "v1beta1",
+        operationResourceName,
+      });
+    }
+  });
+
+  describe("createIosApp", () => {
+    it("should resolve with app data if it succeeds", async () => {
+      expectedCalledStubs.push(
+        _createIosAppApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_1 } }),
+        _pollCreateIosAppOperationStub(OPERATION_RESOURCE_NAME_1).resolves({
+          appId: APP_ID,
+          displayName: IOS_APP_DISPLAY_NAME,
+          bundleId: IOS_APP_BUNDLE_ID,
+        })
+      );
+      mockOraWrapper.expects("start").exactly(1);
+      mockOraWrapper.expects("succeed").exactly(1);
+      mockOraWrapper.expects("fail").never();
+
+      const expectedAppMetadata: IosAppMetadata = {
+        appId: APP_ID,
+        displayName: IOS_APP_DISPLAY_NAME,
+        bundleId: IOS_APP_BUNDLE_ID,
+        appPlatform: AppPlatform.IOS,
+      };
+      expect(await createIosApp(PROJECT_ID, IOS_APP_DISPLAY_NAME, IOS_APP_BUNDLE_ID)).to.deep.equal(
+        expectedAppMetadata
+      );
+    });
+
+    it("should reject if app creation api call fails", async () => {
+      const expectedError = new Error("HTTP Error 404: Not Found");
+      expectedCalledStubs.push(_createIosAppApiStub().rejects(expectedError));
+      mockOraWrapper.expects("start").exactly(1);
+      mockOraWrapper.expects("succeed").never();
+      mockOraWrapper.expects("fail").exactly(1);
+
+      let err;
+      try {
+        await createIosApp(PROJECT_ID, IOS_APP_DISPLAY_NAME, IOS_APP_BUNDLE_ID);
+      } catch (e) {
+        err = e;
+      }
+      expect(err.message).to.equal(
+        `Failed to create iOS app for project ${PROJECT_ID}. See firebase-debug.log for more info.`
+      );
+      expect(err.original).to.equal(expectedError);
+      expect(apiRequestStub.callCount).to.equal(1);
+      expect(pollOperationStub.callCount).to.equal(0);
+    });
+
+    it("should reject if polling throws error", async () => {
+      const expectedError = new Error("Permission denied");
+      expectedCalledStubs.push(
+        _createIosAppApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_1 } }),
+        _pollCreateIosAppOperationStub(OPERATION_RESOURCE_NAME_1).rejects(expectedError)
+      );
+      mockOraWrapper.expects("start").exactly(1);
+      mockOraWrapper.expects("succeed").never();
+      mockOraWrapper.expects("fail").exactly(1);
+
+      let err;
+      try {
+        await createIosApp(PROJECT_ID, IOS_APP_DISPLAY_NAME, IOS_APP_BUNDLE_ID);
+      } catch (e) {
+        err = e;
+      }
+      expect(err.message).to.equal(
+        `Failed to create iOS app for project ${PROJECT_ID}. See firebase-debug.log for more info.`
+      );
+      expect(err.original).to.equal(expectedError);
+      expect(apiRequestStub.callCount).to.equal(1);
+      expect(pollOperationStub.callCount).to.equal(1);
+    });
+
+    function _createIosAppApiStub(): sinon.SinonStub {
+      return apiRequestStub.withArgs("POST", `/v1beta1/projects/${PROJECT_ID}/iosApps`, {
+        auth: true,
+        origin: api.firebaseApiOrigin,
+        timeout: 15000,
+        data: { displayName: IOS_APP_DISPLAY_NAME, bundleId: IOS_APP_BUNDLE_ID },
+      });
+    }
+
+    function _pollCreateIosAppOperationStub(operationResourceName: string): sinon.SinonStub {
+      return pollOperationStub.withArgs({
+        pollerName: "Create iOS app Poller",
+        apiOrigin: api.firebaseApiOrigin,
+        apiVersion: "v1beta1",
+        operationResourceName,
+      });
+    }
+  });
+
+  describe("createAndroidApp", () => {
+    it("should resolve with app data if it succeeds", async () => {
+      expectedCalledStubs.push(
+        _createAndroidAppApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_1 } }),
+        _pollCreateAndroidAppOperationStub(OPERATION_RESOURCE_NAME_1).resolves({
+          appId: APP_ID,
+          displayName: ANDROID_APP_DISPLAY_NAME,
+          packageName: ANDROID_APP_PACKAGE_NAME,
+        })
+      );
+      mockOraWrapper.expects("start").exactly(1);
+      mockOraWrapper.expects("succeed").exactly(1);
+      mockOraWrapper.expects("fail").never();
+
+      const expectedAppMetadata: AndroidAppMetadata = {
+        appId: APP_ID,
+        displayName: ANDROID_APP_DISPLAY_NAME,
+        packageName: ANDROID_APP_PACKAGE_NAME,
+        appPlatform: AppPlatform.ANDROID,
+      };
+      expect(
+        await createAndroidApp(PROJECT_ID, ANDROID_APP_DISPLAY_NAME, ANDROID_APP_PACKAGE_NAME)
+      ).to.deep.equal(expectedAppMetadata);
+    });
+
+    it("should resolve with app data with sha certificate", async () => {
+      expectedCalledStubs.push(
+        _createAndroidAppApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_1 } }),
+        _pollCreateAndroidAppOperationStub(OPERATION_RESOURCE_NAME_1).resolves({
+          appId: APP_ID,
+          displayName: ANDROID_APP_DISPLAY_NAME,
+          packageName: ANDROID_APP_PACKAGE_NAME,
+        }),
+        _createShaCertificateApiStub().resolves(SHA_CERTIFICATE)
+      );
+      mockOraWrapper.expects("start").exactly(2);
+      mockOraWrapper.expects("succeed").exactly(2);
+      mockOraWrapper.expects("fail").never();
+
+      const expectedAppMetadata: AndroidAppMetadata = {
+        appId: APP_ID,
+        displayName: ANDROID_APP_DISPLAY_NAME,
+        packageName: ANDROID_APP_PACKAGE_NAME,
+        shaCertificates: [SHA_CERTIFICATE],
+        appPlatform: AppPlatform.ANDROID,
+      };
+      expect(
+        await createAndroidApp(
+          PROJECT_ID,
+          ANDROID_APP_DISPLAY_NAME,
+          ANDROID_APP_PACKAGE_NAME,
+          SHA_CERTIFICATE
+        )
+      ).to.deep.equal(expectedAppMetadata);
+    });
+
+    it("should reject if app creation api call fails", async () => {
+      const expectedError = new Error("HTTP Error 404: Not Found");
+      expectedCalledStubs.push(_createAndroidAppApiStub().rejects(expectedError));
+      mockOraWrapper.expects("start").exactly(1);
+      mockOraWrapper.expects("succeed").never();
+      mockOraWrapper.expects("fail").exactly(1);
+
+      let err;
+      try {
+        await createAndroidApp(PROJECT_ID, ANDROID_APP_DISPLAY_NAME, ANDROID_APP_PACKAGE_NAME);
+      } catch (e) {
+        err = e;
+      }
+      expect(err.message).to.equal(
+        `Failed to create Android app for project ${PROJECT_ID}. See firebase-debug.log for more info.`
+      );
+      expect(err.original).to.equal(expectedError);
+      expect(apiRequestStub.callCount).to.equal(1);
+      expect(pollOperationStub.callCount).to.equal(0);
+    });
+
+    it("should reject if polling throws error", async () => {
+      const expectedError = new Error("Permission denied");
+      expectedCalledStubs.push(
+        _createAndroidAppApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_1 } }),
+        _pollCreateAndroidAppOperationStub(OPERATION_RESOURCE_NAME_1).rejects(expectedError)
+      );
+      mockOraWrapper.expects("start").exactly(1);
+      mockOraWrapper.expects("succeed").never();
+      mockOraWrapper.expects("fail").exactly(1);
+
+      let err;
+      try {
+        await createAndroidApp(PROJECT_ID, ANDROID_APP_DISPLAY_NAME, ANDROID_APP_PACKAGE_NAME);
+      } catch (e) {
+        err = e;
+      }
+      expect(err.message).to.equal(
+        `Failed to create Android app for project ${PROJECT_ID}. See firebase-debug.log for more info.`
+      );
+      expect(err.original).to.equal(expectedError);
+      expect(apiRequestStub.callCount).to.equal(1);
+      expect(pollOperationStub.callCount).to.equal(1);
+    });
+
+    it("should reject if creating sha certificate throws error", async () => {
+      const expectedError = new Error("Permission denied");
+      expectedCalledStubs.push(
+        _createAndroidAppApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_1 } }),
+        _pollCreateAndroidAppOperationStub(OPERATION_RESOURCE_NAME_1).resolves({
+          appId: APP_ID,
+          displayName: ANDROID_APP_DISPLAY_NAME,
+          packageName: ANDROID_APP_PACKAGE_NAME,
+        }),
+        _createShaCertificateApiStub().rejects(expectedError)
+      );
+      mockOraWrapper.expects("start").exactly(2);
+      mockOraWrapper.expects("succeed").exactly(1);
+      mockOraWrapper.expects("fail").exactly(1);
+
+      let err;
+      try {
+        await createAndroidApp(
+          PROJECT_ID,
+          ANDROID_APP_DISPLAY_NAME,
+          ANDROID_APP_PACKAGE_NAME,
+          SHA_CERTIFICATE
+        );
+      } catch (e) {
+        err = e;
+      }
+      expect(err.message).to.equal(
+        "Failed to add sha certificate for your Android app. See firebase-debug.log for more info."
+      );
+      expect(err.original).to.equal(expectedError);
+      expect(apiRequestStub.callCount).to.equal(2);
+      expect(pollOperationStub.callCount).to.equal(1);
+    });
+
+    function _createAndroidAppApiStub(): sinon.SinonStub {
+      return apiRequestStub.withArgs("POST", `/v1beta1/projects/${PROJECT_ID}/androidApps`, {
+        auth: true,
+        origin: api.firebaseApiOrigin,
+        timeout: 15000,
+        data: { displayName: ANDROID_APP_DISPLAY_NAME, packageName: ANDROID_APP_PACKAGE_NAME },
+      });
+    }
+
+    function _pollCreateAndroidAppOperationStub(operationResourceName: string): sinon.SinonStub {
+      return pollOperationStub.withArgs({
+        pollerName: "Create Android app Poller",
+        apiOrigin: api.firebaseApiOrigin,
+        apiVersion: "v1beta1",
+        operationResourceName,
+      });
+    }
+
+    function _createShaCertificateApiStub(): sinon.SinonStub {
+      return apiRequestStub.withArgs("POST", `/v1beta1/projects/-/androidApps/${APP_ID}/sha`, {
+        auth: true,
+        origin: api.firebaseApiOrigin,
+        timeout: 15000,
+        data: SHA_CERTIFICATE,
+      });
+    }
+  });
+
+  describe("createWebApp", () => {
+    it("should resolve with app data if it succeeds", async () => {
+      expectedCalledStubs.push(
+        _createWebAppApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_1 } }),
+        _pollCreateWebAppOperationStub(OPERATION_RESOURCE_NAME_1).resolves({
+          appId: APP_ID,
+          displayName: WEB_APP_DISPLAY_NAME,
+        })
+      );
+      mockOraWrapper.expects("start").exactly(1);
+      mockOraWrapper.expects("succeed").exactly(1);
+      mockOraWrapper.expects("fail").never();
+
+      const expectedAppMetadata: WebAppMetadata = {
+        appId: APP_ID,
+        displayName: WEB_APP_DISPLAY_NAME,
+        appPlatform: AppPlatform.WEB,
+      };
+      expect(await createWebApp(PROJECT_ID, WEB_APP_DISPLAY_NAME)).to.deep.equal(
+        expectedAppMetadata
+      );
+    });
+
+    it("should reject if app creation api call fails", async () => {
+      const expectedError = new Error("HTTP Error 404: Not Found");
+      expectedCalledStubs.push(_createWebAppApiStub().rejects(expectedError));
+      mockOraWrapper.expects("start").exactly(1);
+      mockOraWrapper.expects("succeed").never();
+      mockOraWrapper.expects("fail").exactly(1);
+
+      let err;
+      try {
+        await createWebApp(PROJECT_ID, WEB_APP_DISPLAY_NAME);
+      } catch (e) {
+        err = e;
+      }
+      expect(err.message).to.equal(
+        `Failed to create Web app for project ${PROJECT_ID}. See firebase-debug.log for more info.`
+      );
+      expect(err.original).to.equal(expectedError);
+      expect(apiRequestStub.callCount).to.equal(1);
+      expect(pollOperationStub.callCount).to.equal(0);
+    });
+
+    it("should reject if polling throws error", async () => {
+      const expectedError = new Error("Permission denied");
+      expectedCalledStubs.push(
+        _createWebAppApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_1 } }),
+        _pollCreateWebAppOperationStub(OPERATION_RESOURCE_NAME_1).rejects(expectedError)
+      );
+      mockOraWrapper.expects("start").exactly(1);
+      mockOraWrapper.expects("succeed").never();
+      mockOraWrapper.expects("fail").exactly(1);
+
+      let err;
+      try {
+        await createWebApp(PROJECT_ID, WEB_APP_DISPLAY_NAME);
+      } catch (e) {
+        err = e;
+      }
+      expect(err.message).to.equal(
+        `Failed to create Web app for project ${PROJECT_ID}. See firebase-debug.log for more info.`
+      );
+      expect(err.original).to.equal(expectedError);
+      expect(apiRequestStub.callCount).to.equal(1);
+      expect(pollOperationStub.callCount).to.equal(1);
+    });
+
+    function _createWebAppApiStub(): sinon.SinonStub {
+      return apiRequestStub.withArgs("POST", `/v1beta1/projects/${PROJECT_ID}/webApps`, {
+        auth: true,
+        origin: api.firebaseApiOrigin,
+        timeout: 15000,
+        data: { displayName: WEB_APP_DISPLAY_NAME },
+      });
+    }
+
+    function _pollCreateWebAppOperationStub(operationResourceName: string): sinon.SinonStub {
+      return pollOperationStub.withArgs({
+        pollerName: "Create Web app Poller",
         apiOrigin: api.firebaseApiOrigin,
         apiVersion: "v1beta1",
         operationResourceName,
