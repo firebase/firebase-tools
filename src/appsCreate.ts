275322890d7344fcaf91b977d01de0a83f3462d5
Tri Cao <caot@google.com>
2019-06-28 15:15:14 -0400

    Implement API calls for apps:create command

--- src/appsCreate.ts
+++ src/appsCreate.ts
@@ -8,16 +8,6 @@ import { OraWrapper } from "./oraWrapper";
 
 const ONE_SECOND_MILLIS = 1000;
 
-export enum ParentResourceType {
-  ORGANIZATION = "organization",
-  FOLDER = "folder",
-}
-
-export interface ParentResource {
-  id: string;
-  type: ParentResourceType;
-}
-
 export enum AppPlatform {
   IOS = "iOS",
   ANDROID = "Android",
@@ -46,108 +36,9 @@ export interface WebAppMetadata extends AppMetadata {
 }
 
 export interface ShaCertificate {
+  name?: string;
   certType: "SHA_1" | "SHA_256";
   shaHash: string;
-}
-
-export async function createFirebaseProject(
-  projectId: string,
-  projectDisplayName: string,
-  parentResource?: ParentResource
-): Promise<{ projectId: string }> {
-  await createCloudProject(projectId, projectDisplayName, parentResource);
-  const projectInfo = await addFirebaseToCloudProject(projectId);
-
-  logger.info("");
-  logger.info("🎉🎉🎉 Your Firebase project is ready! 🎉🎉🎉");
-  logger.info("");
-  logger.info("Project information:");
-  logger.info(`   - Project ID: ${clc.bold(projectInfo.projectId)}`);
-  logger.info(`   - Project Name: ${clc.bold(projectInfo.displayName)}`);
-  logger.info("");
-  logger.info("Firebase console is available at");
-  logger.info(`https://console.firebase.google.com/project/${clc.bold(projectId)}/overview`);
-  return { projectId };
-}
-
-/**
- * Send an API request to create a new Google Cloud Platform project and poll the LRO to get the
- * new project information.
- * @return {Promise} this function returns a promise that resolves to the new cloud project
- *     information
- */
-async function createCloudProject(
-  projectId: string,
-  projectDisplayName: string,
-  parentResource?: ParentResource
-): Promise<any> {
-  const spinner = new OraWrapper("Creating Google Cloud Platform project");
-  spinner.start();
-
-  try {
-    const response = await api.request("POST", "/v1/projects", {
-      auth: true,
-      origin: api.resourceManagerOrigin,
-      timeout: 15 * ONE_SECOND_MILLIS,
-      data: { projectId, name: projectDisplayName, parent: parentResource },
-    });
-
-    const projectInfo = await pollOperation<any>({
-      pollerName: "Project Creation Poller",
-      apiOrigin: api.resourceManagerOrigin,
-      apiVersion: "v1",
-      operationResourceName: response.body.name /* LRO resource name */,
-    });
-    spinner.succeed();
-    return projectInfo;
-  } catch (err) {
-    spinner.fail();
-    logger.debug(err.message);
-    throw new FirebaseError(
-      "Failed to create Google Cloud project. See firebase-debug.log for more info.",
-      { exit: 2, original: err }
-    );
-  }
-}
-
-/**
- * Send an API request to add Firebase to the Google Cloud Platform project and poll the LRO
- * to get the new Firebase project information.
- * @return {Promise} this function returns a promise that resolves to the new firebase project
- *    information
- */
-async function addFirebaseToCloudProject(projectId: string): Promise<any> {
-  const spinner = new OraWrapper("Adding Firebase to Google Cloud project");
-  spinner.start();
-
-  // TODO(caot): Removed when "Deferred Analytics" and "Deferred Location" are launched
-  const timeZone = "America/Los_Angeles";
-  const regionCode = "US";
-  const locationId = "us-central";
-
-  try {
-    const response = await api.request("POST", `/v1beta1/projects/${projectId}:addFirebase`, {
-      auth: true,
-      origin: api.firebaseApiOrigin,
-      timeout: 15 * ONE_SECOND_MILLIS,
-      data: { timeZone, regionCode, locationId },
-    });
-    const projectInfo = await pollOperation<any>({
-      pollerName: "Add Firebase Poller",
-      apiOrigin: api.firebaseApiOrigin,
-      apiVersion: "v1beta1",
-      operationResourceName: response.body.name /* LRO resource name */,
-    });
-    spinner.succeed();
-    return projectInfo;
-  } catch (err) {
-    spinner.fail();
-    logger.debug(err.message);
-    throw new FirebaseError(
-      "Failed to add Firebase to Google Cloud Platform project. See firebase-debug.log for more info.",
-      { exit: 2, original: err }
-    );
-  }
 }
 
 /**
@@ -160,7 +51,7 @@ export async function createIosApp(
   displayName: string,
   bundleId: string
 ): Promise<IosAppMetadata> {
-  const spinner = new OraWrapper(`Creating your ${clc.red("iOS")} app`);
+  const spinner = new OraWrapper("Creating your iOS app");
   spinner.start();
 
   try {
@@ -199,7 +90,7 @@ export async function createIosApp(
  * Send an API request to create a new Firebase Android app and poll the LRO to get the new app
  * information. Optionally add a SHA certificate to the app if specified.
  * @return {Promise} this function returns a promise that resolves to the new Android app
- * information
+ *     information
  */
 export async function createAndroidApp(
   projectId: string,
@@ -207,8 +98,8 @@ export async function createAndroidApp(
   packageName: string,
   shaCertificate?: ShaCertificate
 ): Promise<AndroidAppMetadata> {
-  let spinner = new OraWrapper(`Creating your ${clc.red("Android")} app`);
-  let isProjectCreated = false;
+  let spinner = new OraWrapper("Creating your Android app");
+  let isAppCreated = false;
   spinner.start();
 
   try {
@@ -225,7 +116,7 @@ export async function createAndroidApp(
       operationResourceName: response.body.name /* LRO resource name */,
     });
     spinner.succeed();
-    isProjectCreated = true;
+    isAppCreated = true;
     const appMetadata: AndroidAppMetadata = {
       appId: result.appId,
       displayName: result.displayName,
@@ -233,23 +124,29 @@ export async function createAndroidApp(
       appPlatform: AppPlatform.ANDROID,
     };
     logPostAppCreationInformation(appMetadata);
+
     if (shaCertificate) {
       spinner = new OraWrapper(`Adding sha certificate to your app`);
       spinner.start();
-      await api.request("POST", `/v1beta1/projects/-/androidApps/${appMetadata.appId}/sha`, {
-        auth: true,
-        origin: api.firebaseApiOrigin,
-        timeout: 15 * ONE_SECOND_MILLIS,
-        data: shaCertificate,
-      });
+      const shaResponse = await api.request(
+        "POST",
+        `/v1beta1/projects/-/androidApps/${appMetadata.appId}/sha`,
+        {
+          auth: true,
+          origin: api.firebaseApiOrigin,
+          timeout: 15 * ONE_SECOND_MILLIS,
+          data: shaCertificate,
+        }
+      );
       spinner.succeed();
-      appMetadata.shaCertificates = [shaCertificate];
+      appMetadata.shaCertificates = [shaResponse.body];
     }
+
     return appMetadata;
   } catch (err) {
     spinner.fail();
     logger.debug(err.message);
-    const message = !isProjectCreated
+    const message = !isAppCreated
       ? `Failed to create Android app for project ${projectId}`
       : "Failed to add sha certificate for your Android app";
     throw new FirebaseError(`${message}. See firebase-debug.log for more info.`, {
@@ -263,13 +160,13 @@ export async function createAndroidApp(
  * Send an API request to create a new Firebase Web app and poll the LRO to get the new app
  * information.
  * @return {Promise} this function returns a promise that resolves to the resource name of the
- *      create Web app LRO, or rejects if an error is thrown
+ *     create Web app LRO, or rejects if an error is thrown
  */
 export async function createWebApp(
   projectId: string,
   displayName: string
 ): Promise<WebAppMetadata> {
-  const spinner = new OraWrapper(`Creating your ${clc.red("Web")} app`);
+  const spinner = new OraWrapper("Creating your Web app");
   spinner.start();
 
   try {
--- src/test/appsCreate.spec.ts
+++ src/test/appsCreate.spec.ts
@@ -6,40 +6,30 @@ import {
   AndroidAppMetadata,
   AppPlatform,
   createAndroidApp,
-  createFirebaseProject,
   createIosApp,
   createWebApp,
   IosAppMetadata,
-  ParentResource,
-  ParentResourceType,
   ShaCertificate,
   WebAppMetadata,
-} from "../firebase-resource-manager";
+} from "../appsCreate";
 import * as pollUtils from "../operation-poller";
 import { mockAuth } from "./helpers";
 import { OraWrapper } from "../oraWrapper";
 
 const PROJECT_ID = "the-best-firebase-project";
-const PROJECT_NUMBER = "1234567890";
-const PROJECT_NAME = "The Best Project";
-const PARENT_RESOURCE: ParentResource = { id: "1111111111111", type: ParentResourceType.FOLDER };
 const OPERATION_RESOURCE_NAME_1 = "operations/cp.11111111111111111";
-const OPERATION_RESOURCE_NAME_2 = "operations/cp.22222222222222222";
 const APP_ID = "appId";
 const IOS_APP_BUNDLE_ID = "bundleId";
 const IOS_APP_DISPLAY_NAME = "iOS app";
 const ANDROID_APP_PACKAGE_NAME = "com.google.packageName";
 const ANDROID_APP_DISPLAY_NAME = "Android app";
 const WEB_APP_DISPLAY_NAME = "Web app";
+const SHA_HASH = "C18FA898916277D97FC5270B1BBF0068F247E1C3";
 const SHA_CERTIFICATE: ShaCertificate = {
+  name: `projects/${PROJECT_ID}/androidApps/${APP_ID}/sha/${SHA_HASH}`,
   certType: "SHA_1",
-  shaHash: "C18FA898916277D97FC5270B1BBF0068F247E1C3",
+  shaHash: SHA_HASH,
 };
-
-// TODO(caot): Removed when "Deferred Analytics" and "Deferred Location" are launched
-const TIME_ZONE = "America/Los_Angeles";
-const REGION_CODE = "US";
-const LOCATION_ID = "us-central";
 
 describe("FirebaseResourceManager", () => {
   let sandbox: sinon.SinonSandbox;
@@ -61,164 +51,6 @@ describe("FirebaseResourceManager", () => {
     mockOraWrapper.verify();
     expectedCalledStubs.forEach((stub) => expect(stub.calledOnce).to.be.true);
     sandbox.restore();
-  });
-
-  describe("createFirebaseProject", () => {
-    it("should resolve with project data if it succeeds", async () => {
-      expectedCalledStubs.push(
-        _createCloudProjectApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_1 } }),
-        _pollCreateCloudProjectOperationStub(OPERATION_RESOURCE_NAME_1).resolves({
-          response: { projectNumber: PROJECT_NUMBER },
-        }),
-        _addFirebaseApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_2 } }),
-        _pollAddFirebaseOperationStub(OPERATION_RESOURCE_NAME_2).resolves({
-          response: { projectId: PROJECT_ID, displayName: PROJECT_NAME },
-        })
-      );
-      mockOraWrapper.expects("start").exactly(2);
-      mockOraWrapper.expects("succeed").exactly(2);
-      mockOraWrapper.expects("fail").never();
-
-      expect(await createFirebaseProject(PROJECT_ID, PROJECT_NAME, PARENT_RESOURCE)).to.deep.equal({
-        projectId: PROJECT_ID,
-      });
-    });
-
-    it("should reject if Cloud project creation fails", async () => {
-      const expectedError = new Error("HTTP Error 404: Not Found");
-      expectedCalledStubs.push(_createCloudProjectApiStub().rejects(expectedError));
-      mockOraWrapper.expects("start").exactly(1);
-      mockOraWrapper.expects("succeed").never();
-      mockOraWrapper.expects("fail").exactly(1);
-
-      let err;
-      try {
-        await createFirebaseProject(PROJECT_ID, PROJECT_NAME, PARENT_RESOURCE);
-      } catch (e) {
-        err = e;
-      }
-      expect(err.message).to.equal(
-        "Failed to create Google Cloud project. See firebase-debug.log for more info."
-      );
-      expect(err.original).to.equal(expectedError);
-      expect(apiRequestStub.callCount).to.equal(1);
-      expect(pollOperationStub.callCount).to.equal(0);
-    });
-
-    it("should reject if Cloud project creation polling throws error", async () => {
-      const expectedError = new Error("Entity already exists");
-      expectedCalledStubs.push(
-        _createCloudProjectApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_1 } }),
-        _pollCreateCloudProjectOperationStub(OPERATION_RESOURCE_NAME_1).rejects(expectedError)
-      );
-      mockOraWrapper.expects("start").exactly(1);
-      mockOraWrapper.expects("succeed").never();
-      mockOraWrapper.expects("fail").exactly(1);
-
-      let err;
-      try {
-        await createFirebaseProject(PROJECT_ID, PROJECT_NAME, PARENT_RESOURCE);
-      } catch (e) {
-        err = e;
-      }
-      expect(err.message).to.equal(
-        "Failed to create Google Cloud project. See firebase-debug.log for more info."
-      );
-      expect(err.original).to.equal(expectedError);
-      expect(apiRequestStub.callCount).to.equal(1);
-      expect(pollOperationStub.callCount).to.equal(1);
-    });
-
-    it("should reject if add Firebase api call fails", async () => {
-      const expectedError = new Error("HTTP Error 404: Not Found");
-      expectedCalledStubs.push(
-        _createCloudProjectApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_1 } }),
-        _pollCreateCloudProjectOperationStub(OPERATION_RESOURCE_NAME_1).resolves({
-          response: { projectNumber: PROJECT_NUMBER },
-        }),
-        _addFirebaseApiStub().rejects(expectedError)
-      );
-      mockOraWrapper.expects("start").exactly(2);
-      mockOraWrapper.expects("succeed").exactly(1);
-      mockOraWrapper.expects("fail").exactly(1);
-
-      let err;
-      try {
-        await createFirebaseProject(PROJECT_ID, PROJECT_NAME, PARENT_RESOURCE);
-      } catch (e) {
-        err = e;
-      }
-      expect(err.message).to.equal(
-        "Failed to add Firebase to Google Cloud Platform project. See firebase-debug.log for more info."
-      );
-      expect(err.original).to.equal(expectedError);
-      expect(apiRequestStub.callCount).to.equal(2);
-      expect(pollOperationStub.callCount).to.equal(1);
-    });
-
-    it("should reject if polling add Firebase operation returns error response", async () => {
-      const expectedError = new Error("Permission denied");
-      expectedCalledStubs.push(
-        _createCloudProjectApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_1 } }),
-        _pollCreateCloudProjectOperationStub(OPERATION_RESOURCE_NAME_1).resolves({
-          response: { projectNumber: PROJECT_NUMBER },
-        }),
-        _addFirebaseApiStub().resolves({ body: { name: OPERATION_RESOURCE_NAME_2 } }),
-        _pollAddFirebaseOperationStub(OPERATION_RESOURCE_NAME_2).rejects(expectedError)
-      );
-      mockOraWrapper.expects("start").exactly(2);
-      mockOraWrapper.expects("succeed").exactly(1);
-      mockOraWrapper.expects("fail").exactly(1);
-
-      let err;
-      try {
-        await createFirebaseProject(PROJECT_ID, PROJECT_NAME, PARENT_RESOURCE);
-      } catch (e) {
-        err = e;
-      }
-      expect(err.message).to.equal(
-        "Failed to add Firebase to Google Cloud Platform project. See firebase-debug.log for more info."
-      );
-      expect(err.original).to.equal(expectedError);
-      expect(apiRequestStub.callCount).to.equal(2);
-      expect(pollOperationStub.callCount).to.equal(2);
-    });
-
-    function _createCloudProjectApiStub(): sinon.SinonStub {
-      return apiRequestStub.withArgs("POST", "/v1/projects", {
-        auth: true,
-        origin: api.resourceManagerOrigin,
-        timeout: 15000,
-        data: { projectId: PROJECT_ID, name: PROJECT_NAME, parent: PARENT_RESOURCE },
-      });
-    }
-
-    function _addFirebaseApiStub(): sinon.SinonStub {
-      return apiRequestStub.withArgs("POST", `/v1beta1/projects/${PROJECT_ID}:addFirebase`, {
-        auth: true,
-        origin: api.firebaseApiOrigin,
-        timeout: 15000,
-        data: { timeZone: TIME_ZONE, regionCode: REGION_CODE, locationId: LOCATION_ID },
-      });
-    }
-
-    function _pollCreateCloudProjectOperationStub(operationResourceName: string): sinon.SinonStub {
-      return pollOperationStub.withArgs({
-        pollerName: "Project Creation Poller",
-        apiOrigin: api.resourceManagerOrigin,
-        apiVersion: "v1",
-        operationResourceName,
-      });
-    }
-
-    function _pollAddFirebaseOperationStub(operationResourceName: string): sinon.SinonStub {
-      return pollOperationStub.withArgs({
-        pollerName: "Add Firebase Poller",
-        apiOrigin: api.firebaseApiOrigin,
-        apiVersion: "v1beta1",
-        operationResourceName,
-      });
-    }
   });
 
   describe("createIosApp", () => {
@@ -343,7 +175,7 @@ describe("FirebaseResourceManager", () => {
           displayName: ANDROID_APP_DISPLAY_NAME,
           packageName: ANDROID_APP_PACKAGE_NAME,
         }),
-        _createShaCertificateApiStub().resolves(SHA_CERTIFICATE)
+        _createShaCertificateApiStub().resolves({ body: SHA_CERTIFICATE })
       );
       mockOraWrapper.expects("start").exactly(2);
       mockOraWrapper.expects("succeed").exactly(2);
