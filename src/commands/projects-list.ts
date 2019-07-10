e1a3fc911d831ebc6c3ee9bd5a447b017b5136bc
Tri Cao <caot@google.com>
2019-07-10 10:56:32 -0400

    Addressing comments

--- src/commands/projects-list.ts
+++ src/commands/projects-list.ts
@@ -12,24 +12,25 @@ import * as logger from "../logger";
 const NOT_SPECIFIED = clc.yellow("[Not specified]");
 
 function logProjectsList(projects: FirebaseProjectMetadata[], currentProjectId: string): void {
-  if (projects.length > 0) {
-    const tableHead = ["Project Display Name", "Project ID", "Resource Location ID"];
-    const table = new Table({ head: tableHead, style: { head: ["green"] } });
-    projects.forEach(({ projectId, displayName, resources }) => {
-      if (projectId === currentProjectId) {
-        projectId = clc.cyan.bold(`${projectId} (current)`);
-      }
-      table.push([
-        displayName || NOT_SPECIFIED,
-        projectId,
-        (resources && resources.locationId) || NOT_SPECIFIED,
-      ]);
-    });
-
-    logger.info(table.toString());
-  } else {
+  if (projects.length === 0) {
     logger.info(clc.bold("No projects found."));
+    return;
   }
+
+  const tableHead = ["Project Display Name", "Project ID", "Resource Location ID"];
+  const table = new Table({ head: tableHead, style: { head: ["green"] } });
+  projects.forEach(({ projectId, displayName, resources }) => {
+    if (projectId === currentProjectId) {
+      projectId = clc.cyan.bold(`${projectId} (current)`);
+    }
+    table.push([
+      displayName || NOT_SPECIFIED,
+      projectId,
+      (resources && resources.locationId) || NOT_SPECIFIED,
+    ]);
+  });
+
+  logger.info(table.toString());
 }
 
 module.exports = new Command("projects:list")
@@ -38,14 +39,17 @@ module.exports = new Command("projects:list")
   .action(
     async (options: any): Promise<FirebaseProjectMetadata[]> => {
       const spinner = ora("Preparing the list of your Firebase projects").start();
+      let projects;
+
       try {
-        const projects = await listFirebaseProjects();
-        spinner.succeed();
-        logProjectsList(projects, options.project);
-        return projects;
+        projects = await listFirebaseProjects();
       } catch (err) {
         spinner.fail();
         throw err;
       }
+
+      spinner.succeed();
+      logProjectsList(projects, options.project);
+      return projects;
     }
   );
