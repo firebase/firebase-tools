7cdc82f3919073ecf07bdd1e26c8ede8b5ccd3d0
Tri Cao <caot@google.com>
2019-07-10 16:20:39 -0400

    Declare project and app metadata types (#1470) and update dependencies

---
+++ src/management/apps.ts
@@ -0,0 +1,34 @@
+export interface AppMetadata {
+  name: string /* The fully qualified resource name of the Firebase App */;
+  projectId: string;
+  appId: string;
+  platform: AppPlatform;
+  displayName?: string;
+}
+
+export interface IosAppMetadata extends AppMetadata {
+  bundleId: string;
+  appStoreId?: string;
+  platform: AppPlatform.IOS;
+}
+
+export interface AndroidAppMetadata extends AppMetadata {
+  packageName: string;
+  platform: AppPlatform.ANDROID;
+}
+
+export interface WebAppMetadata extends AppMetadata {
+  displayName: string;
+  appUrls?: string[];
+  platform: AppPlatform.WEB;
+}
+
+export enum AppPlatform {
+  PLATFORM_UNSPECIFIED = "PLATFORM_UNSPECIFIED",
+  IOS = "IOS",
+  ANDROID = "ANDROID",
+  WEB = "WEB",
+  ANY = "ANY",
+}
+
+// TODO(caot): Add API methods related to app management into this file
