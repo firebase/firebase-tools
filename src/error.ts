86bbdaf66f7ed015a66759e063d16e6687cc99db
Natan Sągol <m@merlinnot.com>
2019-07-17 22:30:59 +0200

    Convert FirebaseError to TypeScript

---
+++ src/error.ts
@@ -0,0 +1,47 @@
+interface FirebaseErrorOptions {
+  children?: unknown[];
+  context?: unknown;
+  exit?: number;
+  original?: Error;
+  status?: number;
+}
+
+const DEFAULT_CHILDREN: NonNullable<FirebaseErrorOptions["children"]> = [];
+const DEFAULT_EXIT: NonNullable<FirebaseErrorOptions["exit"]> = 1;
+const DEFAULT_STATUS: NonNullable<FirebaseErrorOptions["status"]> = 500;
+
+export class FirebaseError extends Error implements FirebaseErrorOptions {
+  readonly children: unknown[];
+
+  readonly context: unknown | undefined;
+
+  readonly exit: number;
+
+  readonly message: string;
+
+  readonly name = "FirebaseError";
+
+  readonly original: Error | undefined;
+
+  readonly status: number;
+
+  constructor(
+    message: string,
+    {
+      children = DEFAULT_CHILDREN,
+      context,
+      exit = DEFAULT_EXIT,
+      original,
+      status = DEFAULT_STATUS,
+    }: FirebaseErrorOptions = {}
+  ) {
+    super();
+
+    this.message = message;
+    this.children = children;
+    this.context = context;
+    this.status = status;
+    this.exit = exit;
+    this.original = original;
+  }
+}
