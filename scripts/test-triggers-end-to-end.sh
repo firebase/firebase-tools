fafe3f4e7d896c68754c73b6490ad36b1146a63d
Sam Stern <samstern@google.com>
2019-07-22 13:00:48 -0700

    Fix tests

--- scripts/test-triggers-end-to-end.sh
+++ scripts/test-triggers-end-to-end.sh
@@ -10,9 +10,6 @@ then
   chmod ug+x $FIREBASE_CLI
 fi;
 
-$FIREBASE_CLI setup:emulators:firestore
-$FIREBASE_CLI setup:emulators:database
-
 cd ./scripts/triggers-end-to-end-tests
 
 npm install && npm test
