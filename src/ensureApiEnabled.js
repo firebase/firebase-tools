038c7c382e75b1b1702d604cf7f239323d7c962e
Joe Hanley <joehanley@google.com>
2019-07-25 12:20:34 -0700

    fixes prettier

--- src/ensureApiEnabled.js
+++ src/ensureApiEnabled.js
@@ -10,13 +10,13 @@ var POLL_INTERVAL = 10000; // 10 seconds
 var POLLS_BEFORE_RETRY = 12; // Retry enabling the API after 2 minutes
 var _enableApiWithRetries;
 
-var _checkEnabled = function (projectId, apiName, prefix, silent) {
+var _checkEnabled = function(projectId, apiName, prefix, silent) {
   return api
     .request("GET", `/v1/projects/${projectId}/services/${apiName}`, {
       auth: true,
       origin: api.serviceUsageOrigin,
     })
-    .then(function (response) {
+    .then(function(response) {
       var isEnabled = _.get(response.body, "state") === "ENABLED";
       if (isEnabled && !silent) {
         utils.logSuccess(clc.bold.green(prefix + ":") + " all necessary APIs are enabled");
@@ -25,25 +25,25 @@ var _checkEnabled = function (projectId, apiName, prefix, silent) {
     });
 };
 
-var _enableApi = function (projectId, apiName) {
+var _enableApi = function(projectId, apiName) {
   return api.request("POST", `/v1/projects/${projectId}/services/${apiName}:enable`, {
     auth: true,
     origin: api.serviceUsageOrigin,
   });
 };
 
-var _pollCheckEnabled = function (projectId, apiName, prefix, enablementRetries, pollRetries) {
+var _pollCheckEnabled = function(projectId, apiName, prefix, enablementRetries, pollRetries) {
   pollRetries = pollRetries || 0;
   if (pollRetries > POLLS_BEFORE_RETRY) {
     return _enableApiWithRetries(projectId, apiName, prefix, enablementRetries + 1);
   }
 
-  return new Promise(function (resolve) {
-    setTimeout(function () {
+  return new Promise(function(resolve) {
+    setTimeout(function() {
       resolve();
     }, POLL_INTERVAL);
-  }).then(function () {
-    return _checkEnabled(projectId, apiName, prefix).then(function (isEnabled) {
+  }).then(function() {
+    return _checkEnabled(projectId, apiName, prefix).then(function(isEnabled) {
       if (isEnabled) {
         return true;
       }
@@ -53,21 +53,21 @@ var _pollCheckEnabled = function (projectId, apiName, prefix, enablementRetries,
   });
 };
 
-_enableApiWithRetries = function (projectId, apiName, prefix, enablementRetries) {
+_enableApiWithRetries = function(projectId, apiName, prefix, enablementRetries) {
   enablementRetries = enablementRetries || 0;
   if (enablementRetries > 1) {
     return utils.reject("Timed out waiting for APIs to enable. Please try again in a few minutes.");
   }
-  return _enableApi(projectId, apiName, prefix, enablementRetries).then(function () {
+  return _enableApi(projectId, apiName, prefix, enablementRetries).then(function() {
     return _pollCheckEnabled(projectId, apiName, prefix, enablementRetries);
   });
 };
 
-var _ensureApi = function (projectId, apiName, prefix, silent) {
+var _ensureApi = function(projectId, apiName, prefix, silent) {
   if (!silent) {
     utils.logBullet(clc.bold.cyan(prefix + ":") + " ensuring necessary APIs are enabled...");
   }
-  return _checkEnabled(projectId, apiName, prefix, silent).then(function (isEnabled) {
+  return _checkEnabled(projectId, apiName, prefix, silent).then(function(isEnabled) {
     if (isEnabled) {
       return true;
     }
---
+++ src/ensureApiEnabled.ts
@@ -0,0 +1,103 @@
+import * as _ from "lodash";
+
+import * as api from "./api";
+import * as utils from "./utils";
+
+const POLL_INTERVAL = 10000; // 10 seconds
+const POLLS_BEFORE_RETRY = 12; // Retry enabling the API after 2 minutes
+
+export async function check(
+  projectId: string,
+  apiName: string,
+  prefix: string,
+  silent: boolean
+): Promise<boolean> {
+  const response = await api.request(
+    "GET",
+    `/v1/services/${apiName}/projectSettings/${projectId}?view=CONSUMER_VIEW`,
+    {
+      auth: true,
+      origin: "https://servicemanagement.googleapis.com",
+    }
+  );
+
+  const isEnabled = _.get(response.body, "usageSettings.consumerEnableStatus") === "ENABLED";
+  if (isEnabled && !silent) {
+    utils.logLabeledSuccess(prefix, "all necessary APIs are enabled");
+  }
+  return isEnabled;
+}
+
+export async function enable(projectId: string, apiName: string): Promise<void> {
+  return api.request(
+    "PATCH",
+    `/v1/services/${apiName}/projectSettings/${projectId}?updateMask=usageSettings`,
+    {
+      auth: true,
+      data: {
+        usageSettings: { consumerEnableStatus: "ENABLED" },
+      },
+      origin: "https://servicemanagement.googleapis.com",
+    }
+  );
+}
+
+export async function ensure(
+  projectId: string,
+  apiName: string,
+  prefix: string,
+  silent: boolean
+): Promise<void> {
+  if (!silent) {
+    utils.logLabeledBullet(prefix, "ensuring necessary APIs are enabled...");
+  }
+  const isEnabled = await check(projectId, apiName, prefix, silent);
+  if (isEnabled) {
+    return;
+  }
+  if (!silent) {
+    utils.logLabeledWarning(prefix, "missing necessary APIs. Enabling now...");
+  }
+  return enableApiWithRetries(projectId, apiName, prefix, silent);
+}
+
+const pollCheckEnabled = async (
+  projectId: string,
+  apiName: string,
+  prefix: string,
+  silent: boolean,
+  enablementRetries: number,
+  pollRetries = 0
+): Promise<void> => {
+  if (pollRetries > POLLS_BEFORE_RETRY) {
+    return enableApiWithRetries(projectId, apiName, prefix, silent, enablementRetries + 1);
+  }
+
+  await new Promise((resolve) => {
+    setTimeout(resolve, POLL_INTERVAL);
+  });
+  const isEnabled = await check(projectId, apiName, prefix, silent);
+  if (isEnabled) {
+    return;
+  }
+  if (!silent) {
+    utils.logLabeledBullet(prefix, "waiting for APIs to activate...");
+  }
+  return pollCheckEnabled(projectId, apiName, prefix, silent, enablementRetries, pollRetries + 1);
+};
+
+const enableApiWithRetries = async (
+  projectId: string,
+  apiName: string,
+  prefix: string,
+  silent: boolean,
+  enablementRetries = 0
+) => {
+  if (enablementRetries > 1) {
+    return utils.reject(
+      "Timed out while waiting for APIs to enable. Please try again in a few minutes."
+    );
+  }
+  await enable(projectId, apiName);
+  return pollCheckEnabled(projectId, apiName, prefix, silent, enablementRetries);
+};
