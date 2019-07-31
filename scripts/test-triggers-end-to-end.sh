d15d03decfa25ecf869ac8f24d961d7c7dccb521
Sam Stern <samstern@google.com>
2019-07-31 10:20:56 -0700

    Update test-triggers-end-to-end.sh

--- scripts/test-triggers-end-to-end.sh
+++ scripts/test-triggers-end-to-end.sh
@@ -2,7 +2,7 @@
 set -xe
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-$DIR/set-default-credentials.sh
+source $DIR/set-default-credentials.sh
 
 FIREBASE_CLI="./lib/bin/firebase.js"
 
