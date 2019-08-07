af1e4e5e6ec0fb4ef342a4b7e6be1deabf0a9d9d
Tri Cao <caot@google.com>
2019-08-07 12:59:51 -0400

    Remove tools:migrate command and identifierToProjectId method

--- src/commands/index.js
+++ src/commands/index.js
@@ -75,8 +75,6 @@ module.exports = function(client) {
   client.target.apply = loadCommand("target-apply");
   client.target.clear = loadCommand("target-clear");
   client.target.remove = loadCommand("target-remove");
-  client.tools = {};
-  client.tools.migrate = loadCommand("tools-migrate");
   client.use = loadCommand("use");
 
   if (previews.mods) {
--- src/requireAccess.ts
+++ src/requireAccess.ts
@@ -4,7 +4,6 @@ import * as clc from "cli-color";
 import * as getInstanceId from "./getInstanceId";
 import * as getProjectId from "./getProjectId";
 import { FirebaseError } from "./error";
-import * as identifierToProjectId from "./identifierToProjectId";
 import * as requireAuth from "./requireAuth";
 
 export function requireAccess(options: any): Promise<any> {
@@ -28,34 +27,10 @@ export function requireAccess(options: any): Promise<any> {
         return Promise.reject(err);
       }
 
-      return identifierToProjectId(projectId).then((realProjectId) => {
-        if (realProjectId) {
-          let fixCommand = "firebase use " + realProjectId;
-          if (options.projectAlias) {
-            fixCommand += " --alias " + options.projectAlias;
-          }
-
-          return Promise.reject(
-            new FirebaseError(
-              "Tried to access unrecognized project " +
-                clc.bold(projectId) +
-                ", but found matching instance for project " +
-                clc.bold(realProjectId) +
-                ".\n\n" +
-                "To use " +
-                clc.bold(realProjectId) +
-                " instead, run:\n\n  " +
-                clc.bold(fixCommand),
-              { exit: 1 }
-            )
-          );
-        }
-
-        return Promise.reject(
-          new FirebaseError("Unable to authorize access to project " + clc.bold(projectId), {
-            exit: 1,
-          })
-        );
-      });
+      return Promise.reject(
+        new FirebaseError("Unable to authorize access to project " + clc.bold(projectId), {
+          exit: 1,
+        })
+      );
     });
 }
