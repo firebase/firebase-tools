20e480a59d5032c12f562e1e180c98175a9945d7
Tri Cao <caot@google.com>
2019-08-08 14:23:52 -0400

    Deprecate setup:web command

--- src/commands/setup-web.js
+++ src/commands/setup-web.js
@@ -9,10 +9,17 @@ var requirePermissions = require("../requirePermissions");
 
 var JS_TEMPLATE = fs.readFileSync(__dirname + "/../../templates/setup/web.js", "utf8");
 
+/**
+ * This command is deprecated in favor of `apps:sdkconfig web` command
+ * TODO: Remove this command after launching `apps:sdkconfig` command
+ */
 module.exports = new Command("setup:web")
-  .description("display this project's setup information for the Firebase JS SDK")
+  .description("[DEPRECATED] display this project's setup information for the Firebase JS SDK")
   .before(requirePermissions, [])
   .action(function(options) {
+    logger.warn(
+      "This command is deprecated. Instead, use 'firebase apps:sdkconfig web' to get web setup information."
+    );
     return fetchWebSetup(options).then(function(config) {
       logger.info(JS_TEMPLATE.replace("{/*--CONFIG--*/}", JSON.stringify(config, null, 2)));
       return Promise.resolve(config);
