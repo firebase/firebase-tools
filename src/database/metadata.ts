163d93c060ecff46c97a1f99b188a5d5329e58d3
Ryan P. Brewster <RyanPBrewster@gmail.com>
2019-10-17 14:18:54 -0700

    hook up logic to metadata service backend

--- src/database/metadata.ts
+++ src/database/metadata.ts
@@ -2,11 +2,31 @@
  * Package for interacting with Realtime Database metadata.
  */
 
- const BASE_URL = "https://metadata-dot-firebase-prod.appspot.com";
- 
+import * as api from "../api";
+import * as logger from "../logger";
+import * as utils from "../utils";
+
+function handleErrorResponse(response: any): any {
+  if (response.body && response.body.error) {
+    return utils.reject(response.body.error, { code: 2 });
+  }
+
+  logger.debug("[rules] error:", response.status, response.body);
+  return utils.reject("Unexpected error encountered with rules.", {
+    code: 2,
+  });
+}
+
 export interface ListRulesetItem {
-  id: string
+  id: string;
 }
 export async function listAllRulesets(databaseName: string): Promise<ListRulesetItem[]> {
-  return [];
+  const response = await api.request("GET", `/namespaces/${databaseName}/rulesets`, {
+    auth: true,
+    origin: api.rtdbMetadataOrigin,
+  });
+  if (response.status === 200) {
+    return response.body;
+  }
+  return handleErrorResponse(response);
 }
