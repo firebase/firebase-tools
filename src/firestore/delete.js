5bcc69542648277089e2ba9c860109e21897e708
Dylan Cutler <dylancutler@google.com>
2019-10-28 12:03:42 -0400

    rewrite ProgressBar message and delete old method

--- src/firestore/delete.js
+++ src/firestore/delete.js
@@ -244,7 +244,7 @@ FirestoreDelete.prototype._getDescendantBatch = function(allDescendants, batchSi
 /**
  * Progress bar shared by the class.
  */
-FirestoreDelete.progressBar = new ProgressBar("Deleted :current docs (:rate docs/s)", {
+FirestoreDelete.progressBar = new ProgressBar("Deleted :current docs (:rate docs/s)\n", {
   total: Number.MAX_SAFE_INTEGER,
 });
 
@@ -266,7 +266,6 @@ FirestoreDelete.prototype._recursiveBatchDelete = function() {
   // All temporary variables for the deletion queue.
   var queue = [];
   var numPendingDeletes = 0;
-  var deletedDoc = false;
   var pagesRemaining = true;
   var pageIncoming = false;
   var lastDocName;
@@ -361,11 +360,6 @@ FirestoreDelete.prototype._recursiveBatchDelete = function() {
         clearInterval(intervalId);
 
         if (failures.length == 0) {
-          // Log an empty string so that the console adds a newline after
-          // the progress bar is done.
-          if (deletedDoc) {
-            console.log("");
-          }
           resolve();
         } else {
           reject("Failed to delete documents " + failures);
