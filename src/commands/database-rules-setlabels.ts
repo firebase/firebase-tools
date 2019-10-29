5a135145cdfd37997df98f962f2b7e847c6f1cb0
Ryan P. Brewster <RyanPBrewster@gmail.com>
2019-10-29 16:35:04 -0700

    clarify invariants

--- src/commands/database-rules-setlabels.ts
+++ src/commands/database-rules-setlabels.ts
@@ -10,8 +10,8 @@ export default new Command("database:rules:setlabels")
     "--instance <instance>",
     "use the database <instance>.firebaseio.com (if omitted, uses default database instance)"
   )
-  .option("--stable <ruleset_id>", "mark the given ruleset id as 'stable'")
-  .option("--canary <ruleset_id>", "mark the given ruleset id as 'canary'")
+  .option("--stable <ruleset_id>", "[required] mark the given ruleset id as 'stable'")
+  .option("--canary <ruleset_id>", "mark the given ruleset id as 'canary'. an empty string will remove the canary label.")
   .before(requirePermissions, ["firebasedatabase.instances.get"])
   .before(requireInstance)
   .action(async (options: any) => {
