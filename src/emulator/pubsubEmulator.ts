2c22fc9e8969be1f589c307f74946c0d4f8ce5cc
Sam <samstern@google.com>
2019-10-30 10:47:18 -0700

    First end-to-end success

--- src/emulator/pubsubEmulator.ts
+++ src/emulator/pubsubEmulator.ts
@@ -1,7 +1,9 @@
-import { PubSub, Subscription } from "@google-cloud/pubsub";
+import * as childProcess from "child_process";
+import * as request from "request";
+import { PubSub, Subscription, Message } from "@google-cloud/pubsub";
+
 import { EmulatorInfo, EmulatorInstance, Emulators } from "../emulator/types";
 import { Constants } from "./constants";
-import * as childProcess from "child_process";
 
 export interface PubsubEmulatorArgs {
   port?: number;
@@ -16,18 +18,42 @@ export class PubsubEmulator implements EmulatorInstance {
   instance?: childProcess.ChildProcess;
 
   constructor(private args: PubsubEmulatorArgs) {
-    // TODO: Variable port
+    // TODO: Variable port and project
     this.pubsub = new PubSub({
       apiEndpoint: "localhost:8085",
+      projectId: "fir-dumpster",
     });
-    // TODO
+
+    // TODO: Get this from functions
     this.topics = ["test-topic"];
     this.subscriptions = new Map();
   }
 
   async start(): Promise<void> {
-    this.instance = childProcess.spawn("gcloud", ["beta", "emulators", "pubsub", "start"], {
-      stdio: ["inherit", "pipe", "pipe"],
+    // /Library/Java/JavaVirtualMachines/default/Contents/Home/bin/java
+    //    -jar /Users/samstern/google-cloud-sdk/platform/pubsub-emulator/lib/cloud-pubsub-emulator-0.1-SNAPSHOT-all.jar
+    //    --host=localhost --port=8085
+
+    // TODO: Maybe combine this logic with some of the JavaEmulators stuff?
+    // TODO: Do a simpler check for presence of "gcloud" and "pubsub emulator"
+    this.instance = childProcess.spawn(
+      "gcloud",
+      ["--quiet", "beta", "emulators", "pubsub", "start"],
+      {
+        stdio: ["inherit", "pipe", "pipe"],
+      }
+    );
+
+    // TODO: Use Firebase errors
+    if (!this.instance) {
+      throw new Error("Pubsub instance null");
+    }
+
+    this.instance.on("error", (err: any) => {
+      console.warn("error", err);
+    });
+    this.instance.on("exit", (code, signal) => {
+      console.warn("exit", code, signal);
     });
 
     // TODO: Start the emulator
@@ -36,28 +62,10 @@ export class PubsubEmulator implements EmulatorInstance {
   }
 
   async connect(): Promise<void> {
-    console.log(`Pubsub client emulated: ${this.pubsub.isEmulator}`)
+    console.log(`Pubsub client emulated: ${this.pubsub.isEmulator}`);
 
     for (const topicName of this.topics) {
-      const topic = this.pubsub.topic(topicName);
-      const topicExists = await topic.exists();
-      if (!topicExists) {
-        console.log(`Creating topic: ${topicName}`);
-        await topic.create();
-      } else {
-        console.log(`Topic ${topicName} exists`);
-      }
-
-      const sub = this.pubsub.subscription(`emulator-sub-${topicName}`);
-      const subExists = await sub.exists();
-      if (!subExists) {
-        console.log(`Creating sub for topic: ${topicName}`);
-        await sub.create();
-      } else {
-        console.log(`Sub for ${topicName} already exists`);
-      }
-
-      this.subscriptions.set(topicName, sub);
+      await this.subscribeTo(topicName);
     }
 
     return Promise.resolve();
@@ -83,5 +91,63 @@ export class PubsubEmulator implements EmulatorInstance {
 
   getName(): Emulators {
     return Emulators.PUBSUB;
+  }
+
+  private async subscribeTo(topicName: string) {
+    const topic = this.pubsub.topic(topicName);
+    try {
+      console.log(`Creating topic: ${topicName}`);
+      await topic.create();
+    } catch (e) {
+      if (e && e.code === 6) {
+        console.log(`Topic ${topicName} exists`);
+      } else {
+        throw e;
+      }
+    }
+
+    const subName = `emulator-sub-${topicName}`;
+    let sub;
+    try {
+      console.log(`Creating sub for topic: ${topicName}`);
+      [sub] = await topic.createSubscription(subName);
+    } catch (e) {
+      if (e && e.code === 6) {
+        console.log(`Sub for ${topicName} exists`);
+        sub = topic.subscription(`emulator-sub-${topicName}`);
+      } else {
+        console.warn(JSON.stringify(e));
+        throw e;
+      }
+    }
+
+    sub.on("message", (message: Message) => {
+      this.onMessage(topicName, message);
+    });
+
+    this.subscriptions.set(topicName, sub);
+  }
+
+  private onMessage(topicName: string, message: Message) {
+    const data = message.data.toString();
+    const dataObj = JSON.parse(data);
+    console.log(`onMessage(${topicName}): ${JSON.stringify(dataObj)}`);
+
+    const body = {
+      data: {
+        data: message.data,
+        attributes: message.attributes,
+      },
+    };
+
+    // TODO: Take functions stuff as input
+    // TODO: how do I know the name?
+    console.log("POSTING...");
+    const route = "functions/projects/fir-dumpster/triggers/pubsubFn";
+    request.post(`http://localhost:5001/${route}`, {
+      body: JSON.stringify(body),
+    });
+
+    message.ack();
   }
 }
