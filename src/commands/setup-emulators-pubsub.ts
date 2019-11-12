e8dda744a3c46753b6c8c790c2252e7be430ea01
Sam Stern <samstern@google.com>
2019-11-12 10:24:07 -0800

    Fix typo

--- src/commands/setup-emulators-pubsub.ts
+++ src/commands/setup-emulators-pubsub.ts
@@ -3,7 +3,7 @@ const downloadEmulator = require("../emulator/download");
 
 const EMULATOR_NAME = "pubsub";
 
-module.exports = new Command(`setup:emulators:${EMULATOR_NAME}` + EMULATOR_NAME)
+module.exports = new Command(`setup:emulators:${EMULATOR_NAME}`)
   .description(`downloads the ${EMULATOR_NAME} emulator`)
   .action(() => {
     return downloadEmulator(EMULATOR_NAME);
