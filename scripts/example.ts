5bc6c3612a321146833c6fc7f996e56e60f8f736
Bryan Kendall <bkend@google.com>
2019-11-15 19:16:42 -0800

    add test for appsList

--- scripts/example.ts
+++ scripts/example.ts
@@ -28,9 +28,18 @@ async function deployHosting() {
   console.log("Deployed hosting.");
 }
 
+async function appsList() {
+  console.log("Getting app configuration...");
+  const apps = await client.apps.list(
+    "web",
+    { project: process.env.FBTOOLS_TARGET_PROJECT });
+  console.log("Got app configuration.");
+}
+
 async function main() {
   await listProjects();
   await deployHosting();
+  await appsList();
 }
 
 main();
