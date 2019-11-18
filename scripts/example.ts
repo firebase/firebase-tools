e7c8e31c8c9af6f416b5435754f5e45808746039
Bryan Kendall <bkend@google.com>
2019-11-18 10:31:52 -0800

    use mocha for tests

--- scripts/example.ts
+++ scripts/example.ts
@@ -1,43 +1,32 @@
+import { expect } from "chai";
 import { join } from "path";
 
 import firebase = require("../src");
 
 const client: any = firebase;
 
-process.on("unhandledRejection", (err) => {
-  console.error("unhandledRejection", err);
-  process.exit(1);
+describe("listProjects", () => {
+  it("should list projects", async () => {
+    const projects = await client.list();
+
+    expect(projects).to.have.length.greaterThan(0);
+  }).timeout(5 * 1e3); // This can take a while for reasons.
 });
 
-async function listProjects() {
-  console.log("Listing projects...");
-  const projects = await client.list();
-  if (!projects.length) {
-    throw new Error("client.list() should have returned projects");
-  }
-  console.log("Listed projects.");
-}
+describe("deployHosting", () => {
+  it("should deploy hosting", async () => {
+    await client.deploy({
+      project: process.env.FBTOOLS_TARGET_PROJECT,
+      cwd: join(__dirname, "test-project"),
+      only: "hosting",
+    });
+  }).timeout(10 * 1e3); // Deploying takes several steps.
+});
 
-async function deployHosting() {
-  console.log("Deploying hosting...");
-  await client.deploy({
-    project: process.env.FBTOOLS_TARGET_PROJECT,
-    cwd: join(__dirname, "test-project"),
-    only: "hosting",
+describe("appsList", () => {
+  it("should list apps configuration", async () => {
+    const apps = await client.apps.list("web", { project: process.env.FBTOOLS_TARGET_PROJECT });
+
+    expect(apps).to.have.length.greaterThan(0);
   });
-  console.log("Deployed hosting.");
-}
-
-async function appsList() {
-  console.log("Getting app configuration...");
-  const apps = await client.apps.list("web", { project: process.env.FBTOOLS_TARGET_PROJECT });
-  console.log("Got app configuration.");
-}
-
-async function main() {
-  await listProjects();
-  await deployHosting();
-  await appsList();
-}
-
-main();
+});
