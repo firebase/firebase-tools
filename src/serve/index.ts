661b0a9f8ee7526958989ecd8ec7ed2844ea255a
hayato iida <hayato.iida.0213@gmail.com>
2019-12-19 15:38:57 +0900

    convert serve/index js to typescript code

--- src/serve/index.ts
+++ src/serve/index.ts
@@ -1,48 +1,48 @@
-"use strict";
+import { EmulatorServer } from "../emulator/emulatorServer";
 
-var _ = require("lodash");
+const _ = require("lodash");
 
-var logger = require("../logger");
+const logger = require("../logger");
 
-var TARGETS = {
+const TARGETS: {
+  [key: string]:
+    | EmulatorServer
+    | { start: (o: any) => void; stop: (o: any) => void; connect: () => void };
+} = {
   hosting: require("./hosting"),
   functions: require("./functions"),
   database: require("./database"),
   firestore: require("./firestore"),
 };
 
-var _serve = function(options) {
-  var targetNames = options.targets;
+const _serve = async function(options: any) {
+  const targetNames = options.targets;
   options.port = parseInt(options.port, 10);
-  return Promise.all(
-    _.map(targetNames, function(targetName) {
-      var target = TARGETS[targetName];
+  await Promise.all(
+    _.map(targetNames, function(targetName: string) {
+      const target = TARGETS[targetName];
       return target.start(options);
     })
-  )
-    .then(function() {
-      return Promise.all(
-        _.map(targetNames, function(targetName) {
-          var target = TARGETS[targetName];
-          return target.connect();
-        })
-      );
+  );
+  await Promise.all(
+    _.map(targetNames, function(targetName: string) {
+      const target = TARGETS[targetName];
+      return target.connect();
     })
-    .then(function() {
-      return new Promise(function(resolve) {
-        process.on("SIGINT", function() {
-          logger.info("Shutting down...");
-          return Promise.all(
-            _.map(targetNames, function(targetName) {
-              var target = TARGETS[targetName];
-              return target.stop(options);
-            })
-          )
-            .then(resolve)
-            .catch(resolve);
-        });
-      });
+  );
+  await new Promise(function(resolve) {
+    process.on("SIGINT", function() {
+      logger.info("Shutting down...");
+      return Promise.all(
+        _.map(targetNames, function(targetName: string) {
+          const target = TARGETS[targetName];
+          return target.stop(options);
+        })
+      )
+        .then(resolve)
+        .catch(resolve);
     });
+  });
 };
 
-module.exports = _serve;
+export = _serve;
