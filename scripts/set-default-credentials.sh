e93d258c0942c08f7d45e384f043f6a6a0390b06
Sam Stern <samstern@google.com>
2020-01-07 11:00:16 -0800

    Review comments

--- scripts/set-default-credentials.sh
+++ scripts/set-default-credentials.sh
@@ -3,15 +3,13 @@ set -e
 
 CWD="$(pwd)"
 
-if [ "${CI}" != "true" ]; then
+if [[ -z $CI ]]; then
+  echo "CI is unset, assuming local testing."
   export COMMIT_SHA="localtesting"
   export CI_JOB_ID="$(echo $RANDOM)"
-fi
-
-if [[ -z $LOCAL ]]; then
-  export GOOGLE_APPLICATION_CREDENTIALS="${CWD}/scripts/service-account.json"
 else
-  echo "Not setting GOOGLE_APPLICATION_CREDENTIALS because LOCAL=${LOCAL}"
+  echo "CI=${CI}, setting GOOGLE_APPLICATION_CREDENTIALS"
+  export GOOGLE_APPLICATION_CREDENTIALS="${CWD}/scripts/service-account.json"
 fi
 
 echo "Application Default Credentials: ${GOOGLE_APPLICATION_CREDENTIALS}"
