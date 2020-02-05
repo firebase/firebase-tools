0ea86670fcfbff328490ceef1e65262f50015bdd
Fred Zhang <fredzqm@google.com>
2020-02-05 10:01:45 -0800

    linters

--- src/test/serve/javaEmulators.spec.ts
+++ src/test/serve/javaEmulators.spec.ts
@@ -6,13 +6,13 @@ import { Emulators } from "../../emulator/types";
 
 type DownloadableEmulator = Emulators.FIRESTORE | Emulators.DATABASE | Emulators.PUBSUB;
 
-function checkDownloadPath(name: DownloadableEmulator) {
+function checkDownloadPath(name: DownloadableEmulator): void {
   const emulator = javaEmulators.getDownloadDetails(name);
   expect(path.basename(emulator.opts.remoteUrl)).to.include(path.basename(emulator.downloadPath));
 }
 
 describe("downloadDetails", () => {
-  it("should match part of remoteUrl", async () => {
+  it("should match part of remoteUrl", () => {
     checkDownloadPath(Emulators.FIRESTORE);
     checkDownloadPath(Emulators.DATABASE);
     checkDownloadPath(Emulators.PUBSUB);
