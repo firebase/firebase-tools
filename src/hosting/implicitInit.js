b34d40670c4fad3d8547063ea30a1e82950c89da
Sam Stern <samstern@google.com>
2020-02-13 16:26:18 -0800

    Automatically connect to running emulators

--- src/hosting/implicitInit.js
+++ src/hosting/implicitInit.js
@@ -6,8 +6,11 @@ var fs = require("fs");
 var { fetchWebSetup, getCachedWebSetup } = require("../fetchWebSetup");
 var utils = require("../utils");
 var logger = require("../logger");
+const { EmulatorRegistry } = require("../emulator/registry");
 
 var INIT_TEMPLATE = fs.readFileSync(__dirname + "/../../templates/hosting/init.js", "utf8");
+const FIREBASE_CONFIG = "/*--CONFIG--*/";
+const EMULATORS_CONFIG = "/*--EMULATORCONFIG--*/";
 
 module.exports = async function(options) {
   let config;
@@ -43,9 +46,31 @@ module.exports = async function(options) {
     );
   }
 
-  const configJson = JSON.stringify(config, null, 2);
+  const firebaseJson = JSON.stringify(config, null, 2);
+
+  const emulatorConfig = {};
+  if (EmulatorRegistry.isRunning("firestore")) {
+    const { host, port } = EmulatorRegistry.get("firestore").getInfo();
+    emulatorConfig["firestore"] = `http://${host}:${port}`;
+  }
+  if (EmulatorRegistry.isRunning("functions")) {
+    const { host, port } = EmulatorRegistry.get("functions").getInfo();
+    emulatorConfig["functions"] = `http://${host}:${port}`;
+  }
+  if (EmulatorRegistry.isRunning("database")) {
+    const { host, port } = EmulatorRegistry.get("database").getInfo();
+    if (options.project) {
+      emulatorConfig["database"] = `http://${host}:${port}/?ns=${options.project}`;
+    }
+  }
+  const emulatorsJson = JSON.stringify(emulatorConfig, null, 2);
+
+  let configJs = INIT_TEMPLATE;
+  configJs = configJs.replace(FIREBASE_CONFIG, `var firebaseConfig = ${firebaseJson};`);
+  configJs = configJs.replace(EMULATORS_CONFIG, `var emulatorConfig = ${emulatorsJson};`);
+
   return {
-    js: INIT_TEMPLATE.replace("/*--CONFIG--*/", `var firebaseConfig = ${configJson};`),
-    json: configJson,
+    js: configJs,
+    json: firebaseJson,
   };
 };
--- templates/hosting/init.js
+++ templates/hosting/init.js
@@ -1,5 +1,30 @@
 if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+function hasFirebaseFeature(feature) {
+  return firebase && typeof firebase[feature] === "function";
+}
 /*--CONFIG--*/
+/*--EMULATORCONFIG--*/
 if (firebaseConfig) {
+  // Database has to be configured *before* initializeApp, while others can be configured after.
+  if (emulatorConfig.database) {
+    console.log("Configuring Database SDK to point at emulator: " + emulatorConfig.database);
+    firebaseConfig.databaseURL = emulatorConfig.database
+  }
+
   firebase.initializeApp(firebaseConfig);
+
+  if (emulatorConfig) {
+    if (hasFirebaseFeature("firestore") && emulatorConfig.firestore) {
+      console.log("Configuring Firestore SDK to point at emulator: " + emulatorConfig.firestore);
+      firebase.firestore().settings({
+        host: emulatorConfig.firestore,
+        ssl: false
+      });
+    }
+
+    if (hasFirebaseFeature("functions") && emulatorConfig.functions) {
+      console.log("Configuring Functions SDK to point at emulator: " + emulatorConfig.functions);
+      firebase.functions().useFunctionsEmulator(emulatorConfig.functions);
+    }
+  }
 }
