91af6b38d00d1d29e0f8d30d9b357c36c4aa4327
Abe Haskins <abehaskins@google.com>
2020-02-25 13:50:29 -0600

    prettier command.ts

--- src/command.ts
+++ src/command.ts
@@ -221,10 +221,9 @@ export class Command {
       logger.add(
         new winston.transports.Console({
           level: process.env.DEBUG ? "debug" : "info",
-          format: winston.format.printf((info) =>
-            [info.message, ...(info[SPLAT] || [])].join(" ")),
+          format: winston.format.printf((info) => [info.message, ...(info[SPLAT] || [])].join(" ")),
         })
-      )
+      );
     }
 
     try {
