255d06b7b50453a1184c0c36a3a7841e0b633126
Efi Shtain <efi.shtain@gmail.com>
2020-02-27 13:11:04 +0200

    support maxInstances for gcp function

--- src/deploy/functions/release.js
+++ src/deploy/functions/release.js
@@ -213,6 +213,7 @@ module.exports = function(context, options, payload) {
                   runtime: runtime,
                   availableMemoryMb: functionInfo.availableMemoryMb,
                   timeout: functionInfo.timeout,
+                  maxInstances: functionInfo.maxInstances
                 })
                 .then((createRes) => {
                   if (_.has(functionTrigger, "httpsTrigger")) {
@@ -286,6 +287,7 @@ module.exports = function(context, options, payload) {
               labels: _.assign({}, deploymentTool.labels, functionInfo.labels),
               availableMemoryMb: functionInfo.availableMemoryMb,
               timeout: functionInfo.timeout,
+              maxInstances: functionInfo.maxInstances
             };
             if (context.runtimeChoice) {
               options.runtime = context.runtimeChoice;
