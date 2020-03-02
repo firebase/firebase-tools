ca3fbd2ed738c46817b47c0f18a263b67aeb83d9
Abe Haskins <abeisgreat@abeisgreat.com>
2020-03-02 16:33:01 -0600

    Adds support for stringifying debug splats

--- src/bin/firebase.js
+++ src/bin/firebase.js
@@ -43,13 +43,25 @@ if (!process.env.DEBUG && _.includes(args, "--debug")) {
   process.env.DEBUG = true;
 }
 
+function tryStringify(value) {
+  if (typeof value === "string") return value;
+
+  try {
+    return JSON.stringify(value);
+  } catch {
+    return value;
+  }
+}
+
 logger.add(
   new winston.transports.File({
     level: "debug",
     filename: logFilename,
-    json: false,
     format: winston.format.printf(
-      (info) => `[${info.level}] ` + ansiStrip([info.message, ...(info[SPLAT] || [])].join(" "))
+      (info) =>
+        `[${info.level}] ${ansiStrip(
+          [info.message, ...(info[SPLAT] || [])].map(tryStringify).join(" ")
+        )}`
     ),
   })
 );
