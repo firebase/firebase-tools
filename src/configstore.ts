91cfee33bab1ba1ea9ef90a53ec53208818342cc
Bryan Kendall <bkend@google.com>
2020-03-17 13:54:52 -0700

    remove some comments

--- src/configstore.ts
+++ src/configstore.ts
@@ -18,6 +18,4 @@ class CLIConfigstore extends Configstore {
   }
 }
 
-// Init a Configstore instance with an unique ID eg. package name
-// and optionally some default values
 export const configstore = new CLIConfigstore(pkg.name);
