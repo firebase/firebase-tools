8bc7e74618529183e93e4895c461aacf5a0752d9
Yuchen Shi <yuchenshi@google.com>
2020-03-18 11:48:06 -0700

    Add back FIREBASE_EMULATOR_HUB in emulators:exec.

--- src/emulator/commandUtils.ts
+++ src/emulator/commandUtils.ts
@@ -13,6 +13,7 @@ import { DatabaseEmulator } from "../emulator/databaseEmulator";
 import { EmulatorRegistry } from "../emulator/registry";
 import { FirestoreEmulator } from "../emulator/firestoreEmulator";
 import * as getProjectId from "../getProjectId";
+import { EmulatorHub } from "./hub";
 
 export const FLAG_ONLY = "--only <emulators>";
 export const DESC_ONLY =
@@ -110,6 +111,13 @@ async function runScript(script: string, extraEnv: Record<string, string>): Prom
 
     env[FirestoreEmulator.FIRESTORE_EMULATOR_ENV] = address;
     env[FirestoreEmulator.FIRESTORE_EMULATOR_ENV_ALT] = address;
+  }
+
+  const hubInstance = EmulatorRegistry.get(Emulators.HUB);
+  if (hubInstance) {
+    const info = hubInstance.getInfo();
+    const address = `${info.host}:${info.port}`;
+    env[EmulatorHub.EMULATOR_HUB_ENV] = address;
   }
 
   const proc = childProcess.spawn(script, {
