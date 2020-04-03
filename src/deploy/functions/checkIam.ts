e3963f2b6be21271427091748fc1009ce69234c1
Michael Bleigh <bleigh@google.com>
2020-04-03 09:44:15 -0700

    skip new iam checks when SKIP_INFORMATIONAL_IAM is present

--- src/deploy/functions/checkIam.ts
+++ src/deploy/functions/checkIam.ts
@@ -10,21 +10,32 @@ import { testIamPermissions, testResourceIamPermissions } from "../../gcp/iam";
 const PERMISSION = "cloudfunctions.functions.setIamPolicy";
 
 export async function checkServiceAccountIam(projectId: string): Promise<void> {
+  if (process.env.FIREBASE_SKIP_INFORMATIONAL_IAM) {
+    "[iam] skipping informational functions service account IAM permission check as FIREBASE_SKIP_INFORMATIONAL_IAM is present";
+    return;
+  }
+
   const saEmail = `${projectId}@appspot.gserviceaccount.com`;
-  const { passed } = await testResourceIamPermissions(
-    "https://iam.googleapis.com",
-    "v1",
-    `projects/${projectId}/serviceAccounts/${saEmail}`,
-    ["iam.serviceAccounts.actAs"]
-  );
-  if (!passed) {
-    throw new FirebaseError(
-      "Missing permissions required for functions deploy. You must have permission\n" +
-        `${bold("iam.serviceAccounts.ActAs")} on service account ${bold(saEmail)}.\n\n` +
-        `To address this error, ask a ${bold("project owner")} to grant your account the\n` +
-        `"Service Account User" role from this URL:\n\n` +
-        `https://console.cloud.google.com/iam-admin/iam?project=${projectId}`
+  try {
+    const { passed } = await testResourceIamPermissions(
+      "https://iam.googleapis.com",
+      "v1",
+      `projects/${projectId}/serviceAccounts/${saEmail}`,
+      ["iam.serviceAccounts.actAs"]
     );
+    if (!passed) {
+      throw new FirebaseError(
+        `Missing permissions required for functions deploy. You must have permission ${bold(
+          "iam.serviceAccounts.ActAs"
+        )} on service account ${bold(saEmail)}.\n\n` +
+          `To address this error, ask a ${bold(
+            "project owner"
+          )} to grant your account the "Service Account User" role from this URL:\n\n` +
+          `https://console.cloud.google.com/iam-admin/iam?project=${projectId}`
+      );
+    }
+  } catch (err) {
+    debug("[functions] service account IAM check errored, deploy may fail");
   }
 }
 
@@ -41,6 +52,11 @@ export async function checkHttpIam(
   options: unknown,
   payload: { functions: { triggers: { name: string; httpsTrigger?: {} }[] } }
 ): Promise<void> {
+  if (process.env.FIREBASE_SKIP_INFORMATIONAL_IAM) {
+    "[iam] skipping informational functions HTTP IAM permission check as FIREBASE_SKIP_INFORMATIONAL_IAM is present";
+    return;
+  }
+
   const triggers = payload.functions.triggers;
   const functionsInfo = getFunctionsInfo(triggers, context.projectId);
   const filterGroups = getFilterGroups(options);
@@ -72,12 +88,11 @@ export async function checkHttpIam(
     throw new FirebaseError(
       `Missing required permission on project ${bold(
         context.projectId
-      )} to create HTTPS functions.\n` +
-        `Permission ${bold(PERMISSION)} is required for this deploy. Affected functions:\n\n- ` +
+      )} to create HTTPS functions. Permission ${bold(
+        PERMISSION
+      )} is required for this deploy. Affected functions:\n\n- ` +
         newHttpFunctions.map((name) => last(name.split("/"))).join("\n- ") +
-        "\n\n" +
-        'To address this error, please ask a project owner to grant your account the "Cloud Functions Admin" role\n' +
-        `at the following URL: https://console.cloud.google.com/iam-admin/iam?project=${context.projectId}`
+        `\n\nTo address this error, please ask a project owner to grant your account the "Cloud Functions Admin" role at the following URL:\n\nhttps://console.cloud.google.com/iam-admin/iam?project=${context.projectId}`
     );
   }
   debug("[functions] found setIamPolicy permission, proceeding with deploy");
