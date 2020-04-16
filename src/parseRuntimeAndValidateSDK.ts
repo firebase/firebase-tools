cb68d9dad976edb6ba5654691e3df08c6d574dee
Michael Bleigh <bleigh@google.com>
2020-04-16 10:24:21 -0700

    Fix missing import

--- src/parseRuntimeAndValidateSDK.ts
+++ src/parseRuntimeAndValidateSDK.ts
@@ -7,6 +7,7 @@ import { getFunctionsSDKVersion } from "./checkFirebaseSDKVersion";
 import { FirebaseError } from "./error";
 import * as utils from "./utils";
 import * as logger from "./logger";
+import * as track from "./track";
 
 // have to require this because no @types/cjson available
 // eslint-disable-next-line @typescript-eslint/no-var-requires
