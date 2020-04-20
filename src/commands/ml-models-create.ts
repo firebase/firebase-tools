ef8d8c334f2dc2db5869bad77f4c4f5f84fbe90c
ifielker <ifielker@gmail.com>
2020-04-20 16:21:49 -0400

    review comments

--- src/commands/ml-models-create.ts
+++ src/commands/ml-models-create.ts
@@ -2,10 +2,7 @@ import { Command } from "../command";
 import { FirebaseModel } from "../ml/models";
 
 export default new Command("ml:models:create <displayName> [source]")
-  .description(
-    "creates a new Firebase ML Model with the given displayName. " +
-      "If present source must be either a reference to an AutoML model, or a tflite file stored in a gcs bucket."
-  )
+  .description("creates a new Firebase ML Model with the given displayName.")
   .option(
     "-t, --tags <tag1,tag2>",
     "(optional) one or more comma separated tags to add to the model"
