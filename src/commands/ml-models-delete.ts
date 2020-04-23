ee6cfa2d2589840cbb01cfc838e0d1c28588abdb
ifielker <ifielker@gmail.com>
2020-04-23 15:47:40 -0400

    review comments

--- src/commands/ml-models-delete.ts
+++ src/commands/ml-models-delete.ts
@@ -5,6 +5,7 @@ import { requirePermissions } from "../requirePermissions";
 import { ensureFirebaseMlApiEnabled, isValidModelId } from "../ml/mlHelper";
 import * as mlApi from "../ml/mlApi";
 import * as utils from "../utils";
+import { FirebaseError } from "../error";
 
 export default new Command("ml:models:delete <modelId>")
   .description("delete the Firebase ML Model with the given modelId")
@@ -15,19 +16,17 @@ export default new Command("ml:models:delete <modelId>")
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     async (modelId: string, options: any): Promise<void> => {
       if (!isValidModelId(modelId)) {
-        return utils.reject("Must specify a valid model ID", { exit: 1 });
+        throw new FirebaseError("Must specify a valid model ID");
       }
       const projectId = getProjectId(options);
       try {
         await mlApi.deleteModel(projectId, modelId);
       } catch (err) {
         if (err.status === 404) {
-          return utils.reject(`No model ${modelId} in project ${projectId}.`, {
-            exit: 1,
-          });
+          return utils.reject(`No model ${modelId} in project ${projectId}.`);
         }
         throw err;
       }
-      utils.logSuccess(`Deleted Model ${clc.bold(modelId)}`);
+      utils.logLabeledSuccess("ml", `Deleted Model ${clc.bold(modelId)}`);
     }
   );
