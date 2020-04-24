0b38eb116a5fb9f4e2819cbe1b094342ad4fa37f
Sam <samstern@google.com>
2020-04-24 13:22:44 -0400

    Fix error message for requireAuth

--- src/requireAuth.ts
+++ src/requireAuth.ts
@@ -57,7 +57,12 @@ export async function requireAuth(options: any): Promise<void> {
   } else if (user) {
     logger.debug("> authorizing via signed-in user");
   } else {
-    return autoAuth(options, options.authScopes);
+    return autoAuth(options, options.authScopes).catch((e) => {
+      throw new FirebaseError(
+        `Failed to authenticate, have you run ${clc.bold("firebase login")}?`,
+        { original: e }
+      );
+    });
   }
 
   tokenOpt = tokenOpt || process.env.FIREBASE_TOKEN;
