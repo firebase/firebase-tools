81618c63692d50d8720f5cd952366522bd11e8f3
Bryan Kendall <bkend@google.com>
2020-04-27 12:36:21 -0700

    link global module and run test file

--- scripts/extensions-emulator-tests/run.sh
+++ scripts/extensions-emulator-tests/run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-
 set -e # Immediately exit on failure
+
+# Globally link the CLI for the testing framework.
+npm link
 
 cd scripts/extensions-emulator-tests/greet-the-world
 npm i
 cd - # Return to root so that we don't need a relative path for mocha
+
 mocha \
   --require ts-node/register \
   --require source-map-support/register \
   --require src/test/helpers/mocha-bootstrap.js \
-  scripts/extensions-emulator-tests/test.spec.ts
+  scripts/extensions-emulator-tests/tests.ts
