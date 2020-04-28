ac26da22a8d9f2234a26700a69c7c2007f26dc50
Lauren Long <longlauren@google.com>
2020-04-28 14:33:11 -0700

    Set FIREBASE_CONFIG env variable functions functions creates and updates

--- src/deploy/functions/release.js
+++ src/deploy/functions/release.js
@@ -172,6 +172,10 @@ module.exports = function(context, options, payload) {
       deleteReleaseNames = functionFilterGroups.length > 0 ? releaseNames : existingNames;
       helper.logFilters(existingNames, releaseNames, functionFilterGroups);
 
+      const envVariables = {
+        FIREBASE_CONFIG: JSON.stringify(context.firebaseConfig),
+      };
+
       // Create functions
       _.chain(uploadedNames)
         .difference(existingNames)
@@ -212,6 +216,7 @@ module.exports = function(context, options, payload) {
                   availableMemoryMb: functionInfo.availableMemoryMb,
                   timeout: functionInfo.timeout,
                   maxInstances: functionInfo.maxInstances,
+                  environmentVariables: envVariables,
                 })
                 .then((createRes) => {
                   if (_.has(functionTrigger, "httpsTrigger")) {
@@ -287,6 +292,7 @@ module.exports = function(context, options, payload) {
               timeout: functionInfo.timeout,
               runtime: runtime,
               maxInstances: functionInfo.maxInstances,
+              environmentVariables: envVariables,
             };
             utils.logBullet(
               clc.bold.cyan("functions: ") +
