7bb0da44defed7de93b7f9420aede9eb8a6c6e03
Giorgos Papadakis <xalikoutis@users.noreply.github.com>
2020-05-05 18:26:23 +0300

    API command that controls the SHA certificates in Android firebase apps (#2202)

---
+++ src/commands/apps-android-sha-list.ts
@@ -0,0 +1,55 @@
+import Table = require("cli-table");
+
+import { Command } from "../command";
+import * as getProjectId from "../getProjectId";
+import { listAppAndroidSha, AppAndroidShaData } from "../management/apps";
+import { requireAuth } from "../requireAuth";
+import * as logger from "../logger";
+import { promiseWithSpinner } from "../utils";
+
+function logCertificatesList(certificates: AppAndroidShaData[]): void {
+  if (certificates.length === 0) {
+    logger.info("No SHA certificate hashes found.");
+    return;
+  }
+  const tableHead = ["App Id", "SHA Id", "SHA Hash", "SHA Hash Type"];
+  const table = new Table({ head: tableHead, style: { head: ["green"] } });
+  certificates.forEach(({ name, shaHash, certType }) => {
+    /* the name property has the following value 
+    "projects/projectId/androidApps/appId/sha/shaId" as it comes from the json response
+    so we slpit the string in order to get appId and shaId. 
+    Property shaId can be used by the end user to delete a SHA hash record.*/
+    const splitted = name.split("/");
+    const appId = splitted[3];
+    const shaId = splitted[5];
+    table.push([appId, shaId, shaHash, certType]);
+  });
+
+  logger.info(table.toString());
+}
+
+function logCertificatesCount(count: number = 0): void {
+  if (count === 0) {
+    return;
+  }
+  logger.info("");
+  logger.info(`${count} SHA hash(es) total.`);
+}
+
+module.exports = new Command("apps:android:sha:list <appId>")
+  .description("list the SHA certificate hashes for a given app id. ")
+  .before(requireAuth)
+  .action(
+    async (appId: string = "", options: any): Promise<AppAndroidShaData[]> => {
+      const projectId = getProjectId(options);
+
+      const shaCertificates = await promiseWithSpinner<AppAndroidShaData[]>(
+        async () => await listAppAndroidSha(projectId, appId),
+        "Preparing the list of your Firebase Android app SHA certificate hashes"
+      );
+
+      logCertificatesList(shaCertificates);
+      logCertificatesCount(shaCertificates.length);
+      return shaCertificates;
+    }
+  );
