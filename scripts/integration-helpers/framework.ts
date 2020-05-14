8e106294c71819a5796b14abea8f1109829a01d8
Abe Haskins <abeisgreat@abeisgreat.com>
2020-05-14 16:07:37 -0500

    Change emulator start success log in e2e

--- scripts/integration-helpers/framework.ts
+++ scripts/integration-helpers/framework.ts
@@ -11,7 +11,8 @@ const FIREBASE_PROJECT_ZONE = "us-central1";
 const RTDB_FUNCTION_LOG = "========== RTDB FUNCTION ==========";
 const FIRESTORE_FUNCTION_LOG = "========== FIRESTORE FUNCTION ==========";
 const PUBSUB_FUNCTION_LOG = "========== PUBSUB FUNCTION ==========";
-const ALL_EMULATORS_STARTED_LOG = "All emulators started, it is now safe to connect.";
+const ALL_EMULATORS_STARTED_LOG =
+  "│ ✔  All emulators ready! It is now safe to connect your apps. │";
 
 interface ConnectionInfo {
   host: string;
