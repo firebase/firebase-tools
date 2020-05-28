c19c7cad0fc244388f7c8692bf4673d6b8c5fb9e
Sam <samstern@google.com>
2020-05-28 18:05:01 -0400

    Some lint fixes

--- src/firestore/checkDatabaseType.ts
+++ src/firestore/checkDatabaseType.ts
@@ -1,6 +1,15 @@
 import * as api from "../api";
 import * as logger from "../logger";
 
+/**
+ * Determine the Firestore database type for a given project. One of:
+ *   - DATABASE_TYPE_UNSPECIFIED (unspecified)
+ *   - CLOUD_DATASTORE (Datastore legacy)
+ *   - CLOUD_FIRESTORE (Firestore native mode)
+ *   - CLOUD_DATASTORE_COMPATIBILITY (Firestore datastore mode)
+ *
+ * @param projectId the Firebase project ID.
+ */
 export async function checkDatabaseType(projectId: string): Promise<string | undefined> {
   try {
     const resp = await api.request("GET", "/v1/apps/" + projectId, {
