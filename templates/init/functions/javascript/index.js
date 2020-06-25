fe529721c83ec3d4598fa6ac98b66ae500666dd6
Michael Bleigh <bleigh@google.com>
2020-06-25 09:31:30 -0700

    Updates functions init to default to Node.js 10 runtime.

--- templates/init/functions/javascript/index.js
+++ templates/init/functions/javascript/index.js
@@ -4,5 +4,6 @@ const functions = require('firebase-functions');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
 // });
