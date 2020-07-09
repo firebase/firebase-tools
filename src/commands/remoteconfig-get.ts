35a51ffbcbc18d92264c1ec3d90f0b0bfe04ac87
jackiechu <jackiechu@google.com>
2020-07-09 11:06:08 -0700

    Get table works and in good format without the brackets. Template version command continues to work. Output command is new and working

--- src/commands/remoteconfig-get.ts
+++ src/commands/remoteconfig-get.ts
@@ -3,6 +3,7 @@ import { Command } from "../command";
 import { requireAuth } from "../requireAuth";
 import Table = require("cli-table");
 import * as logger from "../logger";
+import * as fs from "fs";
 var getProjectId = require("../getProjectId");
 
 const util = require('util');
@@ -10,18 +11,18 @@ const tableHead = [
   "Entry Name", "Value"
 ];
 
-/**Function below helps create new array of values to be pushed into the table */
-function getItems(command: any) {
-  const updatedArray = [];
+ function getItems(command: any) {
+    var updatedArray = '';
   for(let item in command){
-    updatedArray.push(item);
+    updatedArray = updatedArray.concat(item, '\n');
   }
   return updatedArray;
-}
+  }
 
 module.exports = new Command("remoteconfig:get")
   .description("Get Firebase projects you have access to")
   .option("--template_version <version_number>", "grabs the specified version of the template")
+  .option("-o, --output <file path>", "save the output to the default file path")
   .before(requireAuth)
   .action(
     async (options) => {
@@ -30,19 +31,31 @@ module.exports = new Command("remoteconfig:get")
     
     const table = new Table({ head: tableHead, style: { head: ["green"] } });
 
-    const updatedConditions = [];
+    //const updatedConditions = [];
+    var updatedConditions = '';
     for(let item in template.conditions){
-      updatedConditions.push(template.conditions[item].name);
+      //updatedConditions.push(template.conditions[item].name);
+      updatedConditions += template.conditions[item].name + '\n';
       }
-    table.push(["conditions", util.inspect(updatedConditions, {showHidden: false, depth: null})])
+    table.push(["conditions", updatedConditions])
+    //table.push(["conditions", util.inspect(updatedConditions, {showHidden: false, depth: null})])
   
     const updatedParameters = getItems(template.parameters);
-    table.push(["parameters", util.inspect(updatedParameters, {showHidden: false, depth: null})])
+    table.push(["parameters",updatedParameters])
 
     const updatedParameterGroups = getItems(template.parameterGroups);
-    table.push(["parameterGroups", util.inspect(updatedParameterGroups, {showHidden: false, depth: null})])
+    table.push(["parameterGroups", updatedParameterGroups])
+    
     table.push(["version", util.inspect(template.version, {showHidden: false, depth: null})])
-    logger.info(table.toString());
+
+    var fileOut = !!options.output;
+    if(fileOut){
+      var outStream= fs.createWriteStream(options.output);
+      outStream.write(table.toString());
+    }
+    else{
+      logger.info(table.toString());
+    }
   }
   )
 
