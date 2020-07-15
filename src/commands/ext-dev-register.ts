cfaaa0364d811d7b980b2ecd521e8d66ea8bb653
Tina Liang <tinaliang@google.com>
2020-07-15 10:34:30 -0700

    implement registerpublisherprofile command (#569)
    implement registerpublisherprofile command

---
+++ src/commands/ext-dev-register.ts
@@ -0,0 +1,58 @@
+import * as clc from "cli-color";
+
+import { Command } from "../command";
+import { registerPublisherProfile } from "../extensions/extensionsApi";
+import * as getProjectId from "../getProjectId";
+import { promptOnce } from "../prompt";
+import { ensureExtensionsApiEnabled, logPrefix } from "../extensions/extensionsHelper";
+import { requirePermissions } from "../requirePermissions";
+import { FirebaseError } from "../error";
+import * as utils from "../utils";
+
+/**
+ * Register a publisher ID; run this before publishing any extensions.
+ */
+export default new Command("ext:dev:register")
+  .description("register a publisher ID; run this before publishing your first extension.")
+  // temporary until registry-specific permissions are available
+  .before(requirePermissions, ["firebaseextensions.sources.create"])
+  .before(ensureExtensionsApiEnabled)
+  .action(async (options: any) => {
+    const projectId = getProjectId(options, false);
+    const msg =
+      "What would you like to register as your publisher ID?" +
+      " This value identifies you to Extensions Registry users as the author of your extensions." +
+      " Examples: my-company-name, MyGitHubUsername\n\n" +
+      "You can only do this once for each project.";
+    const publisherId = await promptOnce({
+      name: "publisherId",
+      type: "input",
+      message: msg,
+      default: projectId,
+    });
+    try {
+      await registerPublisherProfile(projectId, publisherId);
+    } catch (err) {
+      if (err.status === 409) {
+        const error =
+          `Couldn't register the publisher ID ${clc.bold(publisherId)} to the project ${clc.bold(
+            projectId
+          )}.` +
+          " This can happen for either of two reasons:\n\n" +
+          ` - ${clc.bold(publisherId)} is registered to another project\n` +
+          ` - ${clc.bold(projectId)} already has a publisher ID\n\n` +
+          " Try again with a unique publisher ID or a new project.";
+        throw new FirebaseError(error, { exit: 1 });
+      }
+      throw new FirebaseError(
+        `Failed to register publisher ID ${clc.bold(publisherId)} for project ${clc.bold(
+          projectId
+        )}: ${err.message}`,
+        { exit: 1 }
+      );
+    }
+    return utils.logLabeledBullet(
+      logPrefix,
+      `Publisher ID ${clc.bold(publisherId)} has been registered to project ${clc.bold(projectId)}.`
+    );
+  });
--- src/commands/index.js
+++ src/commands/index.js
@@ -70,6 +70,7 @@ module.exports = function(client) {
   if (previews.extdev) {
     client.ext.dev = {};
     client.ext.dev.init = loadCommand("ext-dev-init");
+    client.ext.dev.register = loadCommand("ext-dev-register");
     client.ext.dev.emulators = {};
     client.ext.dev.emulators.start = loadCommand("ext-dev-emulators-start");
     client.ext.dev.emulators.exec = loadCommand("ext-dev-emulators-exec");
