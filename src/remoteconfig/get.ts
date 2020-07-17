c1ebbac6f678d6d120bc4a53769107ea3b7ef628
jackiechu <jackiechu@google.com>
2020-07-17 08:58:20 -0700

    Added type to interfaces for get.ts

--- src/remoteconfig/get.ts
+++ src/remoteconfig/get.ts
@@ -4,22 +4,87 @@ import { FirebaseError } from "../error";
 
 const TIMEOUT = 30000;
 
-export interface RemoteConfigTemplateData {
-  parameterGroups: any;
-  conditions: any;
-  parameters: any;
-  version: any;
+export enum TagColor {
+  BLUE = "Blue",
+  BROWN = "Brown",
+  CYAN = "Cyan",
+  DEEP_ORANGE = "Red Orange",
+  GREEN = "Green",
+  INDIGO = "Indigo",
+  LIME = "Lime",
+  ORANGE = "Orange",
+  PINK = "Pink",
+  PURPLE = "Purple",
+  TEAL = "Teal",
 }
-export interface ParameterGroupsData {
-  name: any;
-  expression: any;
+
+/** Interface representing a Remote Config parameter `value` in value options. */
+export interface ExplicitParameterValue {
+  value: string;
+}
+
+/** Interface representing a Remote Config parameter `useInAppDefault` in value options. */
+export interface InAppDefaultValue {
+  useInAppDefault: boolean;
+}
+
+export type RemoteConfigParameterValue = ExplicitParameterValue | InAppDefaultValue;
+
+/** Interface representing a Remote Config condition. */
+export interface RemoteConfigCondition {
+  name: string;
+  expression: string;
+  tagColor?: TagColor;
+}
+
+/** Interface representing a Remote Config user. */
+export interface RemoteConfigUser {
+  email: string;
+  name?: string;
+  imageUrl?: string;
+}
+
+/** Interface representing a Remote Config parameter. */
+export interface RemoteConfigParameter {
+  defaultValue?: RemoteConfigParameterValue;
+  conditionalValues?: { [key: string]: RemoteConfigParameterValue };
+  description?: string;
+}
+
+/** Interface representing a Remote Config parameter group. */
+export interface RemoteConfigParameterGroup {
+  description?: string;
+  parameters: { [key: string]: RemoteConfigParameter };
+}
+
+/** Interface representing a Remote Config version. */
+export interface Version {
+  versionNumber?: string; // int64 format
+  updateTime?: string; // in UTC
+  updateOrigin?: ('REMOTE_CONFIG_UPDATE_ORIGIN_UNSPECIFIED' | 'CONSOLE' |
+    'REST_API' | 'ADMIN_SDK_NODE');
+  updateType?: ('REMOTE_CONFIG_UPDATE_TYPE_UNSPECIFIED' |
+    'INCREMENTAL_UPDATE' | 'FORCED_UPDATE' | 'ROLLBACK');
+  updateUser?: RemoteConfigUser;
+  description?: string;
+  rollbackSource?: string;
+  isLegacy?: boolean;
+}
+
+
+export interface RemoteConfigTemplate {
+  conditions: RemoteConfigCondition[];
+  parameters: { [key: string]: RemoteConfigParameter };
+  parameterGroups: { [key: string]: RemoteConfigParameterGroup };
+  readonly etag: string;
+  version?: Version;
 }
 
 // Gets project information/template based on Firebase project ID
 export async function getTemplate(
   projectId: string,
   versionNumber = null
-): Promise<RemoteConfigTemplateData> {
+): Promise<RemoteConfigTemplate> {
   try {
     let request = `/v1/projects/${projectId}/remoteConfig`;
     if (versionNumber) {
