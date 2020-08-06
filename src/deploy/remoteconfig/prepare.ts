189097910e8b712c08186274dba8b2e519f739ff
jackiechu <jackiechu@google.com>
2020-08-05 17:35:01 -0700

    Added force option to command

--- src/deploy/remoteconfig/prepare.ts
+++ src/deploy/remoteconfig/prepare.ts
@@ -10,11 +10,14 @@ const getProjectId = require("../../getProjectId");
 const _load = require("../../config");
 
 module.exports = async function(context: any, options: any): Promise<void> {
-  // if (!context.remoteconfig || !context.remoteconfig.deploy) {
-  //   return Promise.resolve();
-  // }
+  if (!context.remoteconfig) {
+    return Promise.resolve();
+  }
   if (!options){
     console.error(new Error().stack)
+  }
+  if (options.force) {
+    return Promise.resolve();
   }
   var filePath = options.config.get("remoteconfig.template");
   const templateString = fs.readFileSync(filePath, 'utf8');
