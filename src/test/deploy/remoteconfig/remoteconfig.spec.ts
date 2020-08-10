a1dc78ee361fccc7d1a48fab8a87f0db3840072a
jackiechu <jackiechu@google.com>
2020-08-10 15:37:20 -0700

    Two out of three tests passing

--- src/test/deploy/remoteconfig/remoteconfig.spec.ts
+++ src/test/deploy/remoteconfig/remoteconfig.spec.ts
@@ -92,41 +92,140 @@ const currentTemplate: RemoteConfigTemplate = {
 describe("Remote Config Deploy", () => {
     let sandbox: sinon.SinonSandbox;
     let apiRequestStub: sinon.SinonStub;
-    let etagStub: sinon.SinonStub;
-    // cannot mock the same api twice
+    let templateStub: sinon.SinonStub;
+    
     beforeEach(() => {
         sandbox = sinon.createSandbox();
         mockAuth(sandbox);
         apiRequestStub = sandbox.stub(api, "request").throws("Unexpected API request call");
-        etagStub = sandbox.stub(rcDeploy, "createEtag");
+        templateStub = sandbox.stub(remoteconfig, "getTemplate");
+
+
+
     });
+
+
+
     afterEach(() => {
+
         sandbox.restore();
+
     });
-    
+
+
+
     describe("Publish the updated template", () => {
+
         it("should publish the latest template", async () => {
+
             apiRequestStub.onFirstCall().resolves({ body: expectedTemplateInfo });
-            etagStub.withArgs(PROJECT_NUMBER).returns("12345");
-            //etagStub.onFirstCall().resolves("123");
+
+            templateStub.withArgs(PROJECT_NUMBER).returns(currentTemplate);
+
             const etag = await rcDeploy.createEtag(PROJECT_NUMBER);
+
             const RCtemplate = await rcDeploy.publishTemplate(PROJECT_NUMBER, currentTemplate);
+
+
             expect(RCtemplate).to.deep.equal(expectedTemplateInfo);
+
             expect(apiRequestStub).to.be.calledOnceWith(
+
                 "PUT",
+
                 `/v1/projects/${PROJECT_NUMBER}/remoteConfig`,
+
                 {
+
                     auth: true,
+
                     origin: api.remoteConfigApiOrigin,
+
                     timeout: 30000,
+
                     headers: { "If-Match": etag },
+
                     data: {
+
                         conditions: currentTemplate.conditions,
+
                         parameters: currentTemplate.parameters,
+
                         parameterGroups: currentTemplate.parameterGroups,
+
                     },
+
                 }
-                );
-            });
+            );
         });
+
+        it("should publish the latest template with * etag", async () => {
+
+            apiRequestStub.onFirstCall().resolves({ body: expectedTemplateInfo });
+
+            templateStub.withArgs(PROJECT_NUMBER).returns(currentTemplate);
+            const options = {"force" : true};
+            const etag = "*";
+
+            const RCtemplate = await rcDeploy.publishTemplate(PROJECT_NUMBER, currentTemplate, options);
+
+
+            expect(RCtemplate).to.deep.equal(expectedTemplateInfo);
+
+            expect(apiRequestStub).to.be.calledOnceWith(
+
+                "PUT",
+
+                `/v1/projects/${PROJECT_NUMBER}/remoteConfig`,
+
+                {
+
+                    auth: true,
+
+                    origin: api.remoteConfigApiOrigin,
+
+                    timeout: 30000,
+
+                    headers: { "If-Match": etag },
+
+                    data: {
+
+                        conditions: currentTemplate.conditions,
+
+                        parameters: currentTemplate.parameters,
+
+                        parameterGroups: currentTemplate.parameterGroups,
+
+                    },
+
+                }
+            );
+        });
+        it("should reject if the api call fails", async () => {
+            const expectedError = new Error("HTTP Error 404: Not Found");
+      
+            apiRequestStub.onFirstCall().rejects(expectedError);
+      
+            let err;
+            try {
+              await rcDeploy.publishTemplate(PROJECT_NUMBER, currentTemplate);
+            } catch (e) {
+              err = e;
+            }
+      
+            expect(err.message).to.equal(
+              `Failed to deploy Firebase project ${PROJECT_NUMBER}. `
+            );
+            expect(err.original).to.equal(expectedError);
+            expect(apiRequestStub).to.be.calledOnceWith(
+              "PUT",
+              `/v1/projects/${PROJECT_NUMBER}/remoteConfig`,
+              {
+                auth: true,
+                origin: api.remoteConfigApiOrigin,
+                timeout: 30000,
+              }
+            );
+          });
     });
+});
