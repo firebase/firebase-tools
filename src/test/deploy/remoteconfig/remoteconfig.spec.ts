83b1eec9da4be4842dc244f07fbc11657c58e1ac
jackiechu <jackiechu@google.com>
2020-08-11 12:53:48 -0700

    Fixed prettier format based on added comments

--- src/test/deploy/remoteconfig/remoteconfig.spec.ts
+++ src/test/deploy/remoteconfig/remoteconfig.spec.ts
@@ -135,7 +135,9 @@ describe("Remote Config Deploy", () => {
         err = e;
       }
 
-      expect(err.message).to.equal(`Failed to deploy Remote Config template for Firebase project ${PROJECT_NUMBER}. `);
+      expect(err.message).to.equal(
+        `Failed to deploy Remote Config template for Firebase project ${PROJECT_NUMBER}. `
+      );
       expect(err.original).to.equal(expectedError);
       expect(apiRequestStub).to.be.calledOnceWith(
         "PUT",
