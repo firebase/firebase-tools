83387b9c34afa9444bfc8db47fd59b51a9fb5bd4
jackiechu <jackiechu@google.com>
2020-08-16 08:14:22 -0700

    Changed formatting for import and require auth. Added sentence for template using versions instead of get template

--- src/commands/remoteconfig-rollback.ts
+++ src/commands/remoteconfig-rollback.ts
@@ -4,20 +4,23 @@ import * as rcGet from "../remoteconfig/get";
 import * as rcRollback from "../remoteconfig/rollback";
 import { requirePermissions } from "../requirePermissions";
 
+import * as rcList from "../remoteconfig/versionslist";
+
 import getProjectId = require("../getProjectId");
-const { prompt } = require("../prompt");
+import { prompt } from "../prompt";
 
 module.exports = new Command("remoteconfig:rollback")
   .description(
     "Roll back a project's published Remote Config template to the one specified by the provided version number"
   )
   .before(requireAuth)
-  .before(requirePermissions, ["cloudconfig.configs.get"])
+  .before(requirePermissions, ["cloudconfig.configs.get", "cloudconfig.configs.update"])
   .option(
     "-v, --version-number <versionNumber>",
     "rollback to the specified version of the template"
   )
   .action(async (options) => {
+    // const template = await rcList.getVersions(getProjectId(options), 1);
     const template = await rcGet.getTemplate(getProjectId(options));
     let targetVersion = 0;
     if (options.versionNumber) {
