3fa717a8c26da9fc8b08e99857561a66122f91e2
jackiechu <jackiechu@google.com>
2020-08-16 08:59:26 -0700

    Made changes based on PR comments, added local interface

--- src/deploy/remoteconfig/release.ts
+++ src/deploy/remoteconfig/release.ts
@@ -1,11 +1,16 @@
 import { publishTemplate, getEtag } from "./functions";
 import getProjectNumber = require("../../getProjectNumber");
+import { RemoteConfigTemplate } from "../../remoteconfig/interfaces";
 
-module.exports = async function(context: any, options: any) {
-  if (!context || !context.template) {
-    return Promise.resolve();
+export interface ReleaseContext {
+  remoteconfigTemplate: RemoteConfigTemplate;
+}
+
+module.exports = async function(context: ReleaseContext, options: any) {
+  if (!context?.remoteconfigTemplate) {
+    return;
   }
-  const template = context.template;
+  const template = context.remoteconfigTemplate;
   const projectNumber = await getProjectNumber(options);
   const etag = await getEtag(projectNumber);
   return publishTemplate(projectNumber, template, etag, options);
