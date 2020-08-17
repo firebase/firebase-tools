d7726d61c024cb1c69274be0eafd859fc8303b25
jackiechu <jackiechu@google.com>
2020-08-17 13:24:57 -0700

    Rerunning github to pass test

--- src/commands/remoteconfig-rollback.ts
+++ src/commands/remoteconfig-rollback.ts
@@ -19,7 +19,7 @@ module.exports = new Command("remoteconfig:rollback")
     "-v, --version-number <versionNumber>",
     "rollback to the specified version of the template"
   )
-  .option("--force", "excludes prompt before overriding existing version")
+  .option("--force", "excludes prompt before overriding the existing version")
   .action(async (options) => {
     const templateVersion = await rcList.getVersions(getProjectId(options), 1);
     let targetVersion = 0;
