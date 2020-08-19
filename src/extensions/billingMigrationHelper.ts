4e03c4a26fda1580dd58d9e3ab11965c303f0d90
Daniel Young Lee <danielylee@google.com>
2020-08-19 16:42:08 -0500

    Make links interactive with terminal markdown rendering.

--- src/extensions/billingMigrationHelper.ts
+++ src/extensions/billingMigrationHelper.ts
@@ -1,8 +1,18 @@
+import * as marked from "marked";
+import TerminalRenderer = require("marked-terminal");
+
 import { FirebaseError } from "../error";
 import * as extensionsApi from "./extensionsApi";
 import { logPrefix } from "./extensionsHelper";
 import { promptOnce } from "../prompt";
 import * as utils from "../utils";
+
+marked.setOptions({
+  renderer: new TerminalRenderer(),
+});
+
+const urlPricingExamples = "https://cloud.google.com/functions/pricing#pricing_examples";
+const urlFAQ = "https://firebase.google.com/support/faq/#extensions-runtime";
 
 const billingMsgUpdate =
   "This update includes an upgrade to Node.js 10 from Node.js 8, which is no" +
@@ -10,16 +20,16 @@ const billingMsgUpdate =
   " small amount (typically around $0.01/month) for each instance of this" +
   " extension you've installed, in addition to any charges associated with" +
   " your use of Firebase services.\n\n" +
-  "See pricing examples: https://cloud.google.com/functions/pricing#pricing_examples\n" +
-  "See the FAQ: https://firebase.google.com/support/faq/#extensions-runtime";
+  `See pricing examples: **[${urlPricingExamples}](${urlPricingExamples})**\n` +
+  `See the FAQ: **[${urlFAQ}](${urlFAQ})**\n`;
 const billingMsgCreate =
   "You will be charged around $0.01/month for each instance of this extension" +
   " you install. Additionally, using this extension will contribute to your" +
   " project's overall usage level of Firebase services. However, you'll only" +
   " be charged for usage that exceeds Firebase's free tier for those" +
   " services.\n\n" +
-  "See pricing examples: https://cloud.google.com/functions/pricing#pricing_examples\n" +
-  "Learn about Firebase pricing: https://firebase.google.com/pricing";
+  `See pricing examples: **[${urlPricingExamples}](${urlPricingExamples})**\n` +
+  `See the FAQ: **[${urlFAQ}](${urlFAQ})**\n`;
 
 const defaultSpecVersion = "v1beta";
 const defaultRuntimes: { [key: string]: string } = {
@@ -46,7 +56,7 @@ export async function displayNode10UpdateBillingNotice(
   prompt: boolean
 ): Promise<void> {
   if (hasRuntime(curSpec, "nodejs8") && hasRuntime(newSpec, "nodejs10")) {
-    utils.logLabeledWarning(logPrefix, billingMsgUpdate);
+    utils.logLabeledWarning(logPrefix, marked(billingMsgUpdate));
 
     if (prompt) {
       const continueUpdate = await promptOnce({
@@ -72,7 +82,7 @@ export async function displayNode10CreateBillingNotice(
   prompt: boolean
 ): Promise<void> {
   if (hasRuntime(spec, "nodejs10")) {
-    utils.logLabeledWarning(logPrefix, billingMsgCreate);
+    utils.logLabeledWarning(logPrefix, marked(billingMsgCreate));
     if (prompt) {
       const continueUpdate = await promptOnce({
         type: "confirm",
