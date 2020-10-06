519e3332121e90b4325f34b4cc3314ca4ca49700
sgr-ksmt <melodydance.k.s@gmail.com>
2020-10-06 23:50:12 +0900

    rename to firestore:rules:compile.

--- src/commands/firestore-rules-compile.ts
+++ src/commands/firestore-rules-compile.ts
@@ -2,12 +2,12 @@ import { Command } from "../command";
 import { RulesDeploy, RulesetServiceType } from "../rulesDeploy";
 import { requireAuth } from "../requireAuth";
 
-export default new Command("firestore:rules:lint")
-  .description("Analyze `firestore.rules`")
+export default new Command("firestore:rules:compile")
+  .description("Compile `firestore.rules`")
   .before(requireAuth)
   .action(async (options: any) => {
     const rulesFile = options.config.get("firestore.rules");
     const rulesDeploy = new RulesDeploy(options, RulesetServiceType.CLOUD_FIRESTORE);
     rulesDeploy.addFile(rulesFile);
-    return await rulesDeploy.compile();
+    return rulesDeploy.compile();
   });
