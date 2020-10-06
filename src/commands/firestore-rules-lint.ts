b0083db9d11e74334593c2ff32c06979229cdde0
sgr-ksmt <melodydance.k.s@gmail.com>
2020-10-06 23:48:17 +0900

    implement logic.

--- src/commands/firestore-rules-lint.ts
+++ src/commands/firestore-rules-lint.ts
@@ -1,8 +1,13 @@
 import { Command } from "../command";
+import { RulesDeploy, RulesetServiceType } from "../rulesDeploy";
+import { requireAuth } from "../requireAuth";
 
 export default new Command("firestore:rules:lint")
   .description("Analyze `firestore.rules`")
-  .action((options: any) => {
-    console.log(options);
-    return undefined;
+  .before(requireAuth)
+  .action(async (options: any) => {
+    const rulesFile = options.config.get("firestore.rules");
+    const rulesDeploy = new RulesDeploy(options, RulesetServiceType.CLOUD_FIRESTORE);
+    rulesDeploy.addFile(rulesFile);
+    return await rulesDeploy.compile();
   });
