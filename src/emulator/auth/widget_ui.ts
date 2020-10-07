587595df1da1c7442a26923da68ae31f52f4112e
Sam Olsen <samgho@google.com>
2020-10-07 16:10:36 -0700

    Overhaul auth widget UI: remove floating form

--- src/emulator/auth/widget_ui.ts
+++ src/emulator/auth/widget_ui.ts
@@ -140,7 +140,7 @@ function finishWithUser(urlEncodedIdToken) {
 
 document.querySelector('.js-new-account').addEventListener('click', function (e) {
   e.preventDefault();
-  document.getElementById('add-user').style.display = 'flex';
+  toggleForm(true);
 });
 
 var inputs = document.querySelectorAll('.mdc-text-field');
@@ -180,8 +180,8 @@ document.getElementById('main-form').addEventListener('submit', function(e) {
   }
 });
 
-document.getElementById('close-button').addEventListener('click', function() {
-  document.getElementById('add-user').style.display = 'none';
+document.getElementById('back-button').addEventListener('click', function() {
+  toggleForm(false);
 });
 
 function createFakeClaims(info) {
@@ -225,7 +225,7 @@ function validateForm() {
     emailErrorMessage('');
   }
 
-  document.querySelector('#main-action button').disabled = !valid;
+  document.querySelector('#main-action #sign-in').disabled = !valid;
 
   return valid;
 }
@@ -268,6 +268,13 @@ function emailErrorMessage(value) {
 function capitalize(a) {
   return a.charAt(0).toUpperCase() + a.slice(1);
 }
+
+function toggleForm(showForm) {
+  document.getElementById('add-user').style.display =
+      showForm ? 'block' : 'none';
+  document.getElementById('accounts-list').style.display =
+      showForm ? 'none' : 'block';
+}
 `;
 
 const STYLE = `
@@ -294,6 +301,12 @@ body {
   width: 100%;
 }
 
+#content {
+  box-sizing: border-box;
+  padding: 12px;
+  width: 500px;
+}
+
 button {
   text-transform: none !important;
 }
@@ -304,7 +317,7 @@ button {
   color: #476282;
   display: flex;
   flex-direction: row;
-  padding: 8px 24px;
+  padding: 12px 24px;
 }
 
 .callout .content {
@@ -346,51 +359,18 @@ button {
 }
 
 #main-action {
+  display: flex;
+  flex-direction: row;
   margin-top: 15px;
-  text-align: center;
   width: 100%;
+}
+
+#main-action > button {
+  margin-right: 8px;
 }
 
 #add-user {
-  align-items: center;
-  background: rgba(0, 0, 0, 0.1);
   display: none;
-  flex-direction: column;
-  height: 100%;
-  left: 0;
-  overflow-y: auto;
-  position: fixed;
-  top: 0;
-  width: 100%;
-  z-index: 10;
-}
-
-#form-content {
-  background: white;
-  box-sizing: border-box;
-  padding: 8px;
-}
-
-@media only screen and (max-width: 600px) {
-  #form-content {
-    height: 100%;
-    left: 0;
-    overflow: auto;
-    position: fixed;
-    top: 0;
-    width: 100%;
-    z-index: 10;
-  }
-}
-
-@media only screen and (min-width: 600px) {
-  #form-content {
-    border-radius: 16px;
-    margin: 40px 0;
-    padding: 20px;
-    width: 500px;
-    z-index: 10;
-  }
 }
 `;
 
@@ -405,93 +385,98 @@ export const WIDGET_UI = `
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 <style>${STYLE}</style>
-<h1>Sign-in with <span class="js-provider-id">Provider</span></h1>
-<p>Please select an existing account in the Auth Emulator or add a new one.</p>
-<p><b>This page is a work in progress and is subject to change.</b></p>
-<div>
-  <ul>
-  ${PROVIDERS_LIST_PLACEHOLDER}
-  <li class="js-new-account"><a href="#">Add Another Account</a></li>
-  </ul>
-</div>
-<div id="add-user">
-  <div id="form-content" class="mdc-elevation--z5">
-    <div id="title">
-      <span>Sign-in with <span class="js-provider-id">Provider</span></span>
-      <button id="close-button" class="mdc-icon-button material-icons">close</button>
-    </div>
-    <div class="callout vs">
-      <i class="material-icons">info</i>
-      <div class="content">
-        Custom claims can be added after an account is created
+<div id="content">
+  <div id="title">
+    <span>Sign-in with <span class="js-provider-id">Provider</span></span>
+  </div>
+  <div id="accounts-list">
+    <ul>
+    ${PROVIDERS_LIST_PLACEHOLDER}
+    <li class="js-new-account"><a href="#">Add Another Account</a></li>
+    </ul>
+  </div>
+  <div id="add-user">
+    <div id="form-content">
+      <div class="callout vs">
+        <i class="material-icons">info</i>
+        <div class="content">
+          Custom claims can be added after an account is created
+        </div>
       </div>
+      <button id="autogen-button" class="vs mdc-button mdc-button--outlined" type="button">
+        <div class="mdc-button__ripple"></div>
+        <span class="mdc-button__label">Auto Generate User Information</span>
+      </button>
+      <form id="main-form">
+        <span class="form-label">Email</span>
+        <label class="mdc-text-field mdc-text-field--outlined">
+          <input required id="email-input" type="text"
+              class="mdc-text-field__input test" aria-labelledby="my-label-id">
+          <span class="mdc-notched-outline">
+            <span class="mdc-notched-outline__leading"></span>
+            <span class="mdc-notched-outline__notch">
+            <span class="custom-label" id="email-label">Email</span>
+            </span>
+            <span class="mdc-notched-outline__trailing"></span>
+          </span>
+        </label>
+        <span class="error-info vs" id="email-error"></span>
+
+        <span class="form-label">Display name (optional)</span>
+        <label class="mdc-text-field mdc-text-field--outlined vs">
+          <input id="display-name-input" type="text"
+              class="mdc-text-field__input test" aria-labelledby="my-label-id">
+          <span class="mdc-notched-outline">
+            <span class="mdc-notched-outline__leading"></span>
+            <span class="mdc-notched-outline__notch">
+            <span class="custom-label" id="email-label">Display name</span>
+            </span>
+            <span class="mdc-notched-outline__trailing"></span>
+          </span>
+        </label>
+
+        <span class="form-label">Screen name (optional)</span>
+        <label class="mdc-text-field mdc-text-field--outlined vs">
+          <input id="screen-name-input" type="text"
+              class="mdc-text-field__input test" aria-labelledby="my-label-id">
+          <span class="mdc-notched-outline">
+            <span class="mdc-notched-outline__leading"></span>
+            <span class="mdc-notched-outline__notch">
+            <span class="custom-label" id="email-label">Screen name</span>
+            </span>
+            <span class="mdc-notched-outline__trailing"></span>
+          </span>
+        </label>
+
+        <span class="form-label">Profile photo URL (optional)</span>
+        <label class="mdc-text-field mdc-text-field--outlined vs">
+          <input id="profile-photo-input" type="text"
+              class="mdc-text-field__input test" aria-labelledby="my-label-id">
+          <span class="mdc-notched-outline">
+            <span class="mdc-notched-outline__leading"></span>
+            <span class="mdc-notched-outline__notch">
+            <span class="custom-label" id="email-label">Profile photo URL</span>
+            </span>
+            <span class="mdc-notched-outline__trailing"></span>
+          </span>
+        </label>
+
+        <div id="main-action" class="vs">
+          <button class="mdc-button" id="back-button" type="button">
+            <div class="mdc-button__ripple"></div>
+            <i class="material-icons mdc-button__icon" aria-hidden="true"
+              >arrow_back</i
+            >
+            <span class="mdc-button__label">Back</span>
+          </button>
+          <button class="mdc-button mdc-button--raised" id="sign-in" type="submit">
+            <span class="mdc-button__label">
+              Sign in with <span class="js-provider-id">Provider</span>
+            </span>
+          </button>
+        </div>
+      </form>
     </div>
-    <button id="autogen-button" class="vs mdc-button mdc-button--outlined">
-      <div class="mdc-button__ripple"></div>
-      <span class="mdc-button__label">Auto Generate User Information</span>
-    </button>
-    <form id="main-form">
-      <span class="form-label">Email</span>
-      <label class="mdc-text-field mdc-text-field--outlined">
-        <input required id="email-input" type="text"
-            class="mdc-text-field__input test" aria-labelledby="my-label-id">
-        <span class="mdc-notched-outline">
-          <span class="mdc-notched-outline__leading"></span>
-          <span class="mdc-notched-outline__notch">
-          <span class="custom-label" id="email-label">Email</span>
-          </span>
-          <span class="mdc-notched-outline__trailing"></span>
-        </span>
-      </label>
-      <span class="error-info vs" id="email-error"></span>
-
-      <span class="form-label">Display name (optional)</span>
-      <label class="mdc-text-field mdc-text-field--outlined vs">
-        <input id="display-name-input" type="text"
-            class="mdc-text-field__input test" aria-labelledby="my-label-id">
-        <span class="mdc-notched-outline">
-          <span class="mdc-notched-outline__leading"></span>
-          <span class="mdc-notched-outline__notch">
-          <span class="custom-label" id="email-label">Display name</span>
-          </span>
-          <span class="mdc-notched-outline__trailing"></span>
-        </span>
-      </label>
-
-      <span class="form-label">Screen name (optional)</span>
-      <label class="mdc-text-field mdc-text-field--outlined vs">
-        <input id="screen-name-input" type="text"
-            class="mdc-text-field__input test" aria-labelledby="my-label-id">
-        <span class="mdc-notched-outline">
-          <span class="mdc-notched-outline__leading"></span>
-          <span class="mdc-notched-outline__notch">
-          <span class="custom-label" id="email-label">Screen name</span>
-          </span>
-          <span class="mdc-notched-outline__trailing"></span>
-        </span>
-      </label>
-
-      <span class="form-label">Profile photo URL (optional)</span>
-      <label class="mdc-text-field mdc-text-field--outlined vs">
-        <input id="profile-photo-input" type="text"
-            class="mdc-text-field__input test" aria-labelledby="my-label-id">
-        <span class="mdc-notched-outline">
-          <span class="mdc-notched-outline__leading"></span>
-          <span class="mdc-notched-outline__notch">
-          <span class="custom-label" id="email-label">Profile photo URL</span>
-          </span>
-          <span class="mdc-notched-outline__trailing"></span>
-        </span>
-      </label>
-
-      <div id="main-action" class="vs">
-        <button class="mdc-button mdc-button--raised">
-          <span class="mdc-button__label">
-            Sign in with <span class="js-provider-id">Provider</span>
-          </span>
-        </button>
-      </div>
-    </form>
   </div>
 </div>
 <script src="https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js"></script>
