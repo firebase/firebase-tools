4170b9ac3fdb670db1d017e8d5ffc5588e415a49
Bryan Kendall <bkend@google.com>
2020-10-22 10:17:39 -0700

    allow a site name to be passed on --only hosting: parameters

--- src/hosting/normalizedHostingConfigs.ts
+++ src/hosting/normalizedHostingConfigs.ts
@@ -1,13 +1,18 @@
 import { bold } from "cli-color";
 
 import { FirebaseError } from "../error";
+import logger = require("../logger");
 
 interface HostingConfig {
   site: string;
   target: string;
 }
 
-function filterOnly(configs: HostingConfig[], onlyString: string): HostingConfig[] {
+function filterOnly(
+  configs: HostingConfig[],
+  onlyString: string,
+  defaultSite: string
+): HostingConfig[] {
   if (!onlyString) {
     return configs;
   }
@@ -24,16 +29,29 @@ function filterOnly(configs: HostingConfig[], onlyString: string): HostingConfig
     .filter((target) => target.startsWith("hosting:"))
     .map((target) => target.replace("hosting:", ""));
 
+  // There is a case where users have not set any targets but use --only to
+  // specify the site name. This was working before because we weren't checking
+  // the validity of the target name before doing the final filter below. Since
+  // the case exists where a single (non-array, non-multi-site) config will
+  // have `config.site` set, we are going to short circuit the validity check
+  // of the targets to ensure the site name can be specified in a non-multi-
+  // site setup.
+  const configsHaveTargets = configs.some((c) => c.target);
+  const onlyDefaultSite = onlyTargets.length === 1 && onlyTargets[0] === defaultSite;
+  if (!configsHaveTargets && onlyDefaultSite) {
+    logger.debug("filtering configs down to default site");
+    return configs.filter((c) => c.site === defaultSite);
+  }
+
   // Check to see that all the hosting deploy targets exist in the hosting config
-  onlyTargets.forEach((onlyTarget) => {
+  for (const onlyTarget of onlyTargets) {
     if (!configs.some((config) => config.target === onlyTarget)) {
       throw new FirebaseError(`Hosting target ${bold(onlyTarget)} not detected in firebase.json`);
     }
-  });
+  }
 
-  return configs.filter((config: HostingConfig) =>
-    onlyTargets.includes(config.target || config.site)
-  );
+  const filtered = configs.filter((config) => onlyTargets.includes(config.target));
+  return filtered;
 }
 
 /**
@@ -46,6 +64,7 @@ export function normalizedHostingConfigs(
   cmdOptions: any, // eslint-disable-line @typescript-eslint/no-explicit-any
   options: { resolveTargets?: boolean } = {}
 ): HostingConfig[] {
+  let hostingConfigs: HostingConfig[] = [];
   let configs = cmdOptions.config.get("hosting");
   if (!configs) {
     return [];
@@ -61,10 +80,10 @@ export function normalizedHostingConfigs(
     configs = [configs];
   }
 
-  configs = filterOnly(configs, cmdOptions.only);
+  hostingConfigs = filterOnly(configs, cmdOptions.only, cmdOptions.site);
 
   if (options.resolveTargets) {
-    configs.forEach((cfg: HostingConfig) => {
+    for (const cfg of hostingConfigs) {
       if (cfg.target) {
         const matchingTargets = cmdOptions.rc.requireTarget(
           cmdOptions.project,
@@ -82,8 +101,8 @@ export function normalizedHostingConfigs(
       } else if (!cfg.site) {
         throw new FirebaseError('Must supply either "site" or "target" in each "hosting" config.');
       }
-    });
+    }
   }
 
-  return configs;
+  return hostingConfigs;
 }
