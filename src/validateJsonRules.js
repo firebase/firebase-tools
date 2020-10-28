b1ac5d28f9b4c0a44e9b0954f58d51dc16d13bb7
Bryan Kendall <bkend@google.com>
2020-10-28 11:35:51 -0700

    remove validateJsonRules

--- src/validateJsonRules.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var cjson = require("cjson");
-var _ = require("lodash");
-
-module.exports = function(rules) {
-  var parsed = cjson.parse(rules);
-  return _.has(parsed, "rules");
-};
