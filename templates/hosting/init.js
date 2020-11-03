2f53d4bd0d70390b6f4f355a027a54e70a5e78af
Sam Stern <samstern@google.com>
2020-11-03 14:33:14 -0500

    Automatically call useEmulator in Hosting emulator (#2753)

--- templates/hosting/init.js
+++ templates/hosting/init.js
@@ -2,4 +2,30 @@ if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Fireba
 /*--CONFIG--*/
 if (firebaseConfig) {
   firebase.initializeApp(firebaseConfig);
+
+  /*--EMULATORS--*/
+  if (firebaseEmulators) {
+    console.log("Automatically connecting Firebase SDKs to running emulators:");
+    Object.keys(firebaseEmulators).forEach(function(key) {
+      console.log('\t' + key + ': http://' +  firebaseEmulators[key].host + ':' + firebaseEmulators[key].port );
+    });
+
+    if (firebaseEmulators.database && typeof firebase.database === 'function') {
+      firebase.database().useEmulator(firebaseEmulators.database.host, firebaseEmulators.database.port);
+    }
+
+    if (firebaseEmulators.firestore && typeof firebase.firestore === 'function') {
+      firebase.firestore().useEmulator(firebaseEmulators.firestore.host, firebaseEmulators.firestore.port);
+    }
+
+    if (firebaseEmulators.functions && typeof firebase.functions === 'function') {
+      firebase.functions().useEmulator(firebaseEmulators.functions.host, firebaseEmulators.functions.port);
+    }
+
+    if (firebaseEmulators.auth && typeof firebase.auth === 'function') {
+      firebase.auth().useEmulator('http://' + firebaseEmulators.auth.host + ':' + firebaseEmulators.auth.port);
+    }
+  } else {
+    console.log("To automatically connect the Firebase SDKs to running emulators, replace '/__/firebase/init.js' with '/__/firebase/init.js?useEmulator=true' in your index.html");
+  }
 }
