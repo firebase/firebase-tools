d71941938e311b1fd700b53aca9ba23bfc1738f9
Bryan Kendall <bkend@google.com>
2020-11-05 12:46:36 -0800

    normalize channel names on hosting:channel:create and remove hashes (#2784)
    * normalize channel names on :create and remove hashes
    
    * update comments

--- src/hosting/api.ts
+++ src/hosting/api.ts
@@ -153,14 +153,14 @@ export interface Version {
 
 /**
  * normalizeName normalizes a name given to it. Most useful for normalizing
- * user provided names. This removes any `/`, ':', or '_' characters and
+ * user provided names. This removes any `/`, ':', '_', or '#' characters and
  * replaces them with dashes (`-`).
  * @param s the name to normalize.
  * @return the normalized name.
  */
 export function normalizeName(s: string): string {
-  // Using a regex replaces *all* bad characters.
-  return s.replace(/[/:_]/g, "-");
+  // Using a regex replaces *all* specified characters at once.
+  return s.replace(/[/:_#]/g, "-");
 }
 
 const apiClient = new Client({
