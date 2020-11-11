e6fd6b4bd70adefab3dc8cdd7bb9c750d760f4ae
Bryan Kendall <bkend@google.com>
2020-11-10 16:36:58 -0800

    replace String types with string

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,7 +36,6 @@ module.exports = {
         "jsdoc/require-returns-type": "off",
 
         "@typescript-eslint/await-thenable": "warn", // TODO(bkendall): remove, allow to error.
-        "@typescript-eslint/ban-types": "warn", // TODO(bkendall): remove, allow to error.
         "@typescript-eslint/camelcase": "warn", // TODO(bkendall): remove, allow to error.
         "@typescript-eslint/explicit-function-return-type": ["warn", { allowExpressions: true }], // TODO(bkendall): SET to error.
         "@typescript-eslint/no-inferrable-types": "warn", // TODO(bkendall): remove, allow to error.
