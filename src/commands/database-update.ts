70a708db3e5c9539ae0c84a7b677a859a19c4867
Bryan Kendall <bkend@google.com>
2020-11-12 09:58:26 -0800

    fix database:update

--- src/commands/database-update.ts
+++ src/commands/database-update.ts
@@ -30,7 +30,7 @@ export default new Command("database:update <path> [infile]")
     if (!path.startsWith("/")) {
       throw new FirebaseError("Path must begin with /");
     }
-    const origin = realtimeOriginOrEmulatorOrCustomUrl(options);
+    const origin = realtimeOriginOrEmulatorOrCustomUrl(options.instanceDetails.databaseUrl);
     const url = utils.getDatabaseUrl(origin, options.instance, path);
     if (!options.confirm) {
       const confirmed = await promptOnce({
