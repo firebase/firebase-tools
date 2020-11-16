cbb9292bb796fb72f9e6b41df8fbcd51ac797f66
Bryan Kendall <bkend@google.com>
2020-11-16 14:14:11 -0800

    fix database get to be more resiliant

--- src/commands/database-get.ts
+++ src/commands/database-get.ts
@@ -128,19 +128,22 @@ export default new Command("database:get <path>")
       throw responseToError({ statusCode: res.status }, d);
     }
 
-    res.body.pipe(outStream);
-    // Tack on a single newline at the end of the stream.
-    res.body.once("end", () => {
-      if (outStream === process.stdout) {
-        // `stdout` can simply be written to.
-        outStream.write("\n");
-      } else if (outStream instanceof fs.WriteStream) {
-        // .pipe closes the output file stream, so we need to re-open the file.
-        const s = fs.createWriteStream(options.output, { flags: "a" });
-        s.write("\n");
-        s.close();
-      } else {
-        logger.debug("[database:get] Could not write line break to outStream");
-      }
+    res.body.pipe(outStream, { end: false });
+
+    return new Promise((resolve) => {
+      // Tack on a single newline at the end of the stream.
+      res.body.once("end", () => {
+        if (outStream === process.stdout) {
+          // `stdout` can simply be written to.
+          outStream.write("\n");
+          resolve();
+        } else if (outStream instanceof fs.WriteStream) {
+          outStream.write("\n");
+          outStream.on("close", () => resolve());
+          outStream.close();
+        } else {
+          logger.debug("[database:get] Could not write line break to outStream");
+        }
+      });
     });
   });
