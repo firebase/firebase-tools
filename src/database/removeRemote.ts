772e2d550573008b21f29d227aaffa3d3a1dc589
Bryan Kendall <bkend@google.com>
2020-11-16 12:16:51 -0800

    replace request usage with apiv2

--- src/database/removeRemote.ts
+++ src/database/removeRemote.ts
@@ -1,9 +1,7 @@
-import * as request from "request";
-import { Response } from "request";
-import * as utils from "../utils";
-import { FirebaseError } from "../error";
+import { Client } from "../apiv2";
+import { URL } from "url";
 import * as logger from "../logger";
-import * as api from "../api";
+import * as utils from "../utils";
 
 export interface RemoveRemote {
   /**
@@ -23,10 +21,14 @@ export interface RemoveRemote {
 export class RTDBRemoveRemote implements RemoveRemote {
   private instance: string;
   private host: string;
+  private apiClient: Client;
 
   constructor(instance: string, host: string) {
     this.instance = instance;
     this.host = host;
+
+    const url = new URL(utils.getDatabaseUrl(this.host, this.instance, "/"));
+    this.apiClient = new Client({ urlPrefix: url.origin, auth: true });
   }
 
   deletePath(path: string): Promise<boolean> {
@@ -41,41 +43,21 @@ export class RTDBRemoveRemote implements RemoveRemote {
     return this.patch(path, body, `${subPaths.length} subpaths`);
   }
 
-  private patch(path: string, body: any, note: string): Promise<boolean> {
+  private async patch(path: string, body: any, note: string): Promise<boolean> {
     const t0 = Date.now();
-    return new Promise((resolve, reject) => {
-      const url = utils.getDatabaseUrl(
-        this.host,
-        this.instance,
-        path + ".json?print=silent&writeSizeLimit=tiny"
+    const url = new URL(utils.getDatabaseUrl(this.host, this.instance, path + ".json"));
+    const queryParams = { print: "silent", writeSizeLimit: "tiny" };
+    try {
+      await this.apiClient.patch(url.pathname, body, { queryParams });
+    } catch (e) {
+      const dt = Date.now() - t0;
+      logger.debug(
+        `[database] Failed to remove ${note} at ${path} time: ${dt}ms, will try recursively chunked deletes.`
       );
-      return api
-        .addRequestHeaders({
-          url,
-          body,
-          json: true,
-        })
-        .then((reqOptionsWithToken) => {
-          request.patch(reqOptionsWithToken, (err: Error, res: Response, resBody: any) => {
-            if (err) {
-              return reject(
-                new FirebaseError(`Unexpected error while removing data at ${path}`, {
-                  exit: 2,
-                  original: err,
-                })
-              );
-            }
-            const dt = Date.now() - t0;
-            if (res.statusCode >= 400) {
-              logger.debug(
-                `[database] Failed to remove ${note} at ${path} time: ${dt}ms, will try recursively chunked deletes.`
-              );
-              return resolve(false);
-            }
-            logger.debug(`[database] Sucessfully removed ${note} at ${path} time: ${dt}ms`);
-            return resolve(true);
-          });
-        });
-    });
+      return false;
+    }
+    const dt = Date.now() - t0;
+    logger.debug(`[database] Sucessfully removed ${note} at ${path} time: ${dt}ms`);
+    return true;
   }
 }
