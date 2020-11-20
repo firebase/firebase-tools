be1b4c9af210c192a1a937181776e610dfbf4a9c
Saraj Munjal <saraj.munjal@gmail.com>
2020-11-19 16:49:49 -0800

    Fix

--- src/requireDatabaseInstance.ts
+++ src/requireDatabaseInstance.ts
@@ -12,20 +12,21 @@ export async function requireDatabaseInstance(options: any): Promise<void> {
   if (options.instance) {
     return Promise.resolve();
   }
+  let instance;
   try {
-    const instance = await getDefaultDatabaseInstance(options);
-    if (instance === "") {
-      throw new FirebaseError(
-        `It looks like you haven't created a Realtime Database instance in this project before. Go to ${clc.bold.underline(
-          `https://console.firebase.google.com/project/${options.project}/database`
-        )} to create your default Realtime Database instance.`
-      );
-    }
-    options.instance = instance;
-    return;
+    instance = await getDefaultDatabaseInstance(options);
   } catch (err) {
     return utils.reject(`Failed to get details for project: ${options.project}.`, {
       original: err,
     });
   }
+  if (instance === "") {
+    throw new FirebaseError(
+      `It looks like you haven't created a Realtime Database instance in this project before. Go to ${clc.bold.underline(
+        `https://console.firebase.google.com/project/${options.project}/database`
+      )} to create your default Realtime Database instance.`
+    );
+  }
+  options.instance = instance;
+  return;
 }
