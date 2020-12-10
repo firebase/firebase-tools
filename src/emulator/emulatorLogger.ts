7c870cba8316f2eef0ffa7b5bf236803a9e314d2
meijin <TeXmeijin@users.noreply.github.com>
2020-12-10 18:59:54 +0900

    Suggested changed message
    Co-authored-by: Sam Stern <samstern@google.com>

--- src/emulator/emulatorLogger.ts
+++ src/emulator/emulatorLogger.ts
@@ -237,7 +237,7 @@ You can probably fix this by running "npm install ${systemLog.data.name}@latest"
         utils.logWarning(helper.join("\n"), "warn", this.data);
         break;
       case "function-runtimeconfig-json-invalid":
-        this.log("WARN", "Your .runtimeconfig.json is exist. But the JSON format seems invalid.");
+        this.log("WARN", "Found .runtimeconfig.json but the JSON format is invalid.");
       default:
       // Silence
     }
