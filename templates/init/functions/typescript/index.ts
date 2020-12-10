dd35a2fed8a6eb0bd1e6a01be0899a84fe80c2de
Bryan Kendall <bkend@google.com>
2020-12-10 12:04:48 -0800

    update template files

--- templates/init/functions/typescript/index.ts
+++ templates/init/functions/typescript/index.ts
@@ -1,4 +1,4 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
