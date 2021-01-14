d8b7180dc1da962e2b812a8c1e1b87eb0b8a6e18
Bryan Kendall <bkend@google.com>
2021-01-14 11:19:12 -0800

    setup the emulator tests to be compiled and then run

--- scripts/emulator-tests/run.sh
+++ scripts/emulator-tests/run.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+set -ex
 
+# Clean the destination of the upcoming build.
 rm -rf dev
-tsc --build tsconfig.dev.json
+# Run a special build for these tests and the source code.
+tsc --build scripts/emulator-tests/tsconfig.dev.json
+# Need to copy `package.json` to the directory so it can be referenced in code.
 cp package.json dev/package.json
 
+# Run the tests from the built dev directory.
 mocha \
   --bail \
   --require ts-node/register \
   --require src/test/helpers/mocha-bootstrap.ts \
   dev/scripts/emulator-tests/*.spec.*
+
+# Remove the built artifacts.
+rm -rf dev
