23954f4e33be9a2c585980e8517b62144748eaa6
Joe Hanley <joehanley@google.com>
2021-01-20 11:06:20 -0800

    Typescriptifying prepareFunctionsUpload, and updating filesize package to get types

--- src/deploy/functions/deploy.ts
+++ src/deploy/functions/deploy.ts
@@ -4,7 +4,7 @@ import { setGracefulCleanup } from "tmp";
 import { functionsUploadRegion } from "../../api";
 import * as gcp from "../../gcp";
 import { logBullet, logSuccess, logWarning } from "../../utils";
-import * as prepareFunctionsUpload from "../../prepareFunctionsUpload";
+import { prepareFunctionsUpload } from "../../prepareFunctionsUpload";
 import { checkHttpIam } from "./checkIam";
 
 const GCP_REGION = functionsUploadRegion;
