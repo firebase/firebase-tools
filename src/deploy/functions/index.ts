b4944a4425670a94a8b3d764dc32f372ee226325
joehan <joehanley@google.com>
2021-01-21 15:26:42 -0800

    Refactoring prepare stage of functions deploy (#3067)
    * refactoring prepare setp of functions deploy to use typescript, check for failure policies, and parse function triggers
    
    * refactoring prepare setp of functions deploy to use typescript, check for failure policies, and parse function triggers
    
    * commenting out functionNamesAreValid
    
    * formats

--- src/deploy/functions/index.ts
+++ src/deploy/functions/index.ts
@@ -1,4 +1,4 @@
-import * as prepare from "./prepare";
 import * as release from "./release";
+export { prepare } from "./prepare";
 export { deploy } from "./deploy";
-export { prepare, release };
+export { release };
---
+++ src/deploy/functions/prepare.ts
@@ -0,0 +1,65 @@
+import * as _ from "lodash";
+import * as clc from "cli-color";
+
+import * as ensureApiEnabled from "../../ensureApiEnabled";
+import * as functionsConfig from "../../functionsConfig";
+import * as getProjectId from "../../getProjectId";
+import { logBullet } from "../../utils";
+import { getRuntimeChoice } from "../../parseRuntimeAndValidateSDK";
+import { getFunctionsInfo, promptForFailurePolicies } from "../../functionsDeployHelper";
+import * as prepareFunctionsUpload from "../../prepareFunctionsUpload";
+import * as validate from "./validate";
+import { checkRuntimeDependencies } from "./checkRuntimeDependencies";
+
+export async function prepare(context: any, options: any, payload: any): Promise<void> {
+  if (!options.config.has("functions")) {
+    return;
+  }
+
+  const sourceDirName = options.config.get("functions.source");
+  const sourceDir = options.config.path(sourceDirName);
+  const projectDir = options.config.projectDir;
+  const projectId = getProjectId(options);
+
+  // Check what runtime to use, first in firebase.json, then in 'engines' field.
+  const runtimeFromConfig = options.config.get("functions.runtime");
+  context.runtimeChoice = getRuntimeChoice(sourceDir, runtimeFromConfig);
+
+  // Check that all necessary APIs are enabled.
+  const checkAPIsEnabled = await Promise.all([
+    ensureApiEnabled.ensure(options.project, "cloudfunctions.googleapis.com", "functions"),
+    ensureApiEnabled.check(projectId, "runtimeconfig.googleapis.com", "runtimeconfig", true),
+    checkRuntimeDependencies(projectId, context.runtimeChoice),
+  ]);
+  _.set(context, "runtimeConfigEnabled", checkAPIsEnabled[1]);
+
+  // Get the Firebase Config.
+  const firebaseConfig = await functionsConfig.getFirebaseConfig(options);
+  _.set(context, "firebaseConfig", firebaseConfig);
+
+  // Prepare the functions directory for upload, and set context.triggers.
+  logBullet(
+    clc.cyan.bold("functions:") +
+      " preparing " +
+      clc.bold(options.config.get("functions.source")) +
+      " directory for uploading..."
+  );
+  const source = await prepareFunctionsUpload(context, options);
+  _.set(context, "functionsSource", source);
+
+  // Get a list of CloudFunctionTriggers, with duplicates for each region.
+  payload.functions = {};
+  payload.functions.triggers = getFunctionsInfo(
+    options.config.get("functions.triggers"),
+    projectId
+  );
+
+  // Validate the function code that is being deployed.
+  validate.functionsDirectoryExists(options, sourceDirName);
+  // validate.functionNamesAreValid(payload.functionNames);
+  // TODO: This doesn't do anything meaningful right now because payload.functions is not defined
+  validate.packageJsonIsValid(sourceDirName, sourceDir, projectDir, !!runtimeFromConfig);
+
+  // Display a warning and prompt if any functions have failurePolicies.
+  await promptForFailurePolicies(options, payload.functions.triggers);
+}
