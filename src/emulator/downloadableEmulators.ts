3014db6ee1c839b76675823ef13119f4ef487a6b
Sam Stern <samstern@google.com>
2021-04-01 15:17:50 -0400

    Release Firestore emulator v1.11.13 (#3251)
    * Release Firestore emulator v1.11.13
    
    * Version fix

--- src/emulator/downloadableEmulators.ts
+++ src/emulator/downloadableEmulators.ts
@@ -39,14 +39,14 @@ export const DownloadDetails: { [s in DownloadableEmulators]: EmulatorDownloadDe
     },
   },
   firestore: {
-    downloadPath: path.join(CACHE_DIR, "cloud-firestore-emulator-v1.11.12.jar"),
-    version: "1.11.12",
+    downloadPath: path.join(CACHE_DIR, "cloud-firestore-emulator-v1.11.13.jar"),
+    version: "1.11.13",
     opts: {
       cacheDir: CACHE_DIR,
       remoteUrl:
-        "https://storage.googleapis.com/firebase-preview-drop/emulator/cloud-firestore-emulator-v1.11.12.jar",
-      expectedSize: 63822243,
-      expectedChecksum: "9cdb00c6e05680830f77f63269aba765",
+        "https://storage.googleapis.com/firebase-preview-drop/emulator/cloud-firestore-emulator-v1.11.13.jar",
+      expectedSize: 61711111,
+      expectedChecksum: "c6f94e5ac6a18d654843bf28845b4218",
       namePrefix: "cloud-firestore-emulator",
     },
   },
