0dd344dde1e274e0845c0a13733d5b2f07bef587
Sam Stern <samstern@google.com>
2021-04-06 12:39:23 -0400

    Support multiple accounts (#3192)

---
+++ src/init/features/account.ts
@@ -0,0 +1,85 @@
+import { logger } from "../../logger";
+import * as utils from "../../utils";
+import {
+  getAllAccounts,
+  loginAdditionalAccount,
+  setActiveAccount,
+  findAccountByEmail,
+  Account,
+  setProjectAccount,
+} from "../../auth";
+import { promptOnce } from "../../prompt";
+import { FirebaseError } from "../../error";
+
+async function promptForAccount() {
+  logger.info();
+  logger.info(
+    `Which account do you want to use for this project? Choose an account or add a new one now`
+  );
+  logger.info();
+
+  const allAccounts = getAllAccounts();
+  const choices = allAccounts.map((a) => {
+    return {
+      name: a.user.email,
+      value: a.user.email,
+    };
+  });
+
+  choices.push({
+    name: "(add a new account)",
+    value: "__add__",
+  });
+
+  const emailChoice: string = await promptOnce({
+    type: "list",
+    name: "email",
+    message: "Please select an option:",
+    choices,
+  });
+
+  if (emailChoice == "__add__") {
+    const newAccount = await loginAdditionalAccount(/* useLocalhost= */ true);
+    if (!newAccount) {
+      throw new FirebaseError("Failed to add new account", { exit: 1 });
+    }
+
+    return newAccount;
+  } else {
+    return findAccountByEmail(emailChoice);
+  }
+}
+
+/**
+ * Sets up the project default account.
+ * @param setup A helper object to use for the rest of the init features.
+ * @param config Configuration for the project.
+ * @param options Command line options.
+ */
+export async function doSetup(setup: any, config: any, options: any): Promise<void> {
+  let account: Account | undefined;
+
+  if (options.account) {
+    account = findAccountByEmail(options.account);
+    if (!account) {
+      throw new FirebaseError(`Invalid account ${options.account}`, { exit: 1 });
+    }
+  } else {
+    account = await promptForAccount();
+  }
+
+  if (!account) {
+    throw new FirebaseError(`No account selected, have you run "firebase login"?`, { exit: 1 });
+  }
+
+  // Set the global auth state
+  setActiveAccount(options, account);
+
+  // Set the project default user
+  if (config.projectDir) {
+    setProjectAccount(config.projectDir, account.user.email);
+  }
+
+  logger.info();
+  utils.logSuccess(`Using account: ${account.user.email}`);
+}
--- src/requireAuth.ts
+++ src/requireAuth.ts
@@ -3,11 +3,11 @@ import * as clc from "cli-color";
 
 import * as api from "./api";
 import * as apiv2 from "./apiv2";
-import { configstore } from "./configstore";
 import { FirebaseError } from "./error";
 import { logger } from "./logger";
 import * as utils from "./utils";
 import * as scopes from "./scopes";
+import { Tokens, User, setRefreshToken, setActiveAccount } from "./auth";
 
 const AUTH_ERROR_MESSAGE = `Command requires authentication, please run ${clc.bold(
   "firebase login"
@@ -48,8 +48,8 @@ export async function requireAuth(options: any): Promise<void> {
   api.setScopes([scopes.CLOUD_PLATFORM, scopes.FIREBASE_PLATFORM]);
   options.authScopes = api.getScopes();
 
-  const tokens = configstore.get("tokens");
-  const user = configstore.get("user");
+  const tokens = options.tokens as Tokens | undefined;
+  const user = options.user as User | undefined;
 
   let tokenOpt = utils.getInheritedOption(options, "token");
   if (tokenOpt) {
@@ -57,7 +57,7 @@ export async function requireAuth(options: any): Promise<void> {
   } else if (process.env.FIREBASE_TOKEN) {
     logger.debug("> authorizing via FIREBASE_TOKEN environment variable");
   } else if (user) {
-    logger.debug("> authorizing via signed-in user");
+    logger.debug(`> authorizing via signed-in user (${user.email})`);
   } else {
     try {
       return await autoAuth(options, options.authScopes);
@@ -72,8 +72,7 @@ export async function requireAuth(options: any): Promise<void> {
   tokenOpt = tokenOpt || process.env.FIREBASE_TOKEN;
 
   if (tokenOpt) {
-    api.setRefreshToken(tokenOpt);
-    apiv2.setRefreshToken(tokenOpt);
+    setRefreshToken(tokenOpt);
     return;
   }
 
@@ -81,8 +80,5 @@ export async function requireAuth(options: any): Promise<void> {
     throw new FirebaseError(AUTH_ERROR_MESSAGE);
   }
 
-  options.user = user;
-  options.tokens = tokens;
-  api.setRefreshToken(tokens.refresh_token);
-  apiv2.setRefreshToken(tokens.refresh_token);
+  setActiveAccount(options, { user, tokens });
 }
