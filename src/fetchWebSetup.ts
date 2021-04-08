79c5727609bccf3f375ec01b6d5214c53ce9a753
Sam <samstern@google.com>
2021-04-08 14:27:59 +0100

    Include appId in WebConfig

--- src/fetchWebSetup.ts
+++ src/fetchWebSetup.ts
@@ -1,7 +1,8 @@
 import { Client } from "./apiv2";
 import { configstore } from "./configstore";
-import { firebaseApiOrigin } from "./api";
+import { firebaseApiOrigin, hostingApiOrigin } from "./api";
 import * as getProjectId from "./getProjectId";
+import { logger } from "./logger";
 
 export interface WebConfig {
   projectId: string;
@@ -14,7 +15,28 @@ export interface WebConfig {
   messagingSenderId?: string;
 }
 
+/**
+ * See
+ * https://firebase.google.com/docs/reference/hosting/rest/v1beta1/projects.sites#Site
+ */
+interface Site {
+  name: string;
+  defaultUrl: string;
+  appId?: string;
+  labels?: Record<string, string>;
+  type: "DEFAULT_SITE" | "USER_SITE" | "SITE_UNSPECIFIED";
+}
+
+interface ListSitesResponse {
+  sites: Site[];
+}
+
 const apiClient = new Client({ urlPrefix: firebaseApiOrigin, auth: true, apiVersion: "v1beta1" });
+const hostingApiClient = new Client({
+  urlPrefix: hostingApiOrigin,
+  auth: true,
+  apiVersion: "v1beta1",
+});
 
 const CONFIGSTORE_KEY = "webconfig";
 
@@ -42,8 +64,29 @@ export function getCachedWebSetup(options: any): WebConfig | undefined {
  */
 export async function fetchWebSetup(options: any): Promise<WebConfig> {
   const projectId = getProjectId(options, false);
-  const res = await apiClient.get<WebConfig>(`/projects/${projectId}/webApps/-/config`);
+
+  // Try to determine the appId from the default Hosting site, if it is linked.
+  let hostingAppId: string | undefined = undefined;
+  try {
+    const sites = await hostingApiClient.get<ListSitesResponse>(`/projects/${projectId}/sites`);
+    const defaultSite = sites.body.sites.find((s) => s.type === "DEFAULT_SITE");
+    if (defaultSite && defaultSite.appId) {
+      hostingAppId = defaultSite.appId;
+    }
+  } catch (e) {
+    logger.debug("Failed to list hosting sites");
+    logger.debug(e);
+  }
+
+  // Get the web app config for the appId, or use the '-' special value if the appId is not known
+  const appId = hostingAppId || "-";
+  const res = await apiClient.get<WebConfig>(`/projects/${projectId}/webApps/${appId}/config`);
   const config = res.body;
+
+  if (!config.appId && hostingAppId) {
+    config.appId = hostingAppId;
+  }
+
   setCachedWebSetup(config.projectId, config);
   return config;
 }
