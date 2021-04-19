0aaa8edbbfe20b07a759d7bb97c5ae23a8e88a9e
Sam <samstern@google.com>
2021-04-19 14:52:07 +0100

    Test fix

--- src/test/emulators/adminSdkConfig.test.ts
+++ src/test/emulators/adminSdkConfig.test.ts
@@ -5,7 +5,7 @@ describe("adminSdkConfig", () => {
   describe("getProjectAdminSdkConfigOrCached", () => {
     it("should return a fake config for a demo project id", async () => {
       const projectId = "demo-project-1234";
-      await expect(getProjectAdminSdkConfigOrCached(projectId)).to.eventually.equal({
+      await expect(getProjectAdminSdkConfigOrCached(projectId)).to.eventually.deep.equal({
         projectId: "demo-project-1234",
         databaseURL: "https://demo-project-1234.firebaseio.com",
         storageBucket: "demo-project-1234.appspot.com",
--- src/test/fetchWebSetup.spec.ts
+++ src/test/fetchWebSetup.spec.ts
@@ -66,7 +66,7 @@ describe("fetchWebSetup module", () => {
 
     it("should return a fake config for a demo project id", async () => {
       const projectId = "demo-project-1234";
-      await expect(fetchWebSetup({ project: projectId })).to.eventually.equal({
+      await expect(fetchWebSetup({ project: projectId })).to.eventually.deep.equal({
         projectId: "demo-project-1234",
         databaseURL: "https://demo-project-1234.firebaseio.com",
         storageBucket: "demo-project-1234.appspot.com",
