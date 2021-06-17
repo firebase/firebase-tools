de6142ddc2b9d80d293681863e58ecd8606df232
Sam <samstern@google.com>
2021-06-17 14:45:40 +0100

    Lint

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,5 +1,5 @@
 //
-// NOTE: 
+// NOTE:
 // The contents of this file are used to generate the JSON Schema documents in
 // the schema/ directory. After changing this file you will need to run
 // 'npm run generate:json-schema' to regenerate the schema files.
