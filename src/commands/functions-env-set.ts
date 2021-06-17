ed2a0d3ec80b47d80cf42ca3f42dce97a51ed145
Daniel Young Lee <danielylee@google.com>
2021-06-17 17:54:19 +0900

    Make set command safer with a prompt.

--- src/commands/functions-env-set.ts
+++ src/commands/functions-env-set.ts
@@ -5,6 +5,7 @@ import * as path from "path";
 
 import { Command } from "../command";
 import { logger } from "../logger";
+import { promptOnce } from "../prompt";
 import { requirePermissions } from "../requirePermissions";
 import * as fenv from "../functions/env";
 import * as getProjectId from "../getProjectId";
@@ -13,12 +14,13 @@ import * as utils from "../utils";
 export default new Command("functions:env:set [values...]")
   .description("set environment variables, completely replacing the existing set")
   .option("--file <envFile>", "path to file with environment variables in .env format.")
+  .option("-f, --force", "No confirmation. Otherwise, a confirmation prompt will appear.")
   .before(requirePermissions, [
     "firebase.envstores.create",
     "firebase.envstores.delete",
     "firebase.envstores.update",
   ])
-  .action(async (args: string[], options: any) => {
+  .action(async (args: string[], options: { file?: string; force: boolean }) => {
     if (!options.file && !args.length) {
       return utils.reject("Must supply at least one key/value pair, e.g. " + clc.bold("FOO=bar"));
     }
@@ -29,7 +31,30 @@ export default new Command("functions:env:set [values...]")
     } else {
       setEnvs = fenv.parseKvArgs(args);
     }
+
     const projectId = getProjectId(options);
+    const curEnvs = await fenv.getEnvs(projectId);
+
+    if (curEnvs && Object.keys(curEnvs).length) {
+      const confirm = await promptOnce(
+        {
+          type: "confirm",
+          name: "force",
+          default: false,
+          message:
+            "You about to replace current set of environment variables:\n" +
+            `\n${fenv.formatEnv(curEnvs)}\n\n` +
+            "with the following environment variables:\n" +
+            `\n${fenv.formatEnv(setEnvs)}\n\n` +
+            "Are you sure you want to do this?",
+        },
+        options
+      );
+      if (!confirm) {
+        return utils.reject("Command aborted.", { exit: 1 });
+      }
+    }
+
     const envs = await fenv.setEnvs(projectId, setEnvs);
     logger.info(fenv.formatEnv(envs));
     return envs;
