b8bcd2ce84cc0f66aa7d1055cf3391bd5e5906a5
Thomas Bouldin <inlined@google.com>
2021-06-17 15:13:04 -0700

    Use vendoring to fetch SDK

--- src/init/features/functions/golang.ts
+++ src/init/features/functions/golang.ts
@@ -1,13 +1,16 @@
+import { promisify } from "util";
 import * as fs from "fs";
 import * as path from "path";
 import * as spawn from "cross-spawn";
+import * as stream from "stream";
+import * as unzipper from "unzipper";
+import fetch from "node-fetch"
 
 import { FirebaseError } from "../../../error";
 import { Config } from "../../../config";
 import { promptOnce } from "../../../prompt";
 import * as utils from "../../../utils";
 import { logger } from "../../../logger";
-import { options } from "../../../commands/auth-export";
 
 const clc = require("cli-color");
 
@@ -17,11 +20,20 @@ const RUNTIME_VERSION = "1.13";
 const TEMPLATE_ROOT = path.resolve(__dirname, "../../../../templates/init/functions/golang");
 const MAIN_TEMPLATE = fs.readFileSync(path.join(TEMPLATE_ROOT, "functions.go"), "utf8");
 const GITIGNORE_TEMPLATE = fs.readFileSync(path.join(TEMPLATE_ROOT, "_gitignore"), "utf8");
+const SDK_DROP = "https://storage.googleapis.com/firebase-preview-drop/go/functions/latest.zip";
+const SDK_PATH = "github.com/FirebaseExtended/firebase-functions-go";
 
 async function init(setup: unknown, config: Config) {
   await writeModFile(config);
   await config.askWriteProjectFile("functions/functions.go", MAIN_TEMPLATE);
   await config.askWriteProjectFile("functions/.gitignore", GITIGNORE_TEMPLATE);
+  utils.logLabeledBullet("functions",
+    "Welcome to the preview version of the Firebase Functions Go SDK. " +
+    "Please be a respectful member of the community and keep this a secret. " +
+    "Until the GitHub repo is public, we have set up a vendored module for you. " + 
+    "To update to the latest preview of the SDK, download " + SDK_DROP +
+    " and extract its contents into your " + clc.bold("firebase-functions-go") +
+    " directory");
 }
 
 // writeModFile is meant to look like askWriteProjectFile but it generates the contents
@@ -48,12 +60,28 @@ async function writeModFile(config: Config) {
     message: "What would you like to name your module?",
     default: "acme.com/functions",
   });
+
   // Manually create a go mod file because (A) it's easier this way and (B) it seems to be the only
   // way to set the min Go version to anything but what the user has installed.
   config.writeProjectFile(
     "functions/go.mod",
-    "module " + modName + "\n\ngo " + RUNTIME_VERSION + "\n"
+    "module " + modName + "\n\ngo " + RUNTIME_VERSION + "\n\n" +
+    "require " + SDK_PATH + " v0.0.0\n\n" + "replace " + SDK_PATH + " => ./firebase-functions-go\n"
   );
+
+  const download = await fetch(SDK_DROP);
+  const pipeAsync = promisify(stream.pipeline);
+  if (!download.body) {
+    logger.debug("Unexpected empty body response when downloading firebase-functions-go SDK");
+    throw new FirebaseError("Failed to download firebase-functions-go SDK");
+  }
+  if (!download.ok) {
+    throw new FirebaseError("Faield to download firebase-functions-go SDK");
+  }
+  // for some reason, a ReadableStream<T> isn't a ReadableStream according to TS. This is
+  // what the docs do though.
+  await pipeAsync(download.body as any, unzipper.Extract({path: config.path("functions/firebase-functions-go")}));
+
 
   // Should this come later as "would you like to install dependencies" to mirror Node?
   // It's less clearly distinct from node where you can edit the package.json file w/o installing.
