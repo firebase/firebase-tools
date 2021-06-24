fb10f238627a56bd95c608762f9c66aef4a0d6a4
Daniel Young Lee <danielylee@google.com>
2021-06-24 21:16:37 +0900

    Improve set command description.

--- src/commands/functions-env-set.ts
+++ src/commands/functions-env-set.ts
@@ -12,7 +12,9 @@ import * as getProjectId from "../getProjectId";
 import * as utils from "../utils";
 
 export default new Command("functions:env:set [values...]")
-  .description("set environment variables, completely replacing the existing set")
+  .description(
+    "set environment variables with KEY=value syntax, completely replacing the existing set"
+  )
   .option("--file <envFile>", "path to file with environment variables in .env format.")
   .option("-f, --force", "No confirmation. Otherwise, a confirmation prompt will appear.")
   .before(requirePermissions, [
