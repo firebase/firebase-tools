64fa5913d903da9441752a3dee7b63de2edb03a1
Daniel Young Lee <danielylee@google.com>
2021-07-23 20:15:08 -0700

    Get service name from cloud run.

--- src/commands/functions-log.ts
+++ src/commands/functions-log.ts
@@ -29,7 +29,7 @@ module.exports = new Command("functions:log")
         const apiFuncFilters = _.map(funcNames, (funcName) => {
           return (
             `resource.labels.function_name="${funcName}" ` +
-            `OR resource.label.service_name="${funcName}`
+            `OR resource.labels.service_name="${funcName}"`
           );
         });
         apiFilter += `\n(${apiFuncFilters.join(" OR ")})`;
