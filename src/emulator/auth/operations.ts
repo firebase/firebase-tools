098b4176525e6efefc1a9b85ef6895f12b705069
Yuchen Shi <yuchenshi@google.com>
2021-07-28 10:18:33 -0700

    Format code.

--- src/emulator/auth/operations.ts
+++ src/emulator/auth/operations.ts
@@ -1726,11 +1726,12 @@ function generateJwt(
     // This field is only set for anonymous sign-in but not for any other
     // provider (such as email or Google) in production. Let's match that.
     provider_id: signInProvider === "anonymous" ? signInProvider : undefined,
-    auth_time: user.lastLoginAt != null
-      ? toUnixTimestamp(new Date(user.lastLoginAt))
-      : user.lastRefreshAt != null
-      ? toUnixTimestamp(new Date(user.lastRefreshAt))
-      : toUnixTimestamp(new Date()),
+    auth_time:
+      user.lastLoginAt != null
+        ? toUnixTimestamp(new Date(user.lastLoginAt))
+        : user.lastRefreshAt != null
+        ? toUnixTimestamp(new Date(user.lastRefreshAt))
+        : toUnixTimestamp(new Date()),
     user_id: user.localId,
     firebase: {
       identities,
