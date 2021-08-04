55a4f61dbb504b7a92c89246784de13af42aa72c
Cole Rogers <colerogers@google.com>
2021-08-04 17:56:31 -0400

    fix lambda newline issue

--- src/deploy/functions/containerCleaner.ts
+++ src/deploy/functions/containerCleaner.ts
@@ -196,9 +196,9 @@ export async function listGcfPaths(
       .filter((loc) => locationsSet.has(loc))
       .map((loc) => `${SUBDOMAIN_MAPPING[loc]}.${containerRegistryDomain}/${projectId}/gcf/${loc}`);
   }
-  return gcfDirs.map(
-    (loc) => `${SUBDOMAIN_MAPPING[loc] || "us"}.${containerRegistryDomain}/${projectId}/gcf/${loc}`
-  );
+  return gcfDirs.map((loc) => {
+    return `${SUBDOMAIN_MAPPING[loc] || "us"}.${containerRegistryDomain}/${projectId}/gcf/${loc}`;
+  });
 }
 
 /**
