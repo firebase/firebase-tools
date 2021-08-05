25693520e1ec90d3ee56cc258976ec63c4ea7bba
Thomas Bouldin <inlined@google.com>
2021-08-05 09:27:18 -0700

    Separate error messages for v2; allow numbers

--- src/deploy/functions/validate.ts
+++ src/deploy/functions/validate.ts
@@ -37,17 +37,24 @@ export function functionsDirectoryExists(
  */
 export function functionIdsAreValid(functions: { id: string; platform: string }[]): void {
   const v1FunctionName = /^[a-zA-Z][a-zA-Z0-9_-]{0,62}$/;
-  const v2FunctionName = /^[a-z][a-z-]{0,62}$/;
-  const invalidIds = functions.filter((fn) => {
-    return (
-      (fn.platform === "gcfv1" && !v1FunctionName.test(fn.id)) ||
-      (fn.platform === "gcfv2" && !v2FunctionName.test(fn.id))
-    );
+  const invalidV1Ids = functions.filter((fn) => {
+    return fn.platform === "gcfv1" && !v1FunctionName.test(fn.id);
   });
-  if (invalidIds.length !== 0) {
+  if (invalidV1Ids.length !== 0) {
     const msg =
-      `${invalidIds.join(", ")} function name(s) can only contain letters, ` +
+      `${invalidV1Ids.join(", ")} function name(s) can only contain letters, ` +
       `numbers, hyphens, and not exceed 62 characters in length`;
+    throw new FirebaseError(msg);
+  }
+
+  const v2FunctionName = /^[a-z][a-z0-9-]{0,62}$/;
+  const invalidV2Ids = functions.filter((fn) => {
+    return fn.platform === "gcfv2" && !v2FunctionName.test(fn.id);
+  });
+  if (invalidV2Ids.length !== 0) {
+    const msg =
+    `${invalidV2Ids.join(", ")} v2 function name(s) can only contin lower ` +
+    `case letters, numbers, hyphens, and not exceed 62 characters in length`;
     throw new FirebaseError(msg);
   }
 }
