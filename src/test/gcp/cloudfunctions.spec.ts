ac2859340c8ea9a5f6f48a4e102565776fa11b64
Thomas Bouldin <inlined@google.com>
2021-09-02 17:30:07 -0700

    Fork translator implementations

--- src/test/gcp/cloudfunctions.spec.ts
+++ src/test/gcp/cloudfunctions.spec.ts
@@ -34,6 +34,230 @@ describe("cloudfunctions", () => {
     status: "ACTIVE",
   };
 
+  describe("functionFromEndpoint", () => {
+    const UPLOAD_URL = "https://storage.googleapis.com/projects/-/buckets/sample/source.zip";
+    it("should guard against version mixing", () => {
+      expect(() => {
+        cloudfunctions.functionFromSpec({ ...FUNCTION_SPEC, platform: "gcfv2" }, UPLOAD_URL);
+      }).to.throw;
+    });
+
+    it("should copy a minimal function", () => {
+      expect(cloudfunctions.functionFromSpec(FUNCTION_SPEC, UPLOAD_URL)).to.deep.equal({
+        ...CLOUD_FUNCTION,
+        sourceUploadUrl: UPLOAD_URL,
+        httpsTrigger: {
+          securityLevel: "SECURE_ALWAYS",
+        },
+      });
+
+      const eventFunction = {
+        ...FUNCTION_SPEC,
+        trigger: {
+          eventType: "google.pubsub.topic.publish",
+          eventFilters: {
+            resource: "projects/p/topics/t",
+          },
+          retry: false,
+        },
+      };
+      const eventGcfFunction = {
+        ...CLOUD_FUNCTION,
+        sourceUploadUrl: UPLOAD_URL,
+        eventTrigger: {
+          eventType: "google.pubsub.topic.publish",
+          resource: "projects/p/topics/t",
+          failurePolicy: undefined,
+        },
+      };
+      expect(cloudfunctions.functionFromSpec(eventFunction, UPLOAD_URL)).to.deep.equal(
+        eventGcfFunction
+      );
+    });
+
+    it("should copy trival fields", () => {
+      const fullFunction: backend.FunctionSpec = {
+        ...FUNCTION_SPEC,
+        availableMemoryMb: 128,
+        minInstances: 1,
+        maxInstances: 42,
+        vpcConnector: "connector",
+        vpcConnectorEgressSettings: "ALL_TRAFFIC",
+        ingressSettings: "ALLOW_ALL",
+        timeout: "15s",
+        serviceAccountEmail: "inlined@google.com",
+        labels: {
+          foo: "bar",
+        },
+        environmentVariables: {
+          FOO: "bar",
+        },
+      };
+
+      const fullGcfFunction: Omit<cloudfunctions.CloudFunction, cloudfunctions.OutputOnlyFields> = {
+        ...CLOUD_FUNCTION,
+        sourceUploadUrl: UPLOAD_URL,
+        httpsTrigger: {
+          securityLevel: "SECURE_ALWAYS",
+        },
+        labels: {
+          foo: "bar",
+        },
+        environmentVariables: {
+          FOO: "bar",
+        },
+        maxInstances: 42,
+        minInstances: 1,
+        vpcConnector: "connector",
+        vpcConnectorEgressSettings: "ALL_TRAFFIC",
+        ingressSettings: "ALLOW_ALL",
+        availableMemoryMb: 128,
+        timeout: "15s",
+        serviceAccountEmail: "inlined@google.com",
+      };
+
+      expect(cloudfunctions.functionFromSpec(fullFunction, UPLOAD_URL)).to.deep.equal(
+        fullGcfFunction
+      );
+    });
+
+    it("should calculate non-trivial fields", () => {
+      const complexFunction: backend.FunctionSpec = {
+        ...FUNCTION_SPEC,
+        trigger: {
+          eventType: "google.pubsub.topic.publish",
+          eventFilters: {
+            resource: "projects/p/topics/t",
+          },
+          retry: true,
+        },
+      };
+
+      const complexGcfFunction: Omit<
+        cloudfunctions.CloudFunction,
+        cloudfunctions.OutputOnlyFields
+      > = {
+        ...CLOUD_FUNCTION,
+        sourceUploadUrl: UPLOAD_URL,
+        eventTrigger: {
+          eventType: "google.pubsub.topic.publish",
+          resource: "projects/p/topics/t",
+          failurePolicy: {
+            retry: {},
+          },
+        },
+      };
+
+      expect(cloudfunctions.functionFromSpec(complexFunction, UPLOAD_URL)).to.deep.equal(
+        complexGcfFunction
+      );
+    });
+  });
+
+  describe("endpointFromFunction", () => {
+    it("should copy a minimal version", () => {
+      expect(
+        cloudfunctions.specFromFunction({
+          ...HAVE_CLOUD_FUNCTION,
+          httpsTrigger: {
+            securityLevel: "SECURE_ALWAYS",
+          },
+        })
+      ).to.deep.equal(FUNCTION_SPEC);
+    });
+
+    it("should translate event triggers", () => {
+      expect(
+        cloudfunctions.specFromFunction({
+          ...HAVE_CLOUD_FUNCTION,
+          eventTrigger: {
+            eventType: "google.pubsub.topic.publish",
+            resource: "projects/p/topics/t",
+            failurePolicy: {
+              retry: {},
+            },
+          },
+        })
+      ).to.deep.equal({
+        ...FUNCTION_SPEC,
+        trigger: {
+          eventType: "google.pubsub.topic.publish",
+          eventFilters: {
+            resource: "projects/p/topics/t",
+          },
+          retry: true,
+        },
+      });
+
+      // And again w/o the failure policy
+      expect(
+        cloudfunctions.specFromFunction({
+          ...HAVE_CLOUD_FUNCTION,
+          eventTrigger: {
+            eventType: "google.pubsub.topic.publish",
+            resource: "projects/p/topics/t",
+          },
+        })
+      ).to.deep.equal({
+        ...FUNCTION_SPEC,
+        trigger: {
+          eventType: "google.pubsub.topic.publish",
+          eventFilters: {
+            resource: "projects/p/topics/t",
+          },
+          retry: false,
+        },
+      });
+    });
+
+    it("should copy optional fields", () => {
+      const extraFields: Partial<backend.FunctionSpec> = {
+        availableMemoryMb: 128,
+        minInstances: 1,
+        maxInstances: 42,
+        vpcConnector: "connector",
+        vpcConnectorEgressSettings: "ALL_TRAFFIC",
+        ingressSettings: "ALLOW_ALL",
+        serviceAccountEmail: "inlined@google.com",
+        timeout: "15s",
+        labels: {
+          foo: "bar",
+        },
+        environmentVariables: {
+          FOO: "bar",
+        },
+      };
+      expect(
+        cloudfunctions.specFromFunction({
+          ...HAVE_CLOUD_FUNCTION,
+          ...extraFields,
+          httpsTrigger: {},
+        } as cloudfunctions.CloudFunction)
+      ).to.deep.equal({
+        ...FUNCTION_SPEC,
+        ...extraFields,
+        trigger: {
+          allowInsecure: true,
+        },
+      });
+    });
+
+    it("should transform fields", () => {
+      expect(
+        cloudfunctions.specFromFunction({
+          ...HAVE_CLOUD_FUNCTION,
+          httpsTrigger: {
+            securityLevel: "SECURE_OPTIONAL",
+          },
+        })
+      ).to.deep.equal({
+        ...FUNCTION_SPEC,
+        trigger: {
+          allowInsecure: true,
+        },
+      });
+    });
+  });
   describe("functionFromSpec", () => {
     const UPLOAD_URL = "https://storage.googleapis.com/projects/-/buckets/sample/source.zip";
     it("should guard against version mixing", () => {
