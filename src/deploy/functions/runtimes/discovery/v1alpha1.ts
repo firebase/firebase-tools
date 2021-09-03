98058cca3742072be741527ac720bb46a30e2919
Thomas Bouldin <inlined@users.noreply.github.com>
2021-09-03 12:35:19 -0700

    Create new endpoint type definition (#3731)
    * Create new endpoint type definition
    
    * Fix build breaks
    
    * Remove HttpsTrigger.allowInsecure
    
    * Formatter
    
    * Add type discrimiation functions and fix break

--- src/deploy/functions/runtimes/discovery/v1alpha1.ts
+++ src/deploy/functions/runtimes/discovery/v1alpha1.ts
@@ -19,6 +19,7 @@ function tryValidate(typed: backend.Backend) {
   // Use a helper type to help guide code complete when writing this function
   assertKeyTypes("", typed, {
     requiredAPIs: "object",
+    endpoints: "array",
     cloudFunctions: "array",
     topics: "array",
     schedules: "array",
@@ -129,6 +130,7 @@ function fillDefaults(
   want.environmentVariables = want.environmentVariables || {};
   want.schedules = want.schedules || [];
   want.topics = want.topics || [];
+  want.endpoints = want.endpoints || [];
 
   for (const cloudFunction of want.cloudFunctions) {
     if (!cloudFunction.project) {
