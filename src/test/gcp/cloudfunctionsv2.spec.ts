5a33eb41ba314033d0b9031303c940ddf15c75ee
Thomas Bouldin <inlined@users.noreply.github.com>
2021-09-09 14:40:11 -0700

    Add converters between API types and Endpoint types (#3735)
    * Fork translator implementations
    
    * Alter forked implementations to be about Endpoints not FunctionSpecs

--- src/test/gcp/cloudfunctionsv2.spec.ts
+++ src/test/gcp/cloudfunctionsv2.spec.ts
@@ -18,6 +18,14 @@ describe("cloudfunctionsv2", () => {
     runtime: "nodejs16",
   };
 
+  // Omit a random trigger to get this fragment to compile.
+  const ENDPOINT: Omit<backend.Endpoint, "httpsTrigger"> = {
+    platform: "gcfv2",
+    ...FUNCTION_NAME,
+    entryPoint: "function",
+    runtime: "nodejs16",
+  };
+
   const CLOUD_FUNCTION_V2_SOURCE: cloudfunctionsv2.StorageSource = {
     bucket: "sample",
     object: "source.zip",
@@ -49,6 +57,273 @@ describe("cloudfunctionsv2", () => {
     state: "ACTIVE",
     updateTime: new Date(),
   };
+
+  describe("functionFromEndpoint", () => {
+    const UPLOAD_URL = "https://storage.googleapis.com/projects/-/buckets/sample/source.zip";
+    it("should guard against version mixing", () => {
+      expect(() => {
+        cloudfunctionsv2.functionFromSpec(
+          { ...FUNCTION_SPEC, platform: "gcfv1" },
+          CLOUD_FUNCTION_V2_SOURCE
+        );
+      }).to.throw;
+    });
+
+    it("should copy a minimal function", () => {
+      expect(
+        cloudfunctionsv2.functionFromSpec(
+          {
+            ...FUNCTION_SPEC,
+            platform: "gcfv2",
+          },
+          CLOUD_FUNCTION_V2_SOURCE
+        )
+      ).to.deep.equal(CLOUD_FUNCTION_V2);
+
+      const eventFunction: backend.FunctionSpec = {
+        ...FUNCTION_SPEC,
+        platform: "gcfv2",
+        trigger: {
+          eventType: "google.cloud.audit.log.v1.written",
+          eventFilters: {
+            resource: "projects/p/regions/r/instances/i",
+            serviceName: "compute.googleapis.com",
+          },
+          retry: false,
+        },
+      };
+      const eventGcfFunction: Omit<
+        cloudfunctionsv2.CloudFunction,
+        cloudfunctionsv2.OutputOnlyFields
+      > = {
+        ...CLOUD_FUNCTION_V2,
+        eventTrigger: {
+          eventType: "google.cloud.audit.log.v1.written",
+          eventFilters: [
+            {
+              attribute: "resource",
+              value: "projects/p/regions/r/instances/i",
+            },
+            {
+              attribute: "serviceName",
+              value: "compute.googleapis.com",
+            },
+          ],
+        },
+      };
+      expect(
+        cloudfunctionsv2.functionFromSpec(eventFunction, CLOUD_FUNCTION_V2_SOURCE)
+      ).to.deep.equal(eventGcfFunction);
+    });
+
+    it("should copy trival fields", () => {
+      const fullFunction: backend.FunctionSpec = {
+        ...FUNCTION_SPEC,
+        platform: "gcfv2",
+        availableMemoryMb: 128,
+        vpcConnector: "connector",
+        vpcConnectorEgressSettings: "ALL_TRAFFIC",
+        ingressSettings: "ALLOW_ALL",
+        serviceAccountEmail: "inlined@google.com",
+        labels: {
+          foo: "bar",
+        },
+        environmentVariables: {
+          FOO: "bar",
+        },
+      };
+
+      const fullGcfFunction: Omit<
+        cloudfunctionsv2.CloudFunction,
+        cloudfunctionsv2.OutputOnlyFields
+      > = {
+        ...CLOUD_FUNCTION_V2,
+        labels: {
+          foo: "bar",
+        },
+        serviceConfig: {
+          ...CLOUD_FUNCTION_V2.serviceConfig,
+          environmentVariables: {
+            FOO: "bar",
+          },
+          vpcConnector: "connector",
+          vpcConnectorEgressSettings: "ALL_TRAFFIC",
+          ingressSettings: "ALLOW_ALL",
+          availableMemoryMb: 128,
+          serviceAccountEmail: "inlined@google.com",
+        },
+      };
+
+      expect(
+        cloudfunctionsv2.functionFromSpec(fullFunction, CLOUD_FUNCTION_V2_SOURCE)
+      ).to.deep.equal(fullGcfFunction);
+    });
+
+    it("should calculate non-trivial fields", () => {
+      const complexFunction: backend.FunctionSpec = {
+        ...FUNCTION_SPEC,
+        platform: "gcfv2",
+        trigger: {
+          eventType: cloudfunctionsv2.PUBSUB_PUBLISH_EVENT,
+          eventFilters: {
+            resource: "projects/p/topics/t",
+          },
+          retry: false,
+        },
+        maxInstances: 42,
+        minInstances: 1,
+        timeout: "15s",
+      };
+
+      const complexGcfFunction: Omit<
+        cloudfunctionsv2.CloudFunction,
+        cloudfunctionsv2.OutputOnlyFields
+      > = {
+        ...CLOUD_FUNCTION_V2,
+        eventTrigger: {
+          eventType: cloudfunctionsv2.PUBSUB_PUBLISH_EVENT,
+          pubsubTopic: "projects/p/topics/t",
+        },
+        serviceConfig: {
+          ...CLOUD_FUNCTION_V2.serviceConfig,
+          maxInstanceCount: 42,
+          minInstanceCount: 1,
+          timeoutSeconds: 15,
+        },
+      };
+
+      expect(
+        cloudfunctionsv2.functionFromSpec(complexFunction, CLOUD_FUNCTION_V2_SOURCE)
+      ).to.deep.equal(complexGcfFunction);
+    });
+  });
+
+  describe("endpointFromFunction", () => {
+    it("should copy a minimal version", () => {
+      expect(cloudfunctionsv2.specFromFunction(HAVE_CLOUD_FUNCTION_V2)).to.deep.equal({
+        ...FUNCTION_SPEC,
+        platform: "gcfv2",
+        uri: RUN_URI,
+      });
+    });
+
+    it("should translate event triggers", () => {
+      expect(
+        cloudfunctionsv2.specFromFunction({
+          ...HAVE_CLOUD_FUNCTION_V2,
+          eventTrigger: {
+            eventType: cloudfunctionsv2.PUBSUB_PUBLISH_EVENT,
+            pubsubTopic: "projects/p/topics/t",
+          },
+        })
+      ).to.deep.equal({
+        ...FUNCTION_SPEC,
+        platform: "gcfv2",
+        uri: RUN_URI,
+        trigger: {
+          eventType: cloudfunctionsv2.PUBSUB_PUBLISH_EVENT,
+          eventFilters: {
+            resource: "projects/p/topics/t",
+          },
+          retry: false,
+        },
+      });
+
+      // And again w/ a normal event trigger
+      expect(
+        cloudfunctionsv2.specFromFunction({
+          ...HAVE_CLOUD_FUNCTION_V2,
+          eventTrigger: {
+            eventType: "google.cloud.audit.log.v1.written",
+            eventFilters: [
+              {
+                attribute: "resource",
+                value: "projects/p/regions/r/instances/i",
+              },
+              {
+                attribute: "serviceName",
+                value: "compute.googleapis.com",
+              },
+            ],
+          },
+        })
+      ).to.deep.equal({
+        ...FUNCTION_SPEC,
+        platform: "gcfv2",
+        uri: RUN_URI,
+        trigger: {
+          eventType: "google.cloud.audit.log.v1.written",
+          eventFilters: {
+            resource: "projects/p/regions/r/instances/i",
+            serviceName: "compute.googleapis.com",
+          },
+          retry: false,
+        },
+      });
+    });
+
+    it("should copy optional fields", () => {
+      const extraFields: Partial<backend.FunctionSpec> = {
+        availableMemoryMb: 128,
+        vpcConnector: "connector",
+        vpcConnectorEgressSettings: "ALL_TRAFFIC",
+        ingressSettings: "ALLOW_ALL",
+        serviceAccountEmail: "inlined@google.com",
+        environmentVariables: {
+          FOO: "bar",
+        },
+      };
+      expect(
+        cloudfunctionsv2.specFromFunction({
+          ...HAVE_CLOUD_FUNCTION_V2,
+          serviceConfig: {
+            ...HAVE_CLOUD_FUNCTION_V2.serviceConfig,
+            ...extraFields,
+          },
+          labels: {
+            foo: "bar",
+          },
+        })
+      ).to.deep.equal({
+        ...FUNCTION_SPEC,
+        platform: "gcfv2",
+        uri: RUN_URI,
+        ...extraFields,
+        labels: {
+          foo: "bar",
+        },
+      });
+    });
+
+    it("should transform fields", () => {
+      const extraFields: Partial<backend.FunctionSpec> = {
+        minInstances: 1,
+        maxInstances: 42,
+        timeout: "15s",
+      };
+
+      const extraGcfFields: Partial<cloudfunctionsv2.ServiceConfig> = {
+        minInstanceCount: 1,
+        maxInstanceCount: 42,
+        timeoutSeconds: 15,
+      };
+
+      expect(
+        cloudfunctionsv2.specFromFunction({
+          ...HAVE_CLOUD_FUNCTION_V2,
+          serviceConfig: {
+            ...HAVE_CLOUD_FUNCTION_V2.serviceConfig,
+            ...extraGcfFields,
+          },
+        })
+      ).to.deep.equal({
+        ...FUNCTION_SPEC,
+        platform: "gcfv2",
+        uri: RUN_URI,
+        ...extraFields,
+      });
+    });
+  });
 
   describe("functionFromSpec", () => {
     const UPLOAD_URL = "https://storage.googleapis.com/projects/-/buckets/sample/source.zip";
