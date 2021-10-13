ff5e5f8889c1d116a6a7d6bc135fe57ffb5c44b2
Joe Hanley <joehanley@google.com>
2021-10-13 15:29:11 -0700

    slightly refactor tests

--- scripts/extensions-deploy-tests/tests.ts
+++ scripts/extensions-deploy-tests/tests.ts
@@ -6,12 +6,9 @@ import { CLIProcess } from "../integration-helpers/cli";
 
 const FIREBASE_PROJECT = process.env.FBTOOLS_TARGET_PROJECT || "";
 
-/*
- * Various delays that are needed because this test spawns
- * parallel emulator subprocesses.
- */
-const TEST_SETUP_TIMEOUT = 600000;
-const TEST_TIMEOUT = 50000;
+
+const TEST_SETUP_TIMEOUT = 10000;
+const TEST_TIMEOUT = 600000;
 describe("firebase deploy --only extensions", () => {
   let cli: CLIProcess;
   before(async function (this) {
@@ -19,20 +16,8 @@ describe("firebase deploy --only extensions", () => {
 
     expect(FIREBASE_PROJECT).to.exist.and.not.be.empty;
 
-    // // TODO(joehan): Delete the --open-sesame call when extdev flag is removed.
-    // const p = subprocess.spawnSync("firebase", ["--open-sesame", "extdev"], { cwd: __dirname });
-    // console.log("open-sesame output:", p.stdout.toString());
     cli = new CLIProcess("default", __dirname);
-    await cli.start(
-      "deploy",
-      FIREBASE_PROJECT,
-      ["--only", "extensions", "--non-interactive", "--force"],
-      (data: any) => {
-        if (`${data}`.match(/Deploy complete/)) {
-          return true;
-        }
-      }
-    );
+
   });
 
   after(function (this) {
@@ -43,6 +28,16 @@ describe("firebase deploy --only extensions", () => {
   it("should have deployed the expected extensions", async function (this) {
     this.timeout(TEST_TIMEOUT);
 
+    await cli.start(
+      "deploy",
+      FIREBASE_PROJECT,
+      ["--only", "extensions", "--non-interactive", "--force"],
+      (data: any) => {
+        if (`${data}`.match(/Deploy complete/)) {
+          return true;
+        }
+      }
+    );
     let output: any;
     await cli.start("ext:list", FIREBASE_PROJECT, ["--json"], (data: any) => {
       output = JSON.parse(data);
