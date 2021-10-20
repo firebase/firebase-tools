e59dd66d2a08251f99e260fca07d998b41ea72d4
Pavel Jbanov <pavelgj@gmail.com>
2021-10-20 18:52:36 -0400

    Added `--non-interactive` and `--force` options to `ext:dev:unpublish` (#3846)

--- src/commands/ext-dev-unpublish.ts
+++ src/commands/ext-dev-unpublish.ts
@@ -11,13 +11,14 @@ import { checkMinRequiredVersion } from "../checkMinRequiredVersion";
 
 module.exports = new Command("ext:dev:unpublish <extensionRef>")
   .description("unpublish an extension")
+  .withForce()
   .help(
     "use this command to unpublish an extension, and make it unavailable for developers to install or reconfigure. " +
       "Specify the extension you want to unpublish using the format '<publisherId>/<extensionId>."
   )
   .before(requireAuth)
   .before(checkMinRequiredVersion, "extDevMinVersion")
-  .action(async (extensionRef: string) => {
+  .action(async (extensionRef: string, options: any) => {
     const { publisherId, extensionId, version } = refs.parse(extensionRef);
     utils.logLabeledWarning(
       logPrefix,
@@ -35,7 +36,7 @@ module.exports = new Command("ext:dev:unpublish <extensionRef>")
       );
     }
     await getExtension(extensionRef);
-    const consent = await comfirmUnpublish(publisherId, extensionId);
+    const consent = await comfirmUnpublish(publisherId, extensionId, options);
     if (!consent) {
       throw new FirebaseError("unpublishing cancelled.");
     }
@@ -43,7 +44,17 @@ module.exports = new Command("ext:dev:unpublish <extensionRef>")
     utils.logLabeledSuccess(logPrefix, "successfully unpublished all versions of this extension.");
   });
 
-async function comfirmUnpublish(publisherId: string, extensionId: string): Promise<boolean> {
+async function comfirmUnpublish(
+  publisherId: string,
+  extensionId: string,
+  options: any
+): Promise<boolean> {
+  if (options.nonInteractive && !options.force) {
+    throw new FirebaseError("Pass the --force flag to use this command in non-interactive mode");
+  }
+  if (options.nonInteractive && options.force) {
+    return true;
+  }
   const message = `You are about to unpublish ALL versions of ${clc.green(
     `${publisherId}/${extensionId}`
   )}.\nDo you wish to continue? `;
