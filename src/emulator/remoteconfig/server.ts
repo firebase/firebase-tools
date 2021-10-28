9c9853c64c287694857dedce6b8388a2837e4125
Arthur Thompson <arthurthompson@google.com>
2021-10-27 18:09:40 -0700

    extract entries from RC template

--- src/emulator/remoteconfig/server.ts
+++ src/emulator/remoteconfig/server.ts
@@ -1,16 +1,54 @@
 import * as express from "express";
 import { RemoteConfigEmulator } from "./index";
+import * as cors from "cors";
+
+const TEMPLATE = {
+  "conditions": [
+    {
+      "name": "web_users",
+      "expression": "device.os == 'web'",
+      "tagColor": "GREEN"
+    },
+    {
+      "name": "android_users",
+      "expression": "device.os == 'android'",
+      "tagColor": "BLUE"
+    }
+  ],
+  "parameters": {
+    "welcome_message": {
+      "defaultValue": {
+        "value": "Hi Welcome!"
+      },
+      "valueType": "STRING"
+    },
+    "discount_percentage": {
+      "defaultValue": {
+        "value": "10"
+      },
+      "conditionalValues": {
+        "web_users": {
+          "value": "15"
+        },
+        "android_users": {
+          "value": "5"
+        }
+      },
+      "valueType": "NUMBER"
+    }
+  }
+};
 
 /**
  * @param defaultProjectId
  * @param emulator
  */
 export function createApp(
-  defaultProjectId: string,
-  emulator: RemoteConfigEmulator
+    defaultProjectId: string,
+    emulator: RemoteConfigEmulator
 ): Promise<express.Express> {
   const app = express();
-
+  app.use(cors("*" as any));
   app.get("/", (req, res) => {
     res.send("Oh yes, it's me, remote config 🥸");
   });
@@ -20,17 +58,25 @@ export function createApp(
   app.post("/v1/projects/:projectId/namespaces/firebase:fetch", (req, res) => {
     res.header("etag", `etag-${req.params["projectId"]}-firebase-fetch--${new Date().getTime()}`);
     res.header(
-      "Access-Control-expose-Headers",
-      "etag,vary,vary,vary,content-encoding,date,server,content-length"
+        "Access-Control-expose-Headers",
+        "etag,vary,vary,vary,content-encoding,date,server,content-length"
     );
 
-    res.json({
-      entries: {
-        remoteConfigFieldName: "remoteConfigFieldValue",
-      },
-      state: "UPDATE",
-    });
+    res.json(generateClientResponse(TEMPLATE));
   });
 
   return Promise.resolve(app);
+}
+
+function generateClientResponse(template: any): any {
+  const parameters = template['parameters'];
+  const entriesObj: any = {};
+  for (const parameterName of Object.keys(parameters)) {
+    entriesObj[parameterName] = parameters[parameterName].defaultValue.value;
+  }
+  console.log(entriesObj);
+  return {
+    entries: entriesObj,
+    state: 'UPDATE'
+  };
 }
