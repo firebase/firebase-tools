97e00d83434179bab9b0dcfb6c6a205f158145b9
Arthur Thompson <arthurthompson@google.com>
2021-10-29 11:02:12 -0700

    add isEmulator conditional value

--- src/emulator/remoteconfig/server.ts
+++ src/emulator/remoteconfig/server.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 import { RemoteConfigEmulator } from "./index";
 import * as cors from "cors";
 
-const TEMPLATE = {
+const PROD_TEMPLATE: any = {
   "conditions": [
     {
       "name": "web_users",
@@ -39,6 +39,8 @@ const TEMPLATE = {
   }
 };
 
+let EMULATOR_TEMPLATE: any = {};
+
 /**
  * @param defaultProjectId
  * @param emulator
@@ -47,6 +49,8 @@ export function createApp(
     defaultProjectId: string,
     emulator: RemoteConfigEmulator
 ): Promise<express.Express> {
+  EMULATOR_TEMPLATE = prodToEmulatorTemplate(PROD_TEMPLATE);
+  console.log(JSON.stringify(EMULATOR_TEMPLATE, null, 2))
   const app = express();
   app.use(cors("*" as any));
   app.get("/", (req, res) => {
@@ -62,17 +66,40 @@ export function createApp(
         "etag,vary,vary,vary,content-encoding,date,server,content-length"
     );
 
-    res.json(generateClientResponse(TEMPLATE));
+    res.json(generateClientResponse(EMULATOR_TEMPLATE));
   });
 
   return Promise.resolve(app);
+}
+
+function prodToEmulatorTemplate(prodTemplate: any): any {
+  const prodParameters = prodTemplate['parameters'];
+  const emulatorParameters: any = {};
+  for (const parameterName of Object.keys(prodParameters)) {
+    const emulatorParameter = Object.assign({}, prodParameters[parameterName]);
+    if (emulatorParameter.hasOwnProperty('conditionalValues')) {
+      // add is_emulator
+      const conditionalValues = emulatorParameter['conditionalValues'];
+      conditionalValues['isEmulator'] = Object.assign({}, emulatorParameter['defaultValue']);
+    } else {
+      // add conditional value object
+      emulatorParameter['conditionalValues'] = {
+        // add is_emulator
+        isEmulator: Object.assign({}, emulatorParameter['defaultValue'])
+      };
+    }
+    emulatorParameters[parameterName] = emulatorParameter;
+  }
+  return {
+    parameters: emulatorParameters
+  };
 }
 
 function generateClientResponse(template: any): any {
   const parameters = template['parameters'];
   const entriesObj: any = {};
   for (const parameterName of Object.keys(parameters)) {
-    entriesObj[parameterName] = parameters[parameterName].defaultValue.value;
+    entriesObj[parameterName] = parameters[parameterName].conditionalValues.isEmulator.value;
   }
   console.log(entriesObj);
   return {
