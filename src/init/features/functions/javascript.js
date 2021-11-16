9978b2d799725e5c7435dde880d0a709f7f6b0fb
Phu Thong Pham <ptpham4@myseneca.ca>
2021-11-15 22:39:07 -0500

    Added support for validating and dynamically setting `engines.node` in `package.json`

--- src/init/features/functions/javascript.js
+++ src/init/features/functions/javascript.js
@@ -6,6 +6,8 @@ var path = require("path");
 
 var npmDependencies = require("./npm-dependencies");
 var { prompt } = require("../../../prompt");
+var utils = require("../../../utils");
+const { isValidRuntime } = require("../../../deploy/functions/runtimes/index");
 
 var TEMPLATE_ROOT = path.resolve(__dirname, "../../../../templates/init/functions/javascript/");
 var INDEX_TEMPLATE = fs.readFileSync(path.join(TEMPLATE_ROOT, "index.js"), "utf8");
@@ -33,12 +35,12 @@ module.exports = function (setup, config) {
       if (setup.functions.lint) {
         _.set(setup, "config.functions.predeploy", ['npm --prefix "$RESOURCE_DIR" run lint']);
         return config
-          .askWriteProjectFile("functions/package.json", PACKAGE_LINTING_TEMPLATE)
+          .askWriteProjectFile("functions/package.json", getPackage(true))
           .then(function () {
             config.askWriteProjectFile("functions/.eslintrc.js", ESLINT_TEMPLATE);
           });
       }
-      return config.askWriteProjectFile("functions/package.json", PACKAGE_NO_LINTING_TEMPLATE);
+      return config.askWriteProjectFile("functions/package.json", getPackage(false));
     })
     .then(function () {
       return config.askWriteProjectFile("functions/index.js", INDEX_TEMPLATE);
@@ -50,3 +52,18 @@ module.exports = function (setup, config) {
       return npmDependencies.askInstallDependencies(setup.functions, config);
     });
 };
+
+function getPackage(useLint) {
+  const nodeEngineVersion = utils.getNodeVersionString();
+  if (!isValidRuntime(`nodejs${nodeEngineVersion}`)) {
+    utils.logWarning(`Node ${nodeEngineVersion} is no longer supported in Google Cloud Functions.`);
+    utils.logWarning(
+      "See https://firebase.google.com/docs/functions/manage-functions for more details"
+    );
+  }
+
+  if (useLint) {
+    return PACKAGE_LINTING_TEMPLATE.replace(/{{NODE_VERSION}}/g, nodeEngineVersion);
+  }
+  return PACKAGE_NO_LINTING_TEMPLATE.replace(/{{NODE_VERSION}}/g, nodeEngineVersion);
+}
