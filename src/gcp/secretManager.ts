2cbb0ab08ba911faffb2f63bbf435433f0aa938a
Daniel Young Lee <danielylee@google.com>
2021-12-21 12:35:39 -0800

    Correct API_VERSION for secret manager api.

--- src/gcp/secretManager.ts
+++ src/gcp/secretManager.ts
@@ -43,7 +43,7 @@ interface AddVersionRequest {
   payload: { data: string };
 }
 
-const API_VERSION = "v1beta";
+const API_VERSION = "v1beta1";
 
 const client = new Client({
   urlPrefix: secretManagerOrigin,
