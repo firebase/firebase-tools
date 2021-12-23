fadef0c75987547ff26c9473fe394a7234e2c584
Daniel Young Lee <danielylee@google.com>
2021-12-22 23:23:27 -0800

    Add more tests.

--- src/test/deploy/functions/prepare.spec.ts
+++ src/test/deploy/functions/prepare.spec.ts
@@ -1,8 +1,11 @@
+import * as sinon from "sinon";
 import { expect } from "chai";
 
 import * as backend from "../../../deploy/functions/backend";
 import * as prepare from "../../../deploy/functions/prepare";
+import * as secretManager from "../../../gcp/secretManager";
 import { FirebaseError } from "../../../error";
+import { ensureAccesses, resolveVersions } from "../../../deploy/functions/prepare";
 
 describe("prepare", () => {
   const ENDPOINT_BASE: Omit<backend.Endpoint, "httpsTrigger"> = {
@@ -178,6 +181,169 @@ describe("prepare", () => {
           FirebaseError,
           /Only GCFv1 supports secret environments/
         );
+      });
+    });
+
+    describe("resolveVersions", () => {
+      let secretVersionStub: sinon.SinonStub;
+
+      beforeEach(() => {
+        secretVersionStub = sinon
+          .stub(secretManager, "getSecretVersion")
+          .rejects("Unexpected call");
+      });
+
+      afterEach(() => {
+        secretVersionStub.restore();
+      });
+
+      const projectId = "project";
+      const secret: secretManager.Secret = { projectId, name: "my-secret" };
+
+      it("fills in missing version id", async () => {
+        secretVersionStub
+          .withArgs(projectId, secret.name, "latest")
+          .resolves({ secret, versionId: "1" });
+
+        const e: backend.Endpoint = {
+          ...ENDPOINT,
+          platform: "gcfv1",
+          secretEnvironmentVariables: [
+            {
+              projectId,
+              secret: secret.name,
+              key: secret.name,
+            },
+          ],
+        };
+
+        await resolveVersions(backend.of(e));
+
+        expect(e.secretEnvironmentVariables).to.be.deep.equal([
+          {
+            projectId,
+            secret: secret.name,
+            key: secret.name,
+            version: "1",
+          },
+        ]);
+      });
+
+      it("skips api call if version id already exists", async () => {
+        const e: backend.Endpoint = {
+          ...ENDPOINT,
+          platform: "gcfv1",
+          secretEnvironmentVariables: [
+            {
+              projectId,
+              secret: secret.name,
+              key: secret.name,
+              version: "1",
+            },
+          ],
+        };
+
+        await resolveVersions(backend.of(e));
+
+        expect(e.secretEnvironmentVariables).to.be.deep.equal([
+          {
+            projectId,
+            version: "1",
+            secret: secret.name,
+            key: secret.name,
+          },
+        ]);
+      });
+    });
+
+    describe("ensureAccesses", () => {
+      const project0 = "project-0";
+      const project1 = "project-1";
+      const secret0: backend.SecretEnvVar = {
+        projectId: project0,
+        key: "MY_SECRET_0",
+        secret: "MY_SECRET_0",
+        version: "2",
+      };
+      const secret1: backend.SecretEnvVar = {
+        projectId: project1,
+        key: "MY_SECRET_1",
+        secret: "MY_SECRET_1",
+        version: "2",
+      };
+
+      const e: backend.Endpoint = {
+        ...ENDPOINT,
+        project: project0,
+        platform: "gcfv1",
+        secretEnvironmentVariables: [],
+      };
+
+      let secretManagerMock: sinon.SinonMock;
+
+      beforeEach(() => {
+        secretManagerMock = sinon.mock(secretManager);
+      });
+
+      afterEach(() => {
+        secretManagerMock.verify();
+        secretManagerMock.restore();
+      });
+
+      it("ensures access to default service account", async () => {
+        const b = backend.of({
+          ...e,
+          secretEnvironmentVariables: [secret0],
+        });
+
+        secretManagerMock
+          .expects("ensureServiceAgentRole")
+          .once()
+          .withExactArgs(
+            { name: secret0.secret, projectId: project0 },
+            // TODO: refactor default SA.
+            [`${e.project}@appspot.gserviceaccount.com`],
+            "roles/secretmanager.secretAccessor"
+          );
+
+        await ensureAccesses(b);
+      });
+
+      it("ensures access to all secrets", async () => {
+        const b = backend.of({
+          ...e,
+          secretEnvironmentVariables: [secret0, secret1],
+        });
+
+        secretManagerMock.expects("ensureServiceAgentRole").twice();
+
+        await ensureAccesses(b);
+      });
+
+      it("combines service account to make one call per secret", async () => {
+        const b = backend.of(
+          {
+            ...e,
+            secretEnvironmentVariables: [secret0],
+          },
+          {
+            ...e,
+            id: "another-id",
+            serviceAccountEmail: "foo@bar.com",
+            secretEnvironmentVariables: [secret0],
+          }
+        );
+
+        secretManagerMock
+          .expects("ensureServiceAgentRole")
+          .once()
+          .withExactArgs(
+            { name: secret0.secret, projectId: project0 },
+            [`${e.project}@appspot.gserviceaccount.com`, "foo@bar.com"],
+            "roles/secretmanager.secretAccessor"
+          );
+
+        await ensureAccesses(b);
       });
     });
   });
