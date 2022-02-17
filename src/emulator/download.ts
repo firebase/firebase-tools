687aa6a7d95221ffb7dd2a5f8069425a57382f65
Elvis Sun <lihes@google.com>
2022-02-17 15:49:33 -0500

    Improve logging

--- src/emulator/download.ts
+++ src/emulator/download.ts
@@ -48,6 +48,11 @@ export async function downloadExtensionVersion(
   sourceDownloadUri: string,
   targetDir: string
 ): Promise<void> {
+  EmulatorLogger.forExtension(extensionVersionRef).logLabeled(
+    "BULLET",
+    "extensions",
+    `Starting download for ${extensionVersionRef} source code...`
+  );
   try {
     fs.mkdirSync(targetDir);
   } catch (err) {
