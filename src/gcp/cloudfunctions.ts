660886cf0909b96bf2b0e661c7062be9a498f8ee
Cole Rogers <colerogers@google.com>
2022-02-22 18:00:48 -0500

    calling update mask with the correct object & add changelog entry

--- src/gcp/cloudfunctions.ts
+++ src/gcp/cloudfunctions.ts
@@ -356,11 +356,16 @@ export async function updateFunction(
   cloudFunction: Omit<CloudFunction, OutputOnlyFields>
 ): Promise<Operation> {
   const endpoint = `/${cloudFunction.name}`;
+  // TODO(colerogers): remove cloudFunctionWithoutHttpsTrigger and set securityLevel in functionFromEndpoint during a breaking change release
+  const cloudFunctionWithoutHttpsTrigger = { ...cloudFunction };
+  delete cloudFunctionWithoutHttpsTrigger.httpsTrigger;
   // Keys in labels and environmentVariables are user defined, so we don't recurse
   // for field masks.
-  const fieldMasks = proto
-    .fieldMasks(cloudFunction, /* doNotRecurseIn...=*/ "labels", "environmentVariables")
-    .filter((fm) => fm !== "httpsTrigger"); // TODO(colerogers): remove and set securityLevel in functionFromEndpoint during a breaking change
+  const fieldMasks = proto.fieldMasks(
+    cloudFunctionWithoutHttpsTrigger,
+    /* doNotRecurseIn...=*/ "labels",
+    "environmentVariables"
+  );
 
   // Failure policy is always an explicit policy and is only signified by the presence or absence of
   // a protobuf.Empty value, so we have to manually add it in the missing case.
