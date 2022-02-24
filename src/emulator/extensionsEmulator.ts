fd20afd971fc4dae8240e8b1278110d86b13166e
Joe Hanley <joehanley@google.com>
2022-02-24 10:47:26 -0800

    Changing backendInfo to include extension & support local Extensions in the future

--- src/emulator/extensionsEmulator.ts
+++ src/emulator/extensionsEmulator.ts
@@ -139,12 +139,14 @@ export class ExtensionsEmulator {
     const extensionDir = await this.ensureSourceCode(instance);
     // TODO: This should find package.json, then use that as functionsDir.
     const functionsDir = path.join(extensionDir, "functions");
+    // TODO(b/213335255): For local extensions, this should include extensionSpec instead of extensionVersion
     const env = Object.assign(this.autoPopulatedParams(instance), instance.params);
     const { extensionTriggers, nodeMajorVersion } = await getExtensionFunctionInfo(
       extensionDir,
       instance.instanceId,
       env
     );
+    const extension = await planner.getExtension(instance);
     const extensionVersion = await planner.getExtensionVersion(instance);
     return {
       functionsDir,
@@ -152,6 +154,7 @@ export class ExtensionsEmulator {
       predefinedTriggers: extensionTriggers,
       nodeMajorVersion: nodeMajorVersion,
       extensionInstanceId: instance.instanceId,
+      extension,
       extensionVersion,
     };
   }
