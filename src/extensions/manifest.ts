2584a9b5e82050fae90bc65ee59840c2c66cd3ef
Elvis Sun <lihes@google.com>
2022-02-24 09:31:01 -0500

    simplify

--- src/extensions/manifest.ts
+++ src/extensions/manifest.ts
@@ -67,9 +67,7 @@ export function loadConfig(options: any): Config {
  * Checks if an instance name already exists in the manifest.
  */
 export function instanceExists(instanceId: string, config: Config): boolean {
-  return Object.keys(config.get("extensions", {})).some(
-    (manifestKey) => manifestKey === instanceId
-  );
+  return !!config.get("extensions",{})[instanceId];
 }
 
 function writeExtensionsToFirebaseJson(specs: InstanceSpec[], config: Config): void {
