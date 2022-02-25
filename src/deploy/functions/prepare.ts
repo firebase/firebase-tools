f69a3ef3d909e462caddd0f72a481b806e8c10e4
Cole Rogers <colerogers@google.com>
2022-02-25 13:39:25 -0500

    formatter

--- src/deploy/functions/prepare.ts
+++ src/deploy/functions/prepare.ts
@@ -205,7 +205,7 @@ export function inferDetailsFromExisting(
       wantE.availableMemoryMb = haveE.availableMemoryMb;
     }
 
-    wantE.securityLevel = (haveE.securityLevel) ? haveE.securityLevel : "SECURE_ALWAYS";
+    wantE.securityLevel = haveE.securityLevel ? haveE.securityLevel : "SECURE_ALWAYS";
 
     maybeCopyTriggerRegion(wantE, haveE);
   }
