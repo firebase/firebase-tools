bf1fe855c551f97879b60d9e0906d262023f5b2e
Tony Huang <tony.huang.jun@gmail.com>
2022-02-28 15:03:21 -0800

    formatting

--- src/emulator/storage/rules/utils.ts
+++ src/emulator/storage/rules/utils.ts
@@ -1,4 +1,3 @@
-
 import { StorageRulesetInstance } from "./runtime";
 import { RulesResourceMetadata } from "../metadata";
 import { RulesetOperationMethod } from "./types";
@@ -7,40 +6,40 @@ import { Emulators } from "../../types";
 
 /** Authorizes file access given a set of security rules. */
 export async function isPermitted(opts: {
-    ruleset?: StorageRulesetInstance;
-    file: {
-        before?: RulesResourceMetadata;
-        after?: RulesResourceMetadata;
-    };
-    path: string;
-    method: RulesetOperationMethod;
-    authorization?: string;
+  ruleset?: StorageRulesetInstance;
+  file: {
+    before?: RulesResourceMetadata;
+    after?: RulesResourceMetadata;
+  };
+  path: string;
+  method: RulesetOperationMethod;
+  authorization?: string;
 }): Promise<boolean> {
-    if (!opts.ruleset) {
-        EmulatorLogger.forEmulator(Emulators.STORAGE).log(
-            "WARN",
-            `Can not process SDK request with no loaded ruleset`
-        );
-        return false;
-    }
+  if (!opts.ruleset) {
+    EmulatorLogger.forEmulator(Emulators.STORAGE).log(
+      "WARN",
+      `Can not process SDK request with no loaded ruleset`
+    );
+    return false;
+  }
 
-    // Skip auth for UI
-    if (["Bearer owner", "Firebase owner"].includes(opts.authorization || "")) {
-        return true;
-    }
+  // Skip auth for UI
+  if (["Bearer owner", "Firebase owner"].includes(opts.authorization || "")) {
+    return true;
+  }
 
-    const { permitted, issues } = await opts.ruleset.verify({
-        method: opts.method,
-        path: opts.path,
-        file: opts.file,
-        token: opts.authorization ? opts.authorization.split(" ")[1] : undefined,
+  const { permitted, issues } = await opts.ruleset.verify({
+    method: opts.method,
+    path: opts.path,
+    file: opts.file,
+    token: opts.authorization ? opts.authorization.split(" ")[1] : undefined,
+  });
+
+  if (issues.exist()) {
+    issues.all.forEach((warningOrError) => {
+      EmulatorLogger.forEmulator(Emulators.STORAGE).log("WARN", warningOrError);
     });
+  }
 
-    if (issues.exist()) {
-        issues.all.forEach((warningOrError) => {
-            EmulatorLogger.forEmulator(Emulators.STORAGE).log("WARN", warningOrError);
-        });
-    }
-
-    return !!permitted;
+  return !!permitted;
 }
