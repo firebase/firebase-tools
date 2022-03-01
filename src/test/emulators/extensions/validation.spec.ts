19b6a0683ecac8b6a651dd286c33a18a0277bcdc
Joe Hanley <joehanley@google.com>
2022-03-01 09:11:22 -0800

    adding renamed files

---
+++ src/test/emulators/extensions/validation.spec.ts
@@ -0,0 +1,67 @@
+import { expect } from "chai";
+import * as sinon from "sinon";
+
+import * as utils from "../../../emulator/extensions/validation";
+import * as ensureApiEnabled from "../../../ensureApiEnabled";
+import { InstanceSpec } from "../../../deploy/extensions/planner";
+
+function getTestInstanceSpecWithAPI(instanceId: string, apiName: string): InstanceSpec {
+  return {
+    instanceId,
+    params: {},
+    extensionVersion: {
+      name: "publishers/test/extensions/test/versions/0.1.0",
+      ref: "test/test@0.1.0",
+      state: "PUBLISHED",
+      sourceDownloadUri: "test.com",
+      hash: "abc123",
+      spec: {
+        name: "test",
+        version: "0.1.0",
+        sourceUrl: "test.com",
+        resources: [],
+        params: [],
+        apis: [{ apiName, reason: "because" }],
+      },
+    },
+  };
+}
+
+describe("ExtensionsEmulator validation utils", () => {
+  describe(`${utils.getUnemulatedAPIs.name}`, () => {
+    const testProjectId = "test-project";
+    const testAPI = "test.googleapis.com";
+    const sandbox = sinon.createSandbox();
+
+    beforeEach(() => {
+      const checkStub = sandbox.stub(ensureApiEnabled, "check");
+      checkStub.withArgs(testProjectId, testAPI, "extensions", true).resolves(true);
+      checkStub.throws("Unexpected API checked in test");
+    });
+
+    afterEach(() => {
+      sandbox.restore();
+    });
+
+    it("should check only unemulated APIs", async () => {
+      const instanceIdWithUnemulatedAPI = "unemulated";
+      const instanceIdWithEmulatedAPI = "emulated";
+
+      const result = await utils.getUnemulatedAPIs(testProjectId, [
+        getTestInstanceSpecWithAPI(instanceIdWithEmulatedAPI, "firestore.googleapis.com"),
+        getTestInstanceSpecWithAPI(instanceIdWithUnemulatedAPI, testAPI),
+      ]);
+
+      expect(result).to.deep.equal([
+        {
+          apiName: testAPI,
+          instanceIds: [instanceIdWithUnemulatedAPI],
+          enabled: true,
+        },
+      ]);
+      // expect(result[0].apiName).to.equal(testAPI);
+      // expect(result[0].instanceIds).to.equal([instanceIdWithEmulatedAPI]);
+      // expect(result[0].enabled).to.equal(true);
+    });
+  });
+});
