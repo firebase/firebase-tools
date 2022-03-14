22b85d6c99b925606e42f66f4c855ecbc296b20f
Salakar <mike.diarmid@gmail.com>
2022-03-14 11:03:43 +0000

    -

---
+++ src/deploy/functions/runtimes/python/index.ts
@@ -0,0 +1,11 @@
+import * as runtimes from "..";
+
+export async function tryCreateDelegate(
+  context: runtimes.DelegateContext
+): Promise<Delegate | undefined> {
+  throw new Error("Method not implemented.");
+}
+
+class Delegate {
+  // TODO
+}
