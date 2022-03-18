c8fb01813aa5cd22b80e124eed5ec941078c5656
Chris Brown <chris@chrisbrown.io>
2022-03-18 20:03:03 +0000

    Revert "Updated comments to be more explicit post invoker changes"
    This reverts commit b7fd9ffeed813e840bd59811e68e71808912b1ac.

--- src/deploy/functions/release/fabricator.ts
+++ src/deploy/functions/release/fabricator.ts
@@ -241,7 +241,7 @@ export class Fabricator {
         })
         .catch(rethrowAs(endpoint, "set invoker"));
     } else if (backend.isTaskQueueTriggered(endpoint)) {
-      // Like other triggers, taskQueueTriggers can have an invoker, but they don't default
+      // Like HTTPS triggers, taskQueueTriggers have an invoker, but unlike HTTPS they don't default
       // public.
       const invoker = endpoint.taskQueueTrigger.invoker;
       if (invoker && !invoker.includes("private")) {
@@ -310,7 +310,7 @@ export class Fabricator {
         .run(() => run.setInvokerCreate(endpoint.project, serviceName, ["public"]))
         .catch(rethrowAs(endpoint, "set invoker"));
     } else if (backend.isTaskQueueTriggered(endpoint)) {
-      // Like other triggers, taskQueueTriggers can have an invoker, but they don't default
+      // Like HTTPS triggers, taskQueueTriggers have an invoker, but unlike HTTPS they don't default
       // public.
       const invoker = endpoint.taskQueueTrigger.invoker;
       if (invoker && !invoker.includes("private")) {
