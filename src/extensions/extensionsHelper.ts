0101dcc031ba8b525d543fde6563e5850be1ba20
Elvis Sun <lihes@google.com>
2022-03-21 11:36:28 -0400

    fix pipe that needs project ID

--- src/extensions/extensionsHelper.ts
+++ src/extensions/extensionsHelper.ts
@@ -21,7 +21,7 @@ import { diagnose } from "./diagnose";
 import { checkResponse } from "./askUserForParam";
 import { ensure } from "../ensureApiEnabled";
 import { deleteObject, uploadObject } from "../gcp/storage";
-import { needProjectId } from "../projectUtils";
+import { assertProjectId, getProjectId, needProjectId } from "../projectUtils";
 import {
   createSource,
   ExtensionSource,
@@ -369,7 +369,10 @@ export async function promptForValidInstanceId(instanceId: string): Promise<stri
 }
 
 export async function ensureExtensionsApiEnabled(options: any): Promise<void> {
-  const projectId = needProjectId(options);
+  const projectId = getProjectId(options);
+  if (!projectId) {
+    return;
+  }
   return await ensure(
     projectId,
     "firebaseextensions.googleapis.com",
@@ -762,7 +765,10 @@ export async function confirm(args: {
 }
 
 export async function diagnoseAndFixProject(options: any): Promise<void> {
-  const projectId = needProjectId(options);
+  const projectId = getProjectId(options);
+  if (!projectId) {
+    return;
+  }
   const ok = await diagnose(projectId);
   if (!ok) {
     throw new FirebaseError("Unable to proceed until all issues are resolved.");
