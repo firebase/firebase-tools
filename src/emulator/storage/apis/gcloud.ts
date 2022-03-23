0e5b515df19c133b79b8a5d0d1cabcbb82cc0781
Tony Huang <tony.huang.jun@gmail.com>
2022-03-23 15:01:05 -0400

    Refactor copyFile in StorageLayer (#4351)
    * multipart and firebase.ts integration
    
    * update gcloud.ts
    
    * refactor persistence out into StorageEmulator
    
    * fix lint and method references
    
    * fix tests
    
    * upload.ts
    
    * update callers of UploadService and tests
    
    * more tests
    
    * more tests
    
    * md -> metadata
    
    * address pr comments
    
    * lint
    
    * fix tests
    
    * fix tests
    
    * stash
    
    * more integration tests, fix upload bug
    
    * more fixing
    
    * fix remaining
    
    * lint
    
    * metadata
    
    * deleteobject
    
    * list
    
    * get
    
    * fix tests
    
    * lint
    
    * address pr comments
    
    * lint
    
    * restructure
    
    * cleanup
    
    * fix merge
    
    * remove skipAuth param from StorageLayer
    
    * rename to adminStorageLayer to make StorageLayer type explicit
    
    * fix imports
    
    * change to use passthrough logic
    
    * share state across storagelayers
    
    * fix tests
    
    * fix import
    
    * address pr comments
    
    * lint
    
    * revert
    
    * pr comments
    
    * more cleanup
    
    * fix tests
    
    * remove only
    
    * move internal tests to new file
    
    * internals test file
    
    * refactor gcloud to use same list method in StorageLayer
    
    * fix imports
    
    * delete object
    
    * lint
    
    * copyfile
    
    * public -> private
    
    * override auth
    
    * lint

--- src/emulator/storage/apis/gcloud.ts
+++ src/emulator/storage/apis/gcloud.ts
@@ -290,27 +290,31 @@ export function createCloudEndpoints(emulator: StorageEmulator): Router {
   gcloudStorageAPI.post(
     "/b/:bucketId/o/:objectId/:method(rewriteTo|copyTo)/b/:destBucketId/o/:destObjectId",
     (req, res, next) => {
-      const md = adminStorageLayer.getMetadata(req.params.bucketId, req.params.objectId);
-
-      if (!md) {
-        return sendObjectNotFound(req, res);
-      }
-
       if (req.params.method === "rewriteTo" && req.query.rewriteToken) {
         // Don't yet support multi-request copying
         return next();
       }
-
-      const metadata = adminStorageLayer.copyFile(
-        md,
-        req.params.destBucketId,
-        req.params.destObjectId,
-        req.body
-      );
-
-      if (!metadata) {
-        res.sendStatus(400);
-        return;
+      let metadata: StoredFileMetadata;
+      try {
+        metadata = adminStorageLayer.copyObject({
+          sourceBucket: req.params.bucketId,
+          sourceObject: req.params.objectId,
+          destinationBucket: req.params.destBucketId,
+          destinationObject: req.params.destObjectId,
+          incomingMetadata: req.body,
+          // TODO(tonyjhuang): Until we have a way of validating OAuth tokens passed by
+          // the GCS sdk or gcloud tool, we must assume all requests have valid admin creds.
+          // authorization: req.header("authorization")
+          authorization: "Bearer owner",
+        });
+      } catch (err) {
+        if (err instanceof NotFoundError) {
+          return sendObjectNotFound(req, res);
+        }
+        if (err instanceof ForbiddenError) {
+          return res.sendStatus(403);
+        }
+        throw err;
       }
 
       const resource = new CloudStorageObjectMetadata(metadata);
