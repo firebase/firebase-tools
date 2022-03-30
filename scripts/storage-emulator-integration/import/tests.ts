7c852be06934a7979897f3cd18ad4f3b3d2c42d5
Hsin-pei Toh <hsinpei@google.com>
2022-03-30 15:32:03 +0000

    Add test for importing exported Storage Emulator data

--- scripts/storage-emulator-integration/import/tests.ts
+++ scripts/storage-emulator-integration/import/tests.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import supertest = require("supertest");
 
+import { createTmpDir } from "../../../src/test/emulators/fixtures";
 import { Emulators } from "../../../src/emulator/types";
 import { TriggerEndToEndTest } from "../../integration-helpers/framework";
 import {
@@ -13,10 +14,10 @@ import {
 
 describe("Import Emulator Data", () => {
   const FIREBASE_PROJECT = "fake-project-id";
-  const bucket = `${FIREBASE_PROJECT}.appspot.com`;
-  const emulatorConfig = readEmulatorConfig(FIREBASE_EMULATOR_CONFIG);
-  const STORAGE_EMULATOR_HOST = getStorageEmulatorHost(emulatorConfig);
-  const test = new TriggerEndToEndTest(FIREBASE_PROJECT, __dirname, emulatorConfig);
+  const BUCKET = `${FIREBASE_PROJECT}.appspot.com`;
+  const EMULATOR_CONFIG = readEmulatorConfig(FIREBASE_EMULATOR_CONFIG);
+  const STORAGE_EMULATOR_HOST = getStorageEmulatorHost(EMULATOR_CONFIG);
+  const test = new TriggerEndToEndTest(FIREBASE_PROJECT, __dirname, EMULATOR_CONFIG);
 
   it("retrieves file from imported flattened emulator data", async function (this) {
     this.timeout(TEST_SETUP_TIMEOUT);
@@ -25,10 +26,12 @@ describe("Import Emulator Data", () => {
       Emulators.STORAGE,
       "--import",
       path.join(__dirname, "flattened-emulator-data"),
+      "--export-on-exit",
+      path.join(__dirname, "other-emulator-data"),
     ]);
 
     await supertest(STORAGE_EMULATOR_HOST)
-      .get(`/v0/b/${bucket}/o/test_upload.jpg`)
+      .get(`/v0/b/${BUCKET}/o/test_upload.jpg`)
       .set({ Authorization: "Bearer owner" })
       .expect(200);
   });
@@ -43,7 +46,39 @@ describe("Import Emulator Data", () => {
     ]);
 
     await supertest(STORAGE_EMULATOR_HOST)
-      .get(`/v0/b/${bucket}/o/test_upload.jpg`)
+      .get(`/v0/b/${BUCKET}/o/test_upload.jpg`)
+      .set({ Authorization: "Bearer owner" })
+      .expect(200);
+  });
+
+  it("retrieves file from importing previously exported emulator data", async function (this) {
+    this.timeout(TEST_SETUP_TIMEOUT);
+    const tmpDir = createTmpDir("exported-emulator-data");
+
+    // Upload file to Storage and export emulator data to tmp directory
+    await test.startEmulators(["--only", Emulators.STORAGE, "--export-on-exit", tmpDir]);
+    const uploadURL = await supertest(STORAGE_EMULATOR_HOST)
+      .post(`/v0/b/${BUCKET}/o/test_upload.jpg?uploadType=resumable&name=test_upload.jpg`)
+      .set({
+        Authorization: "Bearer owner",
+        "X-Goog-Upload-Protocol": "resumable",
+        "X-Goog-Upload-Command": "start",
+      })
+      .then((res) => new URL(res.header["x-goog-upload-url"]));
+
+    await supertest(STORAGE_EMULATOR_HOST)
+      .put(uploadURL.pathname + uploadURL.search)
+      .set({
+        "X-Goog-Upload-Protocol": "resumable",
+        "X-Goog-Upload-Command": "upload, finalize",
+      });
+
+    await test.stopEmulators();
+
+    // Import emulator data from tmp directory and retrieve file from Storage
+    await test.startEmulators(["--only", Emulators.STORAGE, "--import", tmpDir]);
+    await supertest(STORAGE_EMULATOR_HOST)
+      .get(`/v0/b/${BUCKET}/o/test_upload.jpg`)
       .set({ Authorization: "Bearer owner" })
       .expect(200);
   });
