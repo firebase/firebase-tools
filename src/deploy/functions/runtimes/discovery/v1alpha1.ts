26442981f01c4c7590bf1df04fe84ee771364628
Daniel Lee <danielylee@google.com>
2022-03-31 22:52:24 -0700

    Update TQ options to match the options specified in approved container contract design. (#4384)

--- src/deploy/functions/runtimes/discovery/v1alpha1.ts
+++ src/deploy/functions/runtimes/discovery/v1alpha1.ts
@@ -171,9 +171,9 @@ function parseEndpoints(
       if (ep.taskQueueTrigger.retryConfig) {
         assertKeyTypes(prefix + ".taskQueueTrigger.retryConfig", ep.taskQueueTrigger.retryConfig, {
           maxAttempts: "number",
-          maxRetryDuration: "string",
-          minBackoff: "string",
-          maxBackoff: "string",
+          maxRetrySeconds: "number",
+          minBackoffSeconds: "number",
+          maxBackoffSeconds: "number",
           maxDoublings: "number",
         });
       }
