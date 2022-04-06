3920ca1fed8ace6d65e2e0bd5e7d6c3def46560e
Joe Hanley <joehanley@google.com>
2022-04-06 13:04:56 -0700

    fix merge conflict

--- scripts/extensions-emulator-tests/run.sh
+++ scripts/extensions-emulator-tests/run.sh
@@ -8,15 +8,4 @@ source scripts/set-default-credentials.sh
   npm install
 )
 
-<<<<<<< HEAD
-npx mocha \
-  --require ts-node/register \
-  --require source-map-support/register \
-  --require src/test/helpers/mocha-bootstrap.ts \
-  --exit \
-  scripts/extensions-emulator-tests/tests.ts
-
-rm scripts/extensions-emulator-tests/functions/package.json
-=======
 mocha scripts/extensions-emulator-tests/tests.ts
->>>>>>> public/master
