4fb7aa50f9df5b57412d638bab116c9578de9f89
Yuchen Shi <yuchenshi@google.com>
2022-04-13 11:45:57 -0700

    Get lodash off the track.

--- src/track.ts
+++ src/track.ts
@@ -1,18 +1,17 @@
 import ua from "universal-analytics";
 
-import _ from "lodash";
 import { configstore } from "./configstore";
 import uuid from "uuid";
 import { logger } from "./logger";
 const pkg = require("../package.json");
 
-var anonId = configstore.get("analytics-uuid");
+let anonId = configstore.get("analytics-uuid");
 if (!anonId) {
   anonId = uuid.v4();
   configstore.set("analytics-uuid", anonId);
 }
 
-var visitor = ua(process.env.FIREBASE_ANALYTICS_UA || "UA-29174744-3", anonId, {
+const visitor = ua(process.env.FIREBASE_ANALYTICS_UA || "UA-29174744-3", anonId, {
   strictCidFormat: false,
   https: true,
 });
@@ -23,11 +22,6 @@ visitor.set("cd3", process.env.FIREPIT_VERSION || "none"); // FirepitVersion
 
 export function track(action: string, label: string, duration: number = 0): Promise<void> {
   return new Promise(function (resolve) {
-    if (!_.isString(action) || !_.isString(label)) {
-      logger.debug("track received non-string arguments:", action, label);
-      resolve();
-    }
-
     if (configstore.get("tokens") && configstore.get("usage")) {
       visitor.event("Firebase CLI " + pkg.version, action, label, duration).send(() => {
         // we could handle errors here, but we won't
