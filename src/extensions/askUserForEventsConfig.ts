ddcb2bae4387eb9fa851832bfe456b43a43d3154
huangjeff5 <64040981+huangjeff5@users.noreply.github.com>
2022-05-03 16:04:45 -0400

    Update CLI events prompt to include legal copy + ux audits (#4513)

--- src/extensions/askUserForEventsConfig.ts
+++ src/extensions/askUserForEventsConfig.ts
@@ -2,6 +2,9 @@ import * as _ from "lodash";
 import { promptOnce } from "../prompt";
 import * as extensionsApi from "../extensions/extensionsApi";
 import * as utils from "../utils";
+import * as clc from "cli-color";
+import { logger } from "../logger";
+const { marked } = require("marked");
 
 export interface InstanceEventsConfig {
   channel: string;
@@ -32,6 +35,11 @@ export async function askForEventsConfig(
   projectId: string,
   instanceId: string
 ): Promise<InstanceEventsConfig | undefined> {
+  logger.info(
+    `\n${clc.bold("Enable Events")}: ${marked(
+      "If you enable events, you can write custom event handlers ([https://firebase.google.com/docs/extensions/install-extensions#eventarc](https://firebase.google.com/docs/extensions/install-extensions#eventarc)) that respond to these events.\n\nYou can always enable or disable events later. Events will be emitted via Eventarc. Fees apply ([https://cloud.google.com/eventarc/pricing](https://cloud.google.com/eventarc/pricing))."
+    )}`
+  );
   if (!(await askShouldCollectEventsConfig())) {
     return undefined;
   }
@@ -82,7 +90,7 @@ export async function askShouldCollectEventsConfig(): Promise<boolean> {
   return promptOnce({
     type: "confirm",
     name: "shouldCollectEvents",
-    message: `Would you like to enable events? If you enable events, you can write custom event handlers (https://cloud.google.com/eventarc/pricing) that respond to these events. These events would be published to an Eventarc Channel. You can always enable or disable events later.\n\nEvents will be emitted via Eventarc. Fees apply (https://cloud.google.com/eventarc/pricing).`,
+    message: `Would you like to enable events?`,
     default: false,
   });
 }
