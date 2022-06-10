da54e19835c47d3891d9a50402be12144dc4bd75
Bryan Kendall <bkend@google.com>
2022-06-10 09:04:14 -0700

    clean up script

--- scripts/npm-link.sh
+++ scripts/npm-link.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-if [ "$CI" = "true" ]; then
-  echo "Doing normal npm link for now..."
-  npm link
-  # echo "Running sudo npm link..."
-  # sudo npm link
-else
-  echo "Running npm link..."
-  npm link
-fi
+echo "Running npm link..."
+npm link
