2057040644872ff86265949b1e230ab990392d1f
Jeff Huang <huangjeff@google.com>
2022-06-23 16:24:27 -0400

    explicitly enable eventarc in e2e extensions test

--- scripts/extensions-emulator-tests/tests.ts
+++ scripts/extensions-emulator-tests/tests.ts
@@ -57,7 +57,7 @@ describe("CF3 and Extensions emulator", () => {
     process.env.FIRESTORE_EMULATOR_HOST = `localhost:${firestorePort}`;
 
     test = new TriggerEndToEndTest(FIREBASE_PROJECT, __dirname, config);
-    await test.startEmulators(["--only", "functions,extensions,storage"]);
+    await test.startEmulators(["--only", "functions,extensions,storage,eventarc"]);
 
     admin.initializeApp({
       projectId: FIREBASE_PROJECT,
