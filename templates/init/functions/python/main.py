aebfbd35d2e631dfaece1ac8b12f1b24326efb56
Salakar <mike.diarmid@gmail.com>
2022-06-28 13:35:43 +0100

    chore: update python template

--- templates/init/functions/python/main.py
+++ templates/init/functions/python/main.py
@@ -2,22 +2,9 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
-from firebase_functions import functions
+from firebase_functions import https, options
 
 
-@functions.https(memory_mb=256)
+@https.on_call(memory=options.Memory.MB_256)
 def hellofunction(request):
-    return ('HELLO World!', 200, {})
-
-
-@functions.https(memory_mb=256)
-def foofunction(request):
-    return ('FOO World!', 200, {})
-
-
-@functions.pubsub(
-    topic='news',
-    min_instances=1,
-)
-def pubsub_function(event, context):
-    print('pubsub:', event, context)
+    return ('Hello World!', 200, {})
