2f303ecdd89c4a1291dca057637598b56f3b348e
Salakar <mike.diarmid@gmail.com>
2022-07-13 13:51:25 +0100

    chore: update python main.py template

--- templates/init/functions/python/main.py
+++ templates/init/functions/python/main.py
@@ -2,9 +2,20 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
-from firebase_functions import https, options
+from firebase_functions import https, pubsub, options
 
 
 @https.on_call(memory=options.Memory.MB_256)
-def hellofunction(request):
-    return ('Hello World!', 200, {})
+def hellofunctiononcall(request: https.CallableRequest):
+    return 'Hello, world!'
+
+
+@https.on_request(memory=options.Memory.MB_256)
+def hellofunctiononrequest(request: https.FlaskRequest, response: https.FlaskResponse):
+    response.status_code = 200
+    response.set_data('Hello World (on_request)!')
+
+
+# @pubsub.on_message_published(topic='cool_things', memory=options.Memory.MB_512)
+# def pubsubfunction(message: pubsub.CloudEventMessage):
+#     return ''
