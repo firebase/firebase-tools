e1238d1dd20a5249334aa3464dc5c09f41b8c908
Daniel Young Lee <danielylee@google.com>
2022-07-27 20:56:10 -0700

    Modify run script to install deps in functions source.

--- scripts/functions-deploy-tests/run.sh
+++ scripts/functions-deploy-tests/run.sh
@@ -4,4 +4,6 @@ set -e # Immediately exit on failure
 # Globally link the CLI for the testing framework
 ./scripts/npm-link.sh
 
+(cd scripts/functions-deploy-test/functions && npm i)
+
 mocha scripts/functions-deploy-tests/tests.ts
