536cd799d12f16be088fa07801e011bd47a98d7a
Tony Huang <tony.huang.jun@gmail.com>
2022-08-02 16:54:17 -0400

    revert run.sh

--- scripts/storage-emulator-integration/run.sh
+++ scripts/storage-emulator-integration/run.sh
@@ -7,12 +7,12 @@ set -e # Immediately exit on failure
 # Prepare the storage emulator rules runtime
 firebase setup:emulators:storage
 
-# mocha scripts/storage-emulator-integration/rules/*.test.ts
+mocha scripts/storage-emulator-integration/rules/*.test.ts
 
-# mocha scripts/storage-emulator-integration/import/tests.ts
+mocha scripts/storage-emulator-integration/import/tests.ts
 
-# mocha scripts/storage-emulator-integration/multiple-targets/tests.ts
+mocha scripts/storage-emulator-integration/multiple-targets/tests.ts
 
 mocha scripts/storage-emulator-integration/tests.ts
 
-# mocha scripts/storage-emulator-integration/*.test.ts
+mocha scripts/storage-emulator-integration/*.test.ts
