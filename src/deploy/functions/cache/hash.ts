03a25fbb357b1d2fd4466c92569ba52731d23d42
Tyler Stark <theirondeveloper@gmail.com>
2022-08-11 13:55:00 -0500

    Beefed up tests to cover environment variables + secretVersion use cases

--- src/deploy/functions/cache/hash.ts
+++ src/deploy/functions/cache/hash.ts
@@ -2,34 +2,50 @@ import * as fs from "fs";
 import * as crypto from "crypto";
 import * as args from "../args";
 import * as secrets from "../../../functions/secrets";
-import * as backend from "../backend";
+import { Backend, Endpoint, EnvironmentVariables } from "../backend";
 
-export async function getLocalHash(source: args.Source, wantBackend: backend.Backend) {
+export async function getBackendHash(backend: Backend, source?: args.Source) {
   const hash = crypto.createHash("sha256");
-  const sourceFile = source.functionsSourceV2 || source.functionsSourceV1;
-  // Hash the contents of the source file
-  if (sourceFile) {
-    const readStream = fs.createReadStream(sourceFile);
-    readStream.pipe(hash);
-    await new Promise((resolve, reject) => {
-      hash.on("end", () => resolve(hash.read()));
-      readStream.on("error", reject);
-    });
+
+  // If present, hash the contents of the source file
+  if (source) {
+    const sourceFile = source?.functionsSourceV2 || source?.functionsSourceV1;
+    if (sourceFile) {
+      const readStream = fs.createReadStream(sourceFile);
+      readStream.pipe(hash);
+      await new Promise((resolve, reject) => {
+        hash.on("end", () => resolve(hash.read()));
+        readStream.on("error", reject);
+      });
+    }
   }
 
   // Hash the contents of the dotenv variables
-  hash.push(wantBackend?.environmentVariables);
+  if (hasEnvironmentVariables(backend.environmentVariables)) {
+    hash.push(JSON.stringify(backend.environmentVariables));
+  }
 
   // Hash the secret versions.
-  hash.push(getSecretVersions(wantBackend));
+  const secretVersions = getSecretVersions(backend);
+  if (hasSecretVersions(secretVersions)) {
+    hash.push(JSON.stringify(secretVersions));
+  }
 
   return hash.read().toString("hex");
 }
 
+function hasEnvironmentVariables(environmentVariables: EnvironmentVariables) {
+  return Object.keys(environmentVariables).length;
+}
+
+function hasSecretVersions(secretVersions: Record<string, string>) {
+  return Object.keys(secretVersions).length;
+}
+
 // Hash the secret versions.
-function getSecretVersions(wantBackend: backend.Backend): Record<string, string> {
-  const endpointsById = Object.values(wantBackend?.endpoints || {});
-  const endpointsList: backend.Endpoint[] = endpointsById
+function getSecretVersions(backend: Backend): Record<string, string> {
+  const endpointsById = Object.values(backend?.endpoints || {});
+  const endpointsList: Endpoint[] = endpointsById
     .map((endpoints) => Object.values(endpoints))
     .reduce((memo, endpoints) => [...memo, ...endpoints], []);
   return secrets.of(endpointsList).reduce((memo, { secret, version }) => {
--- src/test/functions/cache/hash.spec.ts
+++ src/test/functions/cache/hash.spec.ts
@@ -1,44 +1,205 @@
 import { expect } from "chai";
-import { getLocalHash } from "../../../deploy/functions/cache/hash";
-import * as backend from "../../../deploy/functions/backend";
+import { getBackendHash } from "../../../deploy/functions/cache/hash";
+import { Backend } from "../../../deploy/functions/backend";
 import { resolve } from "path";
+import { Endpoint } from "../../../deploy/functions/backend";
 
 const EMPTY_WANT_BACKEND = {
   requiredAPIs: [],
   environmentVariables: {},
   endpoints: {},
-} as backend.Backend;
+} as Backend;
 
-// eslint-disable-next-line @typescript-eslint/require-await
-describe("getLocalHash", () => {
-  it("should return different hash given different files", async (done) => {
+const EMPTY_ENDPOINT: Endpoint = {
+  id: "id",
+  region: "region",
+  project: "project",
+  platform: "gcfv2",
+  runtime: "nodejs16",
+  entryPoint: "ep",
+  httpsTrigger: {},
+  secretEnvironmentVariables: [
+    {
+      key: "key",
+      secret: "secret",
+      projectId: "projectId",
+      version: "1",
+    },
+  ],
+};
+
+describe("getBackendHash", () => {
+  it("should return different hash given different env variables", async () => {
+    const backend1 = {
+      ...EMPTY_WANT_BACKEND,
+      environmentVariables: {
+        PROJECT: "v1",
+      },
+    };
+    const backend2 = {
+      ...EMPTY_WANT_BACKEND,
+      environmentVariables: {
+        PROJECT: "v2",
+      },
+    };
+    const hash1 = await getBackendHash(backend1);
+    const hash2 = await getBackendHash(backend2);
+
+    expect(hash1).to.not.equal(hash2);
+  });
+
+  it("should return same hash given same env variables", async () => {
+    const backend1 = {
+      ...EMPTY_WANT_BACKEND,
+      environmentVariables: {
+        PROJECT: "v0",
+      },
+    };
+    const backend2 = {
+      ...EMPTY_WANT_BACKEND,
+      environmentVariables: {
+        PROJECT: "v0",
+      },
+    };
+    const hash1 = await getBackendHash(backend1);
+    const hash2 = await getBackendHash(backend2);
+
+    expect(hash1).to.equal(hash2);
+  });
+
+  it("should return different hash given different secret versions", async () => {
+    const backend1 = {
+      ...EMPTY_WANT_BACKEND,
+      endpoints: {
+        region: {
+          id: {
+            ...EMPTY_ENDPOINT,
+            secretEnvironmentVariables: [
+              {
+                key: "key_same",
+                secret: "secret_same",
+                projectId: "projectId",
+                version: "1",
+              },
+              {
+                key: "key_test",
+                secret: "secret_test",
+                projectId: "projectId",
+                version: "1",
+              },
+            ],
+          },
+        },
+      },
+    };
+    const backend2 = {
+      ...EMPTY_WANT_BACKEND,
+      endpoints: {
+        region: {
+          id: {
+            ...EMPTY_ENDPOINT,
+            secretEnvironmentVariables: [
+              {
+                key: "key_same",
+                secret: "secret_same",
+                projectId: "projectId",
+                version: "1",
+              },
+              {
+                key: "key_test",
+                secret: "secret_test",
+                projectId: "projectId",
+                version: "2", // different from backend1
+              },
+            ],
+          },
+        },
+      },
+    };
+    const hash1 = await getBackendHash(backend1);
+    const hash2 = await getBackendHash(backend2);
+
+    expect(hash1).to.not.equal(hash2);
+  });
+
+  it("should return same hash given same secret versions", async () => {
+    const backend1 = {
+      ...EMPTY_WANT_BACKEND,
+      endpoints: {
+        region: {
+          id: {
+            ...EMPTY_ENDPOINT,
+            secretEnvironmentVariables: [
+              {
+                key: "key_same",
+                secret: "secret_same",
+                projectId: "projectId",
+                version: "1",
+              },
+              {
+                key: "key_test",
+                secret: "secret_test",
+                projectId: "projectId",
+                version: "1",
+              },
+            ],
+          },
+        },
+      },
+    };
+    const backend2 = {
+      ...EMPTY_WANT_BACKEND,
+      endpoints: {
+        region: {
+          id: {
+            ...EMPTY_ENDPOINT,
+            secretEnvironmentVariables: [
+              {
+                key: "key_same",
+                secret: "secret_same",
+                projectId: "projectId",
+                version: "1",
+              },
+              {
+                key: "key_test",
+                secret: "secret_test",
+                projectId: "projectId",
+                version: "1", // same as backend1
+              },
+            ],
+          },
+        },
+      },
+    };
+    const hash1 = await getBackendHash(backend1);
+    const hash2 = await getBackendHash(backend2);
+
+    expect(hash1).to.equal(hash2);
+  });
+
+  it("should return different hash given different files", async () => {
     const file1 = resolve("./mockdata/function_source_v1.txt");
     const file2 = resolve("./mockdata/function_source_v2.txt");
 
     const source1 = { functionsSourceV1: file1 };
     const source2 = { functionsSourceV1: file2 };
 
-    const hash1 = await getLocalHash(source1, EMPTY_WANT_BACKEND);
-    const hash2 = await getLocalHash(source2, EMPTY_WANT_BACKEND);
+    const hash1 = await getBackendHash(EMPTY_WANT_BACKEND, source1);
+    const hash2 = await getBackendHash(EMPTY_WANT_BACKEND, source2);
 
     expect(hash1).to.not.equal(hash2);
-    done();
   });
 
-  it("should return the same hash given the same file", async (done) => {
+  it("should return the same hash given the same file", async () => {
     const file1 = resolve("./mockdata/function_source_v1.txt");
     const file2 = resolve("./mockdata/function_source_v1.txt");
 
     const source1 = { functionsSourceV1: file1 };
     const source2 = { functionsSourceV1: file2 };
 
-    const hash1 = await getLocalHash(source1, EMPTY_WANT_BACKEND);
-    const hash2 = await getLocalHash(source2, EMPTY_WANT_BACKEND);
+    const hash1 = await getBackendHash(EMPTY_WANT_BACKEND, source1);
+    const hash2 = await getBackendHash(EMPTY_WANT_BACKEND, source2);
 
     expect(hash1).to.equal(hash2);
-    done();
   });
-
-  // TODO(tystark) test case for secret versions
-  // TODO(tystark) test case for env variables
 });
