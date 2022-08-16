47f3bc842e2608341e387315a4984a2641986a14
Victor Fan <vsfan@google.com>
2022-08-15 21:03:34 -0700

    error out for all missing params at once

--- src/deploy/functions/params.ts
+++ src/deploy/functions/params.ts
@@ -219,6 +219,7 @@ function canSatisfyParam(param: Param, value: ParamValue): boolean {
  * - the value of a Cloud Secret in the same project with name == param name (not implemented yet), but only if it's a SecretParam
  * - a literal value of the same type already defined in one of the .env files with key == param name
  * - the value returned by interactively prompting the user
+ *   - it is an error to have params that need to be prompted if the CLI is running in non-interactive mode
  *   - the default value of the prompt comes from the SDK via param.default, which may be a literal value or a CEL expression
  *   - if the default CEL expression is not resolvable--it depends on a param whose value is not yet known--we throw an error
  *   - yes, this means that the same set of params may or may not throw depending on the order the SDK provides them to us in
@@ -248,16 +249,15 @@ export async function resolveParams(
     paramValues[param.name] = userEnvs[param.name];
   }
 
+  if (nonInteractive && outstanding.length > 0) {
+    const envNames = outstanding.map((p) => p.name).join(", ");
+    throw new FirebaseError(
+      `In non-interactive mode but have no value for the following environment variables: ${envNames}\n` +
+        "To continue, either run `firebase deploy` with an interactive terminal, or add values to a dotenv file. " +
+        "For information regarding how to use dotenv files, see https://firebase.google.com/docs/functions/config-env"
+    );
+  }
   for (const param of outstanding) {
-    if (nonInteractive) {
-      throw new FirebaseError(
-        `In non-interactive mode but no value for ${
-          param.name || param.label
-        } found in .env.${projectId} file. ` +
-          "To set the value of a parameter non-interactively, add the key-value pair to the environment file."
-      );
-    }
-
     let paramDefault: ParamValue | undefined = param.default;
     if (paramDefault && isCEL(paramDefault)) {
       paramDefault = resolveDefaultCEL(param.type, paramDefault, paramValues);
