1fe63a3840386b3bebf03031151d7886da6db832
Daniel Young Lee <danielylee@google.com>
2022-09-08 21:01:00 -0700

    Fix cleanup script for Windows.

---
+++ scripts/functions-deploy-tests/functions/options.js
@@ -0,0 +1,7 @@
+export const v1Opts = {};
+export const v2Opts = {};
+export const v1TqOpts = {};
+export const v2TqOpts = {};
+export const v1IdpOpts = {};
+export const v2IdpOpts = {};
+export const v1ScheduleOpts = {};
