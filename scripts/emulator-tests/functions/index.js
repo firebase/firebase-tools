485d16ec3f8a9884aadef64df03a51c49ef391f3
Daniel Young Lee <danielylee@google.com>
2022-09-08 21:08:13 -0700

    Remove accidentally added file.

--- scripts/emulator-tests/functions/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = (() => {
-                    return {
-                        functionId: require("firebase-functions")
-                            .runWith({})
-                            .https.onRequest(async () => {
-                            return Promise.reject(new Error("not a thing"));
-                        }),
-                    };
-                })();
