fe9bec1b56f7c53f89d6e7528292ada5761feb6f
Victor Fan <vsfan@google.com>
2022-09-14 02:53:29 -0700

    more test coverage

--- src/test/deploy/functions/params.spec.ts
+++ src/test/deploy/functions/params.spec.ts
@@ -85,7 +85,15 @@ describe("resolveParams", () => {
     promptOnce.restore();
   });
 
-  it("can pull a literal value out of the dotenvs, with any type valid", async () => {
+  it("always contains the precanned internal param values", async () => {
+    const paramsToResolve: params.Param[] = [];
+    const userEnv: Record<string, params.ParamValue> = {};
+    await expect(
+      params.resolveParams(paramsToResolve, fakeConfig, userEnv)
+    ).to.eventually.deep.equal(expectedInternalParams);
+  });
+
+  it("can pull a literal value out of the dotenvs", async () => {
     const paramsToResolve: params.Param[] = [
       {
         name: "foo",
@@ -97,21 +105,61 @@ describe("resolveParams", () => {
       },
     ];
     const userEnv: Record<string, params.ParamValue> = {
-      foo: new params.ParamValue("bar", false, { string: true, number: true, boolean: true }),
-      bar: new params.ParamValue("24", false, { string: true, number: true, boolean: true }),
-      baz: new params.ParamValue("true", false, { string: true, number: true, boolean: true }),
+      foo: new params.ParamValue("bar", false, { string: true, number: false, boolean: false }),
+      bar: new params.ParamValue("24", false, { string: false, number: true, boolean: false }),
+      baz: new params.ParamValue("true", false, { string: false, number: false, boolean: true }),
     };
     await expect(
       params.resolveParams(paramsToResolve, fakeConfig, userEnv)
     ).to.eventually.deep.equal(
       Object.assign(
         {
-          foo: new params.ParamValue("bar", false, { string: true, number: true, boolean: true }),
-          bar: new params.ParamValue("24", false, { string: true, number: true, boolean: true }),
+          foo: new params.ParamValue("bar", false, { string: true, number: false, boolean: false }),
+          bar: new params.ParamValue("24", false, { string: false, number: true, boolean: false }),
         },
         expectedInternalParams
       )
     );
+  });
+
+  it("params from dotenvs override internal params of the same name", async () => {
+    const paramsToResolve: params.Param[] = [
+      {
+        name: "PROJECT_ID",
+        type: "string",
+      },
+    ];
+    const userEnv: Record<string, params.ParamValue> = {
+      PROJECT_ID: new params.ParamValue("other_value", false, {
+        string: true,
+        number: false,
+        boolean: false,
+      }),
+    };
+    await expect(
+      params.resolveParams(paramsToResolve, fakeConfig, userEnv)
+    ).to.eventually.deep.equal({
+      DATABASE_URL: new params.ParamValue(fakeConfig.databaseURL, true, {
+        string: true,
+        boolean: false,
+        number: false,
+      }),
+      GCLOUD_PROJECT: new params.ParamValue(fakeConfig.projectId, true, {
+        string: true,
+        boolean: false,
+        number: false,
+      }),
+      PROJECT_ID: new params.ParamValue("other_value", false, {
+        string: true,
+        boolean: false,
+        number: false,
+      }),
+      STORAGE_BUCKET: new params.ParamValue(fakeConfig.storageBucket, true, {
+        string: true,
+        boolean: false,
+        number: false,
+      }),
+    });
   });
 
   it("does not create the corresponding internal params if database url/storage bucket are not configured", async () => {
