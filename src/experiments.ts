a5cc083d3f4bedcd3ac9fa2cf6c1f8ab57f8413b
Thomas Bouldin <inlined@google.com>
2022-09-21 17:53:10 -0700

    Add experiments commands

--- src/experiments.ts
+++ src/experiments.ts
@@ -1,9 +1,10 @@
-import { configstore } from "./configstore";
 import * as clc from "colorette";
+import { KeyPairSyncResult } from "crypto";
+import * as leven from "leven";
+import { configstore } from "./configstore";
 import { FirebaseError } from "./error";
 
-export interface Experiment<Name extends string> {
-  name: Name;
+export interface Experiment {
   shortDescription: string;
   fullDescription?: string;
   public?: boolean;
@@ -11,107 +12,126 @@ export interface Experiment<Name extends string> {
   default?: boolean;
 }
 
-function experiment<Name extends string>(
-  name: Name,
-  shortDescription: string,
-  opts?: {
-    fullDescription?: string;
-    public?: boolean;
-    docsUri?: string;
-    default?: boolean;
-  }
-): Experiment<Name> {
-  return {
-    ...opts,
-    name,
-    shortDescription,
-  } as Experiment<Name>;
+// Utility method to ensure there are no typos in defining ALL_EXPERIMENTS
+function experiments<Keys extends string>(exp: Record<Keys, Experiment>): Record<Keys, Experiment> {
+  return Object.freeze(exp);
 }
 
-export const EXPERIMENTS = Object.freeze([
+export const ALL_EXPERIMENTS = experiments({
   // meta:
-  experiment("experiments", "enables the experiments family of commands"),
+  experiments: {
+    shortDescription: "enables the experiments family of commands",
+  },
 
   // Realtime Database experiments
-  experiment("rtdbrules", "TODO"),
-  experiment("rtdbmanagement", "TODO"),
+  rtdbrules: {
+    shortDescription: "TODO",
+  },
+  rtdbmanagement: {
+    shortDescription: "TODO",
+  },
 
   // Extensions experiments
-  experiment("ext", "Enables the 'ext:sources:create' command"),
-  experiment("extdev", "Enalbes the 'ext:dev' family of commands"),
+  ext: {
+    shortDescription: "Enables the 'ext:sources:create' command",
+  },
+  extdev: {
+    shortDescription: "Enalbes the 'ext:dev' family of commands",
+  },
 
   // Cloud Functions for Firebase experiments
-  experiment("pythonfunctions", "Python support for Cloud Functions for Firebase", {
+  pythonfunctions: {
+    shortDescription: "Python support for Cloud Functions for Firebase",
     fullDescription:
       "Adds the ability to initializea and deploy Cloud " +
       "Functions for Firebase in Python. While this feature is experimental " +
       "breaking API changes are allowed in MINOR API revisions",
-  }),
-  experiment("golang", "Golang support for Cloud Functions for Firebase. Does not work.", {
+  },
+  golang: {
+    shortDescription: "Golang support for Cloud Functions for Firebase. Does not work.",
     fullDescription:
       "Code paths to play with adding support for Golang to Cloud " +
       "Functions for Firebase. This was written to target 1st gen functions, which " +
       "will never be released (all new languages will be 2nd gen only). If you " +
       "are lurking in our experiments folder and are excited about the prospect of " +
       "Golang support, tell our support team.",
-  }),
-  experiment(
-    "deletegcfartifacts",
-    `Add the ${clc.bold(
+  },
+  deletegcfartifacts: {
+    shortDescription: `Add the ${clc.bold(
       "firebase functions:deletegcfartifacts"
     )} command to purge docker build images`,
-    {
-      fullDescription:
-        `Add the ${clc.bold("firebase functions:deletegcfartifacts")}` +
-        "command. Google Cloud Functions creates Docker images when building your " +
-        "functions. Cloud Functions for Firebase automatically cleans up these " +
-        "images for you on deploy. Customers who predated this cleanup, or customers " +
-        "who also deploy Google Cloud Functions with non-Firebase tooling may have " +
-        "old Docker images stored in either Google Container Repository or Artifact " +
-        `Registry. The ${clc.bold("firebase functions:deletegcfartifacts")} command ` +
-        "will delete all Docker images created by Google Cloud Functions irrespective " +
-        "of how that image was created.",
-      public: true,
-    }
-  ),
-  experiment("functionsparams", "Adds support for paramaterizing functions deployments"),
-  experiment(
-    "skipdeployingnoopfunctions",
-    "Detect that there have been no changes to a function and skip deployment"
-  ),
+    fullDescription:
+      `Add the ${clc.bold("firebase functions:deletegcfartifacts")}` +
+      "command. Google Cloud Functions creates Docker images when building your " +
+      "functions. Cloud Functions for Firebase automatically cleans up these " +
+      "images for you on deploy. Customers who predated this cleanup, or customers " +
+      "who also deploy Google Cloud Functions with non-Firebase tooling may have " +
+      "old Docker images stored in either Google Container Repository or Artifact " +
+      `Registry. The ${clc.bold("firebase functions:deletegcfartifacts")} command ` +
+      "will delete all Docker images created by Google Cloud Functions irrespective " +
+      "of how that image was created.",
+    public: true,
+  },
+  functionsparams: {
+    shortDescription: "Adds support for paramaterizing functions deployments",
+  },
+  skipdeployingnoopfunctions: {
+    shortDescription: "Detect that there have been no changes to a function and skip deployment",
+  },
 
   // Emulator experiments
-  experiment("emulatoruisnapshot", "Load pre-release versions of hte emulator UI"),
+  emulatoruisnapshot: {
+    shortDescription: "Load pre-release versions of hte emulator UI",
+  },
 
   // Hosting experiments
-  experiment("frameworkawareness", "Native support for popular web frameworks", {
+  frameworkawareness: {
+    shortDescription: "Native support for popular web frameworks",
     fullDescription:
       "Adds support for popular web frameworks such as Next.js " +
       "Nuxt, Netlify, Angular, and Vite-compatible frameworks. Firebase is " +
       "committed to support these platforms long-term, but a manual migration " +
       "may be required when the non-experimental support for these frameworks " +
       "is released",
-  }),
+  },
 
   // Access experiments
-  experiment("crossservicerules", "Allow Firebase Rules to reference resources in other services"),
-] as const);
+  crossservicerules: {
+    shortDescription: "Allow Firebase Rules to reference resources in other services",
+  },
+});
 
-export type ExperimentName = typeof EXPERIMENTS[number]["name"];
-
-export const ALL_EXPERIMENTS: Array<ExperimentName> = EXPERIMENTS.map((e) => e.name);
+export type ExperimentName = keyof typeof ALL_EXPERIMENTS;
 
 /** Determines whether a name is a valid experiment name. */
 export function isValidExperiment(name: string): name is ExperimentName {
-  return ALL_EXPERIMENTS.includes(name as ExperimentName);
+  return Object.keys(ALL_EXPERIMENTS).includes(name);
 }
 
+/**
+ * Detects experiment names that were potentially what a customer intended to
+ * type when they provided malformed.
+ * Returns null if the malformed name is actually an experiment. Returns all
+ * possible typos.
+ */
+export function experimentNameAutocorrect(malformed: string): string[] | null {
+  if (isValidExperiment(malformed)) {
+    return null;
+  }
+
+  // N.B. I personally would use < (name.length + malformed.length) * 0.2
+  // but this logic matches src/index.ts. I neither want to change something
+  // with such potential impact nor to create divergent behavior.
+  return Object.keys(ALL_EXPERIMENTS).filter(
+    (name) => leven(name, malformed) < malformed.length * 0.4
+  );
+}
 let localPreferencesCache: Record<ExperimentName, boolean> | undefined = undefined;
 function localPreferences(): Record<ExperimentName, boolean> {
   if (!localPreferencesCache) {
     localPreferencesCache = configstore.get("previews") as Record<ExperimentName, boolean>;
     for (const key of Object.keys(localPreferencesCache)) {
-      if (!EXPERIMENTS.find((e) => e.name === key)) {
+      if (!isValidExperiment(key)) {
         delete localPreferencesCache[key as ExperimentName];
       }
     }
@@ -121,8 +141,7 @@ function localPreferences(): Record<ExperimentName, boolean> {
 
 /** Returns whether an experiment is enabled. */
 export function isEnabled(name: ExperimentName): boolean {
-  const explicit = localPreferences()[name];
-  return explicit ?? EXPERIMENTS.find((e) => e.name === name)?.default ?? false;
+  return localPreferences()[name] ?? ALL_EXPERIMENTS[name]?.default ?? false;
 }
 
 /**
@@ -153,22 +172,6 @@ export function assertEnabled(name: ExperimentName, task: string): void {
       )} run ${clc.bold(`firebase experiments:enable ${name}`)}`
     );
   }
-}
-
-/** Gets the list of experiments that are enabled. */
-export function getEnabled(): ExperimentName[] {
-  const allEnabled = new Set<ExperimentName>();
-  for (const [name, isEnabled] of Object.entries(localPreferences())) {
-    if (isEnabled) {
-      allEnabled.add(name as ExperimentName);
-    }
-  }
-  for (const exp of EXPERIMENTS) {
-    if (exp.default) {
-      allEnabled.add(exp.name);
-    }
-  }
-  return [...allEnabled];
 }
 
 /** Saves the current set of enabled experiments to disk. */
