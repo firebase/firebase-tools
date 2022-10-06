dc118cdc6e03b1327f244ca4e7db2d53344ae0db
Tyler Stark <theirondeveloper@gmail.com>
2022-10-05 23:53:51 -0500

    Fixed integration test (it works now!!!!)

--- scripts/webframeworks-deploy-tests/run.sh
+++ scripts/webframeworks-deploy-tests/run.sh
@@ -4,6 +4,6 @@ set -e # Immediately exit on failure
 # Globally link the CLI for the testing framework
 ./scripts/npm-link.sh
 
-(cd scripts/webframeworks-deploy-tests/hosting && npm i && npm run build)
+(cd scripts/webframeworks-deploy-tests/hosting; npm i; npm run build)
 
 mocha scripts/webframeworks-deploy-tests/tests.ts
