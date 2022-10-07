6d9a79ef1de6ecaf2a44a0ba0b5c54857f6bc141
Yuchen Shi <yuchenshi@google.com>
2022-10-07 12:45:11 -0700

    Assign emulator ports and resolve hostnames upfront. (#5083)
    * Assign emulator ports and resolve hostnames upfront.
    
    * Fix test ref.
    
    * Add changelog.
    
    * Restore IPv6 dual stack on wildcard.
    
    * Update hard-coded localhost in tests.

--- src/test/emulators/auth/cloudFunctions.spec.ts
+++ src/test/emulators/auth/cloudFunctions.spec.ts
@@ -2,19 +2,14 @@ import { expect } from "chai";
 import * as nock from "nock";
 
 import { AuthCloudFunction } from "../../../emulator/auth/cloudFunctions";
-import { findAvailablePort } from "../../../emulator/portUtils";
 import { EmulatorRegistry } from "../../../emulator/registry";
 import { Emulators } from "../../../emulator/types";
 import { FakeEmulator } from "../fakeEmulator";
 
 describe("cloudFunctions", () => {
   describe("dispatch", () => {
-    const host = "localhost";
-    let port = 4000;
     before(async () => {
-      port = await findAvailablePort(host, port);
-
-      const emu = new FakeEmulator(Emulators.FUNCTIONS, host, port);
+      const emu = await FakeEmulator.create(Emulators.FUNCTIONS);
       await EmulatorRegistry.start(emu);
       nock(EmulatorRegistry.url(Emulators.FUNCTIONS).toString())
         .post("/functions/projects/project-foo/trigger_multicast", {
