178bbd58c12b7fe2a487d755b211efcb3d376310
Thomas Bouldin <inlined@users.noreply.github.com>
2022-10-10 15:50:37 -0700

    Add link to associate sites with app ids (#5105)

--- src/frameworks/index.ts
+++ src/frameworks/index.ts
@@ -197,12 +197,12 @@ export async function discover(dir: string, warn = true) {
       }
     }
     if (frameworksDiscovered.length > 1) {
-      if (warn) console.error("Multiple conflicting frameworks discovered. TODO link");
+      if (warn) console.error("Multiple conflicting frameworks discovered.");
       return;
     }
     if (frameworksDiscovered.length === 1) return frameworksDiscovered[0];
   }
-  if (warn) console.warn("We can't detirmine the web framework in use. TODO link");
+  if (warn) console.warn("Could not determine the web framework in use.");
   return;
 }
 
@@ -335,7 +335,8 @@ export async function prepareFrameworks(
           const defaultConfig = await implicitInit(options);
           if (defaultConfig.json) {
             console.warn(
-              `Site ${site} is not associated with an app ID. Injecting default app config`
+              `No Firebase app associated with site ${site}, injecting project default config.
+  You can link a Web app to a Hosting site here https://console.firebase.google.com/project/${project}/settings/general/web`
             );
             firebaseDefaults ||= {};
             firebaseDefaults.config = JSON.parse(defaultConfig.json);
@@ -345,7 +346,7 @@ export async function prepareFrameworks(
             // on a project that never initialized hosting?
             console.warn(
               `No Firebase app associated with site ${site}, unable to provide authenticated server context.
-  You can link a Web app to a Hosting site here https://console.firebase.google.com/project/_/settings/general/web`
+  You can link a Web app to a Hosting site here https://console.firebase.google.com/project/${project}/settings/general/web`
             );
             if (!options.nonInteractive) {
               const continueDeploy = await promptOnce({
