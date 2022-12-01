4a9053a1a61583c9fda91966bfe198fb0d1f9e09
Wei Wang <weiwng@google.com>
2022-12-01 14:59:21 -0600

    test

--- scripts/storage-emulator-integration/conformance/firebase-js-sdk.test.ts
+++ scripts/storage-emulator-integration/conformance/firebase-js-sdk.test.ts
@@ -6,7 +6,7 @@ import * as fs from "fs";
 import * as puppeteer from "puppeteer";
 import { TEST_ENV } from "./env";
 import { IMAGE_FILE_BASE64 } from "../../../src/test/emulators/fixtures";
-import { EmulatorEndToEndTest } from "../../integration-helpers/framework";
+import { TriggerEndToEndTest } from "../../integration-helpers/framework";
 import {
   createRandomFile,
   EMULATORS_SHUTDOWN_DELAY_MS,
@@ -31,7 +31,7 @@ describe("Firebase Storage JavaScript SDK conformance tests", () => {
   const smallFilePath: string = createRandomFile("small_file", SMALL_FILE_SIZE, tmpDir);
   const emptyFilePath: string = createRandomFile("empty_file", 0, tmpDir);
 
-  let test: EmulatorEndToEndTest;
+  let test: TriggerEndToEndTest;
   let testBucket: Bucket;
   let authHeader: { Authorization: string };
   let browser: puppeteer.Browser;
@@ -84,7 +84,7 @@ describe("Firebase Storage JavaScript SDK conformance tests", () => {
     this.timeout(TEST_SETUP_TIMEOUT);
     TEST_ENV.applyEnvVars();
     if (!TEST_ENV.useProductionServers) {
-      test = new EmulatorEndToEndTest(TEST_ENV.fakeProjectId, __dirname, TEST_ENV.emulatorConfig);
+      test = new TriggerEndToEndTest(TEST_ENV.fakeProjectId, __dirname, TEST_ENV.emulatorConfig);
       await test.startEmulators(["--only", "auth,storage"]);
     }
 
