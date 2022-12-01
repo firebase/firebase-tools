9f50206ef06c47214abe0044e0f79f3e16eaf649
Wei Wang <weiwng@google.com>
2022-12-01 16:06:51 -0600

    test

--- scripts/storage-emulator-integration/run.sh
+++ scripts/storage-emulator-integration/run.sh
@@ -10,13 +10,13 @@ source scripts/set-default-credentials.sh
 # Prepare the storage emulator rules runtime
 firebase setup:emulators:storage
 
-mocha scripts/storage-emulator-integration/rules/*.test.ts
+# mocha scripts/storage-emulator-integration/rules/*.test.ts
 
-mocha scripts/storage-emulator-integration/import/tests.ts
+# mocha scripts/storage-emulator-integration/import/tests.ts
 
-mocha scripts/storage-emulator-integration/internal/tests.ts
+# mocha scripts/storage-emulator-integration/internal/tests.ts
 
-mocha scripts/storage-emulator-integration/multiple-targets/tests.ts
+# mocha scripts/storage-emulator-integration/multiple-targets/tests.ts
 
-mocha scripts/storage-emulator-integration/conformance/*.test.ts
+mocha scripts/storage-emulator-integration/conformance/firebase-js-sdk.test.ts
 
