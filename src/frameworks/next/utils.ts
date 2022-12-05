9a230e3a36439f4f93810054107cfad9269f9cc1
Leonardo Ortiz <leo@monogram.io>
2022-12-05 15:56:54 -0300

    improve Next.js utils documentation

--- src/frameworks/next/utils.ts
+++ src/frameworks/next/utils.ts
@@ -25,28 +25,68 @@ export function cleanEscapedChars(path: string): string {
 }
 
 /**
- * Whether a Next.js rewrite is supported by Firebase.
+ * Whether a Next.js rewrite is supported by `firebase.json`.
+ *
+ * See: https://firebase.google.com/docs/hosting/full-config#rewrites
+ *
+ * Next.js unsupported rewrites includes:
+ * - Rewrites with the `has` property that is used by Next.js for Header,
+ *   Cookie, and Query Matching.
+ *     - https://nextjs.org/docs/api-reference/next.config.js/rewrites#header-cookie-and-query-matching
+ *
+ * - Rewrites using regex for path matching.
+ *     - https://nextjs.org/docs/api-reference/next.config.js/rewrites#regex-path-matching
+ *
+ * - Rewrites to external URLs
  */
 export function isRewriteSupportedByFirebase(rewrite: Rewrite): boolean {
   return !("has" in rewrite || pathHasRegex(rewrite.source) || isUrl(rewrite.destination));
 }
 
 /**
- * Whether a Next.js redirect is supported by Firebase.
+ * Whether a Next.js redirect is supported by `firebase.json`.
+ *
+ * See: https://firebase.google.com/docs/hosting/full-config#redirects
+ *
+ * Next.js unsupported redirects includes:
+ * - Redirects with the `has` property that is used by Next.js for Header,
+ *   Cookie, and Query Matching.
+ *     - https://nextjs.org/docs/api-reference/next.config.js/redirects#header-cookie-and-query-matching
+ *
+ * - Redirects using regex for path matching.
+ *     - https://nextjs.org/docs/api-reference/next.config.js/redirects#regex-path-matching
+ *
+ * - Next.js internal redirects
  */
 export function isRedirectSupportedByFirebase(redirect: Redirect): boolean {
   return !("has" in redirect || pathHasRegex(redirect.source) || "internal" in redirect);
 }
 
 /**
- * Whether a Next.js header is supported by Firebase.
+ * Whether a Next.js custom header is supported by `firebase.json`.
+ *
+ * See: https://firebase.google.com/docs/hosting/full-config#headers
+ *
+ * Next.js unsupported headers includes:
+ * - Custom header with the `has` property that is used by Next.js for Header,
+ *   Cookie, and Query Matching.
+ *     - https://nextjs.org/docs/api-reference/next.config.js/headers#header-cookie-and-query-matching
+ *
+ * - Custom header using regex for path matching.
+ *     - https://nextjs.org/docs/api-reference/next.config.js/headers#regex-path-matching
  */
 export function isHeaderSupportedByFirebase(header: Header): boolean {
   return !("has" in header || pathHasRegex(header.source));
 }
 
 /**
- * Firebase Rewrites that can be supported by firebase.json.
+ * Get which Next.js rewrites will be used before checking supported items individually.
+ *
+ * Next.js rewrites can be arrays or objects:
+ * - For arrays, all supported items can be used.
+ * - For objects only `beforeFiles` can be used.
+ *
+ * See: https://nextjs.org/docs/api-reference/next.config.js/rewrites
  */
 export function getNextjsRewritesToUse(
   nextJsRewrites: Manifest["rewrites"]
