a0f4c057ee8640d654d820c4dae22361f6f8c7fb
Leonardo Ortiz <leo@monogram.io>
2022-12-05 19:02:49 -0300

    readJSON util with type

--- src/frameworks/utils.ts
+++ src/frameworks/utils.ts
@@ -1,6 +1,19 @@
+import { readJSON as originalReadJSON } from "fs-extra";
+import type { ReadOptions } from "fs-extra";
+
 /**
  * Whether the given string starts with http:// or https://
  */
 export function isUrl(url: string): boolean {
   return /^https?:\/\//.test(url);
+}
+
+/**
+ * add type to readJSON
+ */
+export function readJSON<JsonType>(
+  file: string,
+  options?: ReadOptions | BufferEncoding | string
+): Promise<JsonType> {
+  return originalReadJSON(file, options) as Promise<JsonType>;
 }
