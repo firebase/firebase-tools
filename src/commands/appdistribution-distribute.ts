8359df94335456c2f8c7aa0a116f3fb6b7306a7f
Emmanuel Jimenez <32231345+mannyjimenez0810@users.noreply.github.com>
2023-01-08 14:48:17 -0500

    Update appdistribution-distribute.ts

--- src/commands/appdistribution-distribute.ts
+++ src/commands/appdistribution-distribute.ts
@@ -131,7 +131,9 @@ export const command = new Command("appdistribution:distribute <release-binary-f
       }
       utils.logSuccess(`View this release in the Firebase console: ${release.firebaseConsoleUri}`);
       utils.logSuccess(`Share this release with testers: ${release.testingUri}`);
-      utils.logSuccess(`Download the release binary (link expires in 1 hour): ${release.binaryDownloadUri}`);
+      utils.logSuccess(
+        `Download the release binary (link expires in 1 hour): ${release.binaryDownloadUri}`
+      );
       releaseName = uploadResponse.release.name;
     } catch (err: any) {
       if (err.status === 404) {
