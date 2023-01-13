92ff7c5bd3b11a97c8d0f158e2defafc2e866f1c
Daniel Young Lee <danielylee@google.com>
2023-01-13 11:50:56 -0800

    Wrap up python emulator support.

--- src/functions/python.ts
+++ src/functions/python.ts
@@ -20,11 +20,14 @@ export function runWithVirtualEnv(
   const command = process.platform === "win32" ? venvActivate : "source";
   const args = [process.platform === "win32" ? "" : venvActivate, "&&", ...commandAndArgs];
   logger.debug(`Running command with virtualenv: command=${command}, args=${JSON.stringify(args)}`);
+  logger.debug(JSON.stringify(envs));
 
   return spawn(command, args, {
     shell: true,
     cwd,
     stdio: [/* stdin= */ "ignore", /* stdout= */ "pipe", /* stderr= */ "inherit"],
-    ...envs,
+    // Linting disabled since internal types expect NODE_ENV which does not apply to Python runtimes.
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any
+    env: envs as any,
   });
 }
