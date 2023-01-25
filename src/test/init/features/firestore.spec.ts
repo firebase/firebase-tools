92f8291c9164b50e82eb3208f9cb347c853b0911
Joe Hanley <joehanley@google.com>
2023-01-25 10:18:13 -0800

    remove outdated test

--- src/test/init/features/firestore.spec.ts
+++ src/test/init/features/firestore.spec.ts
@@ -46,15 +46,6 @@ describe("firestore", () => {
       expect(_.get(setup, "config.firestore")).to.deep.equal({});
     });
 
-    it("should error when cloud resource location is not set", async () => {
-      const setup = { config: {}, projectId: "my-project-123" };
-
-      await expect(firestore.doSetup(setup, {}, {})).to.eventually.be.rejectedWith(
-        FirebaseError,
-        "Cloud resource location is not set"
-      );
-    });
-
     it("should error when the firestore API is not enabled", async () => {
       checkApiStub.returns(false);
 
