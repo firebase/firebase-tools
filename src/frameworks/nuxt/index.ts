29b6c8da514dcb5510ada22d7f19d15b1965a577
Leonardo Ortiz <leo@monogram.io>
2023-02-15 14:08:25 -0300

    remove nuxt bc it was breaking the build

--- src/frameworks/nuxt/index.ts
+++ src/frameworks/nuxt/index.ts
@@ -5,7 +5,6 @@ import { join } from "path";
 import { gte } from "semver";
 import { findDependency, FrameworkType, relativeRequire, SupportLevel } from "..";
 import { warnIfCustomBuildScript } from "../utils";
-import type * as NuxtKit from "@nuxt/kit";
 
 export const name = "Nuxt";
 export const support = SupportLevel.Experimental;
@@ -86,7 +85,7 @@ export async function ɵcodegenFunctionsDirectory(sourceDir: string, destDir: st
 export async function getDevModeHandle(dir: string, hostingEmulatorInfo?: EmulatorInfo) {
   console.log("In getDevModeHandle...");
 
-  const { loadNuxt }: typeof NuxtKit = await relativeRequire(dir, "@nuxt/kit");
+  const { loadNuxt } = await relativeRequire(dir, "@nuxt/kit");
   // const nuxtApp = await getNuxt3App(dir);
   // const { port } = hostingEmulatorInfo ?? { port: 3000 };
 
