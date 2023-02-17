a572dbc4f64ce04037e90687f00e2444e67d419b
Austin Crim <aust.crim@gmail.com>
2023-02-17 16:16:12 -0600

    reuse vite fast dev mode

--- src/frameworks/sveltekit/index.ts
+++ src/frameworks/sveltekit/index.ts
@@ -10,6 +10,7 @@ export const name = "SvelteKit";
 export const support = SupportLevel.Experimental;
 export const type = FrameworkType.MetaFramework;
 export const discover = viteDiscoverWithNpmDependency("@sveltejs/kit");
+export { getDevModeHandle } from "../vite";
 
 export async function build(root: string) {
   const { build } = relativeRequire(root, "vite");
