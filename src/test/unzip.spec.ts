91dd15ac7a5dff1116cc804c774e0bba2840e0bb
Juraj Carnogursky <durisvk2@gmail.com>
2023-04-20 17:07:02 +0200

    Replace dependency on broken node-unzipper with native zlib
    Fixes:
    https://github.com/firebase/firebase-tools/issues/5614
    https://github.com/firebase/firebase-tools/issues/5677
    https://github.com/firebase/firebase-tools-ui/issues/939
    https://github.com/firebase/firebase-tools-ui/issues/940

---
+++ src/test/unzip.spec.ts
@@ -0,0 +1,161 @@
+import { expect } from "chai";
+import * as fs from "fs";
+import * as path from "path";
+import { unzip } from "../unzip";
+import { DownloadDetails } from "../emulator/downloadableEmulators";
+import { Client } from "../apiv2";
+const fixturesDir = path.resolve(__dirname, "./fixtures");
+
+const ZIP_FIXTURES_PATH = path.join(fixturesDir, "zip-files");
+const ZIP_TEMPORARY_PATH = path.join(ZIP_FIXTURES_PATH, "temp");
+const ZIP_UNZIPPER_PATH = path.join(ZIP_FIXTURES_PATH, "node-unzipper-testData");
+
+describe("unzip", () => {
+  let uiZipPath: string;
+  let pubsubZipPath: string;
+  before(function (done) {
+    // eslint-disable-next-line @typescript-eslint/no-invalid-this
+    this.timeout(5000);
+    (async () => {
+      const [uiVersion, uiRemoteUrl] = [
+        DownloadDetails.ui.version,
+        DownloadDetails.ui.opts.remoteUrl,
+      ];
+      const [pubsubVersion, pubsubRemoteUrl] = [
+        DownloadDetails.pubsub.version,
+        DownloadDetails.pubsub.opts.remoteUrl,
+      ];
+
+      uiZipPath = path.join(ZIP_TEMPORARY_PATH, `ui-v${uiVersion}.zip`);
+      pubsubZipPath = path.join(ZIP_TEMPORARY_PATH, `pubsub-emulator-v${pubsubVersion}.zip`);
+
+      await fs.promises.mkdir(ZIP_TEMPORARY_PATH, { recursive: true });
+      if (!(await fs.promises.access(uiZipPath).catch(() => false))) {
+        await downloadFile(uiRemoteUrl, uiZipPath);
+      }
+
+      if (!(await fs.promises.access(pubsubZipPath).catch(() => false))) {
+        await downloadFile(pubsubRemoteUrl, pubsubZipPath);
+      }
+    })()
+      .then(done)
+      .catch(done);
+  });
+
+  after(async () => {
+    // await fs.promises.rmdir(ZIP_TEMPORARY_PATH, { recursive: true });
+  });
+
+  it("should unzip a ui emulator zip file", async () => {
+    await unzip(uiZipPath, path.join(ZIP_TEMPORARY_PATH, "ui"));
+
+    const files = await fs.promises.readdir(ZIP_TEMPORARY_PATH);
+    expect(files).to.include("ui");
+
+    const uiFiles = await fs.promises.readdir(path.join(ZIP_TEMPORARY_PATH, "ui"));
+    expect(uiFiles).to.include("client");
+    expect(uiFiles).to.include("server");
+
+    const serverFiles = await fs.promises.readdir(path.join(ZIP_TEMPORARY_PATH, "ui", "server"));
+    expect(serverFiles).to.include("server.js");
+  });
+
+  it("should unzip a pubsub emulator zip file", async () => {
+    await unzip(pubsubZipPath, path.join(ZIP_TEMPORARY_PATH, "pubsub"));
+
+    const files = await fs.promises.readdir(ZIP_TEMPORARY_PATH);
+    expect(files).to.include("pubsub");
+
+    const pubsubFiles = await fs.promises.readdir(path.join(ZIP_TEMPORARY_PATH, "pubsub"));
+    expect(pubsubFiles).to.include("pubsub-emulator");
+
+    const pubsubEmulatorFiles = await fs.promises.readdir(
+      path.join(ZIP_TEMPORARY_PATH, "pubsub", "pubsub-emulator")
+    );
+    expect(pubsubEmulatorFiles).to.include("bin");
+    expect(pubsubEmulatorFiles).to.include("lib");
+
+    const binFiles = await fs.promises.readdir(
+      path.join(ZIP_TEMPORARY_PATH, "pubsub", "pubsub-emulator", "bin")
+    );
+    expect(binFiles).to.include("cloud-pubsub-emulator");
+  });
+
+  const cases = [
+    { name: "compressed-cp866" },
+    { name: "compressed-directory-entry" },
+    { name: "compressed-flags-set" },
+    { name: "compressed-standard" },
+    { name: "uncompressed" },
+    { name: "zip-slip" },
+    { name: "zip64" },
+  ];
+
+  for (const { name } of cases) {
+    it(`should unzip a zip file with ${name} case`, async () => {
+      const zipPath = path.join(ZIP_UNZIPPER_PATH, name, "archive.zip");
+      const unzipPath = path.join(ZIP_TEMPORARY_PATH, name);
+      await unzip(zipPath, unzipPath);
+
+      // contains a folder "inflated"
+      const inflatedPath = path.join(ZIP_UNZIPPER_PATH, name, "inflated");
+      expect(await fs.promises.stat(inflatedPath)).to.be.ok;
+
+      // // inflated folder's size is "size"
+      expect(await calculateFolderSize(inflatedPath)).to.eql(await calculateFolderSize(unzipPath));
+    });
+  }
+});
+
+async function downloadFile(url: string, targetPath: string): Promise<string> {
+  const u = new URL(url);
+  const c = new Client({ urlPrefix: u.origin, auth: false });
+
+  const writeStream = fs.createWriteStream(targetPath);
+
+  const res = await c.request<void, NodeJS.ReadableStream>({
+    method: "GET",
+    path: u.pathname,
+    queryParams: u.searchParams,
+    responseType: "stream",
+    resolveOnHTTPError: true,
+  });
+
+  if (res.status !== 200) {
+    throw new Error(
+      `Download failed, file "${url}" does not exist. status ${
+        res.status
+      }: ${await res.response.text()}`,
+      {
+        cause: new Error(
+          `Object DownloadDetails from src/emulator/downloadableEmulators.ts contains invalid URL: ${url}`
+        ),
+      }
+    );
+  }
+
+  return new Promise<string>((resolve, reject) => {
+    writeStream.on("finish", () => {
+      resolve(targetPath);
+    });
+    writeStream.on("error", (err) => {
+      reject(err);
+    });
+    res.body.pipe(writeStream);
+  });
+}
+
+async function calculateFolderSize(folderPath: string): Promise<number> {
+  const files = await fs.promises.readdir(folderPath);
+  let size = 0;
+  for (const file of files) {
+    const filePath = path.join(folderPath, file);
+    const stat = await fs.promises.stat(filePath);
+    if (stat.isDirectory()) {
+      size += await calculateFolderSize(filePath);
+    } else {
+      size += stat.size;
+    }
+  }
+  return size;
+}
---
+++ src/unzip.ts
@@ -0,0 +1,121 @@
+import * as fs from "fs";
+import * as path from "path";
+import * as zlib from "zlib";
+import { Readable, Transform, TransformCallback } from "stream";
+import { promisify } from "util";
+import { FirebaseError } from "./error";
+import { pipeline } from "stream";
+
+const pipelineAsync = promisify(pipeline);
+
+interface ZipEntry {
+  compressedSize: number;
+  uncompressedSize: number;
+  fileNameLength: number;
+  extraLength: number;
+  fileName: string;
+  headerSize: number;
+  compressedData: Buffer;
+}
+
+const readUInt32LE = (buf: Buffer, offset: number): number => {
+  return (
+    (buf[offset] | (buf[offset + 1] << 8) | (buf[offset + 2] << 16) | (buf[offset + 3] << 24)) >>> 0
+  );
+};
+
+const extractEntriesFromBuffer = async (data: Buffer, outputDir: string): Promise<void> => {
+  let position = 0;
+
+  while (position < data.length) {
+    const entryHeader = data.slice(position, position + 30);
+    const entry: ZipEntry = {} as ZipEntry;
+
+    if (readUInt32LE(entryHeader, 0) !== 0x04034b50) {
+      break;
+    }
+
+    entry.compressedSize = readUInt32LE(entryHeader, 18);
+    entry.uncompressedSize = readUInt32LE(entryHeader, 22);
+    entry.fileNameLength = entryHeader.readUInt16LE(26);
+    entry.extraLength = entryHeader.readUInt16LE(28);
+    entry.fileName = data.toString("utf-8", position + 30, position + 30 + entry.fileNameLength);
+    entry.headerSize = 30 + entry.fileNameLength + entry.extraLength;
+    entry.compressedData = data.slice(
+      position + entry.headerSize,
+      position + entry.headerSize + entry.compressedSize
+    );
+
+    const outputFilePath = path.join(outputDir, entry.fileName);
+
+    if (entry.fileName.endsWith("/")) {
+      await fs.promises.mkdir(outputFilePath, { recursive: true });
+    } else {
+      const parentDir = outputFilePath.substring(0, outputFilePath.lastIndexOf("/"));
+      await fs.promises.mkdir(parentDir, { recursive: true });
+
+      const compressionMethod = entryHeader.readUInt16LE(8);
+      if (compressionMethod === 0) {
+        // Store (no compression)
+        await fs.promises.writeFile(outputFilePath, entry.compressedData);
+      } else if (compressionMethod === 8) {
+        // Deflate
+        await pipelineAsync(
+          Readable.from(entry.compressedData),
+          zlib.createInflateRaw(),
+          fs.createWriteStream(outputFilePath)
+        );
+      } else {
+        throw new FirebaseError(`Unsupported compression method: ${compressionMethod}`);
+      }
+    }
+
+    position += entry.headerSize + entry.compressedSize;
+  }
+};
+
+export const unzip = async (inputPath: string, outputDir: string): Promise<void> => {
+  const data = await fs.promises.readFile(inputPath);
+  await extractEntriesFromBuffer(data, outputDir);
+};
+
+class UnzipTransform extends Transform {
+  private chunks: Buffer[] = [];
+  private _resolve?: () => unknown;
+  private _reject?: (e: Error) => unknown;
+
+  constructor(private outputDir: string) {
+    super();
+  }
+
+  _transform(chunk: Buffer, _: unknown, callback: TransformCallback): void {
+    this.chunks.push(chunk);
+    callback();
+  }
+
+  async _flush(callback: TransformCallback): Promise<void> {
+    try {
+      await extractEntriesFromBuffer(Buffer.concat(this.chunks), this.outputDir);
+      callback();
+      this._resolve?.();
+    } catch (error) {
+      const firebaseError = new FirebaseError("Unable to unzip the target", {
+        children: [error],
+        original: error instanceof Error ? error : undefined,
+      });
+      callback(firebaseError);
+      this._reject?.(firebaseError);
+    }
+  }
+
+  async promise(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this._resolve = resolve;
+      this._reject = reject;
+    });
+  }
+}
+
+export const createUnzipTransform = (outputDir: string): UnzipTransform => {
+  return new UnzipTransform(outputDir);
+};
