fdf3664d1200ad7f5ea9b669c6b0cc91a69b11a8
Daniel Young Lee <danielylee@google.com>
2023-04-28 16:50:24 -0700

    Remove python experiments.

--- src/init/features/functions/index.ts
+++ src/init/features/functions/index.ts
@@ -168,12 +168,10 @@ async function languageSetup(setup: any, config: Config): Promise<any> {
       value: "typescript",
     },
   ];
-  if (isEnabled("pythonfunctions")) {
-    choices.push({
-      name: "Python",
-      value: "python",
-    });
-  }
+  choices.push({
+    name: "Python",
+    value: "python",
+  });
   const language = await promptOnce({
     type: "list",
     message: "What language would you like to use to write Cloud Functions?",
