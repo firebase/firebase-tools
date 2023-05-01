7fd7d31d0e5a4eb37ef7692693d1bd4f037c34f8
Leonardo Ortiz <leo@monogram.io>
2023-05-01 16:04:46 -0300

    `..env.FBTOOLS_TARGET_PROJECT` for FIREBASE_PROJECT

--- scripts/webframeworks-deploy-tests/tests.ts
+++ scripts/webframeworks-deploy-tests/tests.ts
@@ -3,7 +3,7 @@ import { expect } from "chai";
 import * as cli from "./cli";
 import { requireAuth } from "../../src/requireAuth";
 
-const FIREBASE_PROJECT = process.env.GCLOUD_PROJECT || "";
+const FIREBASE_PROJECT = process.env.FBTOOLS_TARGET_PROJECT || "";
 const FIREBASE_DEBUG = process.env.FIREBASE_DEBUG || "";
 
 function genRandomId(n = 10): string {
