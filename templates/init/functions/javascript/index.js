7fa57f3c40dca646dee36bdc5d3472233f699f73
Cynthia Wang <hyacccint@gmail.com>
2023-05-02 17:20:11 -0400

    fix: lint, typescript imports

--- templates/init/functions/javascript/index.js
+++ templates/init/functions/javascript/index.js
@@ -1,13 +1,13 @@
 /**
  * Import function triggers from their respective submodules:
  *
- * const { onCall } = require("firebase-functions/v2/https");
- * const { onDocumentWritten } = require("firebase-functions/v2/firestore");
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { onRequest } = require("firebase-functions/v2/https");
+const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
 // // Create and deploy your first functions
--- templates/init/functions/typescript/index.ts
+++ templates/init/functions/typescript/index.ts
@@ -1,5 +1,14 @@
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const { onCall } = require("firebase-functions/v2/https");
+ * const { onDocumentWritten } = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+ import { onRequest } from "firebase-functions/v2/https";
+ import * as logger from "firebase-functions/logger";
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
