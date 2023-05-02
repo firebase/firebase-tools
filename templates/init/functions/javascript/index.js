b3e3c72033ddd9d4b6cd7a9b861a59bcb5e60fc4
Cynthia Wang <44329917+HYACCCINT@users.noreply.github.com>
2023-05-02 17:08:14 -0400

    Update templates/init/functions/javascript/index.js
    Co-authored-by: Jeff <3759507+jhuleatt@users.noreply.github.com>

--- templates/init/functions/javascript/index.js
+++ templates/init/functions/javascript/index.js
@@ -1,3 +1,12 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const { onCall } = require("firebase-functions/v2/https");
+ * const { onDocumentWritten } = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
