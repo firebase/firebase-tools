d1819055ae99ce818b6608caf7610cd057ba7e72
James Daniels <jamesdaniels@google.com>
2023-05-03 01:19:00 -0400

    Enable pintags

--- scripts/webframeworks-deploy-tests/run.sh
+++ scripts/webframeworks-deploy-tests/run.sh
@@ -9,6 +9,6 @@ source scripts/set-default-credentials.sh
 (cd scripts/webframeworks-deploy-tests/hosting;
 npm i;
 cd ..;
-FIREBASE_CLI_EXPERIMENTS=webframeworks firebase emulators:exec "cd ../..; mocha scripts/webframeworks-deploy-tests/tests.ts" --project $FBTOOLS_TARGET_PROJECT --debug > firebase-emulators.log || \
+FIREBASE_CLI_EXPERIMENTS=webframeworks,pintags firebase emulators:exec "cd ../..; mocha scripts/webframeworks-deploy-tests/tests.ts" --project $FBTOOLS_TARGET_PROJECT --debug > firebase-emulators.log || \
 (cat firebase-emulators.log && exit 1);
 cat firebase-emulators.log)
