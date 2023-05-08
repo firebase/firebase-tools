521699faba2022d9f615fd8396a398077a3d5f2d
Cynthia Wang <44329917+HYACCCINT@users.noreply.github.com>
2023-05-08 14:26:51 -0400

    Update templates/init/functions/typescript/index.ts
    Co-authored-by: Jeff <3759507+jhuleatt@users.noreply.github.com>

--- templates/init/functions/typescript/index.ts
+++ templates/init/functions/typescript/index.ts
@@ -1,8 +1,8 @@
 /**
  * Import function triggers from their respective submodules:
  *
- * import { onCall } = from "firebase-functions/v2/https";
- * import { onDocumentWritten } = from "firebase-functions/v2/firestore";
+ * import { onCall } from "firebase-functions/v2/https";
+ * import { onDocumentWritten } from "firebase-functions/v2/firestore";
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
