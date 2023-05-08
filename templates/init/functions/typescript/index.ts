699ca68ad77bda62f2fde546d1b14031eafccd3b
Cynthia Wang <44329917+HYACCCINT@users.noreply.github.com>
2023-05-08 14:27:00 -0400

    Update templates/init/functions/typescript/index.ts
    Co-authored-by: Jeff <3759507+jhuleatt@users.noreply.github.com>

--- templates/init/functions/typescript/index.ts
+++ templates/init/functions/typescript/index.ts
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
- import { onRequest } from "firebase-functions/v2/https";
- import * as logger from "firebase-functions/logger";
+import { onRequest } from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
