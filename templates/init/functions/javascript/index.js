d8362fc6304388fa48fa0c95d8bd2c55e8109493
Cynthia Wang <hyacccint@gmail.com>
2023-05-08 14:29:06 -0400

    fix: lint and imports

--- templates/init/functions/javascript/index.js
+++ templates/init/functions/javascript/index.js
@@ -10,9 +10,9 @@
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
 // exports.helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
--- templates/init/functions/typescript/index.ts
+++ templates/init/functions/typescript/index.ts
@@ -1,18 +1,18 @@
 /**
  * Import function triggers from their respective submodules:
  *
- * import { onCall } = from "firebase-functions/v2/https";
- * import { onDocumentWritten } = from "firebase-functions/v2/firestore";
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
- import { onRequest } from "firebase-functions/v2/https";
+ import {onRequest} from "firebase-functions/v2/https";
  import * as logger from "firebase-functions/logger";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
 // export const helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
