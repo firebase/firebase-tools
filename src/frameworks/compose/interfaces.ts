8a8c5914e5722dd0e6ebb1e7f66d44e11f4f0add
Daniel Young Lee <danielylee@google.com>
2023-05-13 08:42:36 -0700

    Init composer.

---
+++ src/frameworks/compose/interfaces.ts
@@ -0,0 +1,44 @@
+export interface Bundle {
+  version: "v1alpha";
+  server?: ServerConfig;
+}
+
+interface ServerConfig {
+  start: StartConfig;
+  concurrency?: number;
+  cpu?: number;
+  memory?: "256MiB" | "512MiB" | "1GiB" | "2GiB" | "4GiB" | "8GiB" | "16GiB" | string;
+  timeoutSeconds?: number;
+  minInstances?: number;
+  maxInstances?: number;
+}
+
+interface StartConfig {
+  // Path to local source directory. Defaults to .bundle/server.
+  dir?: string;
+  // Command to start the server (e.g. ["npm", "run", "start"]).
+  cmd: string[];
+  // Runtime required to command execution.
+  runtime: "nodejs18" | string;
+}
+
+export interface AppSpec {
+  baseImage: string;
+  packageManagerInstallCommand?: string;
+  environmentVariables?: Record<string, string>;
+  installCommand: string;
+  buildCommand: string;
+  startCommand: string;
+}
+
+export class Engine {
+  constructor(readonly spec: AppSpec) {}
+
+  install(): void {
+    throw new Error("install() not implemented");
+  }
+
+  build(): void {
+    throw new Error("build() not implemented");
+  }
+}
