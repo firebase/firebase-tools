de698ebdfb996ce024a16999ffbe6f9305c398fe
Sairam Sakhamuri <svnsairam@google.com>
2023-06-01 11:57:24 -0700

    Removed the unused code

--- src/finder/find.ts
+++ src/finder/find.ts
@@ -1,3 +1,34 @@
-// Read the project folder to detect the Runtime environment.
+import { Runtime, FileSystem, RuntimeMatch } from "./types";
+import { NodejsRuntime } from "./runtimes/NodejsRuntime";
+
+/**
+ *
+ */
+export function interpolate(template: string | null, vars?: Record<string, string>): string | null {
+  if (!template) {
+    return template;
+  }
+  return template.replaceAll(/\${(.*)}/g, (_, varName: string) => vars?.[varName] || "");
+}
+
+const allRuntimes: Runtime[] = [new NodejsRuntime()];
+
+/**
+ * Read the project folder to detect the Runtime environment.
+ */
+export async function findRuntimeCommand(fs: FileSystem): Promise<RuntimeMatch | null> {
+  const matches = await Promise.all(allRuntimes.map((runtime) => runtime.match(fs)));
+  let match: RuntimeMatch | null = null;
+  for (const res of matches) {
+    if (!res) {
+      continue;
+    }
+    if (match) {
+      throw new Error("More than one runtime matched codebase");
+    }
+    match = res;
+  }
+  return match;
+}
 
 // Detect the FrameworkSpec required.
