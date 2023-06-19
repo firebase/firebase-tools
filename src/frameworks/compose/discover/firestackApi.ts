7eac41d33602f575ea53c9288d14d672bcffacb6
Sairam Sakhamuri <svnsairam@google.com>
2023-06-19 16:50:04 -0700

    Added more properties to the Stack and Build object

--- src/frameworks/compose/discover/firestackApi.ts
+++ src/frameworks/compose/discover/firestackApi.ts
@@ -11,12 +11,31 @@ const client = new Client({
 
 export type State = "BUILDING" | "BUILD" | "DEPLOYING" | "READY" | "FAILED";
 
-/** A Stack, the primary resource of Frameworks. */
-interface StackBase {
+type RolloutPolicy = {};
+type Spec = {};
+
+interface TrafficTarget {
+  build: string;
+  percent: number;
+}
+
+interface TrafficTargetList {
+  values: TrafficTarget[];
+}
+
+interface RunService {
   name: string;
-  mode?: string;
-  labels: Record<string, string>;
-  codebase: Codebase;
+}
+
+interface ManagedResource {
+  // oneof managed_resource {
+  runService: RunService;
+  // end oneof managed_resource
+}
+
+interface TrafficTargetStatus {
+  build: string;
+  percent: number;
 }
 
 interface Codebase {
@@ -24,16 +43,33 @@ interface Codebase {
   rootDirectory: string;
 }
 
+/** A Stack, the primary resource of Frameworks. */
+interface StackBase {
+  name: string;
+  mode?: string;
+  codebase: Codebase;
+  labels: Record<string, string>;
+  // oneof traffic_management
+  traffic: TrafficTargetList;
+  rolloutPolicy: RolloutPolicy;
+  // end oneof traffic_management
+}
+
 export type OutputStack = StackBase & {
   createTime: string;
   updateTime: string;
   uri: string;
+  trafficStatuses: TrafficTargetStatus[];
+  managedResources: ManagedResource[];
 };
 
 interface Build {
   name: string;
   state: State;
+  error: Status;
   image: string;
+  spec: Spec;
+  source: BuildSource;
   buildLogsUri: string;
 }
 
@@ -43,28 +79,56 @@ export type OutputBuild = Build & {
   sourceRef: string;
 };
 
+interface BuildSource {
+  codeBaseSource?: CodebaseSource;
+}
+
+interface Status {
+  code: number;
+  message: string;
+  details: any[];
+}
+
+interface CodebaseSource {
+  // oneof reference
+  branch: string;
+  commit: string;
+  tag: string;
+  // end oneof reference
+}
+
+export interface OperationMetadata {
+  createTime: string;
+  endTime: string;
+  target: string;
+  verb: string;
+  statusDetail: string;
+  cancelRequested: boolean;
+  apiVersion: string;
+}
+
+export interface Operation {
+  name: string;
+  metadata?: OperationMetadata;
+  done: boolean;
+  // oneof result
+  error?: Status;
+  response?: any;
+  // end oneof result
+}
+
 /**
  * Creates a new Stack in a given project and location.
  */
-export async function createStack(
-  projectId: string,
-  location: string,
-  functionId: string
-): Promise<OutputStack> {
-  const name = `projects/${projectId}/locations/${location}/functions/${functionId}`;
-  const res = await client.get<OutputStack>(name);
+export async function createStack(stack: StackBase): Promise<Operation> {
+  const res = await client.post<StackBase, Operation>(stack.name, stack, {});
   return res.body;
 }
 
 /**
  * Creates a new Build in a given project and location.
  */
-export async function createBuild(
-  projectId: string,
-  location: string,
-  functionId: string
-): Promise<OutputStack> {
-  const name = `projects/${projectId}/locations/${location}/functions/${functionId}`;
-  const res = await client.get<OutputStack>(name);
+export async function createBuild(build: Build): Promise<Operation> {
+  const res = await client.post<Build, Operation>(build.name, build, {});
   return res.body;
 }
