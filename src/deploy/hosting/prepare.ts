c05d77f496aadee7d3966eff9098f6b3ec0c750a
Christina Holland <chholland@google.com>
2023-06-22 17:09:45 -0700

    Detect framework

--- src/deploy/hosting/prepare.ts
+++ src/deploy/hosting/prepare.ts
@@ -12,7 +12,7 @@ import * as utils from "../../utils";
 import { HostingSource, RunRewrite } from "../../firebaseConfig";
 import * as backend from "../functions/backend";
 import { ensureTargeted } from "../../functions/ensureTargeted";
-import { normalizeAndValidate } from "../../functions/projectConfig";
+import { generateSSRCodebaseId } from "../../frameworks";
 
 function handlePublicDirectoryFlag(options: HostingOptions & Options): void {
   // Allow the public directory to be overridden by the --public flag
@@ -75,21 +75,15 @@ export async function addPinnedFunctionsToOnlyString(
       ]?.[r.function.functionId];
       if (endpoint) {
         options.only = ensureTargeted(options.only, endpoint.codebase || "default", endpoint.id);
+      } else if (c.webFramework) {
+        options.only = ensureTargeted(
+          options.only,
+          generateSSRCodebaseId(c.site),
+          r.function.functionId
+        );
       } else {
-        const functionsConfig = normalizeAndValidate(options.config.src.functions);
-        const codebasesFromConfig = [
-          ...new Set(Object.values(functionsConfig).map((c) => c.codebase)),
-        ];
-        if (codebasesFromConfig.length > 0) {
-          options.only = ensureTargeted(
-            options.only,
-            codebasesFromConfig[0],
-            r.function.functionId
-          );
-        } else {
-          // This endpoint is just being added in this push. We don't know what codebase it is.
-          options.only = ensureTargeted(options.only, r.function.functionId);
-        }
+        // This endpoint is just being added in this push. We don't know what codebase it is.
+        options.only = ensureTargeted(options.only, r.function.functionId);
       }
       addedFunctionsPerSite.push(r.function.functionId);
     }
