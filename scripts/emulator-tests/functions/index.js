1080a5f69a3a8eaaa5d91ad13e9ca7ff3c812caa
abegizaw3 <135074156+abegizaw3@users.noreply.github.com>
2023-07-21 11:25:54 -0700

    added scripts

---
+++ scripts/emulator-tests/functions/index.js
@@ -0,0 +1,13 @@
+module.exports = (() => {
+	return {
+	  functionId: require("firebase-functions").https.onRequest((req, resp) => {
+		return new Promise((resolve) => {
+		  setTimeout(() => {
+			resp.sendStatus(200);
+			resolve();
+		  }, 3000);
+		});
+	  }),
+	};
+  })();
+  
