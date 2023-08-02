e9a35cc1a85677823a1dd4a4f00bd963eeded25d
abegizaw3 <135074156+abegizaw3@users.noreply.github.com>
2023-08-02 11:20:42 -0700

    Update constants.ts by adding unexpected error const

--- src/emulator/storage/constants.ts
+++ src/emulator/storage/constants.ts
@@ -6,3 +6,4 @@ export const SIGNED_URL_PRIVATE_KEY =
   "5c39d11c20b96241c42e07218724d6726ee27c3db33eb43256c21f92bb1f52dc";
 export const INVALID_TTL_PARAM = "X-Firebase-Expires: TTL";
 export const INVALID_DATE_PARAM = "X-Firebase-Date: YYYYMMDD'T'HHMMSS'Z'";
+export const UNEXPECTED_ERROR = -1;
