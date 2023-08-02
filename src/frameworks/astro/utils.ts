ec60917a55349fcf40508407209c8c35ef2dd035
Austin Crim <austin@monogram.io>
2023-08-02 14:02:34 -0500

    change config loading depending on version

--- src/frameworks/astro/utils.ts
+++ src/frameworks/astro/utils.ts
@@ -1,4 +1,7 @@
 import { dirname, join, relative } from "path";
+import fs from "fs/promises";
+import { findDependency } from "../utils";
+import { gte } from "semver";
 
 const { dynamicImport } = require(true && "../../dynamicImport");
 
@@ -10,15 +13,31 @@ export function getBootstrapScript() {
 
 export async function getConfig(cwd: string) {
   const astroDirectory = dirname(require.resolve("astro/package.json", { paths: [cwd] }));
-  const { openConfig }: typeof import("astro/dist/core/config/config") = await dynamicImport(
-    join(astroDirectory, "dist", "core", "config", "config.js")
-  );
-  const logging: any = undefined; // TODO figure out the types here
-  const { astroConfig: config } = await openConfig({ cmd: "build", cwd, logging });
+  const version = getAstroVersion(cwd);
+
+  let config;
+  if (gte(version!, "2.9.6")) {
+    const { resolveConfig } = await dynamicImport(
+      join(astroDirectory, "dist", "core", "config", "config.js")
+    );
+    const { astroConfig } = await resolveConfig({ root: cwd }, "build");
+    config = astroConfig;
+  } else {
+    const { openConfig }: typeof import("astro/dist/core/config/config") = await dynamicImport(
+      join(astroDirectory, "dist", "core", "config", "config.js")
+    );
+    const logging: any = undefined; // TODO figure out the types here
+    const { astroConfig } = await openConfig({ cmd: "build", cwd, logging });
+    config = astroConfig;
+  }
   return {
     outDir: relative(cwd, config.outDir.pathname),
     publicDir: relative(cwd, config.publicDir.pathname),
     output: config.output,
     adapter: config.adapter,
   };
+}
+
+export function getAstroVersion(cwd: string): string | undefined {
+  return findDependency("astro", { cwd, depth: 0, omitDev: false })?.version;
 }
