9a467ee39191e3d6eb9599b2e3b62667c7545279
TJ Lavelle <tj@lavelle.io>
2023-08-10 10:23:59 -0400

    Tickle

--- firebase-vscode/src/extension.ts
+++ firebase-vscode/src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
 import { ExtensionBroker } from "./extension-broker";
