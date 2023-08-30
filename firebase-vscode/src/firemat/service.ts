73aa7e5a92dc29a01dc2c24aff9633cd749f8cb6
Harold Shen <hlshen@google.com>
2023-08-30 17:59:50 -0400

    Integrate ExecuteGraphqlRead; Update notifications on emulatorStart

--- firebase-vscode/src/firemat/service.ts
+++ firebase-vscode/src/firemat/service.ts
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-import { buildSchema, introspectionFromSchema } from "graphql";
+import { buildSchema, introspectionFromSchema, getIntrospectionQuery } from "graphql";
 import { Signal } from "@preact/signals-core";
 /**
  * Firemat Emulator service
@@ -48,17 +48,42 @@ export class FirematService {
     return result;
   }
 
-  // TODO: use emulator endpoints
+  // TODO: use firemat specific introspectionQuery
   async introspect() {
     try {
-      const resp = await fetch("http://127.0.0.1:8989/__/schema.gql");
-      const text = await resp.text();
-      const introspection = introspectionFromSchema(buildSchema(text));
+      const resp = await this.executeGraphQLRead({ query: getIntrospectionQuery(), variables: {} });
+      const introspection = introspectionFromSchema(buildSchema(resp));
       return { data: introspection };
     } catch (e) {
       // TODO: surface error that emulator is not connected
       console.error("error: ", e);
       return { data: null };
+    }
+  }
+
+  async executeGraphQLRead(params: {
+    query: String;
+    variables: {};
+  }) {
+    try {
+      // TODO: get name programatically
+      const body = { ...params, name: "projects/p/locations/l/services/s" };
+      const resp = await fetch(this.firematEndpoint.value + "/v0/projects/p/locations/l/services/s:executeGraphqlRead", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "x-mantle-admin": "all",
+        },
+        body: JSON.stringify(body),
+      });
+      const result = await resp.json().catch(() => resp.text());
+      return result;
+    }
+    // TODO: actual error handling
+    catch (e) {
+      console.log(e);
+      return null;
     }
   }
 }
