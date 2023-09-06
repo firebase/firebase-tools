7ab07f3539af500e577f641f248576a63a4bdb6b
TJ Lavelle <tj@lavelle.io>
2023-09-06 17:11:33 -0400

    Move emulator lifecycle to disposable wrapper

--- firebase-vscode/src/extension.ts
+++ firebase-vscode/src/extension.ts
@@ -10,8 +10,8 @@ import {
 } from "../common/messaging/protocol";
 import { setupWorkflow } from "./workflow";
 import { pluginLogger } from "./logger-wrapper";
-import { onShutdown } from "./workflow";
 import { registerWebview } from "./webview";
+import { registerEmulators } from "./core/emulators";
 
 const broker = createBroker<
   ExtensionToWebviewParamsMap,
@@ -26,16 +26,11 @@ export function activate(context: vscode.ExtensionContext) {
   setupWorkflow(context, broker);
 
   context.subscriptions.push(
+    registerEmulators(broker),
     registerWebview({
       name: "sidebar",
       broker,
       context,
     })
   );
-}
-
-// This method is called when the extension is deactivated
-export async function deactivate() {
-  // This await is optimistic but it might wait for a moment longer while we run cleanup activities
-  await onShutdown();
 }
