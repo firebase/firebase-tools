5a1f286545f3748378ac7a4cda3a9485ec8f25a2
Brian Li <blidd@google.com>
2023-09-21 15:56:18 -0400

    add missing label key to event for beforeSendEmail

--- src/functions/constants.ts
+++ src/functions/constants.ts
@@ -6,7 +6,9 @@ export const BLOCKING_LABEL = "deployment-blocking";
 export const BLOCKING_LABEL_KEY_TO_EVENT: Record<string, (typeof AUTH_BLOCKING_EVENTS)[number]> = {
   "before-create": "providers/cloud.auth/eventTypes/user.beforeCreate",
   "before-sign-in": "providers/cloud.auth/eventTypes/user.beforeSignIn",
+  "before-send-email": "providers/cloud.auth/eventTypes/user.beforeSendEmail",
 };
+
 export const BLOCKING_EVENT_TO_LABEL_KEY: Record<(typeof AUTH_BLOCKING_EVENTS)[number], string> = {
   "providers/cloud.auth/eventTypes/user.beforeCreate": "before-create",
   "providers/cloud.auth/eventTypes/user.beforeSignIn": "before-sign-in",
