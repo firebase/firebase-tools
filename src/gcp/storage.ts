b9079794918b1180a3d01e9d0cb5ae4970e77c0c
Abhinav Sundaresan <abhisun@google.com>
2023-09-27 01:04:54 -0400

    lint 2

--- src/gcp/storage.ts
+++ src/gcp/storage.ts
@@ -6,7 +6,7 @@ import { Client } from "../apiv2";
 import { FirebaseError } from "../error";
 import { logger } from "../logger";
 import { ensure } from "../ensureApiEnabled";
-//import { getFirebaseProject } from "../management/projects";
+// import { getFirebaseProject } from "../management/projects";
 
 /** Bucket Interface */
 interface BucketResponse {
@@ -144,7 +144,7 @@ export async function getDefaultBucket(projectId: string): Promise<string> {
   await ensure(projectId, "firebasestorage.googleapis.com", "storage", false);
   try {
     const localAPIClient = new Client({ urlPrefix: firebaseStorageOrigin, apiVersion: "v1alpha" });
-    const response = await localAPIClient.get<{name: string}>(
+    const response = await localAPIClient.get<{ name: string }>(
       `/projects/${projectId}/defaultBucket`
     );
     if (!response.body?.name) {
