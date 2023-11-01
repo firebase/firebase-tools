270a0a827c250c7487d9556e9f1bb50e50d2838c
Sairam Sakhamuri <svnsairam@google.com>
2023-11-01 16:13:17 +0000

    List stacks in table

--- src/commands/frameworks-stacks-list.ts
+++ src/commands/frameworks-stacks-list.ts
@@ -4,6 +4,7 @@ import { needProjectId } from "../projectUtils";
 import * as gcp from "../gcp/frameworks";
 import { FirebaseError } from "../error";
 import { logger } from "../logger";
+const Table = require("cli-table");
 
 export const command = new Command("backends:list")
   .description("List backends of a Firebase project.")
@@ -11,14 +12,25 @@ export const command = new Command("backends:list")
   .action(async (options: Options) => {
     const projectId = needProjectId(options);
     const location = options.location as string;
+    const table = new Table({
+      head: ["Backend Id", "Repository Name", "URL", "Location", "Created Date", "Updated Date"],
+      style: { head: ["yellow"] },
+    });
 
-    let backends;
+    let backendsList;
     try {
-      backends = await gcp.listBackend(projectId, location);
-      /**
-       * TODO print this in a prettier way.
-       */
-      logger.info(backends);
+      backendsList = await gcp.listBackend(projectId, location);
+      for (const backend of backendsList.backends) {
+        const entry = [
+          backend.name,
+          backend.codebase.repository,
+          backend.uri,
+          backend.createTime,
+          backend.updateTime,
+        ];
+        table.push(entry);
+      }
+      logger.info(table.toString());
     } catch (err: any) {
       throw new FirebaseError(
         `Unable to list backends present in project: ${projectId}. Please check the parameters you have provided.`,
@@ -26,5 +38,5 @@ export const command = new Command("backends:list")
       );
     }
 
-    return backends;
+    return backendsList;
   });
