7a15c5f01efe2833a965180e65fb9945da1ed0b6
Sairam Sakhamuri <svnsairam@google.com>
2023-11-14 23:59:54 +0000

    Modified backends:get command

--- src/commands/frameworks-backends-get.ts
+++ src/commands/frameworks-backends-get.ts
@@ -17,7 +17,7 @@ const TABLE_HEAD = [
 ];
 export const command = new Command("backends:get")
   .description("Get backend details of a Firebase project")
-  .option("-l, --location <location>", "App Backend location", "")
+  .option("-l, --location <location>", "App Backend location", "-")
   .option("-b, --backend <backend>", "Backend Id", "")
   .action(async (options: Options) => {
     const projectId = needProjectId(options);
@@ -34,28 +34,24 @@ export const command = new Command("backends:get")
     });
     table.colWidths = COLUMN_LENGTH;
     try {
-      if (!location) {
-        for (const region of ALLOWED_REGIONS) {
-          const backendInRegion = await getBackend(projectId, region.value, backendId);
-          if (backendInRegion) {
-            backendsList.push(backendInRegion);
-            populateTable(backendInRegion, region.name, table);
-          }
-        }
+      if (location != "-") {
+        const backendInRegion = await gcp.getBackend(projectId, location, backendId);
+        backendsList.push(backendInRegion);
+        populateTable(backendInRegion, location, table);
       } else {
-        const backendInRegion = await getBackend(projectId, location, backendId);
-        if (backendInRegion) {
-          backendsList.push(backendInRegion);
-          populateTable(backendInRegion, location, table);
-        }
+        const allBackend = await gcp.listBackends(projectId, location);
+        backendsList = allBackend.backends.filter((bkd) => bkd.name.split("/").pop() === backendId);
+        backendsList.forEach((bkd) => populateTable(bkd, location, table));
       }
 
       if (backendsList.length != 0) {
         logger.info(table.toString());
       } else {
-        throw new FirebaseError(
+        logger.info();
+        logger.info(
           `No backends found with the given parameters. Please check the parameters you have provided.`
         );
+        logger.info();
       }
     } catch (err: any) {
       throw new FirebaseError(
@@ -66,20 +62,6 @@ export const command = new Command("backends:get")
 
     return backendsList;
   });
-
-async function getBackend(projectId: string, location: string, backendId: string) {
-  try {
-    const backendInRegion = await gcp.getBackend(projectId, location, backendId);
-    return backendInRegion;
-  } catch (err: any) {
-    if ((err as FirebaseError).status != 404) {
-      throw new FirebaseError(`Error occured while trying to get backend: ${backendId}.`, {
-        original: err,
-      });
-    }
-  }
-  return undefined;
-}
 
 function populateTable(backend: gcp.Backend, location: string, table: any) {
   const entry = [
