0f6850b21c533db7c05797b732068d5f858e5d42
Sairam Sakhamuri <svnsairam@google.com>
2023-11-14 22:23:50 +0000

    Added changes to backends:get operation for M2

--- src/commands/frameworks-backends-get.ts
+++ src/commands/frameworks-backends-get.ts
@@ -4,35 +4,59 @@ import { needProjectId } from "../projectUtils";
 import * as gcp from "../gcp/frameworks";
 import { FirebaseError } from "../error";
 import { logger } from "../logger";
+import { ALLOWED_REGIONS } from "../init/features/frameworks/constants";
 const Table = require("cli-table");
-
+const COLUMN_LENGTH = 20;
+const TABLE_HEAD = [
+  "Backend Id",
+  "Repository Name",
+  "URL",
+  "Location",
+  "Created Date",
+  "Updated Date",
+];
 export const command = new Command("backends:get")
   .description("Get backend details of a Firebase project")
-  .option("-l, --location <location>", "App Backend location", "us-central1")
-  .option("--s, --backendId <backendId>", "Backend Id", "")
+  .option("-l, --location <location>", "App Backend location", "")
+  .option("-b, --backend <backend>", "Backend Id", "")
   .action(async (options: Options) => {
     const projectId = needProjectId(options);
     const location = options.location as string;
-    const backendId = options.backendId as string;
+    const backendId = options.backend as string;
     if (!backendId) {
       throw new FirebaseError("Backend id can't be empty.");
     }
 
-    let backend;
+    let backendsList: gcp.Backend[] = [];
+    const table = new Table({
+      head: TABLE_HEAD,
+      style: { head: ["green"] },
+    });
+    table.colWidths = COLUMN_LENGTH;
     try {
-      backend = await gcp.getBackend(projectId, location, backendId);
-      const table = new Table({
-        head: ["Backend Id", "Repository Name", "URL", "Location", "Created Date", "Updated Date"],
-        style: { head: ["green"] },
-      });
-      table.push([
-        backend.name,
-        backend.codebase.repository,
-        backend.uri,
-        backend.createTime,
-        backend.updateTime,
-      ]);
-      logger.info(table.toString());
+      if (!location) {
+        for (const region of ALLOWED_REGIONS) {
+          const backendInRegion = await getBackend(projectId, region.value, backendId);
+          if (backendInRegion) {
+            backendsList.push(backendInRegion);
+            populateTable(backendInRegion, region.name, table);
+          }
+        }
+      } else {
+        const backendInRegion = await getBackend(projectId, location, backendId);
+        if (backendInRegion) {
+          backendsList.push(backendInRegion);
+          populateTable(backendInRegion, location, table);
+        }
+      }
+
+      if (backendsList.length != 0) {
+        logger.info(table.toString());
+      } else {
+        throw new FirebaseError(
+          `No backends found with the given parameters. Please check the parameters you have provided.`
+        );
+      }
     } catch (err: any) {
       throw new FirebaseError(
         `Failed to get backend: ${backendId}. Please check the parameters you have provided.`,
@@ -40,5 +64,39 @@ export const command = new Command("backends:get")
       );
     }
 
-    return backend;
+    return backendsList;
   });
+
+async function getBackend(projectId: string, location: string, backendId: string) {
+  try {
+    const backendInRegion = await gcp.getBackend(projectId, location, backendId);
+    return backendInRegion;
+  } catch (err: any) {
+    if ((err as FirebaseError).status != 404) {
+      throw new FirebaseError(`Error occured while trying to get backend: ${backendId}.`, {
+        original: err,
+      });
+    }
+  }
+  return undefined;
+}
+
+function populateTable(backend: gcp.Backend, location: string, table: any) {
+  const entry = [
+    backend.name.split("/").pop(),
+    backend.codebase.repository?.split("/").pop(),
+    backend.uri,
+    location,
+    backend.createTime,
+    backend.updateTime,
+  ];
+  const newRow = entry.map((name) => {
+    const maxCellWidth = COLUMN_LENGTH - 2;
+    const chunks = [];
+    for (let i = 0; name && i < name.length; i += maxCellWidth) {
+      chunks.push(name.substring(i, i + maxCellWidth));
+    }
+    return chunks.join("\n");
+  });
+  table.push(newRow);
+}
