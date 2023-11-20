33d5a60c8549a731acf1b6012c04a8f376b3564d
Bob Hogg <rwhogg@google.com>
2023-11-20 20:02:15 +0000

    feat: Add Firestore backup/schedule and restore support

---
+++ src/commands/firestore-backups-schedules-update.ts
@@ -0,0 +1,43 @@
+import * as clc from "colorette";
+
+import { Command } from "../command";
+import { calculateRetention } from "../firestore/backupUtils";
+import { BackupSchedule, updateBackupSchedule } from "../gcp/firestore";
+import { logger } from "../logger";
+import { requirePermissions } from "../requirePermissions";
+import { Emulators } from "../emulator/types";
+import { warnEmulatorNotSupported } from "../emulator/commandUtils";
+import { FirestoreOptions } from "../firestore/options";
+import { PrettyPrint } from "../firestore/pretty-print";
+
+export const command = new Command("firestore:backups:schedules:update <backupSchedule>")
+  .description("Update a backup schedule under your Cloud Firestore database.")
+  .option("-rt, --retention <duration>", "duration string (e.g. 12h or 30d) for backup retention")
+  .before(requirePermissions, ["datastore.backupSchedules.update"])
+  .before(warnEmulatorNotSupported, Emulators.FIRESTORE)
+  .action(async (backupScheduleName: string, options: FirestoreOptions) => {
+    const printer = new PrettyPrint();
+
+    if (!options.retention) {
+      logger.error(
+        "Missing required flag --retention. See firebase firestore:backups:schedules:update --help for more info",
+      );
+      return;
+    }
+    const retention = calculateRetention(options.retention);
+
+    const backupSchedule: BackupSchedule = await updateBackupSchedule(
+      backupScheduleName,
+      retention,
+    );
+
+    if (options.json) {
+      logger.info(JSON.stringify(backupSchedule, undefined, 2));
+    } else {
+      logger.info(
+        clc.bold(`Successfully updated ${printer.prettyBackupScheduleString(backupSchedule)}`),
+      );
+    }
+
+    return backupSchedule;
+  });
