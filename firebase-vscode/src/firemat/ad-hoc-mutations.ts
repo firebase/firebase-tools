de5ce8de8432cb97ed373695c6103d48e6eaf1f8
Harold Shen <hlshen@google.com>
2023-12-13 21:38:29 -0500

    Basic mutation generation implemented

--- firebase-vscode/src/firemat/ad-hoc-mutations.ts
+++ firebase-vscode/src/firemat/ad-hoc-mutations.ts
@@ -1,35 +1,75 @@
 import vscode, { Disposable, ExtensionContext } from "vscode";
 import { ExtensionBrokerImpl } from "../extension-broker";
-import { registerWebview } from "../webview";
 import { ObjectTypeDefinitionNode } from "graphql";
-import { introspectionQuery } from "./explorer";
-import { effect } from "@preact/signals-core";
-import path from "path";
+import { Uri } from "vscode";
 export function registerAdHoc(
     context: ExtensionContext,
     broker: ExtensionBrokerImpl,
 ): Disposable {
 
+
+    const fileMap = {};
+    const pathSuffix = "_insert.gql";
+
+    /** 
+     * Creates a playground file with an ad-hoc mutation
+     * File will be created (unsaved) in operations/ folder, with an auto-generated named based on the schema type
+     * Mutation will be generated with all 
+     * */
     async function schemaAddData(ast: ObjectTypeDefinitionNode, { documentPath, position }) {
-        console.log(ast);
+        // generate content for the file
+        const preamble = '# This is a file for you to write an un-named mutation. \n # Only one un-named mutation is allowed per file.';
+        const dupeMutationInfo = "# Please save this in the operations/ folder with your other un-named mutations."
+        const adhocMutation = generateMutation(ast);
 
-        console.log(introspectionQuery.value);
-        const content = 'This is a playground file for you to write a mutation \n' + ast.name.value;
+        const basePath = vscode.workspace.rootPath + "/api/";
+        const filePath = vscode.Uri.file(basePath + ast.name + pathSuffix);
+        const doesFileExist = await checkIfFileExists(filePath);
 
-        const filePath = path.join(vscode.workspace.rootPath + "/api/", 'playground.gql');
+        if (!doesFileExist) {
+            // opens unsaved text document with name "[mutationName]_insert.gql"
+            const content = [preamble, adhocMutation].join("\n");
 
-        const openPath = vscode.Uri.file(filePath);
-        console.log("opening path");
-        vscode.workspace.openTextDocument(openPath.with({ scheme: 'untitled' })).then(doc => {
-            vscode.window.showTextDocument(doc).then((openDoc) => {
-                openDoc.edit(edit => {
-                    edit.insert(new vscode.Position(0, 0), content);
+            vscode.workspace.openTextDocument(filePath.with({ scheme: 'untitled' })).then(doc => {
+                vscode.window.showTextDocument(doc).then((openDoc) => {
+                    openDoc.edit(edit => {
+                        edit.insert(new vscode.Position(0, 0), content);
+                    });
                 });
             });
-        });
+        } else {
+            // Opens untitled text document
+            vscode.workspace.openTextDocument({
+                content: [preamble, dupeMutationInfo, adhocMutation].join("\n"),
+                language: "graphql"
+            }).then(doc => {
+                vscode.window.showTextDocument(doc);
+            });
+        }
     }
 
-    // broker.on("definedFirematArgs", setExecutionArgs);
+    async function checkIfFileExists(file: Uri) {
+        try {
+            await vscode.workspace.fs.stat(file);
+            return false;
+        } catch {
+            return true;
+        }
+    }
+
+    function generateMutation(ast: ObjectTypeDefinitionNode): string {
+        const name = ast.name.value.toLowerCase();
+        const functionSpacing = "\t";
+        const fieldSpacing = "\t\t";
+        const mutation = [];
+
+        mutation.push("mutation {", functionSpacing + name + "_insert(data: {");
+        for (const field of ast.fields) {
+            mutation.push(fieldSpacing + field.name.value + ": " + '""')
+        }
+        mutation.push(functionSpacing + "})", "}")
+        return mutation.join("\n");
+    }
 
     return Disposable.from(
         vscode.commands.registerCommand(
