ff26ae973b3172e8a80426c944823aac8aa34051
Remi Rousselet <darky12s@gmail.com>
2023-12-14 14:06:48 +0100

    Cleanup

--- firebase-vscode/webviews/FirematExecutionArgumentsApp.tsx
+++ firebase-vscode/webviews/FirematExecutionArgumentsApp.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { broker } from "./globals/html-broker";
-import style from "./firemat-execution-arguments.entry.scss";
 
 export function FirematExecutionArgumentsApp() {
   const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -9,9 +8,7 @@ export function FirematExecutionArgumentsApp() {
 
   return (
     <>
-      <textarea className={style.id} onChange={handleInput}>
-        {"{}"}
-      </textarea>
+      <textarea onChange={handleInput}>{"{}"}</textarea>
     </>
   );
 }
