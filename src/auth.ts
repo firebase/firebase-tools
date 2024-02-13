5c35de4358dea5d77322d724c0173386865427d0
Joe Hanley <joehanley@google.com>
2024-02-13 13:11:57 -0800

    formatted with the right prettier

--- src/auth.ts
+++ src/auth.ts
@@ -154,7 +154,7 @@ export function selectAccount(account?: string, projectRoot?: string): Account |
   }
 
   throw new FirebaseError(
-    `Account ${account} not found, run "firebase login:list" to see existing accounts or "firebase login:add" to add a new one`
+    `Account ${account} not found, run "firebase login:list" to see existing accounts or "firebase login:add" to add a new one`,
   );
 }
 
@@ -237,7 +237,7 @@ function invalidCredentialError(): FirebaseError {
       "\n\n" +
       "For CI servers and headless environments, generate a new token with " +
       clc.bold("firebase login:ci"),
-    { exit: 1 }
+    { exit: 1 },
   );
 }
 
@@ -291,7 +291,7 @@ function getLoginUrl(callbackUrl: string, userHint?: string) {
 async function getTokensFromAuthorizationCode(
   code: string,
   callbackUrl: string,
-  verifier?: string
+  verifier?: string,
 ) {
   const params: Record<string, string> = {
     code: code,
@@ -335,7 +335,7 @@ async function getTokensFromAuthorizationCode(
     {
       expires_at: Date.now() + res.body.expires_in! * 1000,
     },
-    res.body
+    res.body,
   );
   return lastAccessToken;
 }
@@ -383,7 +383,7 @@ async function respondWithFile(
   req: http.IncomingMessage,
   res: http.ServerResponse,
   statusCode: number,
-  filename: string
+  filename: string,
 ) {
   const response = await util.promisify(fs.readFile)(path.join(__dirname, filename));
   res.writeHead(statusCode, {
@@ -440,7 +440,7 @@ async function loginRemotely(): Promise<UserCredentials> {
     const tokens = await getTokensFromAuthorizationCode(
       code,
       `${authProxyOrigin}/complete`,
-      codeVerifier
+      codeVerifier,
     );
 
     void trackGA4("login", { method: "google_remote" });
@@ -464,7 +464,7 @@ async function loginWithLocalhostGoogle(port: number, userHint?: string): Promis
     callbackUrl,
     authUrl,
     successTemplate,
-    getTokensFromAuthorizationCode
+    getTokensFromAuthorizationCode,
   );
 
   void trackGA4("login", { method: "google_localhost" });
@@ -486,7 +486,7 @@ async function loginWithLocalhostGitHub(port: number): Promise<string> {
     callbackUrl,
     authUrl,
     successTemplate,
-    getGithubTokensFromAuthorizationCode
+    getGithubTokensFromAuthorizationCode,
   );
   void trackGA4("login", { method: "github_localhost" });
   return tokens;
@@ -497,7 +497,7 @@ async function loginWithLocalhost<ResultType>(
   callbackUrl: string,
   authUrl: string,
   successTemplate: string,
-  getTokens: (queryCode: string, callbackUrl: string) => Promise<ResultType>
+  getTokens: (queryCode: string, callbackUrl: string) => Promise<ResultType>,
 ): Promise<ResultType> {
   return new Promise<ResultType>((resolve, reject) => {
     const server = http.createServer(async (req, res) => {
@@ -636,7 +636,7 @@ function logoutCurrentSession(refreshToken: string) {
 
 async function refreshTokens(
   refreshToken: string,
-  authScopes: string[]
+  authScopes: string[],
 ): Promise<TokensWithExpiration> {
   logger.debug("> refreshing access token with scopes:", JSON.stringify(authScopes));
   try {
@@ -684,7 +684,7 @@ async function refreshTokens(
         refresh_token: refreshToken,
         scopes: authScopes,
       },
-      res.body
+      res.body,
     );
 
     const account = findAccountByRefreshToken(refreshToken);
@@ -702,7 +702,7 @@ async function refreshTokens(
           "\n\n" +
           "For CI servers and headless environments, generate a new token with " +
           clc.bold("firebase login:ci"),
-        { exit: 1 }
+        { exit: 1 },
       );
     }
 
