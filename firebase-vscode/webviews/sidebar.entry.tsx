d8697c19712d793fac2f202342f3298bd24f048c
Remi Rousselet <darky12s@gmail.com>
2024-02-13 11:36:16 +0100

    Refactor emulator logic to be out of the EmulatorPanel

--- firebase-vscode/webviews/sidebar.entry.tsx
+++ firebase-vscode/webviews/sidebar.entry.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { SidebarApp } from "./SidebarApp";
 import { App } from "./globals/app";
+import { EmulatorProvider } from "./components/EmulatorPanel";
 
 const root = createRoot(document.getElementById("root")!);
 root.render(
   <App>
-    <SidebarApp />
+    <EmulatorProvider>
+      <SidebarApp />
+    </EmulatorProvider>
   </App>
 );
