5fad1c175ef8b0f7d55b5787b53c87e7c6750ee2
Rosalyn Tan <rosalyntan@google.com>
2024-03-22 18:02:57 -0700

    Linting one more time.

--- src/deploy/dataconnect/prepare.ts
+++ src/deploy/dataconnect/prepare.ts
@@ -20,7 +20,9 @@ export default async function (context: any, options: Options): Promise<void> {
   const serviceCfgs = readFirebaseJson(options.config);
   utils.logLabeledBullet("dataconnect", `Preparing to deploy`);
   context.dataconnect = await Promise.all(
-    serviceCfgs.map((c) => load(projectId, c.location, path.join(options.cwd || process.cwd(), c.source))),
+    serviceCfgs.map((c) =>
+      load(projectId, c.location, path.join(options.cwd || process.cwd(), c.source)),
+    ),
   );
 
   utils.logLabeledBullet("dataconnect", `Successfully prepared schema and connectors`);
