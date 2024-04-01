c8bb9b6fd5af90130ddcb04b64a0aaf010b8c188
Thomas Bouldin <inlined@google.com>
2024-04-01 09:56:44 -0700

    run formatter

--- src/apphosting/config.ts
+++ src/apphosting/config.ts
@@ -5,51 +5,57 @@ import * as yaml from "js-yaml";
 import * as fs from "../fsutils";
 
 export interface RunConfig {
-    concurrency?: number;
-    cpu?: number;
-    memoryMiB?: number;
-    minInstances?: number;
-    maxInstances?: number;
+  concurrency?: number;
+  cpu?: number;
+  memoryMiB?: number;
+  minInstances?: number;
+  maxInstances?: number;
 }
 
-interface HasSecret { secret: string; value: never; }
-interface HasValue { secret: never; value: string; }
+interface HasSecret {
+  secret: string;
+  value: never;
+}
+interface HasValue {
+  secret: never;
+  value: string;
+}
 export type Availability = "BUILD" | "RUNTIME";
 
 export type Env = (HasSecret | HasValue) & {
-    variable: string;
-    availability?: Availability[];
-}
+  variable: string;
+  availability?: Availability[];
+};
 
 export interface Config {
-    runConfig?: RunConfig;
-    env?: Env[];
+  runConfig?: RunConfig;
+  env?: Env[];
 }
 
 export function yamlPath(cwd: string): string | null {
-    let dir = cwd;
+  let dir = cwd;
 
-    while (!fs.fileExistsSync(path.resolve(dir, "apphosting.yaml"))) {
-        // We've hit project root
-        if (fs.fileExistsSync(path.resolve(dir, "firebase.json"))) {
-            return null;
-        }
-
-        const parent = path.dirname(dir);
-        // We've hit the filesystem root
-        if (parent === dir) {
-            return null;
-        }
-        dir = parent;
+  while (!fs.fileExistsSync(path.resolve(dir, "apphosting.yaml"))) {
+    // We've hit project root
+    if (fs.fileExistsSync(path.resolve(dir, "firebase.json"))) {
+      return null;
     }
-    return path.resolve(dir, "apphosting.yaml");
+
+    const parent = path.dirname(dir);
+    // We've hit the filesystem root
+    if (parent === dir) {
+      return null;
+    }
+    dir = parent;
+  }
+  return path.resolve(dir, "apphosting.yaml");
 }
 
 export function load(yamlPath: string): Config {
-    const raw = fs.readFile(yamlPath)
-    return yaml.load(raw, yaml.DEFAULT_FULL_SCHEMA) as Config
+  const raw = fs.readFile(yamlPath);
+  return yaml.load(raw, yaml.DEFAULT_FULL_SCHEMA) as Config;
 }
 
 export function store(yamlPath: string, config: Config): void {
-    writeFileSync(yamlPath, yaml.dump(config))    
+  writeFileSync(yamlPath, yaml.dump(config));
 }
--- src/test/apphosting/config.spec.ts
+++ src/test/apphosting/config.spec.ts
@@ -5,48 +5,48 @@ import * as fsImport from "../../fsutils";
 import * as config from "../../apphosting/config";
 
 describe("config", () => {
-    describe("yamlPath", () => {
-        let fs: sinon.SinonStubbedInstance<typeof fsImport>;
+  describe("yamlPath", () => {
+    let fs: sinon.SinonStubbedInstance<typeof fsImport>;
 
-        beforeEach(() => {
-            fs = sinon.stub(fsImport);
-        });
-
-        afterEach(() => {
-            sinon.verifyAndRestore();
-        });
-
-        it("finds apphosting.yaml at cwd", () => {
-            fs.fileExistsSync.withArgs("/cwd/apphosting.yaml").returns(true);
-            expect(config.yamlPath("/cwd")).equals("/cwd/apphosting.yaml");
-        });
-
-        it("finds apphosting.yaml in a parent directory", () => {
-            fs.fileExistsSync.withArgs("/parent/cwd/apphosting.yaml").returns(false);
-            fs.fileExistsSync.withArgs("/parent/cwd/firebase.json").returns(false);
-            fs.fileExistsSync.withArgs("/parent/apphosting.yaml").returns(true);
-
-            expect(config.yamlPath("/parent/cwd")).equals("/parent/apphosting.yaml");
-        });
-
-        it("returns null if it finds firebase.json without finding apphosting.yaml", () => {
-            fs.fileExistsSync.withArgs("/parent/cwd/apphosting.yaml").returns(false);
-            fs.fileExistsSync.withArgs("/parent/cwd/firebase.json").returns(false);
-            fs.fileExistsSync.withArgs("/parent/apphosting.yaml").returns(false);
-            fs.fileExistsSync.withArgs("/parent/firebase.json").returns(true);
-
-            expect(config.yamlPath("/parent/cwd")).equals(null);
-        });
-
-        it("returns if it reaches the fs root", () => {
-            fs.fileExistsSync.withArgs("/parent/cwd/apphosting.yaml").returns(false);
-            fs.fileExistsSync.withArgs("/parent/cwd/firebase.json").returns(false);
-            fs.fileExistsSync.withArgs("/parent/apphosting.yaml").returns(false);
-            fs.fileExistsSync.withArgs("/parent/firebase.json").returns(false);
-            fs.fileExistsSync.withArgs("/apphosting.yaml").returns(false)
-            fs.fileExistsSync.withArgs("/firebase.json").returns(false)
-
-            expect(config.yamlPath("/parent/cwd")).equals(null);
-        });
+    beforeEach(() => {
+      fs = sinon.stub(fsImport);
     });
+
+    afterEach(() => {
+      sinon.verifyAndRestore();
+    });
+
+    it("finds apphosting.yaml at cwd", () => {
+      fs.fileExistsSync.withArgs("/cwd/apphosting.yaml").returns(true);
+      expect(config.yamlPath("/cwd")).equals("/cwd/apphosting.yaml");
+    });
+
+    it("finds apphosting.yaml in a parent directory", () => {
+      fs.fileExistsSync.withArgs("/parent/cwd/apphosting.yaml").returns(false);
+      fs.fileExistsSync.withArgs("/parent/cwd/firebase.json").returns(false);
+      fs.fileExistsSync.withArgs("/parent/apphosting.yaml").returns(true);
+
+      expect(config.yamlPath("/parent/cwd")).equals("/parent/apphosting.yaml");
+    });
+
+    it("returns null if it finds firebase.json without finding apphosting.yaml", () => {
+      fs.fileExistsSync.withArgs("/parent/cwd/apphosting.yaml").returns(false);
+      fs.fileExistsSync.withArgs("/parent/cwd/firebase.json").returns(false);
+      fs.fileExistsSync.withArgs("/parent/apphosting.yaml").returns(false);
+      fs.fileExistsSync.withArgs("/parent/firebase.json").returns(true);
+
+      expect(config.yamlPath("/parent/cwd")).equals(null);
+    });
+
+    it("returns if it reaches the fs root", () => {
+      fs.fileExistsSync.withArgs("/parent/cwd/apphosting.yaml").returns(false);
+      fs.fileExistsSync.withArgs("/parent/cwd/firebase.json").returns(false);
+      fs.fileExistsSync.withArgs("/parent/apphosting.yaml").returns(false);
+      fs.fileExistsSync.withArgs("/parent/firebase.json").returns(false);
+      fs.fileExistsSync.withArgs("/apphosting.yaml").returns(false);
+      fs.fileExistsSync.withArgs("/firebase.json").returns(false);
+
+      expect(config.yamlPath("/parent/cwd")).equals(null);
+    });
+  });
 });
