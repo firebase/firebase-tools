8e0c5fea7d8e96ce514137cdb0bcff91dd99b0b9
Tony Huang <tony.huang.jun@gmail.com>
2024-04-12 12:56:26 -0400

    lint

--- src/apphosting/index.ts
+++ src/apphosting/index.ts
@@ -92,8 +92,7 @@ export async function doSetup(
     name: "rootDir",
     type: "input",
     default: "/",
-    message:
-      "Specify your app's root directory relative to your repository",
+    message: "Specify your app's root directory relative to your repository",
   });
 
   const backend = await createBackend(
