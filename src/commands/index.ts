0a1d7c8d482a73f1ecd56e692973940959c997b4
Mathusan Selvarajah <mathusans52@gmail.com>
2024-04-16 10:11:51 -0400

    Revert "Clean up apphosting commands used for testing purposes (#6978)"
    This reverts commit 8a6e608376bfd00868553d512ea7f00e6e1556cb.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -168,11 +168,17 @@ export function load(client: any): any {
     client.apphosting.backends.create = loadCommand("apphosting-backends-create");
     client.apphosting.backends.get = loadCommand("apphosting-backends-get");
     client.apphosting.backends.delete = loadCommand("apphosting-backends-delete");
+    client.apphosting.builds = {};
+    client.apphosting.builds.get = loadCommand("apphosting-builds-get");
+    client.apphosting.builds.create = loadCommand("apphosting-builds-create");
     client.apphosting.secrets = {};
     client.apphosting.secrets.set = loadCommand("apphosting-secrets-set");
     client.apphosting.secrets.grantaccess = loadCommand("apphosting-secrets-grantaccess");
     client.apphosting.secrets.describe = loadCommand("apphosting-secrets-describe");
     client.apphosting.secrets.access = loadCommand("apphosting-secrets-access");
+    client.apphosting.rollouts = {};
+    client.apphosting.rollouts.create = loadCommand("apphosting-rollouts-create");
+    client.apphosting.rollouts.list = loadCommand("apphosting-rollouts-list");
   }
   client.login = loadCommand("login");
   client.login.add = loadCommand("login-add");
