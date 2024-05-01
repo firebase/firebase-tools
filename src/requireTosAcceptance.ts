b57f6f0fd58649f06aad2f2204a71ee518c35d93
Tony Huang <tony.huang.jun@gmail.com>
2024-05-01 14:28:44 -0400

    add initial tos requirement

--- src/requireTosAcceptance.ts
+++ src/requireTosAcceptance.ts
@@ -18,7 +18,7 @@ const consoleLandingPage = new Map<TosId, string>([
  *
  * Note: When supporting new products, be sure to update `consoleLandingPage` above to avoid surfacing
  * generic ToS error messages.
- **/
+ */
 export function requireTosAcceptance(tosId: TosId): (options: Options) => Promise<void> {
   return () => requireTos(tosId);
 }
@@ -28,8 +28,12 @@ async function requireTos(tosId: TosId): Promise<void> {
   if (isProductTosAccepted(res, tosId)) {
     return;
   }
-  const console = consoleLandingPage.get(tosId) || consoleOrigin();
+  if (consoleLandingPage.has(tosId)) {
+    throw new FirebaseError(
+      `Exiting due to missing terms of service agreement on your account. Visit ${consoleLandingPage.get(tosId)} to get started.`,
+    );
+  }
   throw new FirebaseError(
-    `Your account has not accepted the required Terms of Service for this action. Please accept the Terms of Service and try again. ${console}`,
+    `Exiting due to missing terms of service agreement for ${tosId}. Visit ${consoleOrigin()} to get started.`,
   );
 }
