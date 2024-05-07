44d70522a3ce23752e79ed83420b44db46e3bca2
Sam Olsen <samgho@google.com>
2024-05-07 13:47:49 -0700

    Update genkit onboarding

--- src/init/features/genkit.ts
+++ src/init/features/genkit.ts
@@ -2,7 +2,7 @@ import { logger } from "../../logger";
 import { doSetup as functionsSetup } from "./functions";
 import { Options } from "../../options";
 import { Config } from "../../config";
-import { prompt } from "../../prompt";
+import { promptOnce } from "../../prompt";
 import { wrapSpawn } from "../spawn";
 
 /**
@@ -10,16 +10,13 @@ import { wrapSpawn } from "../spawn";
  */
 export async function doSetup(setup: any, config: Config, options: Options): Promise<void> {
   if (setup.functions?.languageChoice !== "typescript") {
-    await prompt(setup, [
-      {
-        name: "continueFunctions",
-        type: "confirm",
-        message:
-          "Genkit with Firebase uses Cloud Functions for Firebase with TypeScript. Initialize Functions to continue?",
-        default: true,
-      },
-    ]);
-    if (!setup.continueFunctions) {
+    const continueFunctions = await promptOnce({
+      type: "confirm",
+      message:
+        "Genkit's Firebase integration uses Cloud Functions for Firebase with TypeScript. Initialize Functions to continue?",
+      default: true,
+    });
+    if (!continueFunctions) {
       logger.info("Stopped Genkit initialization");
       return;
     }
@@ -33,15 +30,30 @@ export async function doSetup(setup: any, config: Config, options: Options): Pro
 
   const projectDir: string = `${config.projectDir}/${setup.functions.source}`;
 
+  const installType = await promptOnce({
+    type: "list",
+    message: "Install the Genkit CLI globally or locally in this project?",
+    choices: [
+      { name: "Globally", value: "globally" },
+      { name: "Just this project", value: "project" },
+    ],
+  });
+
   try {
     logger.info("Installing Genkit CLI");
-    await wrapSpawn("npm", ["install", "genkit", "--save-dev"], projectDir);
-    await wrapSpawn("npx", ["genkit", "init", "-p", "firebase"], projectDir);
+    if (installType === "globally") {
+      await wrapSpawn("npm", ["install", "-g", "genkit"], projectDir);
+      await wrapSpawn("genkit", ["init", "-p", "firebase"], projectDir);
+      logger.info("Start the Genkit developer experience by running:");
+      logger.info(`    cd ${setup.functions.source} && genkit start`);
+    } else {
+      await wrapSpawn("npm", ["install", "genkit", "--save-dev"], projectDir);
+      await wrapSpawn("npx", ["genkit", "init", "-p", "firebase"], projectDir);
+      logger.info("Start the Genkit developer experience by running:");
+      logger.info(`    cd ${setup.functions.source} && npx genkit start`);
+    }
   } catch (e) {
     logger.error("Genkit initialization failed...");
     return;
   }
-
-  logger.info("To use the Genkit CLI globally, run:");
-  logger.info("    npm install genkit -g");
 }
