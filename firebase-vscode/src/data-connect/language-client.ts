cb21194c99826b0b9a3f8ecd431601e00e296dc0
Harold Shen <hlshen@google.com>
2024-05-08 19:07:27 -0400

    first attempt at generating config

--- firebase-vscode/src/data-connect/language-client.ts
+++ firebase-vscode/src/data-connect/language-client.ts
@@ -7,25 +7,46 @@ import {
   LanguageClient,
 } from "vscode-languageclient/node";
 import * as path from "node:path";
-import { Signal } from "@preact/signals-core";
+import { ResolvedDataConnectConfigs } from "./config";
+import { GraphQLConfig } from "graphql-config";
 
-export function setupLanguageClient(context) {
+export function setupLanguageClient(
+  context,
+  configs: ResolvedDataConnectConfigs,
+) {
   // activate language client/serer
   const outputChannel: vscode.OutputChannel = vscode.window.createOutputChannel(
-    "Firebase GraphQL Language Server"
+    "Firebase GraphQL Language Server",
   );
 
   const serverPath = path.join("dist", "server.js");
   const serverModule = context.asAbsolutePath(serverPath);
 
+  // Pass schema, document paths to generate a graphql-config in the language-server
+  // TODO: Expand to multiple services
+  const config = configs.values[0];
+  const generatedPath = ".dataconnect";
+  const schemaPaths = [`./${config.relativeSchemaPath}/**/*.gql`, `./${config.relativePath}/${generatedPath}/**/*.gql`];
+  const documentPaths = config.relativeConnectorPaths.map(
+    (connectorPath) => `./${connectorPath}/**/*.gql`,
+  );
+
+  const fdcEnv = { schemaPaths: JSON.stringify(schemaPaths), documentPaths: JSON.stringify(documentPaths) };
+  // const fdcEnv = { graphql_config: JSON.stringify(graphqlConfig) };
   const debugOptions = {
     execArgv: ["--nolazy", "--inspect=localhost:6009"],
+    env: fdcEnv,
+  };
+
+  const runOptions = {
+    env: fdcEnv,
   };
 
   const serverOptions: ServerOptions = {
     run: {
       module: serverModule,
       transport: TransportKind.ipc,
+      options: runOptions,
     },
     debug: {
       module: serverModule,
@@ -47,12 +68,12 @@ export function setupLanguageClient(context) {
           // also, it makes sense that it should only re-load on file save, but we need to document that.
           // TODO: perhaps we can intercept change events, and remind the user
           // to save for the changes to take effect
-          true
+          true,
         ),
         // TODO: load ignore file
         // These ignore node_modules and .git by default
         vscode.workspace.createFileSystemWatcher(
-          "**/{*.graphql,*.graphqls,*.gql,*.js,*.mjs,*.cjs,*.esm,*.es,*.es6,*.jsx,*.ts,*.tsx,*.vue,*.svelte,*.cts,*.mts}"
+          "**/{*.graphql,*.graphqls,*.gql,*.js,*.mjs,*.cjs,*.esm,*.es,*.es6,*.jsx,*.ts,*.tsx,*.vue,*.svelte,*.cts,*.mts}",
         ),
       ],
     },
@@ -75,13 +96,13 @@ export function setupLanguageClient(context) {
     "graphQLlanguageServer",
     "GraphQL Language Server",
     serverOptions,
-    clientOptions
+    clientOptions,
   );
 
   // register commands
   const commandShowOutputChannel = vscode.commands.registerCommand(
     "fdc-graphql.showOutputChannel",
-    () => outputChannel.show()
+    () => outputChannel.show(),
   );
 
   context.subscriptions.push(commandShowOutputChannel);
@@ -104,7 +125,7 @@ export function setupLanguageClient(context) {
     "**/.graphqlrc.*", // TODO: extend to schema files, and other config types
     false,
     false,
-    false
+    false,
   );
   watcher.onDidChange(() => restartGraphqlLSP());
 
--- firebase-vscode/src/data-connect/language-server.ts
+++ firebase-vscode/src/data-connect/language-server.ts
@@ -1,11 +1,28 @@
 import { startServer } from "graphql-language-service-server";
-
+import { GraphQLConfig, loadConfig } from "graphql-config";
 // The npm scripts are configured to only build this once before
 // watching the extension, so please restart the extension debugger for changes!
 
 async function start() {
+    const schemaPaths = JSON.parse(process.env.schemaPaths);
+    const documentPaths = JSON.parse(process.env.documentPaths);
+    const graphqlConfig = new GraphQLConfig(
+      {
+        config: {
+          schema: schemaPaths,
+          documents: documentPaths,
+        },
+        filepath: ''
+      },
+      [],
+    );
+  const real = await loadConfig({filepath: ".graphqlrc"});
+  console.log(graphqlConfig);
+  console.log(graphqlConfig.getDefault());
+  console.log(real);
+  console.log(real.getDefault());
   try {
-    await startServer({ method: "node" });
+    await startServer({ method: "node", loadConfigOptions: {filepath: '.firebase/dataconnect/.graphqlrc'}, config: graphqlConfig });
     // eslint-disable-next-line no-console
     console.log("Firebase GraphQL Language Server started!");
   } catch (err) {
