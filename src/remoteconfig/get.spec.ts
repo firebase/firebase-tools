32f1563ca883590455f23eac026cee7cd13a1a07
Yuchen Shi <yuchenshi@google.com>
2024-06-13 16:05:27 -0700

    Move remoteconfig tests (#7318)

--- src/remoteconfig/get.spec.ts
+++ src/remoteconfig/get.spec.ts
@@ -1,10 +1,10 @@
 import { expect } from "chai";
-import { remoteConfigApiOrigin } from "../../api";
+import { remoteConfigApiOrigin } from "../api";
 import * as nock from "nock";
 
-import * as remoteconfig from "../../remoteconfig/get";
-import { RemoteConfigTemplate } from "../../remoteconfig/interfaces";
-import { FirebaseError } from "../../error";
+import * as remoteconfig from "./get";
+import { RemoteConfigTemplate } from "./interfaces";
+import { FirebaseError } from "../error";
 
 const PROJECT_ID = "the-remoteconfig-test-project";
 
