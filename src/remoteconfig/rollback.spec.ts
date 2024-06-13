007ddfccb20653a5587810f765f3d43bcd6648b0
Yuchen Shi <yuchenshi@google.com>
2024-06-13 16:05:27 -0700

    Move remoteconfig tests (#7318)

--- src/remoteconfig/rollback.spec.ts
+++ src/remoteconfig/rollback.spec.ts
@@ -1,10 +1,10 @@
 import { expect } from "chai";
-import { remoteConfigApiOrigin } from "../../api";
+import { remoteConfigApiOrigin } from "../api";
 import * as nock from "nock";
 
-import { RemoteConfigTemplate } from "../../remoteconfig/interfaces";
-import * as remoteconfig from "../../remoteconfig/rollback";
-import { FirebaseError } from "../../error";
+import { RemoteConfigTemplate } from "./interfaces";
+import * as remoteconfig from "./rollback";
+import { FirebaseError } from "../error";
 
 const PROJECT_ID = "the-remoteconfig-test-project";
 
