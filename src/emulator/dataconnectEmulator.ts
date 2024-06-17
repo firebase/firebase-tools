05419b73a35f28dd29d4f39ac2d1eb0f89c929a8
Joe Hanley <joehanley@google.com>
2024-06-17 15:52:23 -0700

    pr fixes

--- src/emulator/dataconnectEmulator.ts
+++ src/emulator/dataconnectEmulator.ts
@@ -80,7 +80,7 @@ export class DataConnectEmulator implements EmulatorInstance {
       config_dir: this.args.configDir,
       service_location: this.args.locationId,
     });
-    this.usingExistingEmulator = true;
+    this.usingExistingEmulator = false;
 
     if (!isVSCodeExtension()) {
       await this.connectToPostgres();
@@ -89,9 +89,15 @@ export class DataConnectEmulator implements EmulatorInstance {
   }
 
   async connect(): Promise<void> {
+    // TODO: Wait for 'Listening on address (HTTP + gRPC)' message to ensure that emulator binary is fully started.
     const emuInfo = await this.emulatorClient.getInfo();
     if (!emuInfo) {
-      Promise.reject();
+      this.logger.logLabeled(
+        "ERROR",
+        "dataconnect",
+        "Could not connect to Data Connect emulator. Check dataconnect-debug.log for more details.",
+      );
+      return Promise.reject();
     }
     return Promise.resolve();
   }
@@ -222,8 +228,8 @@ export class DataConnectEmulator implements EmulatorInstance {
 Run ${clc.bold("firebase setup:emulators:dataconnect")} to set up a Postgres connection.`;
       throw new FirebaseError(msg);
     }
-    // The Data Connect emualtor does not immediately start listening after started
-    // so we retry this call with a brief backoff
+    // The Data Connect emulator does not immediately start listening after started
+    // so we retry this call with a brief backoff.
     const MAX_RETRIES = 3;
     for (let i = 1; i <= MAX_RETRIES; i++) {
       try {
