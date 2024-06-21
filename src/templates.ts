dbf433fbcee44e26d7bbfc6a5642a4900b5c77cd
Yuchen Shi <yuchenshi@google.com>
2024-06-21 14:30:20 -0700

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.6-beta.1

---
+++ src/templates.ts
@@ -0,0 +1,31 @@
+import { readFileSync } from "fs";
+import { readFile } from "fs/promises";
+import { resolve } from "path";
+
+const TEMPLATE_ROOT = resolve(__dirname, "../templates/");
+const TEMPLATE_ENCODING = "utf8";
+
+/**
+ * Get an absolute template file path. (Prefer readTemplateSync instead.)
+ * @param relPath file path relative to the /templates directory under root.
+ */
+export function absoluteTemplateFilePath(relPath: string): string {
+  const p = resolve(TEMPLATE_ROOT, relPath);
+  return readFileSync(p, TEMPLATE_ENCODING);
+}
+
+/**
+ * Read a template file synchronously.
+ * @param relPath file path relative to the /templates directory under root.
+ */
+export function readTemplateSync(relPath: string): string {
+  return readFileSync(absoluteTemplateFilePath(relPath), TEMPLATE_ENCODING);
+}
+
+/**
+ * Read a template file asynchronously.
+ * @param relPath file path relative to the /templates directory under root.
+ */
+export function readTemplate(relPath: string): Promise<string> {
+  return readFile(absoluteTemplateFilePath(relPath), TEMPLATE_ENCODING);
+}
