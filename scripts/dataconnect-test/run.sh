6e6b556c46fbf362a3b22465bf665c5b03ff62c0
Joe Hanley <joehanley@google.com>
2024-06-25 13:50:57 -0700

    Use existing script for this

--- scripts/dataconnect-test/run.sh
+++ scripts/dataconnect-test/run.sh
@@ -2,6 +2,6 @@
 set -e # Immediately exit on failure
 # Globally link the CLI for the testing framework
 ./scripts/clean-install.sh
-
+source scripts/set-default-credentials.sh
 mocha scripts/dataconnect-test/tests.ts
 rm -rf ../../clean
