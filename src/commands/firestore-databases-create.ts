a086824529a1e67c743660bd2907de8215659adf
jinyangtang <jinyangtang@google.com>
2024-07-19 18:19:40 +0000

    fix lint

--- src/commands/firestore-databases-create.ts
+++ src/commands/firestore-databases-create.ts
@@ -26,8 +26,8 @@ export const command = new Command("firestore:databases:create <database>")
     "Whether to enable the PITR feature on this database, for example 'ENABLED' or 'DISABLED'. Default is 'DISABLED'",
   )
   .option(
-    '--kms-key-name <kmsKeyName>',
-    'The resource ID of a Cloud KMS key. If set, the database created will be a Customer-managed Encryption Key (CMEK) database encrypted with this key. This feature is allowlist only in initial launch.',
+    "--kms-key-name·<kmsKeyName>",
+    "The resource ID of a Cloud KMS key. If set, the database created will be a Customer-managed Encryption Key (CMEK) database encrypted with this key. This feature is allowlist only in initial launch.",
   )
   .before(requirePermissions, ["datastore.databases.create"])
   .before(warnEmulatorNotSupported, Emulators.FIRESTORE)
@@ -66,7 +66,7 @@ export const command = new Command("firestore:databases:create <database>")
       options.pointInTimeRecovery === types.PointInTimeRecoveryEnablementOption.ENABLED
         ? types.PointInTimeRecoveryEnablement.ENABLED
         : types.PointInTimeRecoveryEnablement.DISABLED;
-    
+
     let cmekConfig: types.CmekConfig | undefined;
     if (options.kmsKeyName) {
       cmekConfig = {
