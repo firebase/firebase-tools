f3ac6f92879367c0598c7e9a63aaaf7c7ae37524
Brian Li <blidd@google.com>
2024-08-19 13:11:26 -0400

    rebase

--- src/deploy/functions/services/auth.ts
+++ src/deploy/functions/services/auth.ts
@@ -79,6 +79,9 @@ export class AuthBlockingService implements Service {
     const newBlockingConfig = await identityPlatform.getBlockingFunctionsConfig(endpoint.project);
     const oldBlockingConfig = cloneDeep(newBlockingConfig);
 
+    console.log("***** trigger type:", endpoint.blockingTrigger.eventType);
+    console.log("*****\n old config:", oldBlockingConfig);
+
     if (endpoint.blockingTrigger.eventType === events.v1.BEFORE_CREATE_EVENT) {
       newBlockingConfig.triggers = {
         ...newBlockingConfig.triggers,
@@ -118,9 +121,13 @@ export class AuthBlockingService implements Service {
       ...endpoint.blockingTrigger.options,
     };
 
+    console.log("*****\n new config:", newBlockingConfig);
+
     if (!this.configChanged(newBlockingConfig, oldBlockingConfig)) {
       return;
     }
+
+    console.log("*****\nUpdating blocking functions config to:", newBlockingConfig);
 
     await identityPlatform.setBlockingFunctionsConfig(endpoint.project, newBlockingConfig);
   }
