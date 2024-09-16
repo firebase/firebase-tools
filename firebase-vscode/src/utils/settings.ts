b57b8489dcce870f81d0b4b2b72447b832b0bb7d
TJ Lavelle <tj@lavelle.io>
2024-09-16 12:36:05 -0400

    Simplify the config getter

--- firebase-vscode/src/utils/settings.ts
+++ firebase-vscode/src/utils/settings.ts
@@ -2,31 +2,23 @@ import { workspace } from "./test_hooks";
 
 interface Settings {
   readonly shouldWriteDebug: boolean;
-  debugLogPath: string;
-  useFrameworks: boolean;
-  npmPath: string;
+  readonly debugLogPath: string;
+  readonly useFrameworks: boolean;
+  readonly npmPath: string;
+  readonly firebasePath: string;
 }
 
 export function getSettings(): Settings {
-  // Get user-defined VSCode settings if workspace is found.
-  if (workspace.value.workspaceFolders) {
-    const workspaceConfig = workspace.value.getConfiguration(
-      "firebase",
-      workspace.value.workspaceFolders[0].uri,
-    );
-
-    return {
-      shouldWriteDebug: workspaceConfig.get("debug"),
-      debugLogPath: workspaceConfig.get("debugLogPath"),
-      useFrameworks: workspaceConfig.get("hosting.useFrameworks"),
-      npmPath: workspaceConfig.get("npmPath"),
-    };
-  }
+  const config = workspace.value.getConfiguration(
+    "firebase",
+    workspace.value.workspaceFolders[0].uri,
+  );
 
   return {
-    shouldWriteDebug: false,
-    debugLogPath: "",
-    useFrameworks: false,
-    npmPath: "",
+    debugLogPath: config.get<string>("debugLogPath"),
+    firebasePath: config.get<string>("firebasePath"),
+    npmPath: config.get<string>("npmPath"),
+    shouldWriteDebug: config.get<boolean>("debug"),
+    useFrameworks: config.get<boolean>("hosting.useFrameworks"),
   };
 }
