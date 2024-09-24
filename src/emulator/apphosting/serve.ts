7a42aa21d4ab7c6951584d41cd457e02db4d6364
Mathusan Selvarajah <mathusan@google.com>
2024-09-24 16:28:58 +0000

    add discovery function

--- src/emulator/apphosting/serve.ts
+++ src/emulator/apphosting/serve.ts
@@ -5,6 +5,8 @@
 import { isIPv4 } from "net";
 import { checkListenable } from "../portUtils";
 import { wrapSpawn } from "../../init/spawn";
+import { pathExists } from "fs-extra";
+import { join } from "path";
 
 /**
  * Spins up a project locally by running the project's dev command.
@@ -35,4 +37,20 @@ function availablePort(host: string, port: number): Promise<boolean> {
 export async function serve(options: any, port: string) {
   // TODO: update to support other package managers and frameworks other than NextJS
   await wrapSpawn("npm", ["run", "dev", "--", "-H", options.host, "-p", port], process.cwd());
+}
+
+async function discoverPackageManager(rootdir: string): Promise<SupportedPackageManager> {
+  if (await pathExists(join(rootdir, "pnpm-lock.yaml"))) {
+    return SupportedPackageManager.pnpm;
+  } else if (await pathExists(join(rootdir, "yarn.lock"))) {
+    return SupportedPackageManager.yarn;
+  } else {
+    return SupportedPackageManager.npm;
+  }
+}
+
+enum SupportedPackageManager {
+  npm = 1,
+  yarn,
+  pnpm,
 }
