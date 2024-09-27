0f617f38a8848425e51a4d15ac49caac6e594b7c
joehan <joehanley@google.com>
2024-09-27 14:40:20 -0400

    Cleaning up cses where project id is not set (#7760)
    Co-authored-by: Fred Zhang <fredzqm@google.com>

--- firebase-vscode/src/data-connect/terminal.ts
+++ firebase-vscode/src/data-connect/terminal.ts
@@ -27,7 +27,10 @@ export function runCommand(command: string) {
   // TODO: This fails if the interactive shell is not expecting a command, such
   // as when oh-my-zsh asking for (Y/n) to updates during startup.
   // Consider using an non-interactive shell.
-  terminal.sendText(`${command} -P ${currentProjectId.value}`);
+  if (currentProjectId.value) {
+    command = `${command} --project ${currentProjectId.value}`;
+  }
+  terminal.sendText(command);
 }
 
 export function runTerminalTask(
