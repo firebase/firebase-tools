c2fee6069b9c5d05b6509152dd4c7dd20aadf82e
Mathusan Selvarajah <mathusan@google.com>
2024-10-01 19:57:10 +0000

    fix broken test

--- src/emulator/apphosting/serve.spec.ts
+++ src/emulator/apphosting/serve.spec.ts
@@ -4,19 +4,23 @@ import * as spawn from "../../init/spawn";
 import { expect } from "chai";
 import * as serve from "./serve";
 import { DEFAULT_PORTS } from "../constants";
+import * as utils from "./utils";
 
 describe("serve", () => {
   let checkListenableStub: sinon.SinonStub;
   let wrapSpawnStub: sinon.SinonStub;
+  let discoverPackageManagerStub: sinon.SinonStub;
 
   beforeEach(() => {
     checkListenableStub = sinon.stub(portUtils, "checkListenable");
     wrapSpawnStub = sinon.stub(spawn, "wrapSpawn");
+    discoverPackageManagerStub = sinon.stub(utils, "discoverPackageManager");
   });
 
   afterEach(() => {
     checkListenableStub.restore();
     wrapSpawnStub.restore();
+    discoverPackageManagerStub.restore();
   });
 
   describe("start", () => {
@@ -24,8 +28,6 @@ describe("serve", () => {
       checkListenableStub.onFirstCall().returns(false);
       checkListenableStub.onSecondCall().returns(false);
       checkListenableStub.onThirdCall().returns(true);
-
-      wrapSpawnStub.returns(Promise.resolve());
 
       const res = await serve.start();
       expect(res.port).to.equal(DEFAULT_PORTS.apphosting + 2);
