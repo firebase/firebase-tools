0760481200c249d95e7759772fb99ccdbeca6a7f
Brian Li <blidd@google.com>
2024-10-07 18:11:05 -0400

    fix lint error

--- src/init/features/apphosting.ts
+++ src/init/features/apphosting.ts
@@ -1,6 +1,5 @@
 import * as clc from "colorette";
 import * as utils from "../../utils";
-import { Options } from "../../options";
 import { Config } from "../../config";
 import { readTemplateSync } from "../../templates";
 
@@ -9,7 +8,7 @@ const APPHOSTING_YAML_TEMPLATE = readTemplateSync("init/apphosting/apphosting.ya
 /**
  * Set up an apphosting.yaml file for a new App Hosting project.
  */
-export async function doSetup(setup: any, config: Config, options: Options): Promise<void> {
+export async function doSetup(setup: any, config: Config): Promise<void> {
   const path = `apphosting.yaml`;
   utils.logBullet("Writing default settings to " + clc.bold("apphosting.yaml") + "...");
   await config.askWriteProjectFile(path, APPHOSTING_YAML_TEMPLATE);
