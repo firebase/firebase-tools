4df32be995ac3a7aa784f48e0a67e15286a5bd62
Joe Hanley <joehanley@google.com>
2024-10-23 10:25:06 -0700

    repairing missing binaries

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -60,48 +60,48 @@ git clone "git@github.com:${REPOSITORY_ORG}/${REPOSITORY_NAME}.git"
 cd "${REPOSITORY_NAME}"
 echo "Cloned repository."
 
-echo "Making sure there is a changelog..."
-if [ ! -s CHANGELOG.md ]; then
-  echo "CHANGELOG.md is empty. aborting."
-  exit 1
-fi
-echo "Made sure there is a changelog."
+# echo "Making sure there is a changelog..."
+# if [ ! -s CHANGELOG.md ]; then
+#   echo "CHANGELOG.md is empty. aborting."
+#   exit 1
+# fi
+# echo "Made sure there is a changelog."
 
 echo "Running npm install..."
 npm install
 echo "Ran npm install."
 
-echo "Running tests..."
-npm test
-echo "Ran tests."
+# echo "Running tests..."
+# npm test
+# echo "Ran tests."
 
-echo "Making a $VERSION version..."
-npm version $VERSION
-NEW_VERSION=$(jq -r ".version" package.json)
-echo "Made a $VERSION version."
+# echo "Making a $VERSION version..."
+# npm version $VERSION
+# NEW_VERSION=$(jq -r ".version" package.json)
+# echo "Made a $VERSION version."
 
-echo "Making the release notes..."
-RELEASE_NOTES_FILE=$(mktemp)
-echo "[DEBUG] ${RELEASE_NOTES_FILE}"
-echo "v${NEW_VERSION}" >> "${RELEASE_NOTES_FILE}"
-echo "" >> "${RELEASE_NOTES_FILE}"
-cat CHANGELOG.md >> "${RELEASE_NOTES_FILE}"
-echo "Made the release notes."
+# echo "Making the release notes..."
+# RELEASE_NOTES_FILE=$(mktemp)
+# echo "[DEBUG] ${RELEASE_NOTES_FILE}"
+# echo "v${NEW_VERSION}" >> "${RELEASE_NOTES_FILE}"
+# echo "" >> "${RELEASE_NOTES_FILE}"
+# cat CHANGELOG.md >> "${RELEASE_NOTES_FILE}"
+# echo "Made the release notes."
 
 echo "Publishing to npm..."
 npx clean-publish --before-script ./scripts/clean-shrinkwrap.sh
 echo "Published to npm."
 
-echo "Cleaning up release notes..."
-rm CHANGELOG.md
-touch CHANGELOG.md
-git commit -m "[firebase-release] Removed change log and reset repo after ${NEW_VERSION} release" CHANGELOG.md 
-echo "Cleaned up release notes."
+# echo "Cleaning up release notes..."
+# rm CHANGELOG.md
+# touch CHANGELOG.md
+# git commit -m "[firebase-release] Removed change log and reset repo after ${NEW_VERSION} release" CHANGELOG.md 
+# echo "Cleaned up release notes."
 
-echo "Pushing to GitHub..."
-git push origin master --tags
-echo "Pushed to GitHub."
+# echo "Pushing to GitHub..."
+# git push origin master --tags
+# echo "Pushed to GitHub."
 
-echo "Publishing release notes..."
-hub release create --file "${RELEASE_NOTES_FILE}" "v${NEW_VERSION}"
-echo "Published release notes."
+# echo "Publishing release notes..."
+# hub release create --file "${RELEASE_NOTES_FILE}" "v${NEW_VERSION}"
+# echo "Published release notes."
