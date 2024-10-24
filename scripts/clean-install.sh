631da2376f579534333ce8e198d1e94a13b2b743
joehan <joehanley@google.com>
2024-10-24 15:54:21 -0400

    Pin clean-publish to 5.0.0 (#7870)

--- scripts/clean-install.sh
+++ scripts/clean-install.sh
@@ -10,9 +10,9 @@ function cleanup() {
 trap cleanup EXIT
 
 rm -rf ./clean || true
-echo "Running clean-publish --without-publish, as we would before publishing to npm..."
-npx --yes clean-publish --without-publish --before-script ./scripts/clean-shrinkwrap.sh --temp-dir clean
-echo "Ran clean-publish --without-publish."
+echo "Running clean-publish@5.0.0 --without-publish, as we would before publishing to npm..."
+npx --yes clean-publish@5.0.0 --without-publish --before-script ./scripts/clean-shrinkwrap.sh --temp-dir clean
+echo "Ran clean-publish@5.0.0 --without-publish."
 echo "Packaging cleaned firebase-tools..."
 cd ./clean
 PACKED=$(npm pack --pack-destination ./ | tail -n 1)
