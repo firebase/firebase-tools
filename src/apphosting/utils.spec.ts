cc6e0d38507eb5683efa77f14da4d90187235cfb
Mathusan Selvarajah <mathusan@google.com>
2024-10-31 18:25:47 +0000

    fix test

--- src/apphosting/utils.spec.ts
+++ src/apphosting/utils.spec.ts
@@ -36,7 +36,7 @@ describe("utils", () => {
       expect(prompt.promptOnce).to.have.been.calledWith({
         name: "apphosting-yaml",
         type: "list",
-        message: "Please select an app hosting config:",
+        message: "Please select an App Hosting config:",
         choices: [
           {
             name: "base (apphosting.yaml)",
