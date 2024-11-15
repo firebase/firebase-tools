5f77070150dcc22d68747aa4acba34dc1053dd5b
joehan <joehanley@google.com>
2024-11-15 10:50:36 -0800

    Revert "Move isVSCodeExtension func to avoid circular dependency (#7938)" (#7954)
    This reverts commit e7d679dc9af55627fbc178ee705dcc000d152757.

--- src/logger.ts
+++ src/logger.ts
@@ -1,7 +1,7 @@
 import * as winston from "winston";
 import * as Transport from "winston-transport";
 
-import { isVSCodeExtension } from "./vsCodeUtils";
+import { isVSCodeExtension } from "./utils";
 import { EventEmitter } from "events";
 
 /**
