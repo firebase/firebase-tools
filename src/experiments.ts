f11c7c9b0a61e48ceebb1a73ff61db2a718a48b6
Rosalyn Tan <rosalyntan@google.com>
2024-12-04 12:49:52 -0800

    Clean up the `fdccompatiblemode` experiment. (#8022)
    * Clean up the fdccompatiblemode experiment.
    
    * Clean up imports.

--- src/experiments.ts
+++ src/experiments.ts
@@ -137,13 +137,6 @@ export const ALL_EXPERIMENTS = experiments({
     default: true,
     public: false,
   },
-
-  fdccompatiblemode: {
-    shortDescription: "Enable Data Connect schema migrations in Compatible Mode",
-    fullDescription: "Enable Data Connect schema migrations in Compatible Mode",
-    default: true,
-    public: false,
-  },
 });
 
 export type ExperimentName = keyof typeof ALL_EXPERIMENTS;
