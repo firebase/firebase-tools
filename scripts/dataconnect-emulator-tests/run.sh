7256892555eee26278381321f918abf6b6791631
Joe Hanley <joehanley@google.com>
2024-12-06 15:05:05 -0800

    Maybe this?

--- scripts/dataconnect-emulator-tests/run.sh
+++ scripts/dataconnect-emulator-tests/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x # Immediately exit on failure
+set -ex # Immediately exit on failure
 # Globally link the CLI for the testing framework
 ./scripts/clean-install.sh
 source scripts/set-default-credentials.sh
@@ -10,5 +10,5 @@ echo "Running with npm: $(which npm)"
 echo "Running with Application Creds: ${GOOGLE_APPLICATION_CREDENTIALS}"
 
 cd scripts/dataconnect-emulator-tests
-firebase emulators:exec "echo" --only dataconnect -P demo-test --debug
+firebase emulators:exec "cd ." --only dataconnect -P demo-test
 # rm -rf ../../clean
