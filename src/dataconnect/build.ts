ce024237ea76ad48e0a63b3bc055db7d8ae5bc48
Rosalyn Tan <rosalyntan@google.com>
2024-12-17 12:14:57 -0500

    Min repro of options.interactive issue.

--- src/dataconnect/build.ts
+++ src/dataconnect/build.ts
@@ -1,11 +1,24 @@
 import { DataConnectEmulator } from "../emulator/dataconnectEmulator";
 import { Options } from "../options";
+import * as utils from "../utils";
 import { FirebaseError } from "../error";
 import { prettify } from "./graphqlError";
 import { DeploymentMetadata } from "./types";
 
 export async function build(options: Options, configDir: string): Promise<DeploymentMetadata> {
   const buildResult = await DataConnectEmulator.build({ configDir });
+
+  if (options.interactive) {
+    utils.logLabeledBullet("TEST", "is interactive");
+  } else {
+    utils.logLabeledBullet("TEST", "is not interactive");
+  }
+  if (options.nonInteractive) {
+    utils.logLabeledBullet("TEST", "is nonInteractive");
+  } else {
+    utils.logLabeledBullet("TEST", "is not nonInteractive");
+  }
+
   if (buildResult?.errors?.length) {
     throw new FirebaseError(
       `There are errors in your schema and connector files:\n${buildResult.errors.map(prettify).join("\n")}`,
