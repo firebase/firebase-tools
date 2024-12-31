6edab813014c965b833fc46235c509481228e9f4
Rosalyn Tan <rosalyntan@google.com>
2024-12-30 19:58:04 -0500

    Add unit test.

--- src/dataconnect/build.ts
+++ src/dataconnect/build.ts
@@ -5,7 +5,7 @@ import * as experiments from "../experiments";
 import { promptOnce } from "../prompt";
 import * as utils from "../utils";
 import { prettify, prettifyWithWorkaround } from "./graphqlError";
-import { DeploymentMetadata } from "./types";
+import { DeploymentMetadata, GraphqlError } from "./types";
 
 export async function build(
   options: Options,
@@ -24,62 +24,67 @@ export async function build(
   }
   const buildResult = await DataConnectEmulator.build(args);
   if (buildResult?.errors?.length) {
-    if (buildResult.errors.filter((w) => !w.extensions?.warningLevel).length) {
-      // Throw immediately if there are any build errors in the GraphQL schema or connectors.
+    await handleBuildErrors(buildResult.errors, options.nonInteractive, options.force, dryRun);
+  }
+  return buildResult?.metadata ?? {};
+}
+
+export async function handleBuildErrors(
+  errors: GraphqlError[],
+  nonInteractive: boolean,
+  force: boolean,
+  dryRun?: boolean,
+) {
+  if (errors.filter((w) => !w.extensions?.warningLevel).length) {
+    // Throw immediately if there are any build errors in the GraphQL schema or connectors.
+    throw new FirebaseError(
+      `There are errors in your schema and connector files:\n${errors.map(prettify).join("\n")}`,
+    );
+  }
+  const interactiveAcks = errors.filter((w) => w.extensions?.warningLevel === "INTERACTIVE_ACK");
+  const requiredAcks = errors.filter((w) => w.extensions?.warningLevel === "REQUIRE_ACK");
+  const choices = [
+    { name: "Acknowledge all changes and proceed", value: "proceed" },
+    { name: "Reject changes and abort", value: "abort" },
+  ];
+  if (requiredAcks.length) {
+    utils.logLabeledWarning(
+      "dataconnect",
+      `There are changes in your schema or connectors that may break your existing applications. These changes require explicit acknowledgement to proceed. You may either reject the changes and update your sources with the suggested workaround(s), if any, or acknowledge these changes and proceed with the deployment:\n` +
+        prettifyWithWorkaround(requiredAcks),
+    );
+    if (nonInteractive && !force) {
       throw new FirebaseError(
-        `There are errors in your schema and connector files:\n${buildResult.errors.map(prettify).join("\n")}`,
+        "Explicit acknowledgement required for breaking schema or connector changes. Rerun this command with --force to deploy these changes.",
       );
-    }
-    const interactiveAcks = buildResult.errors.filter(
-      (w) => w.extensions?.warningLevel === "INTERACTIVE_ACK",
-    );
-    const requiredAcks = buildResult.errors.filter(
-      (w) => w.extensions?.warningLevel === "REQUIRE_ACK",
-    );
-    const choices = [
-      { name: "Acknowledge all changes and proceed", value: "proceed" },
-      { name: "Reject changes and abort", value: "abort" },
-    ];
-    if (requiredAcks.length) {
-      utils.logLabeledWarning(
-        "dataconnect",
-        `There are changes in your schema or connectors that may break your existing applications. These changes require explicit acknowledgement to proceed. You may either reject the changes and update your sources with the suggested workaround(s), if any, or acknowledge these changes and proceed with the deployment:\n` +
-          prettifyWithWorkaround(requiredAcks),
-      );
-      if (options.nonInteractive && !options.force) {
-        throw new FirebaseError(
-          "Explicit acknowledgement required for breaking schema or connector changes. Rerun this command with --force to deploy these changes.",
-        );
-      } else if (!options.nonInteractive && !options.force && !dryRun) {
-        const result = await promptOnce({
-          message: "Would you like to proceed with these breaking changes?",
-          type: "list",
-          choices,
-          default: "abort",
-        });
-        if (result === "abort") {
-          throw new FirebaseError(`Deployment aborted.`);
-        }
-      }
-    }
-    if (interactiveAcks.length) {
-      utils.logLabeledWarning(
-        "dataconnect",
-        `There are changes in your schema or connectors that may cause unexpected behavior in your existing applications:\n` +
-          interactiveAcks.map(prettify).join("\n"),
-      );
-      if (!options.nonInteractive && !options.force && !dryRun) {
-        const result = await promptOnce({
-          message: "Would you like to proceed with these changes?",
-          type: "list",
-          choices,
-          default: "proceed",
-        });
-        if (result === "abort") {
-          throw new FirebaseError(`Deployment aborted.`);
-        }
+    } else if (!nonInteractive && !force && !dryRun) {
+      const result = await promptOnce({
+        message: "Would you like to proceed with these breaking changes?",
+        type: "list",
+        choices,
+        default: "abort",
+      });
+      if (result === "abort") {
+        throw new FirebaseError(`Deployment aborted.`);
       }
     }
   }
-  return buildResult?.metadata ?? {};
+  if (interactiveAcks.length) {
+    utils.logLabeledWarning(
+      "dataconnect",
+      `There are changes in your schema or connectors that may cause unexpected behavior in your existing applications:\n` +
+        interactiveAcks.map(prettify).join("\n"),
+    );
+    if (!nonInteractive && !force && !dryRun) {
+      const result = await promptOnce({
+        message: "Would you like to proceed with these changes?",
+        type: "list",
+        choices,
+        default: "proceed",
+      });
+      if (result === "abort") {
+        throw new FirebaseError(`Deployment aborted.`);
+      }
+    }
+  }
 }
