ba3fa9954591723f33c2c890217d8407db71fc0a
Rosalyn Tan <rosalyntan@google.com>
2025-01-03 12:26:44 -0800

    Implement FDC connector evolution warnings. (#8023)
    * Implement FDC connector evolution warnings.
    
    * Pass project ID to build command in the connector evolution experiment.
    
    * Some initial handling of connector evolution issues.
    
    * Adjust control flow with some placeholder TODOs.
    
    * Fix warningLevel structure and output log statements.
    
    * Fix up some log formatting and prompt for connector evolution warnings in interactive mode.
    
    * Better formatting of prompts/logs and better messaging.
    
    * Format issues and workarounds as a table.
    
    * Address some review comments.
    
    * Add unit test.
    
    * Fix test?
    
    * Fix test!

--- src/dataconnect/graphqlError.ts
+++ src/dataconnect/graphqlError.ts
@@ -1,4 +1,5 @@
 import { GraphqlError } from "./types";
+const Table = require("cli-table");
 
 export function prettify(err: GraphqlError): string {
   const message = err.message;
@@ -10,4 +11,26 @@ export function prettify(err: GraphqlError): string {
     }
   }
   return header.length ? `${header}: ${message}` : message;
+}
+
+export function prettifyWithWorkaround(errs: GraphqlError[]): string {
+  const table = new Table({
+    head: ["Issue", "Workaround", "Reason"],
+    style: { head: ["yellow"] },
+  });
+  for (const e of errs) {
+    if (!e.extensions?.workarounds?.length) {
+      table.push([prettify(e), "", ""]);
+    } else {
+      const workarounds = e.extensions.workarounds;
+      for (let i = 0; i < workarounds.length; i++) {
+        if (i === 0) {
+          table.push([prettify(e), workarounds[i].Description, workarounds[i].Reason]);
+        } else {
+          table.push(["", workarounds[i].Description, workarounds[i].Reason]);
+        }
+      }
+    }
+  }
+  return table.toString();
 }
