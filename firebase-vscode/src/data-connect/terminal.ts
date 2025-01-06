cc84090a44f8c3c867db4ad88cb087350f420917
joehan <joehanley@google.com>
2025-01-06 09:15:44 -0800

    Fixed trailing space

--- firebase-vscode/src/data-connect/terminal.ts
+++ firebase-vscode/src/data-connect/terminal.ts
@@ -65,7 +65,7 @@ export function runTerminalTask(
       vscode.TaskScope.Workspace,
       taskName,
       "firebase",
-      new vscode.ShellExecution(`${command} ${settings.debug ? "--debug" : ""}`, executionOptions),
+      new vscode.ShellExecution(`${command}${settings.debug ? " --debug" : ""}`, executionOptions),
     );
     task.presentationOptions = presentationOptions;
     await vscode.tasks.executeTask(task);
