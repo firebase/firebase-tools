60be45a17058192ae4451a1b68c19c0851d2f14a
Joanna Wang <annajowang@gmail.com>
2025-02-11 21:12:15 +0000

    Implement apphosting.backends.get command.

--- src/commands/apphosting-backends-get.ts
+++ src/commands/apphosting-backends-get.ts
@@ -8,25 +8,15 @@ import { printBackendsTable } from "./apphosting-backends-list";
 
 export const command = new Command("apphosting:backends:get <backend>")
   .description("print info about a Firebase App Hosting backend")
-  .option("-l, --location <location>", "backend location")
   .before(apphosting.ensureApiEnabled)
   .action(async (backend: string, options: Options) => {
     const projectId = needProjectId(options);
-    if (options.location !== undefined) {
-      logWarning("--location is being removed in the next major release.");
-    }
-    const location = (options.location as string) ?? "-";
 
     let backendsList: apphosting.Backend[] = [];
     try {
-      if (location !== "-") {
-        const backendInRegion = await apphosting.getBackend(projectId, location, backend);
-        backendsList.push(backendInRegion);
-      } else {
-        const resp = await apphosting.listBackends(projectId, "-");
-        const allBackends = resp.backends || [];
-        backendsList = allBackends.filter((bkd) => bkd.name.split("/").pop() === backend);
-      }
+      const resp = await apphosting.listBackends(projectId, "-");
+      const allBackends = resp.backends || [];
+      backendsList = allBackends.filter((bkd) => bkd.name.split("/").pop() === backend);
     } catch (err: unknown) {
       throw new FirebaseError(
         `Failed to get backend: ${backend}. Please check the parameters you have provided.`,
@@ -37,6 +27,13 @@ export const command = new Command("apphosting:backends:get <backend>")
       logWarning(`Backend "${backend}" not found`);
       return;
     }
-    printBackendsTable(backendsList);
+    if (backendsList.length > 1) {
+      logWarning(
+        `Detected multiple backends with the same ${backend} ID. This is not allowed until we can support more locations.\n` +
+          `Please delete and recreate any backends that share an ID with another backend. ` +
+          `Use apphosting:backends:list to see all backends.`,
+      );
+    }
+    printBackendsTable(backendsList.slice(0, 1));
     return backendsList[0];
   });
