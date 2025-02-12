f282665170eaf2598e6749f90289496dcac6e362
Rosalyn Tan <rosalyntan@google.com>
2025-02-11 16:11:58 -0800

    Update warning and prompt wording to reflect insecure operations as well as connector evolution issues.

--- src/dataconnect/build.ts
+++ src/dataconnect/build.ts
@@ -44,6 +44,7 @@ export async function handleBuildErrors(
 
   const requiredForces = errors.filter((w) => w.extensions?.warningLevel === "REQUIRE_FORCE");
   if (requiredForces.length && !force) {
+    // Only INACCESSIBLE issues fall in this category.
     utils.logLabeledError(
       "dataconnect",
       `There are changes in your schema or connectors that will result in broken behavior:\n` +
@@ -59,18 +60,19 @@ export async function handleBuildErrors(
     { name: "Reject changes and abort", value: "abort" },
   ];
   if (requiredAcks.length) {
+    // This category contains BREAKING, POTENTIALLY_BREAKING, and some INSECURE issues.
     utils.logLabeledWarning(
       "dataconnect",
-      `There are changes in your schema or connectors that may break your existing applications. These changes require explicit acknowledgement to proceed. You may either reject the changes and update your sources with the suggested workaround(s), if any, or acknowledge these changes and proceed with the deployment:\n` +
+      `There are changes in your schema or connectors that may break your existing applications or introduce security vulnerabilities. These changes require explicit acknowledgement to proceed. You may either reject the changes and update your sources with the suggested workaround(s), if any, or acknowledge these changes and proceed with the deployment:\n` +
         prettifyWithWorkaround(requiredAcks),
     );
     if (nonInteractive && !force) {
       throw new FirebaseError(
-        "Explicit acknowledgement required for breaking schema or connector changes. Rerun this command with --force to deploy these changes.",
+        "Explicit acknowledgement required for breaking schema or connector changes and new insecure operations. Rerun this command with --force to deploy these changes.",
       );
     } else if (!nonInteractive && !force && !dryRun) {
       const result = await promptOnce({
-        message: "Would you like to proceed with these breaking changes?",
+        message: "Would you like to proceed with these changes?",
         type: "list",
         choices,
         default: "abort",
@@ -81,9 +83,10 @@ export async function handleBuildErrors(
     }
   }
   if (interactiveAcks.length) {
+    // This category contains WARNING and some INSECURE issues.
     utils.logLabeledWarning(
       "dataconnect",
-      `There are changes in your schema or connectors that may cause unexpected behavior in your existing applications:\n` +
+      `There are existing insecure operations or changes in your schema or connectors that may cause unexpected behavior in your existing applications:\n` +
         prettifyWithWorkaround(interactiveAcks),
     );
     if (!nonInteractive && !force && !dryRun) {
