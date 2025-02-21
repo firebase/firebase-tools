62c5cd372945b7f2043f0da8bb0abbe8ab587573
ceifa <gabrinelson27@gmail.com>
2025-02-20 21:56:31 -0300

    fix queue id validation to match the error message guide

--- src/emulator/tasksEmulator.ts
+++ src/emulator/tasksEmulator.ts
@@ -186,12 +186,12 @@ export class TasksEmulator implements EmulatorInstance {
     if (typeof queueId !== "string") {
       return false;
     }
-
-    if (queueId.length > 100) {
+  
+    if (queueId.length < 1 || queueId.length > 62) {
       return false;
     }
-
-    const regex = /^[A-Za-z0-9-]+$/;
+  
+    const regex = /^[A-Za-z][A-Za-z0-9_-]*[A-Za-z0-9]$/;
     return regex.test(queueId);
   }
   createHubServer(): express.Application {
