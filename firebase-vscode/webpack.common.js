b9d895107081f8f5be2a54cc1dfae63510824617
Joe Hanley <joehanley@google.com>
2025-02-25 14:29:36 -0800

    Make it a string instead;

--- firebase-vscode/webpack.common.js
+++ firebase-vscode/webpack.common.js
@@ -207,7 +207,7 @@ const extensionConfig = {
         // },
       ],
     }),
-    new webpack.DefinePlugin({'process.env.IS_FIREBASE_VSCE': true}),
+    new webpack.DefinePlugin({'process.env.IS_FIREBASE_VSCE': '"true"'}),
   ],
   infrastructureLogging: {
     level: "log", // enables logging required for problem matchers
