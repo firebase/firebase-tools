ff109853b13e7683d6aee7dfc7e366517a8caf50
Joe Hanley <joehanley@google.com>
2025-02-25 15:57:04 -0800

    Need to actually access process.env for this plugin to work

--- src/vsCodeUtils.ts
+++ src/vsCodeUtils.ts
@@ -1,8 +1,6 @@
-import { env } from "process";
-
 /**
  * Detect if code is running in a VSCode Extension
  */
 export function isVSCodeExtension(): boolean {
-  return !!env.IS_FIREBASE_VSCE;
+  return !!process.env.IS_FIREBASE_VSCE;
 }
