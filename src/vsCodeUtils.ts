a0d8f7f224a31c86b8d420abef3783603e3a6300
Joe Hanley <joehanley@google.com>
2025-02-26 11:08:47 -0800

    Refactor to not use a env var

--- src/vsCodeUtils.ts
+++ src/vsCodeUtils.ts
@@ -1,6 +1,10 @@
+// @eslint-disable @typescript-eslint/no-namespace
+declare namespace globalThis {
+  let WEBPACK_IS_VSCE: string | undefined;
+}
 /**
  * Detect if code is running in a VSCode Extension
  */
 export function isVSCodeExtension(): boolean {
-  return !!process.env.IS_FIREBASE_VSCE;
+  return !!globalThis.WEBPACK_IS_VSCE;
 }
