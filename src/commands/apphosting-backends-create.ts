75cca8e7613b16cbfefe7196c39f6e3dc3f2c589
Daniel Lee <danielylee@google.com>
2025-03-24 13:04:47 -0700

    Merge next to master (#8360)
    * Update "location" => "primary region" in the backend display table. (#8205)
    
    * Remove --location from apphosting:backend:list. (#8272)
    
    * Remove --location from apphosting:rollouts:create and error when more than one backend is found. (#8271)
    
    * Remove --location from apphosting:backends:get and return the first backend if multiple are found. (#8260)
    
    * Remove `--location` from apphosting:backends:delete (#8262)
    
    * Remove --location from apphosting:backends:create and prompt for primary region. (#8264)
    
    * Enable FDC connector evolution and insecure operations linter. (#8281)
    
    * Enable FDC connector evolution and insecure operations linter.
    
    * Enable in VSCode.
    
    * Actually get VSCode working.
    
    * Replace pkg with yao-pkg/pkg (#8328)
    
    Co-authored-by: aalej <alejandromarco@google.com>
    
    * List regions of duplicate backend in warning/error message (#8285)
    
    * Update dataconnect.yaml template to v1. (#8346)
    
    * Remove support for node 18 (#8334)
    
    ---------
    
    Co-authored-by: annajowang <31288696+annajowang@users.noreply.github.com>
    Co-authored-by: Joe Hanley <joehanley@google.com>
    Co-authored-by: Rosalyn Tan <rosalyntan@google.com>
    Co-authored-by: aalej <alejandromarco@google.com>

--- src/commands/apphosting-backends-create.ts
+++ src/commands/apphosting-backends-create.ts
@@ -6,7 +6,6 @@ import { doSetup } from "../apphosting/backend";
 import { ensureApiEnabled } from "../gcp/apphosting";
 import { APPHOSTING_TOS_ID } from "../gcp/firedata";
 import { requireTosAcceptance } from "../requireTosAcceptance";
-import { logWarning } from "../utils";
 
 export const command = new Command("apphosting:backends:create")
   .description("create a Firebase App Hosting backend")
@@ -14,7 +13,6 @@ export const command = new Command("apphosting:backends:create")
     "-a, --app <webAppId>",
     "specify an existing Firebase web app's ID to associate your App Hosting backend with",
   )
-  .option("-l, --location <location>", "specify the location of the backend")
   .option(
     "-s, --service-account <serviceAccount>",
     "specify the service account used to run the server",
@@ -24,21 +22,9 @@ export const command = new Command("apphosting:backends:create")
   .before(requireInteractive)
   .before(requireTosAcceptance(APPHOSTING_TOS_ID))
   .action(async (options: Options) => {
-    if (options.location !== undefined) {
-      logWarning(
-        "--location is being removed in the next major release. " +
-          "The CLI will prompt for a Primary Region where appropriate.",
-      );
-    }
     const projectId = needProjectId(options);
     const webAppId = options.app;
-    const location = options.location as string;
     const serviceAccount = options.serviceAccount;
 
-    await doSetup(
-      projectId,
-      webAppId as string | null,
-      location as string | null,
-      serviceAccount as string | null,
-    );
+    await doSetup(projectId, webAppId as string | null, serviceAccount as string | null);
   });
