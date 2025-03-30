cff7d1f47a1dc7ded8f5c8ebe06048a6af66e810
Thomas Bouldin <inlined@google.com>
2025-03-29 20:03:04 -0700

    Start creating new prompt library and move apphosting/config to it

--- src/apphosting/config.ts
+++ src/apphosting/config.ts
@@ -5,7 +5,7 @@ import * as clc from "colorette";
 
 import * as fs from "../fsutils";
 import { NodeType } from "yaml/dist/nodes/Node";
-import * as prompt from "../prompt";
+import * as prompt from "../promptV2";
 import * as dialogs from "./secrets/dialogs";
 import { AppHostingYamlConfig, EnvMap, toEnvList } from "./yaml";
 import { logger } from "../logger";
@@ -194,7 +194,7 @@ export async function maybeAddSecretToYaml(
     return;
   }
   if (!path) {
-    path = await prompt.promptOnce({
+    path = await prompt.input({
       message: `It looks like you don't have an ${fileName} yet. Where would you like to store it?`,
       default: process.cwd(),
     });
@@ -289,12 +289,9 @@ export async function overrideChosenEnv(
     return {};
   }
 
-  const toOverwrite = await prompt.promptOnce({
-    type: "checkbox",
+  const toOverwrite = await prompt.checkbox({
     message: "Which environment variables would you like to override?",
-    choices: names.map((name) => {
-      return { name };
-    }),
+    choices: names,
   });
 
   if (!projectId && toOverwrite.some((name) => "secret" in env[name])) {
@@ -306,8 +303,7 @@ export async function overrideChosenEnv(
   const newEnv: Record<string, Env> = {};
   for (const name of toOverwrite) {
     if ("value" in env[name]) {
-      const newValue = await prompt.promptOnce({
-        type: "input",
+      const newValue = await prompt.input({
         message: `What new value would you like for plaintext ${name}?`,
       });
       newEnv[name] = { variable: name, value: newValue };
@@ -317,15 +313,13 @@ export async function overrideChosenEnv(
     let secretRef: string;
     let action: "reuse" | "create" | "pick-new" = "pick-new";
     while (action === "pick-new") {
-      secretRef = await prompt.promptOnce({
-        type: "input",
+      secretRef = await prompt.input({
         message: `What would you like to name the secret reference for ${name}?`,
         default: suggestedTestKeyName(name),
       });
 
       if (await csm.secretExists(projectId!, secretRef)) {
-        action = await prompt.promptOnce({
-          type: "list",
+        action = await prompt.select<"reuse" | "pick-new">({
           message:
             "This secret reference already exists, would you like to reuse it or create a new one?",
           choices: [
@@ -343,8 +337,7 @@ export async function overrideChosenEnv(
       continue;
     }
 
-    const secretValue = await prompt.promptOnce({
-      type: "password",
+    const secretValue = await prompt.password({
       message: `What new value would you like for secret ${name} [input is hidden]?`,
     });
     // TODO: Do we need to support overriding locations? Inferring them from the original?
---
+++ src/promptV2.ts
@@ -0,0 +1,165 @@
+import * as inquirer from "@inquirer/prompts";
+import { FirebaseError } from "./error";
+
+// TOOD: Consider whether the magic of passing a name and an options object which may include
+// options[name] and would otherwise store that value is too magical and instead shoudl be
+// pushed on the call site.
+
+export interface BasicOptions<T> {
+    message: string
+    default?: T;
+    force?: boolean;
+    nonInteractive?: boolean;
+}
+
+export function guard<T>(opts: BasicOptions<T>): { shouldReturn: true, value: T } | { shouldReturn: false, value: undefined } {
+    if (!opts.nonInteractive) {
+        return { shouldReturn: false, value: undefined };
+    }
+    if (typeof opts.default !== "undefined") {
+        return { shouldReturn: true, value: opts.default};
+    }
+    throw new FirebaseError(`Question "${opts.message}" does not have a default and cannot be answered in non-interactive mode`);
+}
+
+/**
+ * Options for the Input function.
+ * 
+ * Exported because Inqurier does not export its own input configs anymore. Some
+ * unused options are missing, such as theme.
+ */
+export type InputConfig = BasicOptions<string> & {
+    transformer?: (value: string, { isFinal }: {
+        isFinal: boolean;
+    }) => string;
+    validate?: (value: string) => boolean | string | Promise<string | boolean>;
+}
+
+/**
+ * Prompt for a string input.
+ */
+export async function input(opts: InputConfig): Promise<string> {
+    return inquirer.input(opts);
+}
+
+/**
+ * Options for the confirm function.
+ * 
+ * Epxorted because Inquirer does not export its own input configs anymore. Some unused
+ * options are missing, such as theme.
+ */
+export type ConfirmConfig = BasicOptions<boolean> & {
+    transformer?: (value: boolean) => string;
+}
+
+/**
+ * Prompt a user to confirm a selection
+ * Will abort if nonInteractive and not force
+ */
+export async function confirm(opts: ConfirmConfig): Promise<boolean> {
+    if (opts.force) {
+        // TODO: Should we print what we've forced?
+        return true;
+    }
+    const { shouldReturn, value } = guard(opts);
+    if (shouldReturn) {
+        return value;
+    }
+    return inquirer.confirm(opts);
+}
+
+/**
+ * A choice in a checkbox prompt.
+ * Strongly typed to allow enum propagation
+ */
+export type Choice<Value> = {
+    value: Value;
+    name?: string;
+    description?: string;
+    short?: string;
+    disabled?: boolean | string;
+    checked?: boolean;
+    type?: never;
+};
+
+// Personal hack deviating from inquirer to allow string values to propagate
+// as strings or enum arrays without needing an explicit type at the call stie.
+type MaybeLiteral<Value> = Value extends string ? Value : never;
+
+/**
+ * Options for the checkbox function.
+ * 
+ * Epxorted because Inquirer does not export its own input configs anymore. Some unused
+ * options are missing, such as theme. Some options are missing to promote consistency
+ * within the CLI.
+ */
+export type CheckboxOptions<Value> = BasicOptions<Value[]> & {
+    message: string;
+    choices: readonly (MaybeLiteral<Value> | inquirer.Separator)[] | readonly (inquirer.Separator | Choice<Value>)[];
+    validate?: ((choices: readonly Choice<Value>[]) => boolean | string | Promise<string | boolean>) | undefined;
+}
+
+/**
+ * Prompt a user for one or more of many options.
+ * Can accept a generic type for enum values.
+ */
+export async function checkbox<Value>(opts: CheckboxOptions<Value>): Promise<Value[]> {
+    const { shouldReturn, value } = guard(opts);
+    if (shouldReturn) {
+        return value;
+    }
+    return inquirer.checkbox({
+        ...opts,
+        loop: true,
+    });
+}
+
+/**
+ * Options for the checkbox function.
+ * 
+ * Epxorted because Inquirer does not export its own input configs anymore. Some unused
+ * options are missing, such as theme. Some options are missing to promote consistency
+ * within the CLI.
+ * TODO: Had difficulty coalescing literals using Choice<Value>[]. Look into it.
+ */
+export type SelectOptions<Value> = BasicOptions<Value> & {
+    choices: readonly (MaybeLiteral<Value> | inquirer.Separator)[] | readonly (inquirer.Separator | Choice<Value>)[];
+}
+
+/**
+ * Prompt a user to make a choice amongst a list.
+ */
+export async function select<Value>(opts: SelectOptions<Value>): Promise<Value> {
+    const { shouldReturn, value } = guard(opts);
+    if (shouldReturn) {
+        return value;
+    }
+    return inquirer.select({
+        ...opts,
+        loop: false,
+    });
+}
+
+/**
+ * Options for the checkbox function.
+ * 
+ * Epxorted because Inquirer does not export its own input configs anymore. Some unused
+ * options are missing, such as theme. Some options are missing to promote consistency
+ * within the CLI.
+ */
+type PasswordOptions = Omit<BasicOptions<string>, "default"> & {
+    validate?: (value: string) => boolean | string | Promise<string | boolean>;
+};
+
+/**
+ * 
+ */
+export async function password(opts: PasswordOptions): Promise<string> {
+    // Note, without default can basically only throw
+    guard(opts);
+    return inquirer.password({
+        ...opts,
+        mask: "",
+    });
+
+}
