b2666bfb2f49613a1da797a5eda1fef5b19f8c90
Joe Hanley <joehanley@google.com>
2025-04-15 13:58:54 -0700

    Adding some clarifying language for free trial/paid instances

--- src/dataconnect/freeTrial.ts
+++ src/dataconnect/freeTrial.ts
@@ -12,6 +12,7 @@ const FREE_TRIAL_METRIC = "sqladmin.googleapis.com/fdc_lifetime_free_trial_per_p
 
 // Checks whether there is already a free trial instance on a project.
 export async function checkFreeTrialInstanceUsed(projectId: string): Promise<boolean> {
+  utils.logLabeledBullet("dataconnect", "Checking Cloud SQL no cost trial eligibility...");
   const past7d = new Date();
   past7d.setDate(past7d.getDate() - 7);
   const query: CmQuery = {
@@ -21,12 +22,17 @@ export async function checkFreeTrialInstanceUsed(projectId: string): Promise<boo
   };
   try {
     const ts = await queryTimeSeries(query, projectId);
+    let used = true
     if (ts.length) {
-      return ts[0].points.some((p) => p.value.int64Value);
+      used = ts[0].points.some((p) => p.value.int64Value);
     }
-    return true;
+    if (used) {
+      utils.logLabeledWarning("dataconnect", "CloudSQL no cost trial has already been used on this project.");
+    }
+    return used;
   } catch (err: any) {
     // If the metric doesn't exist, free trial is not used.
+    utils.logLabeledSuccess("dataconnect", "CloudSQL no cost trial available!")
     return false;
   }
 }
