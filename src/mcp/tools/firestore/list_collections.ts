618ab56b5fb010868ab368e0884cebd0d253b54c
Michael Bleigh <bleigh@google.com>
2025-04-30 11:16:29 -0700

    [MCP] Adds Firestore tools to MCP server.
    - list_collections
    - get_documents
    - get_rules

---
+++ src/mcp/tools/firestore/list_collections.ts
@@ -0,0 +1,36 @@
+import { z } from "zod";
+import { tool } from "../../tool.js";
+import { mcpError, toContent } from "../../util.js";
+import { listCollectionIds } from "../../../gcp/firestore.js";
+import { NO_PROJECT_ERROR } from "../../errors.js";
+
+export const list_collections = tool(
+  {
+    name: "list_collections",
+    description:
+      "Retrieves a list of collections from a Firestore database in the current project.",
+    inputSchema: z.object({
+      database: z
+        .string()
+        .nullish()
+        .describe("Database id to use. Defaults to `(default)` if unspecified."),
+    }),
+    annotations: {
+      title: "List Firestore collections",
+      readOnlyHint: true,
+    },
+  },
+  async ({ database }, { projectId }) => {
+    database ??= "(default)";
+    if (!projectId) return NO_PROJECT_ERROR;
+    try {
+      return toContent(await listCollectionIds(projectId));
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        return mcpError(err.message);
+      } else {
+        return mcpError("unknown error");
+      }
+    }
+  },
+);
--- src/mcp/tools/index.ts
+++ src/mcp/tools/index.ts
@@ -1,11 +1,12 @@
 import { ServerTool } from "../tool.js";
 import { ServerFeature } from "../types.js";
 import { authTools } from "./auth/index.js";
+import { firestoreTools } from "./firestore/index.js";
 import { projectTools } from "./project/index.js";
 
 export const tools: Record<ServerFeature, ServerTool[]> = {
   project: projectTools,
-  firestore: [],
+  firestore: firestoreTools,
   auth: authTools,
   dataconnect: [],
   storage: [],
