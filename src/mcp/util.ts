368e9c9361b48fe808adb6139b0897d4aa21acf3
Fred Zhang <fredzqm@google.com>
2025-04-30 18:21:43 -0700

    add get_dataconnect_schema and get_dataconnect_connector

--- src/mcp/util.ts
+++ src/mcp/util.ts
@@ -6,15 +6,17 @@ import { platform } from "os";
 export function toContent(data: any, options?: { format: "json" | "yaml" }): CallToolResult {
   if (typeof data === "string") return { content: [{ type: "text", text: data }] };
 
-  let text: string = "";
-  const format = options?.format || "yaml"; // use YAML because it's a little more prose-like for the LLM to parse
-  switch (format) {
-    case "json":
-      text = JSON.stringify(data);
-      break;
-    case "yaml":
-      text = dump(data);
-      break;
+  let text: string = "null";
+  if (data) {
+    const format = options?.format || "yaml"; // use YAML because it's a little more prose-like for the LLM to parse
+    switch (format) {
+      case "json":
+        text = JSON.stringify(data);
+        break;
+      case "yaml":
+        text = dump(data);
+        break;
+    }
   }
   return {
     content: [{ type: "text", text }],
