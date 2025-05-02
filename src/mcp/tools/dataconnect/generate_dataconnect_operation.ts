f280607813b939e643976eea1a7a544db28480b3
Alexander Nohe <nohe@google.com>
2025-05-02 08:15:57 -0700

    @fredzqm - assist for loading the local projects serviceName

--- src/mcp/tools/dataconnect/generate_dataconnect_operation.ts
+++ src/mcp/tools/dataconnect/generate_dataconnect_operation.ts
@@ -3,12 +3,13 @@ import { tool } from "../../tool.js";
 import { toContent } from "../../util.js";
 import { generateOperation } from "../../../gif/fdcExperience.js";
 import { Config } from "../../../config.js";
-import { readFirebaseJson } from "../../../dataconnect/fileUtils.js";
+import { pickService } from "../../../dataconnect/fileUtils.js";
 
 export const generate_dataconnect_operation = tool(
   {
     name: "generate_dataconnect_operation",
-    description: "Generates a single Firebase Data Connect query or mutation based on the currently deployed schema and the provided prompt.",
+    description:
+      "Generates a single Firebase Data Connect query or mutation based on the currently deployed schema and the provided prompt.",
     inputSchema: z.object({
       // Lifted guidance from : https://cloud.google.com/gemini/docs/discover/write-prompts
       prompt: z
@@ -35,28 +36,12 @@ export const generate_dataconnect_operation = tool(
     },
   },
   async ({ prompt, service }, { projectId, host }) => {
-    if (!service) {
-      const projectRoot = host.projectRoot;
-      if (!projectRoot) {
-        throw new Error("No project directory supplied. A service must be supplied instead.");
-      }
-      const config = new Config(`${projectRoot}/firebase.json`);
-      const dataConnect = readFirebaseJson(config);
-      console.error(dataConnect, projectRoot, config);
-      if (dataConnect.length === 0) {
-        throw new Error("no data connect sources detected in firebase.json");
-      }
-      if (dataConnect.length !== 1) {
-        const sources = dataConnect.map((dcs) => {
-          return dcs.source;
-        });
-        throw new Error(
-          `muliple services detected in firebase.json. Please select one service from : ${sources.join(",")}`,
-        );
-      }
-      service = dataConnect[0].source;
-    }
-    const schema = await generateOperation(prompt, service, projectId!);
+    const serviceInfo = await pickService(
+      projectId!,
+      Config.load({ cwd: host.projectRoot })!,
+      service || undefined,
+    );
+    const schema = await generateOperation(prompt, serviceInfo.serviceName, projectId!);
     return toContent(schema);
   },
 );
