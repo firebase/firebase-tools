e7e084e3f166d2828df5cd8068d4f0feee5dd0f7
Alexander Nohe <nohe@google.com>
2025-05-08 13:07:13 -0700

    [MCP] getDownloadUrl - Firebase Storage

---
+++ src/mcp/tools/storage/get_download_url.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+import { tool } from "../../tool.js";
+import { toContent } from "../../util.js";
+import { getDownloadUrl } from "../../../gcp/storage.js";
+
+export const get_download_url = tool(
+  {
+    name: "get_download_url",
+    description: "Retrieves the download URL for an object in Firebase Storage.",
+    inputSchema: z.object({
+      bucket: z.string().describe("The bucket name in Firebase Storage."),
+      objectPath: z
+        .string()
+        .describe("The path to the object in Firebase storage without the bucket name attached"),
+    }),
+    annotations: {
+      title: "Get the download url for an obejct in Firebase Storage.",
+      readOnlyHint: true,
+    },
+    _meta: {
+      requiresProject: true,
+      requiresAuth: true,
+    },
+  },
+  async ({ bucket, objectPath }) => {
+    const downloadUrl = await getDownloadUrl(bucket, objectPath);
+    return toContent(downloadUrl);
+  },
+);
