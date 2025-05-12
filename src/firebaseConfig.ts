0a949db05845b71253a7458b814bf7d5aeec6774
Daniel Young Lee <danielylee@google.com>
2025-05-12 16:43:22 -0700

    Pin runtimes on init for functions.

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -7,7 +7,7 @@
 
 import type { HttpsOptions } from "firebase-functions/v2/https";
 import { IngressSetting, MemoryOption, VpcEgressSetting } from "firebase-functions/v2/options";
-import { Runtime, DecommissionedRuntime } from "./deploy/functions/runtimes/supported/types";
+import { ActiveRuntime } from "./deploy/functions/runtimes/supported/types";
 
 /**
  * Creates a type that requires at least one key to be present in an interface
@@ -167,7 +167,7 @@ export type FirestoreConfig = FirestoreSingle | FirestoreMultiple;
 export type FunctionConfig = {
   source?: string;
   ignore?: string[];
-  runtime?: Exclude<Runtime, DecommissionedRuntime>;
+  runtime?: ActiveRuntime;
   codebase?: string;
 } & Deployable;
 
