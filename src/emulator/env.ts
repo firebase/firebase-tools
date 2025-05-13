016ddc621bce872124ea2695216162a8f2cc37f4
Fred Zhang <fredzqm@google.com>
2025-05-13 11:18:47 -0700

    fix

--- src/emulator/env.ts
+++ src/emulator/env.ts
@@ -70,7 +70,14 @@ export async function getCredentialsEnvironment(
 ): Promise<Record<string, string>> {
   // Provide default application credentials when appropriate
   const credentialEnv: Record<string, string> = {};
-  if (account) {
+  if (await hasDefaultCredentials()) {
+    !silent &&
+      logger.logLabeled(
+        "WARN",
+        logLabel,
+        `Application Default Credentials detected. Non-emulated services will access production using these credentials. Be careful!`,
+      );
+  } else if (account) {
     const defaultCredPath = await getCredentialPathAsync(account);
     if (defaultCredPath) {
       logger.log("DEBUG", `Setting GAC to ${defaultCredPath}`);
