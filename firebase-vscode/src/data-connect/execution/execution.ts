b1ae82c178ba51a3ec03b8518f7b8bc0e96f65c7
Harold Shen <hlshen@google.com>
2025-05-15 19:38:25 -0400

    fix async

--- firebase-vscode/src/data-connect/execution/execution.ts
+++ firebase-vscode/src/data-connect/execution/execution.ts
@@ -327,7 +327,7 @@ export function registerExecution(
     executionHistoryTreeView,
     vscode.commands.registerCommand(
       "firebase.dataConnect.executeOperation",
-      (ast, location, instanceType: InstanceType) => {
+      async (ast, location, instanceType: InstanceType) => {
         analyticsLogger.logger.logUsage(
           instanceType === InstanceType.LOCAL
             ? DATA_CONNECT_EVENT_NAME.RUN_LOCAL
