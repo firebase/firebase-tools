eb25a1f5ff41fd1c1e9d319f404499408aa9c214
Viswanathan Munisamy <visumickey@gmail.com>
2025-05-15 13:03:15 -0700

    Fix linters.

--- src/crashlytics/listTopIssues.ts
+++ src/crashlytics/listTopIssues.ts
@@ -20,14 +20,14 @@ export async function listTopIssues(
     queryParams.set("page_size", `${issueCount}`);
 
     const requestProjectId = parseProjectId(appId);
-    if (requestProjectId == undefined) {
-      throw new FirebaseError("Unable to get the projectId from the AppId.")
+    if (requestProjectId === undefined) {
+      throw new FirebaseError("Unable to get the projectId from the AppId.");
     }
 
     const response = await apiClient.request<void, string>({
       method: "GET",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       path: `/projects/${requestProjectId}/apps/${appId}/reports/topIssues`,
       queryParams: queryParams,
@@ -46,8 +46,8 @@ export async function listTopIssues(
 
 function parseProjectId(appId: string): string | undefined {
   const appIdParts = appId.split(":");
-    if (appIdParts.length > 1) {
-      return appIdParts[1];
-    }
+  if (appIdParts.length > 1) {
+    return appIdParts[1];
+  }
   return undefined;
 }
