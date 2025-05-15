3d65857e31fd07bc4d5b1deb9cab02db3c18852e
Harold Shen <hlshen@google.com>
2025-05-15 19:30:45 -0400

    Update firebase-vscode/src/data-connect/execution/execution.ts
    Co-authored-by: Yuchen Shi <yuchenshi@google.com>

--- firebase-vscode/src/data-connect/execution/execution.ts
+++ firebase-vscode/src/data-connect/execution/execution.ts
@@ -333,7 +333,7 @@ export function registerExecution(
             ? DATA_CONNECT_EVENT_NAME.RUN_LOCAL
             : DATA_CONNECT_EVENT_NAME.RUN_PROD,
         );
-        vscode.window.activeTextEditor?.document.save();
+        await vscode.window.activeTextEditor?.document.save();
         executeOperation(ast, location, instanceType);
       },
     ),
