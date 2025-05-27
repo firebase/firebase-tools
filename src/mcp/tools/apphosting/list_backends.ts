39a26ac87576d8a976bd5949d5fd31fa8495dc0d
Bryan Kendall <bkend@google.com>
2025-05-27 12:45:42 -0700

    adds domains logic to the App Hosting tool
    this includes additional tooling to actually fetch domains

--- src/mcp/tools/apphosting/list_backends.ts
+++ src/mcp/tools/apphosting/list_backends.ts
@@ -5,6 +5,7 @@ import {
   Backend,
   getTraffic,
   listBackends,
+  listDomains,
   parseBackendName,
   Traffic,
 } from "../../../gcp/apphosting.js";
@@ -16,7 +17,11 @@ export const list_backends = tool(
       "Retrieves a list of App Hosting backends in the current project. An empty list means that there are no backends. " +
       "The `uri` is the public URL of the backend. " +
       "A working backend will have a `managed_resources` array that will contain a `run_service` entry. That `run_service.service` " +
-      "is the resource name of the Cloud Run service serving the App Hosting backend. The last segment of that name is the service ID.",
+      "is the resource name of the Cloud Run service serving the App Hosting backend. The last segment of that name is the service ID. " +
+      "`domains` is the list of domains that are associated with the backend. They either have type `CUSTOM` or `DEFAULT`. " +
+      "  Every backend should have a `DEFAULT` domain. " +
+      "  The actual domain that a user would use to conenct to the backend is the last parameter of the domain resource name. " +
+      "  If a custom domain is correctly set up, it will have statuses ending in `ACTIVE`.",
     inputSchema: z.object({
       location: z
         .string()
@@ -38,12 +43,15 @@ export const list_backends = tool(
   async ({ location } = {}, { projectId }) => {
     projectId = projectId || "";
     if (!location) location = "-";
-    const data: (Backend & { traffic: Traffic })[] = [];
+    const data: (Backend & { traffic: Traffic; domains: unknown })[] = [];
     const backends = await listBackends(projectId, location);
     for (const backend of backends.backends) {
       const { location, id } = parseBackendName(backend.name);
-      const traffic = await getTraffic(projectId, location, id);
-      data.push({ ...backend, traffic: traffic });
+      const [traffic, domains] = await Promise.all([
+        getTraffic(projectId, location, id),
+        listDomains(projectId, location, id),
+      ]);
+      data.push({ ...backend, traffic: traffic, domains: domains });
     }
     if (!data.length) {
       return toContent(
