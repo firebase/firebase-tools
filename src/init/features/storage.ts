99b9ecb95ef5a79422b7440e85976863eee52c0a
Fred Zhang <fredzqm@google.com>
2025-05-28 16:18:37 -0700

    [MCP] firebase_init support storage (#8660)

--- src/init/features/storage.ts
+++ src/init/features/storage.ts
@@ -3,22 +3,54 @@ import * as clc from "colorette";
 import { logger } from "../../logger";
 import { input } from "../../prompt";
 import { readTemplateSync } from "../../templates";
+import { Config } from "../../config";
+import { Setup } from "..";
+import { FirebaseError } from "../../error";
+
+export interface RequiredInfo {
+  rulesFilename: string;
+  rules: string;
+  writeRules: boolean;
+}
 
 const RULES_TEMPLATE = readTemplateSync("init/storage/storage.rules");
+const DEFAULT_RULES_FILE = "storage.rules";
 
-export async function doSetup(setup: any, config: any): Promise<void> {
-  setup.config.storage = {};
-
+export async function askQuestions(setup: Setup, config: Config): Promise<void> {
   logger.info();
   logger.info("Firebase Storage Security Rules allow you to define how and when to allow");
   logger.info("uploads and downloads. You can keep these rules in your project directory");
   logger.info("and publish them with " + clc.bold("firebase deploy") + ".");
   logger.info();
 
-  const storageRulesFile = await input({
+  const info: RequiredInfo = {
+    rulesFilename: DEFAULT_RULES_FILE,
+    rules: RULES_TEMPLATE,
+    writeRules: true,
+  };
+  info.rulesFilename = await input({
     message: "What file should be used for Storage Rules?",
-    default: "storage.rules",
+    default: DEFAULT_RULES_FILE,
   });
-  setup.config.storage.rules = storageRulesFile;
-  await config.askWriteProjectFile(setup.config.storage.rules, RULES_TEMPLATE);
+  info.writeRules = await config.confirmWriteProjectFile(info.rulesFilename, info.rules);
+  // Populate featureInfo for the actuate step later.
+  setup.featureInfo = setup.featureInfo || {};
+  setup.featureInfo.storage = info;
+}
+
+export async function actuate(setup: Setup, config: Config): Promise<void> {
+  const info = setup.featureInfo?.storage;
+  if (!info) {
+    throw new FirebaseError("storage featureInfo is not found");
+  }
+  // Populate defaults and update `firebase.json` config.
+  info.rules = info.rules || RULES_TEMPLATE;
+  info.rulesFilename = info.rulesFilename || DEFAULT_RULES_FILE;
+  setup.config.storage = {
+    rules: info.rulesFilename,
+  };
+
+  if (info.writeRules) {
+    config.writeProjectFile(info.rulesFilename, info.rules);
+  }
 }
