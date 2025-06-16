9a979961908cad252f7cb9435c411e79cf327b83
Kevin Elko <kjelko@gmail.com>
2025-06-16 11:59:18 -0400

    wip changes to call new api

--- src/apptesting/types.ts
+++ src/apptesting/types.ts
@@ -11,8 +11,60 @@ export interface TestDef {
   testConfig?: TestConfig;
 }
 
-export type Browser = "CHROME";
+export enum Browser {
+  BROWSER_UNSPECIFIED,
+  CHROME
+}
 
 export interface TestConfig {
   browsers?: Browser[];
+}
+
+export interface InvokedTestCases {
+  testInvocation: {
+    name?: string;
+  }
+  testCaseInvocations: TestCaseInvocation[]
+}
+
+export interface TestCaseInvocation {
+  name?: string;
+  testCase: TestCase;
+  testExecution:  TestExecution[];
+}
+
+export interface TestExecution {
+  config: ExecutionConfig;
+  result?: TestExecutionResult;
+}
+
+export interface ExecutionConfig {
+  browser: Browser
+}
+
+export enum CompletionReason {
+  COMPLETION_REASON_UNSPECIFIED,
+  MAX_STEPS_REACHED,
+  GOAL_FAILED,
+  NO_ACTIONS_REQUIRED,
+  GOAL_INCONCLUSIVE,
+  TEST_CANCELLED,
+  GOAL_SUCCEEDED
+}
+
+export interface TestExecutionResult {
+  completionReason: CompletionReason
+}
+
+export interface TestCase {
+  startUri: string;
+  instructions: AiInstructions;
+}
+
+export interface AiInstructions {
+  steps: TestStep[];
+}
+
+export interface InvokeTestCasesRequest {
+  resource: InvokedTestCases
 }
