3255866b0bbf034513da683a7bdb8933c9da74de
Thomas Bouldin <inlined@google.com>
2025-06-18 12:49:19 -0700

    80 character column limit

--- templates/init/functions/typescript/index.ts
+++ templates/init/functions/typescript/index.ts
@@ -16,13 +16,14 @@ import * as logger from "firebase-functions/logger";
 
 // For cost control, you can set the maximum number of containers that can be
 // running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a per-function
-// limit. You can override the limit for each function using the `maxInstances` option
-// in the function's options, e.g. `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1 functions
-// should each use functions.runWith({ maxInstances: 10 }) instead. In the v1 API, each
-// function can only serve one request per container, so this will be the maximum concurrent
-// request count.
+// traffic spikes by instead downgrading performance. This limit is a
+// per-function limit. You can override the limit for each function using the
+// `maxInstances` option in the function's options, e.g.
+// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
+// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
+// functions should each use functions.runWith({ maxInstances: 10 }) instead.
+// In the v1 API, each function can only serve one request per container, so
+// this will be the maximum concurrent request count.
 setGlobalOptions({ maxInstances: 10 });
 
 // export const helloWorld = onRequest((request, response) => {
