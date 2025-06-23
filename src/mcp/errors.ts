f36e5630d9e09ab592d50e2c56cae1d24745fbb0
Fred Zhang <fredzqm@google.com>
2025-06-23 10:51:26 -0700

    feedbacks

--- src/mcp/errors.ts
+++ src/mcp/errors.ts
@@ -1,3 +1,4 @@
+import { CallToolResult } from "@modelcontextprotocol/sdk/types";
 import { commandExistsSync, mcpError } from "./util";
 
 export const NO_PROJECT_ERROR = mcpError(
@@ -5,7 +6,7 @@ export const NO_PROJECT_ERROR = mcpError(
   "PRECONDITION_FAILED",
 );
 
-export function mcpAuthError() {
+export function mcpAuthError(): CallToolResult {
   const cmd = commandExistsSync("firebase") ? "firebase" : "npx -y firebase-tools";
   return mcpError(`The user is not currently logged into the Firebase CLI, which is required to use this tool. Please instruct the user to execute this shell command to sign in or to configure [Application Default Credentials][ADC] on their machine.
 \`\`\`sh
@@ -18,7 +19,7 @@ ${cmd} login
 export function mcpGeminiError(projectId: string) {
   const consoleUrl = `https://firebase.corp.google.com/project/${projectId}/overview`;
   return mcpError(
-    `This tool requires Gemini in Firebase API. Visit Firebase Console to enable Gemini in Firebase API ${consoleUrl} and try again.`,
+    `This tool uses the Gemini in Firebase API. Visit Firebase Console to enable the Gemini in Firebase API ${consoleUrl} and try again.`,
     "PRECONDITION_FAILED",
   );
 }
