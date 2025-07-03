26836976aa1de506d631a5a177094722d0265d61
Daniel Lee <taeold@gmail.com>
2025-07-02 21:05:38 -0700

    chore: update functions.config() deprecation message with approved text
    Update deprecation warning to include comprehensive information about:
    - Action required by Dec 31, 2025
    - What the deprecation means for users
    - Impact on CLI commands and deployments
    - Link to migration documentation

--- src/functions/deprecationWarnings.ts
+++ src/functions/deprecationWarnings.ts
@@ -1,10 +1,14 @@
 import { logWarning } from "../utils";
 
-// Placeholder deprecation message - will be updated with approved text from doc team
 const FUNCTIONS_CONFIG_DEPRECATION_MESSAGE = 
-  "functions:config is deprecated and will be removed on December 31, 2025.\n" +
-  "  Please migrate to environment variables.\n" +
-  "  Use 'firebase functions:config:export' to migrate your existing config to .env files";
+  "DEPRECATION NOTICE: Action required to deploy after Dec 31, 2025\n" +
+  "  functions.config() API is deprecated.\n" +
+  "  Cloud Runtime Configuration API, the Google Cloud service used to store function configuration data, will be shut down on December 31, 2025. As a result, you must migrate away from using functions.config() to continue deploying your functions after December 31, 2025.\n" +
+  "  What this means for you:\n" +
+  "  The Firebase CLI commands for managing this configuration (functions:config:set, get, unset, clone, and export) are deprecated. These commands no longer work after December 31, 2025.\n" +
+  "  firebase deploy command will fail for functions that use the legacy functions.config() API after December 31, 2025.\n" +
+  "  Existing deployments will continue to work with their current configuration.\n" +
+  "  See your migration options at: https://firebase.google.com/docs/functions/config-env#migrate-to-dotenv";
 
 /**
  * Logs a deprecation warning for functions.config() usage
