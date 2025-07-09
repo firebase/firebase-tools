30d688ebe44e44da239ea2e4b7b1b026500cd873
Daniel Young Lee <danielylee@google.com>
2025-07-09 14:08:43 -0700

    remove accidentally commited mcp tools.

--- src/mcp/tools/functions/index.ts
+++
@@ -1,5 +0,0 @@
-import { ServerTool } from "../../tool.js";
-import { list_functions } from "./list_functions.js";
-import { list_supported_runtimes } from "./list_supported_runtimes.js";
-
-export const functionsTools: ServerTool[] = [list_functions, list_supported_runtimes];
