eaf536bc8cf20b8b21d2b36c5bfb3d40652ae3ee
Jeff Huang <huangjeff@google.com>
2025-07-22 10:13:22 -0500

    Fix export command

--- src/commands/ext-export.ts
+++ src/commands/ext-export.ts
@@ -67,6 +67,10 @@ export const command = new Command("ext:export")
 
     const manifestSpecs = withRefSubbed.map((spec) => {
       const paramCopy = { ...spec.params };
+      if (spec.systemParams) {
+        Object.assign(paramCopy, spec.systemParams)
+      }
+      
       if (spec.eventarcChannel) {
         paramCopy.EVENTARC_CHANNEL = spec.eventarcChannel;
       }
