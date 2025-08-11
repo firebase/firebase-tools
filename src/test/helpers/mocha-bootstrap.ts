a644a1b8dc5d85104f5b391bf9f736d984a5ee4c
Joe Hanley <joehanley@google.com>
2025-08-11 09:38:11 -0700

    Maybe this?

--- src/test/helpers/mocha-bootstrap.ts
+++ src/test/helpers/mocha-bootstrap.ts
@@ -1,6 +1,6 @@
 import * as chai from "chai";
 import chaiAsPromised from "chai-as-promised";
-import * as sinonChai from "sinon-chai";
+import sinonChai from "sinon-chai";
 
 chai.use(chaiAsPromised);
 chai.use(sinonChai);
