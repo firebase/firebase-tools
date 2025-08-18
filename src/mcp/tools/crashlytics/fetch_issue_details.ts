9e636d097f91dd477f965c1331e82a6b6447a50e
Viswanathan Munisamy <visumickey@gmail.com>
2025-08-18 16:09:17 -0700

    Change the optional behavior of certain parameters to make it mandatory.

--- src/mcp/tools/crashlytics/fetch_issue_details.ts
+++ src/mcp/tools/crashlytics/fetch_issue_details.ts
@@ -10,13 +10,11 @@ export const fetch_issue_details = tool(
     inputSchema: z.object({
       app_id: z
         .string()
-        .optional()
         .describe(
           "AppId for which the issues list should be fetched. For an Android application, read the mobilesdk_app_id value specified in the google-services.json file for the current package name. For an iOS Application, read the GOOGLE_APP_ID from GoogleService-Info.plist. If neither is available, use the `firebase_list_apps` tool to find an app_id to pass to this tool.",
         ),
       issue_id: z
         .string()
-        .optional()
         .describe(
           "The issue ID for which the details needs to be fetched. This is the value of the field `id` in the list of issues. Defaults to the first id in the list of issues.",
         ),
