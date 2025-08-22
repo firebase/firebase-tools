3170b277d74613f0739f12e4e8653564ecc88a72
Raymond Lam <here.is.raymond.lam@gmail.com>
2025-08-21 18:07:01 -0700

    Adds additional Crashlytics tools for debugging/analyzing crashes (#9020)
    Adds the following tools that can be used to debug and fix app crashes.
    
     - crashlytics_add_note: Adds a Crashlytics note to the issue
     - crashlytics_delete_note: Deletes a Crashlytics note
     - crashlytics_list_notes: Retrieves the notes on a Crashlytics issue
     - crashlytics_list_top_devices: List the top crashing devices for the app or an issue
     - crashlytics_list_top_operating_systems: List the top crashing operating systems for the app or an issue
     - crashlytics_list_top_versions: List the top crashing versions for the app or an issue
     - crashlytics_update_issue: Marks an issue as closed or re-opened.

--- src/mcp/tools/crashlytics/index.ts
+++ src/mcp/tools/crashlytics/index.ts
@@ -1,10 +1,24 @@
 import type { ServerTool } from "../../tool";
+import { add_note } from "./add_note";
+import { delete_note } from "./delete_note";
 import { get_issue_details } from "./get_issue_details";
-import { list_top_issues } from "./list_top_issues";
 import { get_sample_crash } from "./get_sample_crash";
+import { list_notes } from "./list_notes";
+import { list_top_devices } from "./list_top_devices";
+import { list_top_issues } from "./list_top_issues";
+import { list_top_operating_systems } from "./list_top_operating_systems";
+import { list_top_versions } from "./list_top_versions";
+import { update_issue } from "./update_issue";
 
 export const crashlyticsTools: ServerTool[] = [
-  list_top_issues,
+  add_note,
+  delete_note,
   get_issue_details,
   get_sample_crash,
+  list_notes,
+  list_top_devices,
+  list_top_issues,
+  list_top_operating_systems,
+  list_top_versions,
+  update_issue,
 ];
