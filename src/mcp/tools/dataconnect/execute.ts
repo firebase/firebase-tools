609de91011c9bd5afb07353de56baf8f74097691
Fred Zhang <fredzqm@google.com>
2025-08-29 09:41:57 -0700

    update description

--- src/mcp/tools/dataconnect/execute.ts
+++ src/mcp/tools/dataconnect/execute.ts
@@ -13,19 +13,31 @@ export const execute = tool(
     description: "Executes a GraphQL operation against a Data Connect service or its emulator.",
     inputSchema: z.object({
       query: z.string().describe("A GraphQL query or mutation to execute against the service."),
-      service_id: z
+      service_id: z.string().optional()
+        .describe(`Data Connect Service ID to dis-ambulate if there are multiple.
+It's only necessary if there are multiple dataconnect sources in \`firebase.json\`.
+You can find candidate service_id in \`dataconnect.yaml\`
+`),
+      variables_json: z
         .string()
         .optional()
         .describe(
-          "The Firebase Data Connect service ID to look for. If there is only one service defined in firebase.json, this can be omitted and that will be used.",
+          "GraphQL variables to pass into the query. MUST be a valid stringified JSON object.",
         ),
-      variables: z
+      auth_token_json: z
         .string()
         .optional()
         .describe(
-          "A stringified JSON object containing variables for the operation. MUST be valid JSON.",
+          "Firebase Auth Token JWT to use in this query. MUST be a valid stringified JSON object." +
+            'Importantly, when executing queries with `@auth(level: USER)` or `auth.uid`, a valid Firebase Auth Token JWT with "sub" field is required. ' +
+            '"auth.uid" expression in the query evaluates to the value of "sub" field in Firebase Auth token.',
         ),
-      use_emulator: z.boolean().default(false).describe("Target the DataConnect emulator if true."),
+      use_emulator: z
+        .boolean()
+        .default(false)
+        .describe(
+          "If true, target the DataConnect emulator. Run `firebase emulators:start` in the background",
+        ),
     }),
     annotations: {
       title: "Execute Data Connect Operation",
@@ -36,7 +48,13 @@ export const execute = tool(
     },
   },
   async (
-    { query, service_id, variables: unparsedVariables, use_emulator },
+    {
+      query,
+      service_id,
+      variables_json: unparsedVariables,
+      use_emulator,
+      auth_token_json: unparsedAuthToken,
+    },
     { projectId, config, host },
   ) => {
     const serviceInfo = await pickService(projectId, config, service_id || undefined);
@@ -54,6 +72,13 @@ export const execute = tool(
       name: "",
       query,
       variables: parseVariables(unparsedVariables),
+      extensions: {
+        impersonate: unparsedAuthToken
+          ? {
+              authClaims: parseVariables(unparsedAuthToken),
+            }
+          : undefined,
+      },
     });
     return graphqlResponseToToolResponse(response.body);
   },
