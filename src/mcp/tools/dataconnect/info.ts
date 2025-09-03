058707f7e40359641b6095c3bf815b3346f451ea
Fred Zhang <fredzqm@google.com>
2025-09-02 17:08:17 -0700

    done

--- src/mcp/tools/dataconnect/info.ts
+++ src/mcp/tools/dataconnect/info.ts
@@ -1,12 +1,11 @@
 import { z } from "zod";
+import * as path from "path";
 import { tool } from "../../tool";
 import { toContent } from "../../util";
 import * as client from "../../../dataconnect/client";
 import { loadAll } from "../../../dataconnect/load";
 import { Service, Schema, ServiceInfo, Connector } from "../../../dataconnect/types";
-import { connectorToText } from "../../util/dataconnect/converter";
-import { schemaToText } from "../../util/dataconnect/converter";
-import { fieldSize } from "tar";
+import { dump } from "js-yaml";
 import { logger } from "../../../logger";
 
 interface ServiceStatus {
@@ -24,18 +23,7 @@ export const status = tool(
   {
     name: "status",
     description: "Get status about the Firebase Data Connect local and deployed sources.",
-    inputSchema: z.object({
-      include_schema_source: z
-        .boolean()
-        .optional()
-        .default(false)
-        .describe("Include Data Connect schema details."),
-      include_connector_source: z
-        .boolean()
-        .optional()
-        .default(false)
-        .describe("Include Data Connect connector details."),
-    }),
+    inputSchema: z.object({}),
     annotations: {
       title: "Get project status about Firebase Data Connect",
       readOnlyHint: true,
@@ -45,7 +33,7 @@ export const status = tool(
       requiresAuth: false,
     },
   },
-  async ({ include_connector_source, include_schema_source }, { projectId, config }) => {
+  async (_, { projectId, config }) => {
     const serviceInfos = await loadAll(projectId, config);
     const serviceStatuses = new Map<string, ServiceStatus>();
 
@@ -60,25 +48,19 @@ export const status = tool(
       try {
         const [services, schemas, connectors] = await Promise.all([
           client.listAllServices(projectId),
-          client.listSchemas(
-            `projects/${projectId}/services/-`,
-            include_schema_source ? ["datasources", "source"] : ["datasources"],
-          ),
-          client.listConnectors(
-            `projects/${projectId}/services/-`,
-            include_connector_source ? ["source"] : [],
-          ),
+          client.listSchemas(`projects/${projectId}/locations/-/services/-`),
+          client.listConnectors(`projects/${projectId}/locations/-/services/-`),
         ]);
         console.log(services, schemas, connectors);
         for (const s of services) {
-          const k = s.name.split("/").slice(2, 4).join("/");
+          const k = s.name.split("/").slice(2, 6).join("/");
           const st = serviceStatuses.get(k) || {};
           st.deployed = st.deployed || {};
           st.deployed.service = s;
           serviceStatuses.set(k, st);
         }
         for (const s of schemas) {
-          const k = s.name.split("/").slice(2, 4).join("/");
+          const k = s.name.split("/").slice(2, 6).join("/");
           const st = serviceStatuses.get(k) || {};
           st.deployed = st.deployed || {};
           st.deployed.schemas = st.deployed.schemas || [];
@@ -86,7 +68,7 @@ export const status = tool(
           serviceStatuses.set(k, st);
         }
         for (const s of connectors) {
-          const k = s.name.split("/").slice(2, 4).join("/");
+          const k = s.name.split("/").slice(2, 6).join("/");
           const st = serviceStatuses.get(k) || {};
           st.deployed = st.deployed || {};
           st.deployed.connectors = st.deployed.connectors || [];
@@ -94,7 +76,7 @@ export const status = tool(
           serviceStatuses.set(k, st);
         }
       } catch (e: any) {
-        logger.debug("Cannot fetch dataconnect resources");
+        logger.debug("cannot fetch dataconnect resources in the backend", e);
       }
     }
 
@@ -103,39 +85,52 @@ export const status = tool(
 
     const output: string[] = [];
 
-    function includeDeployed(deployed: DeployServiceStatus) {
-      output.push(`It's deployed in the backend.`);
+    function includeDeployedServiceStatus(deployed: DeployServiceStatus): void {
       if (deployed.schemas?.length) {
-        for (const schema of deployed.schemas) {
-          output.push(schemaToText(schema));
+        output.push(`### Schemas`);
+        for (const s of deployed.schemas) {
+          clearCCFEFields(s);
+          output.push(dump(s));
         }
       }
       if (deployed.connectors?.length) {
-        output.push(`- Deployed Connectors:`);
-        for (const connector of deployed.connectors) {
-          output.push(`  - ${connector.name}`);
+        output.push(`### Connectors`);
+        for (const c of deployed.connectors) {
+          clearCCFEFields(c);
+          output.push(dump(c));
         }
       }
     }
 
     if (localServices.length) {
-      output.push(`Local Data Connect sources:`);
+      output.push(`# Local Data Connect Sources`);
       for (const s of localServices) {
-        output.push(JSON.stringify(s.local!.dataConnectYaml, null, 2));
+        const local = s.local!;
+        output.push(dump(local.dataConnectYaml));
+        const schemaDir = path.join(local.sourceDirectory, local.dataConnectYaml.schema.source);
+        output.push(`You can find all of its schema sources in ${schemaDir}/*.{graphql,gql}`);
         if (s.deployed) {
-          includeDeployed(s.deployed);
+          output.push(`It's already deployed in the backend:\n`);
+          includeDeployedServiceStatus(s.deployed);
         }
       }
     }
+
     if (remoteOnlyServices.length) {
-      output.push(`Remote Data Connect sources:`);
+      output.push(`# Other Data Connect Services in project ${projectId}`);
       for (const s of remoteOnlyServices) {
-        output.push(JSON.stringify(s.local!.dataConnectYaml, null, 2));
         if (s.deployed) {
-          includeDeployed(s.deployed);
+          includeDeployedServiceStatus(s.deployed);
         }
       }
     }
     return toContent(output.join("\n"));
   },
 );
+
+function clearCCFEFields(r: any): void {
+  const fieldsToClear = ["updateTime", "uid", "etag"];
+  for (const k of fieldsToClear) {
+    delete r[k];
+  }
+}
