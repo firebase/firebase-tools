b887d002878276c279cbe9466807cf980678ec07
Fred Zhang <fredzqm@google.com>
2025-09-03 17:17:48 -0700

    cleanup emulator ui logic

--- src/emulator/controller.ts
+++ src/emulator/controller.ts
@@ -166,9 +166,7 @@ export function shouldStart(options: Options, name: Emulators): boolean {
     }
     // Emulator UI only starts if we know the project ID AND at least one
     // emulator supported by Emulator UI is launching.
-    return (
-      !!options.project && targets.some((target) => EMULATORS_SUPPORTED_BY_UI.includes(target))
-    );
+    return targets.some((target) => EMULATORS_SUPPORTED_BY_UI.includes(target));
   }
 
   // Don't start the functions emulator if we can't validate the functions config
@@ -1006,9 +1004,8 @@ export async function startAll(
     hubLogger.logLabeled(
       "WARN",
       "emulators",
-      "The Emulator UI is not starting, either because none of the running " +
-        "emulators have a UI component or the Emulator UI cannot " +
-        "determine the Project ID. Pass the --project flag to specify a project.",
+      "The Emulator UI is not starting because none of the running " +
+        "emulators have a UI component.",
     );
   }
 
