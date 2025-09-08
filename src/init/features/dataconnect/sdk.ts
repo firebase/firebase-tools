c1642fcc6e140b4c8fb490c878c5c1adb65dbb59
Fred Zhang <fredzqm@google.com>
2025-09-08 09:36:20 -0700

    m

--- src/init/features/dataconnect/sdk.ts
+++ src/init/features/dataconnect/sdk.ts
@@ -28,6 +28,7 @@ import {
   logLabeledWarning,
   logLabeledBullet,
   newUniqueId,
+  logLabeledError,
 } from "../../../utils";
 import { DataConnectEmulator } from "../../../emulator/dataconnectEmulator";
 import { getGlobalDefaultAccount } from "../../../auth";
@@ -200,10 +201,14 @@ async function actuateWithInfo(setup: Setup, config: Config, info: SdkRequiredIn
 
   logLabeledBullet("dataconnect", `Installing the generated SDKs ...`);
   const account = getGlobalDefaultAccount();
-  await DataConnectEmulator.generate({
-    configDir: connectorInfo.directory,
-    account,
-  });
+  try {
+    await DataConnectEmulator.generate({
+      configDir: connectorInfo.directory,
+      account,
+    });
+  } catch (e: any) {
+    logLabeledError("dataconnect", `Failed to generate Data Connect SDKs\n${e?.message}`);
+  }
 
   logLabeledSuccess(
     "dataconnect",
