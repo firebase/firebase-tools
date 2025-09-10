764af5f135743c5f35459b3d0447f3f4520f4aa6
Joe Hanley <joehanley@google.com>
2025-09-10 15:02:19 -0700

    format

--- src/mcp/tools/index.ts
+++ src/mcp/tools/index.ts
@@ -64,7 +64,7 @@ export function markdownDocsOfTools(): string {
 | --------- | ------------- | ----------- |`;
   for (const tool of allTools) {
     let feature = tool.mcp?._meta?.feature || "";
-     if (feature === "firebase") {
+    if (feature === "firebase") {
       feature = "core";
     }
     doc += `
