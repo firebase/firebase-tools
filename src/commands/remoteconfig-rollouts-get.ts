4a1f9914c4fafbb858dd8935f9d58edc427b4b5d
Varun Rathore <varunrathore@google.com>
2025-09-21 16:31:06 +0530

    fix lint

--- src/commands/remoteconfig-rollouts-get.ts
+++ src/commands/remoteconfig-rollouts-get.ts
@@ -9,20 +9,20 @@ import * as rcRollout from "../remoteconfig/rolloutget";
 import { FirebaseError } from "../error";
 
 export const command = new Command("remoteconfig:rollouts:get [rolloutId]")
-    .description("get a Remote Config rollout")
-    .before(requireAuth)
-    .before(requirePermissions, ["cloud.configs.get"])
-    .action(async (rolloutId: string, options: Options) => {
-        console.log("[VERIFY] ---------- local script is linked ----------- ");
-        if (!rolloutId) {
-            throw new FirebaseError("Rollout ID is required.");
-        }
-        const projectId: string = await needProjectNumber(options);
-        const rollout: RemoteConfigRollout = await rcRollout.getRollout(
-            projectId,
-            NAMESPACE_FIREBASE,
-            rolloutId,
-        );
-        logger.info(rcRollout.parseRolloutIntoTable(rollout));
-        return rollout;
-    });
+  .description("get a Remote Config rollout")
+  .before(requireAuth)
+  .before(requirePermissions, ["cloud.configs.get"])
+  .action(async (rolloutId: string, options: Options) => {
+    console.log("[VERIFY] ---------- local script is linked ----------- ");
+    if (!rolloutId) {
+      throw new FirebaseError("Rollout ID is required.");
+    }
+    const projectId: string = await needProjectNumber(options);
+    const rollout: RemoteConfigRollout = await rcRollout.getRollout(
+      projectId,
+      NAMESPACE_FIREBASE,
+      rolloutId,
+    );
+    logger.info(rcRollout.parseRolloutIntoTable(rollout));
+    return rollout;
+  });
