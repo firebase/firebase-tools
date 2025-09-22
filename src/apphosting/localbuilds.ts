6e36915a4eb9aca2338464e35f7332510575aa2f
Joanna Wang <annajowang@gmail.com>
2025-09-22 21:15:44 +0000

    lint

--- src/apphosting/localbuilds.ts
+++ src/apphosting/localbuilds.ts
@@ -1,38 +1,36 @@
 import { localBuild as localApphostingBuild } from "@apphosting/build";
 import { OutputBundleConfig } from "@apphosting/common";
-import { BuildConfig, Env, Availability } from "../gcp/apphosting";
-import { readFileSync } from "fs";
-import { join } from "path";
-import { fileExistsSync } from "../fsutils";
-import { load } from "js-yaml";
-import { FirebaseError } from "../error";
-
+import { BuildConfig, Env } from "../gcp/apphosting";
 
 export async function localBuild(
-  projectRoot: string, framework: string): Promise<{annotations: Record<string, string>, buildConfig: BuildConfig}> {
+  projectRoot: string,
+  framework: string,
+): Promise<{ annotations: Record<string, string>; buildConfig: BuildConfig }> {
+  const apphostingBuildOutput: OutputBundleConfig = await localApphostingBuild(
+    projectRoot,
+    framework,
+  );
 
-    const apphostingBuildOutput: OutputBundleConfig = await localApphostingBuild(projectRoot, framework);
+  const annotations: Record<string, string> = {};
+  Object.entries(apphostingBuildOutput.metadata).forEach(([key, value]) => {
+    annotations[key] = String(value);
+  });
 
-    const annotations: Record<string, string> = {};
-    Object.entries(apphostingBuildOutput.metadata).forEach(([key, value]) => {
-      annotations[key] = String(value);
-    });
+  const env: Env[] | undefined = apphostingBuildOutput.runConfig.environmentVariables?.map(
+    ({ variable, value, availability }: Env) => {
+      return {
+        variable,
+        value,
+        availability,
+      };
+    },
+  );
 
-    const env: Env[] | undefined = apphostingBuildOutput.runConfig.environmentVariables?.map(
-      ( {variable, value, availability} : Env) => {
-	return {
-	  variable,
-	  value,
-	  availability,
-	};
-      });
-
-    return {
-      annotations,
-      buildConfig: {
-	runCommand: apphostingBuildOutput.runConfig.runCommand,
-	env,
-      },
-    }
-
+  return {
+    annotations,
+    buildConfig: {
+      runCommand: apphostingBuildOutput.runConfig.runCommand,
+      env,
+    },
+  };
 }
