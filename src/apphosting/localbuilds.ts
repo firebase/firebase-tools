8a74eb9a55646ad77002d5bdde6755297acf44f9
Joanna Wang <annajowang@gmail.com>
2025-09-23 00:39:32 +0000

    Call local apphosting build during orchestrated rollouts.

---
+++ src/apphosting/localbuilds.ts
@@ -0,0 +1,36 @@
+import { localBuild as localApphostingBuild } from "@apphosting/build";
+import { OutputBundleConfig } from "@apphosting/common";
+import { BuildConfig, Env } from "../gcp/apphosting";
+
+export async function localBuild(
+  projectRoot: string,
+  framework: string,
+): Promise<{ annotations: Record<string, string>; buildConfig: BuildConfig }> {
+  const apphostingBuildOutput: OutputBundleConfig = await localApphostingBuild(
+    projectRoot,
+    framework,
+  );
+
+  const annotations: Record<string, string> = {};
+  Object.entries(apphostingBuildOutput.metadata).forEach(([key, value]) => {
+    annotations[key] = String(value);
+  });
+
+  const env: Env[] | undefined = apphostingBuildOutput.runConfig.environmentVariables?.map(
+    ({ variable, value, availability }: Env) => {
+      return {
+        variable,
+        value,
+        availability,
+      };
+    },
+  );
+
+  return {
+    annotations,
+    buildConfig: {
+      runCommand: apphostingBuildOutput.runConfig.runCommand,
+      env,
+    },
+  };
+}
