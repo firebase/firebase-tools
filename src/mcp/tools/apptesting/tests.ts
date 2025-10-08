790c9bbde235a1fd35d971397f9d924c86f18779
Jamie Rothfeder <rothbutter@google.com>
2025-10-07 21:51:15 -0400

    Use a datastructure to represent test devices rather than a string.

--- src/mcp/tools/apptesting/tests.ts
+++ src/mcp/tools/apptesting/tests.ts
@@ -1,40 +1,54 @@
 import { z } from "zod";
 import { ApplicationIdSchema } from "../../../crashlytics/filters";
 import { distribute, Distribution } from "../../../appdistribution/distribution";
+
 import { tool } from "../../tool";
-import { mcpError, toContent } from "../../util";
-import {
-  parseIntoStringArray,
-  parseTestDevices,
-  toAppName,
-} from "../../../appdistribution/options-parser-util";
+import { toContent } from "../../util";
+import { parseIntoStringArray, toAppName } from "../../../appdistribution/options-parser-util";
+
+const TestDeviceSchema = z
+  .object({
+    model: z.string(),
+    version: z.string(),
+    locale: z.string(),
+    orientation: z.string(),
+  })
+  .describe(
+    `Device to run automated test on. Can run 'gcloud firebase test android|ios models list' to see available devices.`,
+  );
 
 export const run_tests = tool(
   {
     name: "run_test",
-    description: `Upload an APK and run an existing test against it.`,
+    description: `Run a remote test.`,
     inputSchema: z.object({
       appId: ApplicationIdSchema,
       releaseBinaryFile: z.string().describe("Path to the binary release (APK)."),
-      testDevices: z.string().describe(
-        `Semicolon-separated list of devices to run automated tests on, in the format
-          'model=<model-id>,version=<os-version-id>,locale=<locale>,orientation=<orientation>'. Run 'gcloud firebase test android|ios models list' to see
-          available devices.`,
-      ),
+      testDevices: z.array(TestDeviceSchema).default([
+        {
+          model: "tokay",
+          version: "36",
+          locale: "en",
+          orientation: "portrait",
+        },
+        {
+          model: "e1q",
+          version: "34",
+          locale: "en",
+          orientation: "portrait",
+        },
+      ]),
       testCaseIds: z.string().describe(`A comma-separated list of test case IDs.`),
     }),
   },
   async ({ appId, releaseBinaryFile, testDevices, testCaseIds }) => {
-    if (!appId) return mcpError(`Must specify 'appId' parameter.`);
-    if (!releaseBinaryFile) return mcpError(`Must specify 'releaseBinaryFile' parameter.`);
-    if (!testDevices) return mcpError(`Must specify 'testDevices' parameter.`);
-    if (!testCaseIds) return mcpError(`Must specify 'testCaseIds' parmeter.`);
-
-    const appName = toAppName(appId);
-    const distribution = new Distribution(releaseBinaryFile);
-    const testCases = parseIntoStringArray(testCaseIds);
-    const devices = parseTestDevices(testDevices);
-
-    return toContent(await distribute(appName, distribution, testCases, devices));
+    return toContent(
+      await distribute(
+        toAppName(appId),
+        new Distribution(releaseBinaryFile),
+        parseIntoStringArray(testCaseIds),
+        testDevices,
+      ),
+    );
   },
 );
