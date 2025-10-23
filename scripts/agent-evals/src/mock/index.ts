88683d7c3514aab7179ab9cc9c6aaa658fcd930f
samedson <samedson@google.com>
2025-10-23 16:04:12 -0400

    B

--- scripts/agent-evals/src/mock/index.ts
+++ scripts/agent-evals/src/mock/index.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import {getFirebaseCliRoot} from '../runner/paths.js';
 
 const TOOLS_PATH = 'lib/mcp/tools/';
-const MCP_INDEX_PATH = 'lib/mcp/tools/';
+const MCP_TOOLS_INDEX_PATH = 'lib/mcp/tools/index.js';
 
 const originalRequire = Module.prototype.require;
 
@@ -14,29 +14,13 @@ Module.prototype.require = function(id: string) {
   const parentModule = this;
   const absolutePath = Module.createRequire(parentModule.filename).resolve(id);
   const pathRelativeToCliRoot = path.relative(getFirebaseCliRoot(), absolutePath);
-  // if (pathRelativeToCliRoot.startsWith(TOOLS_PATH)) {
-  //   const toolPath = pathRelativeToCliRoot.replace(TOOLS_PATH, "");
-  //   console.log({
-  //     path: toolPath,
-  //     module: JSON.stringify(requiredModule),
-  //   });
-  // }
-  if ((requiredModule as any)?.FirebaseMcpServer) {
-    const toolPath = pathRelativeToCliRoot.replace(MCP_INDEX_PATH, "");
+  if (pathRelativeToCliRoot.startsWith(MCP_TOOLS_INDEX_PATH)) {
+    const toolPath = pathRelativeToCliRoot.replace(MCP_TOOLS_INDEX_PATH, "");
     console.log({
       path: toolPath,
       pathRelativeToCliRoot: pathRelativeToCliRoot,
       module: JSON.stringify(requiredModule),
-      MCPMod: JSON.stringify((requiredModule as any)?.FirebaseMcpServer),
     });
   }
-  // if (pathRelativeToCliRoot.startsWith(MCP_INDEX_PATH)) {
-  //   const toolPath = pathRelativeToCliRoot.replace(MCP_INDEX_PATH, "");
-  //   console.log({
-  //     path: toolPath,
-  //     pathRelativeToCliRoot: pathRelativeToCliRoot,
-  //     module: JSON.stringify(requiredModule),
-  //   });
-  // }
   return requiredModule;
 };
