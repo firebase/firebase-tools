a887e48ea12615a6c0d5d2a23283399f3ec58df4
Alexander Nohe <nohe427@users.noreply.github.com>
2025-11-25 14:29:33 -0800

    feat: Use custom validation rule in language server

--- firebase-vscode/src/data-connect/language-server.ts
+++ firebase-vscode/src/data-connect/language-server.ts
@@ -1,4 +1,7 @@
+
 import { startServer } from "graphql-language-service-server";
+import { NoUnusedVariablesInCheckDirective } from "./custom-validation-rules";
+
 // The npm scripts are configured to only build this once before
 // watching the extension, so please restart the extension debugger for changes!
 
@@ -7,6 +10,7 @@ async function start() {
     await startServer({
       method: "node",
       loadConfigOptions: { rootDir: ".firebase" },
+      customValidationRules: [NoUnusedVariablesInCheckDirective],
     });
     // eslint-disable-next-line no-console
     console.log("Firebase GraphQL Language Server started!");
