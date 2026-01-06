9ecbfbea4920bb604d2f2c4dd08eedfe2ac5e542
Brittany Cho <brittanycho@google.com>
2026-01-06 20:28:19 +0000

    removing test files

--- manual_verify/bin/server.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-void main() async {
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await HttpServer.bind('0.0.0.0', port);
-  print('Server listening on port $port');
-  await for (final request in server) {
-    request.response..statusCode = HttpStatus.ok..write('Hello!')..close();
-  }
-}
