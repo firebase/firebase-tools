8dac558bfbf39ed889c0bf674376f52ba012f3ca
Fred Zhang <fredzqm@google.com>
2026-01-20 17:49:37 -0800

    dont show UNKNOWN code

--- firebase-vscode/webviews/data-connect/DataConnectExecutionResultsApp.tsx
+++ firebase-vscode/webviews/data-connect/DataConnectExecutionResultsApp.tsx
@@ -161,7 +161,7 @@ function GraphQLErrorView({ error }: { error: any }) {
     <div className={style.errorItem}>
       <div className={style.errorHeader}>
         <i className="codicon codicon-error" />
-        {code && <span className={style.errorCode}>{code}</span>}
+        {code && code !== "UNKNOWN" && <span className={style.errorCode}>{code}</span>}
         <span>{message}</span>
       </div>
       {path && path.length > 0 && (
