4d2326fb2fec7b8844de622fee78be565d0a7b1c
Aryan Falahatpisheh <aryanfalahat@gmail.com>
2026-01-23 14:00:17 -0500

    Create a first draft of a prompt that helps users import Firebase App Hosting projects locally (into apps such as Antigravity).
    It handles the import of the code, creation of a test backend, and the initial deployment to that backend.
    
    It prefers to use local code deployment rather than relying on a Github connection.

--- src/mcp/prompts/core/index.ts
+++ src/mcp/prompts/core/index.ts
@@ -1,6 +1,7 @@
 import { init } from "./init";
 import { deploy } from "./deploy";
+import { importFromGithub } from "./import_from_github";
 
-const corePrompts = [deploy, init];
+const corePrompts = [deploy, init, importFromGithub];
 
 export { corePrompts };
