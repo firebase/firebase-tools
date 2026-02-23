896ef0e2a33b25597c3590c8711e25b46d153ac0
christhompsongoogle <106194718+christhompsongoogle@users.noreply.github.com>
2026-02-23 14:38:51 -0800

    Update src/commands/studio-export.ts
    Co-authored-by: Joe Hanley <joehanley@google.com>

--- src/commands/studio-export.ts
+++ src/commands/studio-export.ts
@@ -3,7 +3,7 @@ import { logger } from "../logger";
 import * as experiments from "../experiments";
 
 export const command = new Command("studio:export")
-  .description("export Firebase Studio apps for migration to Antigravity")
+  .description("export Firebase Studio apps to continue development locally")
   .action(() => {
     experiments.assertEnabled("studioexport", "export Studio apps");
     logger.info("Exporting Studio apps to Antigravity...");
