15f274c0a7ee229bb9dfcc8ec2c6bcbba778a894
Thomas Bouldin <inlined@google.com>
2026-03-03 00:37:17 -0800

    First sample. Creates internal functions.yaml

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -132,6 +132,7 @@ export function load(client: any): any {
   client.functions.config.set = loadCommand("functions-config-set");
   client.functions.config.unset = loadCommand("functions-config-unset");
   client.functions.delete = loadCommand("functions-delete");
+  client.functions.export = loadCommand("functions-export");
   client.functions.log = loadCommand("functions-log");
   client.functions.shell = loadCommand("functions-shell");
   client.functions.list = loadCommand("functions-list");
