7c57ce4ba28eb50c84e0cce3a981042b16176f23
Chris Thompson <christhompson@google.com>
2026-03-10 02:48:36 -0700

    Update the error message for missing path to be friendlier

--- src/commands/studio-export.ts
+++ src/commands/studio-export.ts
@@ -6,15 +6,19 @@ import * as path from "path";
 import * as experiments from "../experiments";
 import { FirebaseError } from "../error";
 
-export const command = new Command("studio:export <path>")
+export const command = new Command("studio:export [path]")
   .description(
     "Bootstrap Firebase Studio apps for migration to Antigravity. Run on the unzipped folder from the Firebase Studio download.",
   )
   .option("--no-start-agy", "skip starting the Antigravity IDE after migration")
+  .help("sup")
   .action(async (exportPath: string, options: Options) => {
     experiments.assertEnabled("studioexport", "export Studio apps");
     if (!exportPath) {
-      throw new FirebaseError("Must specify a path for migration.", { exit: 1 });
+      throw new FirebaseError(
+        "Must specify the path to the Firebase Studio downloaded zip file or the unzipped folder path.",
+        { exit: 1 },
+      );
     }
     const rootPath = path.resolve(exportPath);
     logger.info(`Exporting Studio apps from ${rootPath} to Antigravity...`);
