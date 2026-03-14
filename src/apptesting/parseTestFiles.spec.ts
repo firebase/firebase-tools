de8fd560f63e857850ecbe2b5ad650ccd9c41d8d
Lee Kellogg <lkellogg@google.com>
2026-03-14 16:12:11 +0000

    Improve error message when using an invalid regex with apptesting:execute

--- src/apptesting/parseTestFiles.spec.ts
+++ src/apptesting/parseTestFiles.spec.ts
@@ -203,6 +203,22 @@ describe("parseTestFiles", () => {
       writeFile("bbb", createBasicTest(["bxx", "byy", "bzz"]));
       expect(await getTestCaseNames("a$", "xx")).to.eql(["axx"]);
     });
+
+    it("throws an error for invalid filename regex", async () => {
+      writeFile("aaa", createBasicTest(["axx", "ayy", "azz"]));
+      await expect(getTestCaseNames("*.txt")).to.be.rejectedWith(
+        FirebaseError,
+        "Invalid file pattern regex",
+      );
+    });
+
+    it("throws an error for invalid test case name regex", async () => {
+      writeFile("aaa", createBasicTest(["axx", "ayy", "azz"]));
+      await expect(getTestCaseNames("", "*.txt")).to.be.rejectedWith(
+        FirebaseError,
+        "Invalid test name pattern regex",
+      );
+    });
   });
 
   describe("prerequisite test cases", () => {
