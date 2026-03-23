d33ca440a95df95b885f2671eb4975474b1a27c0
Aryan Falahatpisheh <aryanfalahat@gmail.com>
2026-03-23 15:31:12 -0700

    Move backends config call

--- src/deploy/apphosting/prepare.ts
+++ src/deploy/apphosting/prepare.ts
@@ -41,16 +41,17 @@ export default async function (context: Context, options: Options): Promise<void
   await ensureApiEnabled(options);
   await ensureRequiredApisEnabled(projectId);
   await ensureAppHostingComputeServiceAccount(projectId, /* serviceAccount= */ "");
-  const configs = getBackendConfigs(options);
-  if (configs.some((cfg) => cfg.localBuild) && experiments.isEnabled("apphostinglocalbuilds")) {
-    const projectNumber = await getProjectNumber(options);
-    await ensureAppHostingServiceAgentRoles(projectId, projectNumber);
-  }
 
   context.backendConfigs = {};
   context.backendLocations = {};
   context.backendStorageUris = {};
   context.backendLocalBuilds = {};
+
+  const configs = getBackendConfigs(options);
+  if (configs.some((cfg) => cfg.localBuild) && experiments.isEnabled("apphostinglocalbuilds")) {
+    const projectNumber = await getProjectNumber(options);
+    await ensureAppHostingServiceAgentRoles(projectId, projectNumber);
+  }
 
   const { backends } = await listBackends(projectId, "-");
 
