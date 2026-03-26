407ab4ae7c81b39c9a91a00e6503dc5c196ea66a
Aryan Falahatpisheh <aryanfalahat@gmail.com>
2026-03-26 16:21:22 -0400

    remove todo

--- src/deploy/apphosting/release.ts
+++ src/deploy/apphosting/release.ts
@@ -50,8 +50,7 @@ export default async function (context: Context, options: Options): Promise<void
       backendId,
       location: context.backendLocations[backendId],
       buildInput: {
-        // TODO: Confirm this config behavior
-        config: context.backendLocalBuilds[backendId]?.buildConfig || {},
+        config: context.backendLocalBuilds[backendId]?.buildConfig,
         source: {
           archive: {
             userStorageUri: context.backendStorageUris[backendId],
