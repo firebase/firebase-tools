18910b4b2a5ab418af521094ae738733d4239e5f
Aryan Falahatpisheh <aryanfalahat@gmail.com>
2026-04-01 19:19:39 -0400

    Move changes to the prompts.ts file

--- src/apphosting/prompts.ts
+++ src/apphosting/prompts.ts
@@ -1,17 +1,29 @@
-import { select, confirm } from "../prompt";
-
+import { select, confirm, Choice } from "../prompt";
+import { logWarning } from "../utils";
+import * as apphosting from "../gcp/apphosting";
 export const DEFAULT_RUNTIME = "nodejs";
 
 /**
  * Asks the user for their runtime. This is required for ABIU.
  */
-export async function promptRuntime(): Promise<string> {
+export async function promptRuntime(projectId: string, location: string): Promise<string> {
+  const choices: Choice<string>[] = [{ name: "Node.js (default)", value: DEFAULT_RUNTIME }];
+  try {
+    const supportedRuntimes = await apphosting.listSupportedRuntimes(projectId, location);
+    for (const r of supportedRuntimes) {
+      if (r.runtimeId !== DEFAULT_RUNTIME) {
+        choices.push({ name: r.runtimeId, value: r.runtimeId });
+      }
+    }
+  } catch (err) {
+    logWarning("Failed to list supported runtimes. Falling back to hardcoded list.");
+    // We add this hardcoded nodejs22 to unblock testing.
+    // This line will be removed when the ListSupportedRuntime API is stable.
+    choices.push({ name: "nodejs22", value: "nodejs22" });
+  }
   return await select({
     message: "Which runtime do you want to use?",
-    choices: [
-      { name: "Node.js (default)", value: DEFAULT_RUNTIME },
-      { name: "Node.js 22", value: "nodejs22" },
-    ],
+    choices: choices,
     default: DEFAULT_RUNTIME,
   });
 }
