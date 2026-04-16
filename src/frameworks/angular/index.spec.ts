00a93e2722dea9cf97b8c4de73474d82caf14af7
Sam Severance <swseverance@gmail.com>
2026-04-16 09:37:07 -0400

    fix: detect @angular/platform-server to determine if Angular project uses SSR
    Previously, Angular's discover() always returned mayWantBackend: true,
    causing the SSR warning to appear even for SPA-only Angular projects.
    Now checks for the presence of @angular/platform-server in installed dependencies.
    
    Resolves #10362

--- src/frameworks/angular/index.spec.ts
+++ src/frameworks/angular/index.spec.ts
@@ -2,6 +2,7 @@ import { expect } from "chai";
 import * as sinon from "sinon";
 import * as fsExtra from "fs-extra";
 
+import * as frameworkUtils from "../utils";
 import { discover } from ".";
 
 describe("Angular", () => {
@@ -17,10 +18,25 @@ describe("Angular", () => {
       sandbox.restore();
     });
 
-    it("should find an Angular app", async () => {
+    it("should find an Angular app with SSR", async () => {
       sandbox.stub(fsExtra, "pathExists").resolves(true);
+      sandbox.stub(frameworkUtils, "findDependency").callsFake((name) => {
+        if (name === "@angular/platform-server") {
+          return { version: "17.0.0", resolved: "", overridden: false };
+        }
+        return undefined;
+      });
       expect(await discover(cwd)).to.deep.equal({
         mayWantBackend: true,
+        version: undefined,
+      });
+    });
+
+    it("should find an Angular app without SSR", async () => {
+      sandbox.stub(fsExtra, "pathExists").resolves(true);
+      sandbox.stub(frameworkUtils, "findDependency").returns(undefined);
+      expect(await discover(cwd)).to.deep.equal({
+        mayWantBackend: false,
         version: undefined,
       });
     });
