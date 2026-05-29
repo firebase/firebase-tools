a345a4ba0c9c1340eac8c6d740925d03a62e40aa
aalej <alejandromarco@google.com>
2026-05-30 03:11:47 +0800

    correct test name

--- src/command.spec.ts
+++ src/command.spec.ts
@@ -211,24 +211,6 @@ describe("Command", () => {
     const run = command
       .action((options) => {
         return {
-          except: options.except,
-        };
-      })
-      .runner();
-
-    const result = await run({
-      except: "firestore, hosting,  auth",
-    });
-
-    expect(result).to.deep.eq({
-      except: "firestore,hosting,auth",
-    });
-  });
-
-  it("should normalize space and commas separated values in 'only' options", async () => {
-    const run = command
-      .action((options) => {
-        return {
           only: options.only,
         };
       })
@@ -240,6 +222,24 @@ describe("Command", () => {
 
     expect(result).to.deep.eq({
       only: "firestore,hosting,auth",
+    });
+  });
+
+  it("should normalize space and commas separated values in 'except' options", async () => {
+    const run = command
+      .action((options) => {
+        return {
+          except: options.except,
+        };
+      })
+      .runner();
+
+    const result = await run({
+      except: "firestore, hosting,  auth",
+    });
+
+    expect(result).to.deep.eq({
+      except: "firestore,hosting,auth",
     });
   });
 });
