9981ecdc0d75872a1797b1b98c9e2aeb632d7dc3
Victor Fan <vsfan@google.com>
2026-06-01 15:42:56 -0700

    commander options

--- src/commands/ext-inspect.ts
+++ src/commands/ext-inspect.ts
@@ -4,17 +4,21 @@ import { needProjectId } from "../projectUtils";
 import { ensureExtensionsApiEnabled } from "../extensions/extensionsHelper";
 import { requirePermissions } from "../requirePermissions";
 import { listInstances } from "../extensions/extensionsApi";
-import { logLabeledBullet } from "../utils";
+import { last, logLabeledBullet } from "../utils";
 import { logPrefix } from "../extensions/extensionsHelper";
+import { FirebaseError } from "../error";
+
 import * as clc from "colorette";
 
-export const command = new Command("ext:inspect")
-  .description("shows the current configuration for extensions that are installed in your Firebase project")
+export const command = new Command("ext:inspect [extensionInstanceId]")
+  .description("shows the current configuration for a currently installed Extension")
+  .option("--with-secrets", "shows the parameter name (but not value) of SECRET-type params")
   .before(requirePermissions, ["firebaseextensions.instances.list"])
   .before(requirePermissions, ["firebaseextensions.instances.get"])
   .before(ensureExtensionsApiEnabled)
   .before(checkMinRequiredVersion, "extMinVersion")
-  .action(async (options: any) => {
+  .action(async (wantInstanceId: string, options: any) => {
+    const listAll = !wantInstanceId;
     const projectId = needProjectId(options);
     const instances = await listInstances(projectId);
     if (instances.length < 1) {
@@ -22,22 +26,65 @@ export const command = new Command("ext:inspect")
         logPrefix,
         `there are no extensions installed on project ${clc.bold(projectId)}.`,
       );
-      return
+      return;
     }
-    instances.forEach((instance) => {
-      let liveParams = instance.config.params || {};
-      let liveSystemParams = instance.config.systemParams || {};
-      let specParams = instance.config.source.spec.params || {};
-      let specSystemParams = instance.config.source.spec.systemParams || {};
 
+    let found = false;
+    instances.forEach((instance) => {
+      const instanceId = last(instance.name.split("/")) ?? "";
+      if (!listAll && instanceId !== wantInstanceId) {
+        return;
+      }
+
+      found = true;
+      const liveParams = instance.config.params || [];
+      const liveSystemParams = instance.config.systemParams || [];
+      const specParams = instance.config.source.spec.params || [];
+      const specSystemParams = instance.config.source.spec.systemParams || [];
+
+      if (listAll) {
+        console.log("# " + instanceId);
+      }
+
+      // Every user param must be available, so we replicate the spec's default behavior if not present
       specParams.forEach((specParam) => {
-        if (specParam.param in liveParams) {
-          console.log(specParam.param + '=' + liveParams[specParam.param])
-        } else if ('default' in specParam) {
-          console.log(specParam.param + '=' + specParam.default)
+        if (specParam.type === "SECRET") {
+          if (options.withSecrets) {
+            console.log("# " + specParam.param + " stored in Cloud Secret Manager");
+          }
+        } else if (specParam.param in liveParams) {
+          console.log(specParam.param + "=" + liveParams[specParam.param]);
+        } else if ("default" in specParam) {
+          console.log(specParam.param + "=" + specParam.default);
         } else {
-          console.log(specParam.param + '=""')
+          console.log(specParam.param + '=""');
+        }
+      });
+
+      // System params aren't necessarily defined in the spec, but we do respect any defaults
+      Object.entries(liveSystemParams).forEach(([sysParamName, sysParamValue]) => {
+        const renamed = sysParamName.replace(
+          "firebaseextensions.v1beta.function/",
+          "extensions_system_",
+        );
+        console.log(renamed + "=" + sysParamValue);
+      });
+      specSystemParams.forEach((specSystemParam) => {
+        if (specSystemParam.param in liveSystemParams) {
+          return;
+        }
+        if ("default" in specSystemParam) {
+          const renamed = specSystemParam.param.replace(
+            "firebaseextensions.v1beta.function/",
+            "extensions_system_",
+          );
+          console.log(renamed + "=" + specSystemParam.default);
         }
       });
     });
+    if (!found) {
+      throw new FirebaseError(
+        `Could not find extension instance ${wantInstanceId} in active extensions`,
+      );
+    }
   });
