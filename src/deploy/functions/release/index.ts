2d9cecfbe92f7b4256ee21f9e39b3153860e04bb
demolaf <demolafadumo@gmail.com>
2026-06-02 10:41:32 +0100

    chore: remove temporary Dart console warning now that Firebase Console supports Dart functions

--- src/deploy/functions/release/index.ts
+++ src/deploy/functions/release/index.ts
@@ -115,17 +115,6 @@ export async function release(
   const wantBackend = backend.merge(...Object.values(payload.functions).map((p) => p.wantBackend));
   printTriggerUrls(wantBackend, projectNumber);
 
-  // TODO: Remove once the Firebase console has support.
-  if (
-    backend.someEndpoint(wantBackend, (endpoint) => runtimeIsLanguage(endpoint.runtime, "dart"))
-  ) {
-    utils.logLabeledBullet(
-      "functions",
-      "Dart functions may not yet be visible in the Firebase Console. " +
-        `View them in the Cloud Console at https://console.cloud.google.com/run/services?project=${context.projectId}`,
-    );
-  }
-
   await setupArtifactCleanupPolicies(
     options,
     options.projectId!,
