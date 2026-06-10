a2074700ddff6f2dcc737797ea6e1257949b05ef
aalej <alejandromarco@google.com>
2026-06-11 01:20:03 +0800

    remove async

--- src/init/features/apphosting.ts
+++ src/init/features/apphosting.ts
@@ -113,7 +113,7 @@ export async function doSetup(setup: Setup, config: Config, options: Options): P
   backendConfig.rootDir = await input({
     default: "/",
     message: "Specify your app's root directory relative to your firebase.json directory",
-    validate: async (input: string) => {
+    validate: (input: string) => {
       const absPath = path.join(process.cwd(), input);
       if (!existsSync(absPath)) {
         return `Directory ${absPath} does not exist. Please enter a valid directory.`;
