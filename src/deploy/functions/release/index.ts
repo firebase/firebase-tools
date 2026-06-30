9f8da9b848d7c78d62500d7043111e786ce94ec0
Wanda Mora <42626188+wandamora@users.noreply.github.com>
2026-06-30 11:11:27 -0400

    feat: support Task lifecycle hooks in functions (#10728)
    * Initial prototype of CF3 Lifecycle Hooks
    
    * Change schema to match functions SDK
    
    * Validate, prefix, and authenticate lifecycle hooks
    
    1. Adds validation of each trigger
    2. Ensures they are prefixed properly
    3. Resolves SA for OIDC token
    4. Outputs a Cloud COnsole log
    
    * Add functions label to logs
    
    * Address comments
    
    * formatting
    
    * remove unused arg
    
    * Clean up manifest parsing
    
    * formatting
    
    * refactor: restrict lifecycle hooks to task queue functions only
    
    * feat: parse call and http hooks in manifest but throw unsupported errors during validation
    
    * refactor: simplify lifecycle hook parsing in v1alpha1.ts
    
    * update prefix tests
    
    * Address comments
    
    - Add explanatory comments
    - Update logging
    - Change LifecycleHook to union type
    - Add assertExhaustive checks
    - Update project number detection

--- src/deploy/functions/release/index.ts
+++ src/deploy/functions/release/index.ts
@@ -17,6 +17,7 @@ import { FirebaseError } from "../../../error";
 import { getProjectNumber } from "../../../getProjectNumber";
 import { release as extRelease } from "../../extensions";
 import * as artifacts from "../../../functions/artifacts";
+import { executeLifecycleHooks } from "./lifecycle";
 
 /** Releases new versions of functions and extensions to prod. */
 export async function release(
@@ -113,6 +114,10 @@ export async function release(
   // trigger URLs by calling existingBackend again.
   const wantBackend = backend.merge(...Object.values(payload.functions).map((p) => p.wantBackend));
   printTriggerUrls(wantBackend, projectNumber);
+
+  for (const [codebase, { wantBackend: w, haveBackend: h }] of Object.entries(payload.functions)) {
+    await executeLifecycleHooks(w, h, plan, codebase);
+  }
 
   await setupArtifactCleanupPolicies(
     options,
