ebf862640a759773e8a6f19241a4ae5cd721b4c5
Wanda Mora <morawand@google.com>
2026-08-10 22:33:52 +0000

    Update index-kit.ts template

--- templates/init/functions/typescript/index-kit.ts
+++ templates/init/functions/typescript/index-kit.ts
@@ -1,94 +1,34 @@
+/**
+ *  This exports all functions in the kit so that the Firebase CLI can see it. The
+ *  kit may expose additional, optional features that you can configure after importing,
+ *  such as registering a callback. Check the kit's README for more information.
+ */
+
 import { setGlobalOptions } from "firebase-functions";
 import * as params from "firebase-functions/params";
 
-export const regionParam = params.defineString("FUNCTION_KIT_REGION", {
+// This is how you create a "param". A param is a placeholder for a value
+// which is determined at install/deploy time. Use a param whenever you want
+// the value to differ. Learn more at
+// https://firebase.google.com/docs/functions/config-env#params
+export const regionParam = params.defineString("FUNCTION_DEFAULT_REGION", {
   description: "Region where functions should be deployed.",
 });
 
-// To require setting a global option for each instance of this kit, uncomment the option parameter
-// definition and the line configuring it below. Learn more about these options at:
+
+// This allows you to set default options that apply to all functions in this
+// kit. Learn more about these options and additional configurations at:
 // https://firebase.google.com/docs/reference/functions/2nd-gen/node/firebase-functions.globaloptions
-
-// export const concurrencyParam = params.defineInt("FIREBASE_FUNCTION_KIT_CONCURRENCY", {
-//   description: "Number of concurrent requests a function instance can serve simultaneously.",
-// });
-
-// export const cpuParam = params.defineFloat("FIREBASE_FUNCTION_KIT_CPU", {
-//   description: "Fractional number of CPUs to allocate to a function.",
-// });
-
-// export const enforceAppCheckParam = params.defineBoolean("FIREBASE_FUNCTION_KIT_ENFORCE_APP_CHECK", {
-//   description: "Controls whether Firebase App Check is enforced.",
-// });
-
-// export const ingressSettingsParam = params.defineString("FIREBASE_FUNCTION_KIT_INGRESS_SETTINGS", {
-//   description: "Ingress settings to control network traffic sources (ALLOW_ALL, ALLOW_INTERNAL_ONLY, ALLOW_INTERNAL_AND_GCLB).",
-// });
-
-// export const invokerParam = params.defineString("FIREBASE_FUNCTION_KIT_INVOKER", {
-//   description: "Access control for HTTPS functions (e.g. 'public' or service account email).",
-// });
-
-// export const labelsParam = params.defineJson("FIREBASE_FUNCTION_KIT_LABELS", {
-//   description: "User-defined labels (JSON object) to set on the function.",
-// });
-
-// export const maxInstancesParam = params.defineInt("FIREBASE_FUNCTION_KIT_MAX_INSTANCES", {
-//   description: "Maximum number of instances that can run in parallel.",
-// });
-
-// export const memoryParam = params.defineString("FIREBASE_FUNCTION_KIT_MEMORY", {
-//   description: "Amount of memory to allocate to the function (e.g. 256MiB, 512MiB, 1GiB, 2GiB).",
-// });
-
-// export const minInstancesParam = params.defineInt("FIREBASE_FUNCTION_KIT_MIN_INSTANCES", {
-//   description: "Minimum number of idle instances to keep running.",
-// });
-
-// export const omitParam = params.defineBoolean("FIREBASE_FUNCTION_KIT_OMIT", {
-//   description: "If true, do not deploy or emulate these functions.",
-// });
-
-// export const preserveExternalChangesParam = params.defineBoolean("FIREBASE_FUNCTION_KIT_PRESERVE_EXTERNAL_CHANGES", {
-//   description: "If true, preserves configuration modified outside of function source code.",
-// });
-
-// export const secretParam = params.defineSecret("MY_SECRET");
-
-// export const serviceAccountParam = params.defineString("FIREBASE_FUNCTION_KIT_SERVICE_ACCOUNT", {
-//   description: "Specific service account email for the function to run as.",
-// });
-
-// export const timeoutSecondsParam = params.defineInt("FIREBASE_FUNCTION_KIT_TIMEOUT_SECONDS", {
-//   description: "Timeout for the function in seconds (0 to 540 for event functions, up to 3600 for HTTPS).",
-// });
-
-// export const vpcConnectorParam = params.defineString("FIREBASE_FUNCTION_KIT_VPC_CONNECTOR", {
-//   description: "The VPC connector to connect the function to.",
-// });
-
-// export const vpcConnectorEgressSettingsParam = params.defineString("FIREBASE_FUNCTION_KIT_VPC_CONNECTOR_EGRESS_SETTINGS", {
-//   description: "Egress settings for VPC connector (PRIVATE_RANGES_ONLY or ALL_TRAFFIC).",
-// });
-
 setGlobalOptions({
+  // If you pass a parameter, you will be prompted for new values on each instance.
   region: regionParam,
-  // concurrency: concurrencyParam,
-  // cpu: cpuParam,
-  // enforceAppCheck: enforceAppCheckParam,
-  // ingressSettings: ingressSettingsParam,
-  // invoker: invokerParam,
-  // labels: labelsParam,
-  // maxInstances: maxInstancesParam,
-  // memory: memoryParam,
-  // minInstances: minInstancesParam,
-  // omit: omitParam,
-  // preserveExternalChanges: preserveExternalChangesParam,
-  // secrets: [secretParam],
-  // serviceAccount: serviceAccountParam,
-  // timeoutSeconds: timeoutSecondsParam,
-  // vpcConnector: vpcConnectorParam,
-  // vpcConnectorEgressSettings: vpcConnectorEgressSettingsParam,
+  // If you want the same value for all instances across your kit, you can pass a
+  // normal value.
+  // For cost control, you can set the maximum number of containers that can be
+  // running at the same time. This helps mitigate the impact of unexpected
+  // traffic spikes by instead downgrading performance. This limit is a
+  // per-function limit.
+  maxInstances: 10,
 });
 
 export * from "{{PACKAGE_NAME}}";
