6bbff410203142c6aa6499a66e18e0d60e254da3
Victor Fan <vsfan@google.com>
2026-08-13 16:55:30 -0700

    kits:list

---
+++ src/commands/functions-kits-list.ts
@@ -0,0 +1,26 @@
+import { Command } from "../command";
+import { listKitConfigs } from "../functions/kits/config";
+import { Options } from "../options";
+import { logLabeledBullet } from "../utils";
+import { logger } from "../logger";
+import * as Table from "cli-table3";
+
+export const command = new Command("functions:kits:list")
+  .description("list all the kits that are installed in your firebase.json")
+  .option("--only-deployed", "unimplemented")
+  .action((options: Options) => {
+    const firebaseConfig = options.config;
+    const validatedConfig = firebaseConfig.src;
+    const kitConfigs = listKitConfigs(validatedConfig);
+    if (kitConfigs.length < 1) {
+      logLabeledBullet("kits", `there are no kits in firebase.json`);
+      return;
+    }
+
+    const table = new Table({ head: ["Kit", "Instances"], style: { head: ["yellow"] } });
+    for (const kitConfig of kitConfigs) {
+      const instanceIds = Object.keys(kitConfig.instances);
+      table.push([kitConfig.kit, instanceIds.join(", ")]);
+    }
+    logger.info(table.toString());
+  });
