ee09043ab93590f8f01c5d60b2404fe5ded20744
Victor Fan <vsfan@google.com>
2026-08-13 17:08:10 -0700

    slop

--- src/commands/functions-kits-list.ts
+++ src/commands/functions-kits-list.ts
@@ -3,12 +3,18 @@ import { listKitConfigs } from "../functions/kits/config";
 import { Options } from "../options";
 import { logLabeledBullet } from "../utils";
 import { logger } from "../logger";
+import { FirebaseError } from "../error";
 import * as Table from "cli-table3";
 
 export const command = new Command("functions:kits:list")
   .description("list all the kits that are installed in your firebase.json")
   .action((options: Options) => {
     const firebaseConfig = options.config;
+    if (!firebaseConfig) {
+      throw new FirebaseError(
+        "No firebase.json found. Please run this command from within a Firebase project directory."
+      );
+    }
     const validatedConfig = firebaseConfig.src;
     const kitConfigs = listKitConfigs(validatedConfig);
     if (kitConfigs.length < 1) {
