ac35e000a755459e894015d15ae94fd97d902b04
Victor Fan <vsfan@google.com>
2026-08-17 16:38:51 -0700

    use requireConfig

--- src/commands/functions-kits-list.ts
+++ src/commands/functions-kits-list.ts
@@ -1,20 +1,16 @@
+import { requireConfig } from "../requireConfig";
 import { Command } from "../command";
 import { listKitConfigs } from "../functions/kits/config";
 import { Options } from "../options";
 import { logLabeledBullet } from "../utils";
 import { logger } from "../logger";
-import { FirebaseError } from "../error";
 import * as Table from "cli-table3";
 
 export const command = new Command("functions:kits:list")
   .description("list all the kits that are installed in your firebase.json")
+  .before(requireConfig)
   .action((options: Options) => {
     const firebaseConfig = options.config;
-    if (!firebaseConfig) {
-      throw new FirebaseError(
-        "No firebase.json found. Please run this command from within a Firebase project directory.",
-      );
-    }
     const validatedConfig = firebaseConfig.src;
     const kitConfigs = listKitConfigs(validatedConfig);
     if (kitConfigs.length < 1) {
