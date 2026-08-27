2d3aa6982c3a6c709ec4461b4011b55e0fcbea48
Wanda Mora <42626188+wandamora@users.noreply.github.com>
2026-08-26 17:12:40 -0700

    feat(functions): Support local directory function kits (#10988)
    * feat(functions): Support local directory function kits
    
    * Add options interface for addKitToConfig and disallow templates when using --directory

--- src/commands/functions-kits-install.ts
+++ src/commands/functions-kits-install.ts
@@ -2,15 +2,11 @@ import { Command } from "../command";
 import { FirebaseError } from "../error";
 import * as experiments from "../experiments";
 import { Options } from "../options";
-import {
-  DEFAULT_TEMPLATE,
-  installKitOrInstance,
-  TEMPLATES,
-  TemplateType,
-} from "../functions/kits/install";
+import { installKitOrInstance, TEMPLATES, TemplateType } from "../functions/kits/install";
 
 export interface FunctionsKitsInstallOptions extends Options {
   package?: string;
+  directory?: string;
   template?: string;
 }
 
@@ -18,9 +14,12 @@ export const command = new Command("functions:kits:install")
   .description("install a function kit into your project")
   .option("--package <package>", "NPM package name or specifier to install as a function kit")
   .option(
-    `--template [${Object.keys(TEMPLATES).join("|")}]`,
-    "template to use for the kit index file",
-    DEFAULT_TEMPLATE,
+    "--directory <directory>",
+    "path to a local functions codebase directory to install as a function kit",
+  )
+  .option(
+    `--template <template>`,
+    `template to use for the kit index file (${Object.keys(TEMPLATES).join("|")})`,
   )
   .action(async (options: FunctionsKitsInstallOptions): Promise<void> => {
     experiments.assertEnabled("kits", "install a function kit");
@@ -29,15 +28,21 @@ export const command = new Command("functions:kits:install")
       throw new FirebaseError("Not in a Firebase project directory (firebase.json not found).");
     }
 
-    const rawPkgName = options.package;
-    if (!rawPkgName) {
-      throw new FirebaseError("Set the --package option to a valid NPM package and try again.");
+    if (options.package && options.directory) {
+      throw new FirebaseError("Cannot specify both --package and --directory. Please choose one.");
+    }
+    if (!options.package && !options.directory) {
+      throw new FirebaseError("Must specify either --package or --directory.");
+    }
+    if (options.directory && options.template) {
+      throw new FirebaseError("Cannot specify --template with --directory.");
     }
 
     await installKitOrInstance({
       config: options.config,
-      package: rawPkgName,
-      template: options.template as TemplateType,
+      package: options.package,
+      directory: options.directory,
+      template: options.template as TemplateType | undefined,
       nonInteractive: options.nonInteractive,
       project: options.project,
       projectId: options.projectId,
