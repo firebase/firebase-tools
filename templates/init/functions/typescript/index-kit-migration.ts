1cf507a36d92689c809bb5a097c07043e39a9225
Wanda Mora <morawand@google.com>
2026-09-02 18:00:23 +0000

    Clean up multi-value slop
    Simplify the code a bit by expecting one value per key. This was overlooked in
    the last PR.

--- templates/init/functions/typescript/index-kit-migration.ts
+++ templates/init/functions/typescript/index-kit-migration.ts
@@ -48,11 +48,12 @@ setGlobalOptions({
   labels: process.env.EXT_MIGRATED_SYSTEM_LABELS
     ? process.env.EXT_MIGRATED_SYSTEM_LABELS.split(",").reduce<Record<string, string> | undefined>(
         (acc, curr) => {
-          const [key, ...values] = curr.split(":");
+          const [key, value] = curr.split(":");
           const trimmedKey = key?.trim();
-          if (trimmedKey && values.length > 0) {
+          const trimmedValue = value?.trim();
+          if (trimmedKey && trimmedValue) {
             acc = acc ?? {};
-            acc[trimmedKey] = values.join(":").trim();
+            acc[trimmedKey] = trimmedValue;
           }
           return acc;
         },
