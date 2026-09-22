4acc5ae5737d40e771beee56684918e014badde7
Joe Hanley <joehanley@google.com>
2026-09-22 23:34:51 +0000

    refactor(auth): remove obsolete unused OAuth scopes CLOUD_STORAGE and CLOUD_PUBSUB (b/565073258)

--- src/scopes.ts
+++ src/scopes.ts
@@ -8,5 +8,3 @@ export const FIREBASE_PLATFORM = "https://www.googleapis.com/auth/firebase";
 
 // incremental scopes
 export const CLOUD_PLATFORM = "https://www.googleapis.com/auth/cloud-platform";
-export const CLOUD_STORAGE = "https://www.googleapis.com/auth/devstorage.read_write";
-export const CLOUD_PUBSUB = "https://www.googleapis.com/auth/pubsub";
